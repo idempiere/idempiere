e0f1a979f9f88d40f24695009e444b9ed8474194
Heng Sin Low <hengsin@gmail.com>
2010-07-15 03:05:48 +0800

    * remove activator class, not needed. * remove use of non-official sun api from BrowserToken. * change bundle version to 1.0.0.qualifier.

--- zkwebui/WEB-INF/src/org/adempiere/webui/util/BrowserToken.java
+++ zkwebui/WEB-INF/src/org/adempiere/webui/util/BrowserToken.java
@@ -19,6 +19,7 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.logging.Level;
 
+import org.apache.commons.codec.binary.Base64;
 import org.compiere.Adempiere;
 import org.compiere.model.MSession;
 import org.compiere.model.MSystem;
@@ -26,8 +27,6 @@ import org.compiere.model.MUser;
 import org.compiere.util.CLogger;
 import org.zkoss.zk.au.out.AuScript;
 import org.zkoss.zk.ui.util.Clients;
-
-import sun.misc.BASE64Encoder;
 
 /**
  * class to manage browser token for auto authentication
@@ -37,9 +36,9 @@ import sun.misc.BASE64Encoder;
 public final class BrowserToken {
 
 	private final static CLogger log = CLogger.getCLogger(BrowserToken.class);
-	
+
 	private BrowserToken() {}
-	
+
 	public final static String REMEMBER_ME = "Login.RememberMe";
 	/**
 	 * save session and user as client side token for future auto login
@@ -47,20 +46,20 @@ public final class BrowserToken {
 	 * @param user
 	 */
 	public static void save(MSession session, MUser user) {
-		try 
+		try
 		{
 			String home = getHomeToken();
 			String hash = getPasswordHash(session, user);
 		    String script = "adempiere.saveUserToken('" + home + "', '" + hash + "', '" + session.getAD_Session_ID() + "');";
 			AuScript aus = new AuScript(null, script);
 			Clients.response("saveUserToken", aus);
-		} 
+		}
 		catch (Exception e)
 		{
 			log.log(Level.WARNING, e.getLocalizedMessage(), e);
 		}
 	}
-			
+
 	/**
 	 * remove client side token for auto login
 	 */
@@ -93,7 +92,7 @@ public final class BrowserToken {
         	log.log(Level.WARNING, e.getLocalizedMessage(), e);
         }
 	}
-	
+
 	/**
 	 * validate a stored client side token is valid
 	 * @param session
@@ -102,7 +101,7 @@ public final class BrowserToken {
 	 * @return true if token is valid
 	 */
 	public static boolean validateToken(MSession session, MUser user, String token) {
-		try 
+		try
 		{
 			String hash = getPasswordHash(session, user);
 			return hash.equals(token);
@@ -110,21 +109,21 @@ public final class BrowserToken {
 		catch (Exception e)
 		{
 			log.log(Level.WARNING, e.getLocalizedMessage(), e);
-		}	
+		}
 		return false;
 	}
-	
+
 	private static String getHomeToken() throws UnsupportedEncodingException {
-		String home = Adempiere.getAdempiereHome();	
-		BASE64Encoder encoder = new BASE64Encoder();
-		home = encoder.encode(home.getBytes("UTF-8"));
+		String home = Adempiere.getAdempiereHome();
+		Base64 encoder = new Base64();
+		home = new String(encoder.encode(home.getBytes("UTF-8")), "UTF-8");
 		home = URLEncoder.encode(home, "UTF-8");
 		return home;
 	}
-	
+
 	private static String getPasswordHash(MSession session, MUser user) throws UnsupportedEncodingException, NoSuchAlgorithmException {
 		MessageDigest digest = MessageDigest.getInstance("SHA-512");
-		BASE64Encoder encoder = new BASE64Encoder();
+		Base64 encoder = new Base64();
 	    digest.reset();
 	    digest.update(session.getWebSession().getBytes("UTF-8"));
 	    String password = null;
@@ -133,9 +132,9 @@ public final class BrowserToken {
 	    else
 	    	password = new String("");
 	    byte[] input = digest.digest(password.getBytes("UTF-8"));
-	    String hash = encoder.encode(input);
+	    String hash = new String(encoder.encode(input), "UTF-8");
 	    hash = URLEncoder.encode(hash, "UTF-8");
-	    
+
 	    return hash;
 	}
 }
