c75089559bc6634b60156dfad7ea71bc2ff99c56
Heng Sin Low <hengsin@gmail.com>
2010-11-04 03:51:55 +0800

    Fixed lookup of form extension.

--- org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/Extensions.java
+++ org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/Extensions.java
@@ -5,14 +5,14 @@ import org.adempiere.base.Service;
 import org.adempiere.base.ServiceQuery;
 
 public class Extensions {
-	
+
 	public static Object getForm(String id) {
 		if (Core.isExtension(id)) {
 			id = id.substring(Core.OSGI_PREFIX.length());
 		}
-		
+
 		ServiceQuery query = new ServiceQuery();
-		query.put("id", id);
+		query.put(ServiceQuery.EXTENSION_ID, id);
 		return Service.locate(Object.class, "org.adempiere.webui.Form", query);
 	}
 }
