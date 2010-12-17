73e902300416f25330b06423cce02dfea94b5ad0
Heng Sin Low <hengsin@gmail.com>
2010-12-17 16:03:56 +0800

    License header, extension point schema documentation and some minor extension point schema fixes.

--- org.adempiere.base/src/org/adempiere/base/BaseActivator.java
+++ org.adempiere.base/src/org/adempiere/base/BaseActivator.java
@@ -1,6 +1,10 @@
 /******************************************************************************
- * Product: Adempiere ERP & CRM Smart Business Solution                       *
- * Copyright (C) 2010 Heng Sin Low                							  *
+ * This file is part of Adempiere ERP Bazaar                                  *
+ * http://www.adempiere.org                                                   *
+ *                                                                            *
+ * Copyright (C) Low Heng Sin												  *
+ * Copyright (C) Contributors												  *
+ *                                                                            *
  * This program is free software; you can redistribute it and/or modify it    *
  * under the terms version 2 of the GNU General Public License as published   *
  * by the Free Software Foundation. This program is distributed in the hope   *
@@ -10,6 +14,8 @@
  * You should have received a copy of the GNU General Public License along    *
  * with this program; if not, write to the Free Software Foundation, Inc.,    *
  * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ *                                                                            *
+ * Contributors:                                                              *
  *****************************************************************************/
 package org.adempiere.base;
 
