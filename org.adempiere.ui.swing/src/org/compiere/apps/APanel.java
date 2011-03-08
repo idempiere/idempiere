780725390afb587528e4f645459f9d56c4896fd8
Carlos Ruiz <carg67@gmail.com>
2011-03-08 11:42:27 -0500

    Fix a problem caused when pushing copy button (swing client) with no record on screen raises this exception: ===========> APanel.actionPerformed: Copy [11] java.lang.ArrayIndexOutOfBoundsException: -1 at java.util.ArrayList.get(ArrayList.java:324) at org.compiere.model.GridTable.getDataAtRow(GridTable.java:1020) at org.compiere.model.GridTable.getDataAtRow(GridTable.java:1015) at org.compiere.model.GridTable.dataNew(GridTable.java:2483) at org.compiere.model.GridTab.dataNew(GridTab.java:1131) at org.compiere.apps.APanel.cmd_new(APanel.java:1879) at org.compiere.apps.APanel.actionPerformed(APanel.java:1690)

--- org.adempiere.ui.swing/src/org/compiere/apps/APanel.java
+++ org.adempiere.ui.swing/src/org/compiere/apps/APanel.java
@@ -1884,6 +1884,8 @@ public final class APanel extends CPanel
 			else    //  Don't save
 				m_curTab.dataIgnore();
 		}
+		if (copy && m_curTab.getCurrentRow() < 0)
+			copy = false;
 		m_curTab.dataNew (copy);
 		m_curGC.dynamicDisplay(0);
 	//	m_curTab.getTableModel().setChanged(false);
