a83182ea5bd7ad92118690b9a85c3b4ac780f78b
teo_sarca <devnull@localhost>
2011-03-26 00:22:10 -0500

    Local transaction commits should be logged to FINE level
    Link to SF Tracker: http://sourceforge.net/support/tracker.php?aid=3137365

--- org.adempiere.base/src/org/compiere/util/Trx.java
+++ org.adempiere.base/src/org/compiere/util/Trx.java
@@ -279,7 +279,7 @@ public class Trx implements VetoableChangeListener
 			if (m_connection != null)
 			{
 				m_connection.rollback();
-				log.info ("**** " + m_trxName);
+				log.log(isLocalTrx(m_trxName) ? Level.FINE : Level.INFO, "**** " + m_trxName);
 				m_active = false;
 				return true;
 			}
@@ -636,4 +636,12 @@ public class Trx implements VetoableChangeListener
 		}
 
 	}
+
+	private boolean isLocalTrx(String trxName)
+	{
+		return trxName == null
+			|| trxName.startsWith("POSave") // TODO: hardcoded
+			;
+	}
+
 }	//	Trx
