ade9c34801db18a66429cebd1663b0f3ab102d00
Carlos Ruiz <carg67@gmail.com>
2011-03-29 21:57:58 -0500

    UUID Generator -> Script to define the process UUID Generator in dictionary and attach it to menu

---
+++ migration/360lts-release/postgresql/788_UUIDGenerator.sql
@@ -0,0 +1,105 @@
+-- Mar 28, 2011 10:14:36 PM COT
+-- UUID Generator Process
+INSERT INTO AD_Process (AccessLevel,AD_Client_ID,AD_Org_ID,AD_Process_ID,Classname,CopyFromProcess,Created,CreatedBy,EntityType,IsActive,IsBetaFunctionality,IsDirectPrint,IsReport,IsServerProcess,Name,ShowHelp,Statistic_Count,Statistic_Seconds,Updated,UpdatedBy,Value) VALUES ('4',0,0,53252,'org.adempiere.process.UUIDGenerator','N',TO_TIMESTAMP('2011-03-28 22:14:36','YYYY-MM-DD HH24:MI:SS'),100,'D','Y','N','N','N','N','UUID Generator','Y',0,0,TO_TIMESTAMP('2011-03-28 22:14:36','YYYY-MM-DD HH24:MI:SS'),100,'UUIDGenerator')
+;
+
+-- Mar 28, 2011 10:14:36 PM COT
+INSERT INTO AD_Process_Trl (AD_Language,AD_Process_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy ) SELECT l.AD_Language,t.AD_Process_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Process t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Process_ID=53252 AND NOT EXISTS (SELECT * FROM AD_Process_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Process_ID=t.AD_Process_ID)
+;
+
+-- Mar 28, 2011 10:13:47 PM COT
+INSERT INTO AD_Process_Para (AD_Client_ID,AD_Element_ID,AD_Org_ID,AD_Process_ID,AD_Process_Para_ID,AD_Reference_ID,ColumnName,Created,CreatedBy,Description,EntityType,FieldLength,Help,IsActive,IsCentrallyMaintained,IsMandatory,IsRange,Name,SeqNo,Updated,UpdatedBy) VALUES (0,587,0,53252,53495,10,'TableName',TO_TIMESTAMP('2011-03-28 22:13:47','YYYY-MM-DD HH24:MI:SS'),100,'Name of the table in the database','D',40,'The DB Table Name indicates the name of the table in database.','Y','Y','N','N','DB Table Name',10,TO_TIMESTAMP('2011-03-28 22:13:47','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Mar 28, 2011 10:13:47 PM COT
+INSERT INTO AD_Process_Para_Trl (AD_Language,AD_Process_Para_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy ) SELECT l.AD_Language,t.AD_Process_Para_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Process_Para t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Process_Para_ID=53495 AND NOT EXISTS (SELECT * FROM AD_Process_Para_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Process_Para_ID=t.AD_Process_Para_ID)
+;
+
+-- Mar 28, 2011 10:19:51 PM COT
+INSERT INTO AD_Menu ("action",AD_Client_ID,AD_Menu_ID,AD_Org_ID,AD_Process_ID,Created,CreatedBy,EntityType,IsActive,IsCentrallyMaintained,IsReadOnly,IsSOTrx,IsSummary,Name,Updated,UpdatedBy) VALUES ('P',0,53322,0,53252,TO_TIMESTAMP('2011-03-28 22:19:50','YYYY-MM-DD HH24:MI:SS'),100,'D','Y','Y','N','N','N','UUID Generator',TO_TIMESTAMP('2011-03-28 22:19:50','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Mar 28, 2011 10:19:51 PM COT
+INSERT INTO AD_Menu_Trl (AD_Language,AD_Menu_ID, Description,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy ) SELECT l.AD_Language,t.AD_Menu_ID, t.Description,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Menu t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Menu_ID=53322 AND NOT EXISTS (SELECT * FROM AD_Menu_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Menu_ID=t.AD_Menu_ID)
+;
+
+-- Mar 28, 2011 10:19:51 PM COT
+INSERT INTO AD_TreeNodeMM (AD_Client_ID,AD_Org_ID, IsActive,Created,CreatedBy,Updated,UpdatedBy, AD_Tree_ID, Node_ID, Parent_ID, SeqNo) SELECT t.AD_Client_ID, 0, 'Y', CURRENT_TIMESTAMP, 100, CURRENT_TIMESTAMP, 100,t.AD_Tree_ID, 53322, 0, 999 FROM AD_Tree t WHERE t.AD_Client_ID=0 AND t.IsActive='Y' AND t.IsAllNodes='Y' AND t.TreeType='MM' AND NOT EXISTS (SELECT * FROM AD_TreeNodeMM e WHERE e.AD_Tree_ID=t.AD_Tree_ID AND Node_ID=53322)
+;
+
+-- Mar 28, 2011 10:20:03 PM COT
+UPDATE AD_TreeNodeMM SET Parent_ID=155, SeqNo=0, Updated=CURRENT_TIMESTAMP WHERE AD_Tree_ID=10 AND Node_ID=161
+;
+
+-- Mar 28, 2011 10:20:03 PM COT
+UPDATE AD_TreeNodeMM SET Parent_ID=155, SeqNo=1, Updated=CURRENT_TIMESTAMP WHERE AD_Tree_ID=10 AND Node_ID=367
+;
+
+-- Mar 28, 2011 10:20:03 PM COT
+UPDATE AD_TreeNodeMM SET Parent_ID=155, SeqNo=2, Updated=CURRENT_TIMESTAMP WHERE AD_Tree_ID=10 AND Node_ID=456
+;
+
+-- Mar 28, 2011 10:20:03 PM COT
+UPDATE AD_TreeNodeMM SET Parent_ID=155, SeqNo=3, Updated=CURRENT_TIMESTAMP WHERE AD_Tree_ID=10 AND Node_ID=501
+;
+
+-- Mar 28, 2011 10:20:03 PM COT
+UPDATE AD_TreeNodeMM SET Parent_ID=155, SeqNo=4, Updated=CURRENT_TIMESTAMP WHERE AD_Tree_ID=10 AND Node_ID=326
+;
+
+-- Mar 28, 2011 10:20:03 PM COT
+UPDATE AD_TreeNodeMM SET Parent_ID=155, SeqNo=5, Updated=CURRENT_TIMESTAMP WHERE AD_Tree_ID=10 AND Node_ID=566
+;
+
+-- Mar 28, 2011 10:20:03 PM COT
+UPDATE AD_TreeNodeMM SET Parent_ID=155, SeqNo=6, Updated=CURRENT_TIMESTAMP WHERE AD_Tree_ID=10 AND Node_ID=392
+;
+
+-- Mar 28, 2011 10:20:03 PM COT
+UPDATE AD_TreeNodeMM SET Parent_ID=155, SeqNo=7, Updated=CURRENT_TIMESTAMP WHERE AD_Tree_ID=10 AND Node_ID=113
+;
+
+-- Mar 28, 2011 10:20:03 PM COT
+UPDATE AD_TreeNodeMM SET Parent_ID=155, SeqNo=8, Updated=CURRENT_TIMESTAMP WHERE AD_Tree_ID=10 AND Node_ID=220
+;
+
+-- Mar 28, 2011 10:20:03 PM COT
+UPDATE AD_TreeNodeMM SET Parent_ID=155, SeqNo=9, Updated=CURRENT_TIMESTAMP WHERE AD_Tree_ID=10 AND Node_ID=351
+;
+
+-- Mar 28, 2011 10:20:03 PM COT
+UPDATE AD_TreeNodeMM SET Parent_ID=155, SeqNo=10, Updated=CURRENT_TIMESTAMP WHERE AD_Tree_ID=10 AND Node_ID=289
+;
+
+-- Mar 28, 2011 10:20:03 PM COT
+UPDATE AD_TreeNodeMM SET Parent_ID=155, SeqNo=11, Updated=CURRENT_TIMESTAMP WHERE AD_Tree_ID=10 AND Node_ID=302
+;
+
+-- Mar 28, 2011 10:20:03 PM COT
+UPDATE AD_TreeNodeMM SET Parent_ID=155, SeqNo=12, Updated=CURRENT_TIMESTAMP WHERE AD_Tree_ID=10 AND Node_ID=303
+;
+
+-- Mar 28, 2011 10:20:03 PM COT
+UPDATE AD_TreeNodeMM SET Parent_ID=155, SeqNo=13, Updated=CURRENT_TIMESTAMP WHERE AD_Tree_ID=10 AND Node_ID=321
+;
+
+-- Mar 28, 2011 10:20:03 PM COT
+UPDATE AD_TreeNodeMM SET Parent_ID=155, SeqNo=14, Updated=CURRENT_TIMESTAMP WHERE AD_Tree_ID=10 AND Node_ID=461
+;
+
+-- Mar 28, 2011 10:20:03 PM COT
+UPDATE AD_TreeNodeMM SET Parent_ID=155, SeqNo=15, Updated=CURRENT_TIMESTAMP WHERE AD_Tree_ID=10 AND Node_ID=53193
+;
+
+-- Mar 28, 2011 10:20:03 PM COT
+UPDATE AD_TreeNodeMM SET Parent_ID=155, SeqNo=16, Updated=CURRENT_TIMESTAMP WHERE AD_Tree_ID=10 AND Node_ID=53322
+;
+
+-- Mar 28, 2011 10:20:03 PM COT
+UPDATE AD_TreeNodeMM SET Parent_ID=155, SeqNo=17, Updated=CURRENT_TIMESTAMP WHERE AD_Tree_ID=10 AND Node_ID=383
+;
+
+-- Mar 28, 2011 10:28:46 PM COT
+ALTER TABLE AD_Column_Trl ADD COLUMN AD_Column_Trl_UU VARCHAR(36) DEFAULT NULL 
+;
+
