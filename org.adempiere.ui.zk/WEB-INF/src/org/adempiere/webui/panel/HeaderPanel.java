8301465c67f39b54ad63da41565a1423f1bb02a8
Heng Sin Low <hengsin@gmail.com>
2012-03-02 15:13:43 +0800

    attempt to fix header logo sometimes not being display.

--- org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/panel/HeaderPanel.java
+++ org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/panel/HeaderPanel.java
@@ -39,11 +39,11 @@ import org.zkoss.zul.Vbox;
  * @version $Revision: 0.20 $
  */
 
-public class HeaderPanel extends Panel implements EventListener
+public class HeaderPanel extends Panel implements EventListener<Event>
 {
 	private static final long serialVersionUID = -2351317624519209484L;
 
-	private Image image = new Image();
+	private Image image;
 
     public HeaderPanel()
     {
@@ -57,7 +57,7 @@ public class HeaderPanel extends Panel implements EventListener
 
     	UserPanel userPanel = new UserPanel();
 
-    	image.setSrc(ThemeManager.getSmallLogo());
+    	image  = new Image(ThemeManager.getSmallLogo());
     	image.addEventListener(Events.ON_CLICK, this);
     	image.setStyle("cursor: pointer;");
 
@@ -98,7 +98,7 @@ public class HeaderPanel extends Panel implements EventListener
 			{
 				AboutWindow w = new AboutWindow();
 				w.setPage(this.getPage());
-				w.doModal();
+				w.doHighlighted();
 			}
 		}
 
