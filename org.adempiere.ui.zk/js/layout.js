33fd4e4ae3c7ef375ef05edd04bbc80c020500d6
Heng Sin Low <hengsin@gmail.com>
2012-03-05 12:52:25 +0800

    Added null check and fixed scroll to row for zk6

--- org.adempiere.ui.zk/js/layout.js
+++ org.adempiere.ui.zk/js/layout.js
@@ -6,20 +6,24 @@ function ad_deferRenderBorderLayout(uuid, timeout) {
 }		
 
 function _ad_deferBDL(uuid) {			
-	zk.Widget.$(uuid).resize();
+	var cmp = zk.Widget.$(uuid);
+	if (cmp) { cmp.resize(); }	
 }
 
 function ad_closeBuble(uuid) {
 	var cmp = zk.Widget.$(uuid); 
-	for(var i=0;i<cmp.bandInfos.length;i++){
-		cmp.instance.getBand(i).closeBubble();
+	if (cmp) {
+		for(var i=0;i<cmp.bandInfos.length;i++){
+			cmp.instance.getBand(i).closeBubble();
+		}
 	}
 }
 
 function scrollToRow(uuid){  
 	 var cmp = zk.Widget.$(uuid);  
-	 cmp.$n().style.display='inline';
-	 cmp.focus();
-	 cmp.$n().style.display='none';
+	 if (cmp) {
+	    cmp.scrollIntoView();
+	 	cmp.focus();
+	 }
 }
  
