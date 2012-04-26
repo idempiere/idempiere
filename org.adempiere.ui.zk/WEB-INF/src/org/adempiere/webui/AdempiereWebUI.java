e813293d211c29ba50c81f417422771232ba31bf
Heng Sin Low <hengsin@gmail.com>
2012-04-26 15:11:16 +0800

    IDEMPIERE-231 Zk6: Improve the tablet experience. Fixed ClientInfo NPE  when user refresh browser.

--- org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/AdempiereWebUI.java
+++ org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/AdempiereWebUI.java
@@ -87,7 +87,7 @@ public class AdempiereWebUI extends Window implements EventListener<Event>, IWeb
 
     private IDesktop           appDesktop;
 
-    private ClientInfo		   clientInfo;
+    private ClientInfo		   clientInfo = new ClientInfo();
 
 	private String langSession;
 
@@ -98,6 +98,8 @@ public class AdempiereWebUI extends Window implements EventListener<Event>, IWeb
 	public static final String EXECUTION_CARRYOVER_SESSION_KEY = "execution.carryover";
 
 	public static final String ZK_DESKTOP_SESSION_KEY = "zk.desktop";
+	
+	private static final String CLIENT_INFO = "client.info";
 
     public AdempiereWebUI()
     {
@@ -256,6 +258,10 @@ public class AdempiereWebUI extends Window implements EventListener<Event>, IWeb
 					
 					currSess.setAttribute(ZK_DESKTOP_SESSION_KEY, this.getPage().getDesktop());
 					ctx.put(ZK_DESKTOP_SESSION_KEY, this.getPage().getDesktop());
+					ClientInfo sessionClientInfo = (ClientInfo) currSess.getAttribute(CLIENT_INFO);
+					if (sessionClientInfo != null) {
+						clientInfo = sessionClientInfo;
+					}
 				} catch (Throwable t) {
 					//restore fail
 					appDesktop = null;
@@ -358,6 +364,9 @@ public class AdempiereWebUI extends Window implements EventListener<Event>, IWeb
 				} else if (ua.indexOf("android") >= 0 && ua.indexOf("chrome") >= 0 && ua.indexOf("mobile") < 0) {
 					clientInfo.tablet = true;
 				}
+			}
+			if (getDesktop() != null && getDesktop().getSession() != null) {
+				getDesktop().getSession().setAttribute(CLIENT_INFO, clientInfo);
 			}
 		}
 
