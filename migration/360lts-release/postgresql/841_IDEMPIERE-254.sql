9c0fe55ee658772bc0f73a46f4bcccb836ad8fa8
Heng Sin Low <hengsin@gmail.com>
2012-05-28 15:18:09 +0800

    IDEMPIERE-254 2Pack Stabilization and Enhancement

--- migration/360lts-release/postgresql/841_IDEMPIERE-254.sql
+++ migration/360lts-release/postgresql/841_IDEMPIERE-254.sql
@@ -295,7 +295,7 @@ INSERT INTO AD_Column (AD_Table_ID,EntityType,Version,IsMandatory,IsTranslated,I
 
 -- May 25, 2012 4:12:59 PM MYT
 -- IDEMPIERE-254 2Pack Stabilization and Enhancement
-INSERT INTO AD_Column_Trl (AD_Language,AD_Column_ID, Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,AD_Column_Trl_UU ) SELECT l.AD_Language,t.AD_Column_ID, t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Column t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Column_ID=200167 AND NOT EXISTS (SELECT * FROM AD_Column_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Column_ID=t.AD_Column_ID)
+INSERT INTO AD_Column_Trl (AD_Language,AD_Column_ID, Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy ) SELECT l.AD_Language,t.AD_Column_ID, t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Column t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Column_ID=200167 AND NOT EXISTS (SELECT * FROM AD_Column_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Column_ID=t.AD_Column_ID)
 ;
 
 -- May 25, 2012 4:13:09 PM MYT
