301ce178bbc202f34222752a11a9fb6591be5c0f
Hesham S. Ahmed <devnull@localhost>
2012-07-16 05:52:45 +0300

    IDEMPIERE-320 - Make Swing CTextArea consistent with ZK (transplanted from 994a9b5493ddba3c2a10e64ccf7377d8ae69b398)

---
+++ migration/360lts-release/oracle/850_IDEMPIERE-320.sql
@@ -0,0 +1,5 @@
+-- Jul 16, 2012 5:38:59 AM AST
+-- IDEMPIERE-320 -   Make Swing CTextArea consistent with ZK
+INSERT INTO AD_SysConfig (AD_SysConfig_ID,EntityType,ConfigurationLevel,Value,Description,AD_SysConfig_UU,Created,Updated,AD_Client_ID,AD_Org_ID,CreatedBy,IsActive,UpdatedBy,Name) VALUES (200012,'D','C','N','Override Text Area behaviour, ENTER to change value, TAB to focus next, Shift-ENTER for new line','b8634904-e4d1-48da-8b86-cc46d12cb52d',TO_DATE('2012-07-16 05:38:58','YYYY-MM-DD HH24:MI:SS'),TO_DATE('2012-07-16 05:38:58','YYYY-MM-DD HH24:MI:SS'),0,0,100,'Y',100,'SWING_OVERRIDE_TEXT_AREA_BEHAVIOUR')
+;
+
