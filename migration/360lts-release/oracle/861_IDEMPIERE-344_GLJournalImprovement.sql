05fe56307a9e7e9e0f8230e7dd39b9fb48a616aa
Carlos Ruiz <carg67@gmail.com>
2012-08-02 23:06:04 -0500

    IDEMPIERE-344 Improve usability for GL Journal / add last update to migration script

--- migration/360lts-release/oracle/861_IDEMPIERE-344_GLJournalImprovement.sql
+++ migration/360lts-release/oracle/861_IDEMPIERE-344_GLJournalImprovement.sql
@@ -15,3 +15,9 @@ UPDATE AD_Field SET IsAllowCopy='N',Updated=TO_DATE('2012-08-02 22:35:00','YYYY-
 UPDATE AD_Field SET IsAllowCopy='N',Updated=TO_DATE('2012-08-02 22:35:17','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=200203
 ;
 
+UPDATE AD_System
+  SET LastMigrationScriptApplied='861_IDEMPIERE-344_GLJournalImprovement.sql'
+WHERE LastMigrationScriptApplied<'861_IDEMPIERE-344_GLJournalImprovement.sql'
+   OR LastMigrationScriptApplied IS NULL
+;
+
