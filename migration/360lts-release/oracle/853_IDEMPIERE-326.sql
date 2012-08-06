d7e374a5de7e121cdb69b0c996d2b70993fdf491
Carlos Ruiz <carg67@gmail.com>
2012-08-06 14:36:23 -0500

    Add newlines to migration scripts to allow cat | sqlplus

--- migration/360lts-release/oracle/853_IDEMPIERE-326.sql
+++ migration/360lts-release/oracle/853_IDEMPIERE-326.sql
@@ -1,8 +1,9 @@
 -- Oracle procedure T_InventoryValue_Create is no longer used, can be deleted
-drop procedure "T_INVENTORYVALUE_CREATE";
+drop procedure T_INVENTORYVALUE_CREATE;
 
 UPDATE AD_System
   SET LastMigrationScriptApplied='853_IDEMPIERE-326.sql'
 WHERE LastMigrationScriptApplied<'853_IDEMPIERE-326.sql'
    OR LastMigrationScriptApplied IS NULL
 ;
+
