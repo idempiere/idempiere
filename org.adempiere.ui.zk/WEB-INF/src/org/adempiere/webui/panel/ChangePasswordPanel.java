f9d68d83203c0194f3bc6d310a5da94837f59b34
Elaine Tan <etan@trekglobal.com>
2012-09-10 16:25:11 +0800

    IDEMPIERE-373 Implement User Locking - include copyright text

--- org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/panel/ChangePasswordPanel.java
+++ org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/panel/ChangePasswordPanel.java
@@ -1,3 +1,18 @@
+/******************************************************************************
+ * Copyright (C) 2012 Elaine Tan                                              *
+ * Copyright (C) 2012 Trek Global
+ * This program is free software; you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program; if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ *****************************************************************************/
+
+
 package org.adempiere.webui.panel;
 
 import java.util.Properties;
@@ -27,6 +42,11 @@ import org.zkoss.zk.ui.event.Event;
 import org.zkoss.zk.ui.event.EventListener;
 import org.zkoss.zul.Image;
 
+/**
+ * Change Password Panel
+ * @author Elaine
+ * @date August 30, 2012
+ */
 public class ChangePasswordPanel extends Window implements EventListener<Event>
 {
 	/**
