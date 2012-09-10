aed89262c152eb534e6ad912350ad160025147b0
Elaine Tan <etan@trekglobal.com>
2012-09-10 16:24:47 +0800

    IDEMPIERE-270 Reports should inherit search filter from window - include copyright text

--- org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/panel/action/ReportAction.java
+++ org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/panel/action/ReportAction.java
@@ -1,3 +1,18 @@
+/******************************************************************************
+ * Copyright (C) 2012 Elaine Tan                                              *
+ * Copyright (C) 2012 Trek Global
+ * This program is free software; you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program; if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ *****************************************************************************/
+
+
 package org.adempiere.webui.panel.action;
 
 import static org.compiere.model.SystemIDs.WINDOW_PRINTFORMAT;
@@ -43,6 +58,10 @@ import org.zkoss.zul.Filedownload;
 import org.zkoss.zul.Hbox;
 import org.zkoss.zul.Vbox;
 
+/**
+ * @author Elaine
+ * @date September 6, 2012
+ */
 public class ReportAction implements EventListener<Event>
 {
 	private static CLogger log = CLogger.getCLogger(ReportAction.class);
