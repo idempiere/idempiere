8b0d559ae58a4635cbee9299c9ec8f59fa854ce4
Owner.Owner-PC <devnull@localhost>
2012-10-05 16:28:01 +0700

    add workaround for customer service asset (not fixed assets)

--- org.adempiere.base/src/org/compiere/model/MInOut.java
+++ org.adempiere.base/src/org/compiere/model/MInOut.java
@@ -1482,6 +1482,7 @@ public class MInOut extends X_M_InOut implements DocAction
 			if (product != null
 				&& isSOTrx()
 				&& product.isCreateAsset()
+				&& !product.getM_Product_Category().getA_Asset_Group().isFixedAsset()
 				&& sLine.getMovementQty().signum() > 0
 				&& !isReversal())
 			{
