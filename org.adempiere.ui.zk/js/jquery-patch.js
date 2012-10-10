748a09c3be60233c1550462a08afaf626697b07a
Heng Sin Low <hengsin@gmail.com>
2012-10-10 17:43:03 +0800

    IDEMPIERE-369 Master Detail layout improvements. More correct patch for jquery issue under firefox. Now works fine under firefox and chrome.

--- org.adempiere.ui.zk/js/jquery-patch.js
+++ org.adempiere.ui.zk/js/jquery-patch.js
@@ -2,24 +2,34 @@
 (function(undefined) {
 var rupper = /([A-Z]|^ms)/g;
 if ( document.defaultView && document.defaultView.getComputedStyle ) {
+	var old = document.defaultView.getComputedStyle;
 	getComputedStyle = function( elem, name ) {
 		var ret, defaultView, computedStyle;
 
-		//added try catch for firefox
-		try {
-			name = name.replace( rupper, "-$1" ).toLowerCase();
-	
-			if ( !(defaultView = elem.ownerDocument.defaultView) ) {
-				return undefined;
-			}
+		if ( !(defaultView = elem.ownerDocument.defaultView) ) {
+			return undefined;
+		}
 		
-			if ( (computedStyle = defaultView.getComputedStyle( elem, null )) ) {
+		if (name == undefined || name == null) {
+			try {
+				return old(elem, null);
+			} catch (err) {
+				return "";
+			}
+		}
+		
+		name = name.replace( rupper, "-$1" ).toLowerCase();
+		
+		try {			
+			if ( (computedStyle = old( elem, null )) ) {
 				ret = computedStyle.getPropertyValue( name );
 				if ( ret === "" && !jQuery.contains( elem.ownerDocument.documentElement, elem ) ) {
 					ret = jQuery.style( elem, name );
 				}
 			}
-		} catch (err) {}
+		} catch (err) {
+			return "";
+		}
 
 		return ret;
 	};
