822c42c89a5596cb0e931abd7c175c6c8e5f60a0
Carlos Ruiz <carg67@gmail.com>
2012-10-23 14:53:52 -0500

    IDEMPIERE-293 List of logged users / Fix error on script 935_IDEMPIERE-293.sql

---
+++ migration/360lts-release/oracle/944_IDEMPIERE-293.sql
@@ -0,0 +1,11 @@
+-- IDEMPIERE-293-List of logged users
+-- Fix error on script 935_IDEMPIERE-293.sql
+UPDATE AD_TreeNodeMM SET Parent_ID=53296, SeqNo=10, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=228
+;
+
+UPDATE AD_TreeNodeMM SET Parent_ID=156, SeqNo=9, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=53083
+;
+
+SELECT register_migration_script('944_IDEMPIERE-293.sql') FROM dual
+;
+
