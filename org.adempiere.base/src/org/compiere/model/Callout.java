baa32c99521b1c19d98831cb3c2869939d06e88b
Elaine Tan <etan@trekglobal.com>
2012-11-14 16:59:08 +0800

    IDEMPIERE-462 Ticket #1001503: Credit Cards Online - make the payment processor fields configurable - revert the use of IGridTab

--- org.adempiere.base/src/org/compiere/model/Callout.java
+++ org.adempiere.base/src/org/compiere/model/Callout.java
@@ -45,7 +45,7 @@ public interface Callout
 	 *  @return Error message or ""
 	 */
 	public String start (Properties ctx, String method, int WindowNo,
-		IGridTab mTab, GridField mField, Object value, Object oldValue);
+		GridTab mTab, GridField mField, Object value, Object oldValue);
 
 	/**
 	 *	Conversion Rules.
