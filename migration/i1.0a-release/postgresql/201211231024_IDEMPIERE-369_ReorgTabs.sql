ce09c5137283dc1f27797fdf1b2077ffa4e41959
Carlos Ruiz <carg67@gmail.com>
2012-11-23 10:34:06 -0500

    IDEMPIERE-369 Master Detail layout improvements / Reorganize tabs on some windows for ease access: Affected windows: Charge, Country Region and City, Currency, Form, Payment Term, Reference, Report & Process, Table and Column, Tax Rate, Unit of Measure, Window Tab & Field, Workflow

---
+++ migration/i1.0a-release/postgresql/201211231024_IDEMPIERE-369_ReorgTabs.sql
@@ -0,0 +1,428 @@
+-- Nov 16, 2012 2:47:07 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Tab SET SeqNo=30,Updated=TO_TIMESTAMP('2012-11-16 14:47:07','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=53210
+;
+
+-- Nov 16, 2012 2:47:13 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Tab SET SeqNo=20,Updated=TO_TIMESTAMP('2012-11-16 14:47:13','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=331
+;
+
+-- Nov 16, 2012 2:49:20 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Tab SET SeqNo=40,Updated=TO_TIMESTAMP('2012-11-16 14:49:20','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=549
+;
+
+-- Nov 16, 2012 2:49:27 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Tab SET SeqNo=20,Updated=TO_TIMESTAMP('2012-11-16 14:49:27','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=136
+;
+
+-- Nov 16, 2012 2:49:33 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Tab SET SeqNo=30,Updated=TO_TIMESTAMP('2012-11-16 14:49:33','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=215
+;
+
+-- Nov 16, 2012 2:50:55 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Tab SET SeqNo=40,Updated=TO_TIMESTAMP('2012-11-16 14:50:55','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=557
+;
+
+-- Nov 16, 2012 2:51:30 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Tab SET SeqNo=20,Updated=TO_TIMESTAMP('2012-11-16 14:51:30','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=127
+;
+
+-- Nov 16, 2012 2:56:23 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Tab SET SeqNo=30,Updated=TO_TIMESTAMP('2012-11-16 14:56:23','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=303
+;
+
+-- Nov 16, 2012 2:56:28 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Tab SET SeqNo=20,Updated=TO_TIMESTAMP('2012-11-16 14:56:28','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=309
+;
+
+-- Nov 16, 2012 2:59:06 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Tab SET SeqNo=30,Updated=TO_TIMESTAMP('2012-11-16 14:59:06','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=233
+;
+
+-- Nov 16, 2012 2:59:10 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Tab SET SeqNo=20,Updated=TO_TIMESTAMP('2012-11-16 14:59:10','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=503
+;
+
+-- Nov 16, 2012 3:01:47 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Tab SET SeqNo=60,Updated=TO_TIMESTAMP('2012-11-16 15:01:47','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=171
+;
+
+-- Nov 16, 2012 3:01:51 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Tab SET SeqNo=20,Updated=TO_TIMESTAMP('2012-11-16 15:01:51','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=104
+;
+
+-- Nov 16, 2012 3:01:56 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Tab SET SeqNo=30,Updated=TO_TIMESTAMP('2012-11-16 15:01:56','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=172
+;
+
+-- Nov 16, 2012 3:02:00 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Tab SET SeqNo=40,Updated=TO_TIMESTAMP('2012-11-16 15:02:00','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=103
+;
+
+-- Nov 16, 2012 3:02:05 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Tab SET SeqNo=50,Updated=TO_TIMESTAMP('2012-11-16 15:02:05','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=773
+;
+
+-- Nov 16, 2012 3:08:20 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Tab SET SeqNo=20,Updated=TO_TIMESTAMP('2012-11-16 15:08:20','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=246
+;
+
+-- Nov 16, 2012 3:08:29 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Tab SET SeqNo=30,Updated=TO_TIMESTAMP('2012-11-16 15:08:29','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=247
+;
+
+-- Nov 16, 2012 3:08:36 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Tab SET SeqNo=40,Updated=TO_TIMESTAMP('2012-11-16 15:08:36','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=248
+;
+
+-- Nov 16, 2012 3:08:40 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Tab SET SeqNo=50,Updated=TO_TIMESTAMP('2012-11-16 15:08:40','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=308
+;
+
+-- Nov 16, 2012 3:10:04 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Tab SET SeqNo=20,Updated=TO_TIMESTAMP('2012-11-16 15:10:04','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=101
+;
+
+-- Nov 16, 2012 3:10:10 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Tab SET SeqNo=30,Updated=TO_TIMESTAMP('2012-11-16 15:10:10','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=681
+;
+
+-- Nov 16, 2012 3:10:13 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Tab SET SeqNo=40,Updated=TO_TIMESTAMP('2012-11-16 15:10:13','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=53298
+;
+
+-- Nov 16, 2012 3:10:17 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Tab SET SeqNo=50,Updated=TO_TIMESTAMP('2012-11-16 15:10:17','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=53069
+;
+
+-- Nov 16, 2012 3:10:21 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Tab SET SeqNo=60,Updated=TO_TIMESTAMP('2012-11-16 15:10:21','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=673
+;
+
+-- Nov 16, 2012 3:11:11 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Tab SET SeqNo=20,Updated=TO_TIMESTAMP('2012-11-16 15:11:11','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=333
+;
+
+-- Nov 16, 2012 3:11:15 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Tab SET SeqNo=30,Updated=TO_TIMESTAMP('2012-11-16 15:11:15','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=640
+;
+
+-- Nov 16, 2012 3:11:19 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Tab SET SeqNo=40,Updated=TO_TIMESTAMP('2012-11-16 15:11:19','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=458
+;
+
+-- Nov 16, 2012 3:12:02 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Tab SET SeqNo=20,Updated=TO_TIMESTAMP('2012-11-16 15:12:02','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=134
+;
+
+-- Nov 16, 2012 3:12:06 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Tab SET SeqNo=30,Updated=TO_TIMESTAMP('2012-11-16 15:12:06','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=273
+;
+
+-- Nov 16, 2012 3:13:49 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Tab SET SeqNo=20,Updated=TO_TIMESTAMP('2012-11-16 15:13:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=106
+;
+
+-- Nov 16, 2012 3:13:53 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Tab SET SeqNo=30,Updated=TO_TIMESTAMP('2012-11-16 15:13:53','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=116
+;
+
+-- Nov 16, 2012 3:13:57 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Tab SET SeqNo=40,Updated=TO_TIMESTAMP('2012-11-16 15:13:57','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=430
+;
+
+-- Nov 16, 2012 3:14:04 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Tab SET SeqNo=50,Updated=TO_TIMESTAMP('2012-11-16 15:14:04','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=200009
+;
+
+-- Nov 16, 2012 3:14:08 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Tab SET SeqNo=60,Updated=TO_TIMESTAMP('2012-11-16 15:14:08','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=107
+;
+
+-- Nov 16, 2012 3:14:25 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Tab SET SeqNo=70,Updated=TO_TIMESTAMP('2012-11-16 15:14:25','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=115
+;
+
+-- Nov 16, 2012 3:14:30 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Tab SET SeqNo=80,Updated=TO_TIMESTAMP('2012-11-16 15:14:30','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=200028
+;
+
+-- Nov 16, 2012 3:14:34 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Tab SET SeqNo=90,Updated=TO_TIMESTAMP('2012-11-16 15:14:34','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=117
+;
+
+-- Nov 16, 2012 3:14:39 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Tab SET SeqNo=100,Updated=TO_TIMESTAMP('2012-11-16 15:14:39','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=311
+;
+
+-- Nov 16, 2012 3:17:02 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Tab SET SeqNo=30,Updated=TO_TIMESTAMP('2012-11-16 15:17:02','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=107
+;
+
+-- Nov 16, 2012 3:17:24 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Tab SET SeqNo=70,Updated=TO_TIMESTAMP('2012-11-16 15:17:24','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=116
+;
+
+-- Nov 16, 2012 3:17:28 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Tab SET SeqNo=60,Updated=TO_TIMESTAMP('2012-11-16 15:17:28','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=115
+;
+
+-- Nov 16, 2012 3:22:06 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Field SET SeqNoGrid=0,IsDisplayedGrid='N' WHERE AD_Field_ID=275
+;
+
+-- Nov 16, 2012 3:22:06 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Field SET SeqNoGrid=0,IsDisplayedGrid='N' WHERE AD_Field_ID=6432
+;
+
+-- Nov 16, 2012 3:22:06 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Field SET SeqNoGrid=10,IsDisplayedGrid='Y' WHERE AD_Field_ID=1990
+;
+
+-- Nov 16, 2012 3:22:07 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Field SET SeqNoGrid=20,IsDisplayedGrid='Y' WHERE AD_Field_ID=126
+;
+
+-- Nov 16, 2012 3:22:07 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Field SET SeqNoGrid=30,IsDisplayedGrid='Y' WHERE AD_Field_ID=127
+;
+
+-- Nov 16, 2012 3:22:07 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Field SET SeqNoGrid=40,IsDisplayedGrid='Y' WHERE AD_Field_ID=128
+;
+
+-- Nov 16, 2012 3:22:07 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Field SET SeqNoGrid=50,IsDisplayedGrid='Y' WHERE AD_Field_ID=129
+;
+
+-- Nov 16, 2012 3:22:07 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Field SET SeqNoGrid=60,IsDisplayedGrid='Y' WHERE AD_Field_ID=274
+;
+
+-- Nov 16, 2012 3:22:07 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Field SET SeqNoGrid=70,IsDisplayedGrid='Y' WHERE AD_Field_ID=130
+;
+
+-- Nov 16, 2012 3:22:07 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Field SET SeqNoGrid=80,IsDisplayedGrid='Y' WHERE AD_Field_ID=5808
+;
+
+-- Nov 16, 2012 3:22:07 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Field SET SeqNoGrid=90,IsDisplayedGrid='Y' WHERE AD_Field_ID=4259
+;
+
+-- Nov 16, 2012 3:22:07 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Field SET SeqNoGrid=100,IsDisplayedGrid='Y' WHERE AD_Field_ID=1551
+;
+
+-- Nov 16, 2012 3:22:07 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Field SET SeqNoGrid=110,IsDisplayedGrid='Y' WHERE AD_Field_ID=132
+;
+
+-- Nov 16, 2012 3:22:07 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Field SET SeqNoGrid=120,IsDisplayedGrid='Y' WHERE AD_Field_ID=200276
+;
+
+-- Nov 16, 2012 3:22:07 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Field SET SeqNoGrid=130,IsDisplayedGrid='Y' WHERE AD_Field_ID=924
+;
+
+-- Nov 16, 2012 3:22:07 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Field SET SeqNoGrid=140,IsDisplayedGrid='Y' WHERE AD_Field_ID=133
+;
+
+-- Nov 16, 2012 3:22:07 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Field SET SeqNoGrid=150,IsDisplayedGrid='Y' WHERE AD_Field_ID=136
+;
+
+-- Nov 16, 2012 3:22:07 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Field SET SeqNoGrid=160,IsDisplayedGrid='Y' WHERE AD_Field_ID=142
+;
+
+-- Nov 16, 2012 3:22:07 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Field SET SeqNoGrid=170,IsDisplayedGrid='Y' WHERE AD_Field_ID=137
+;
+
+-- Nov 16, 2012 3:22:07 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Field SET SeqNoGrid=180,IsDisplayedGrid='Y' WHERE AD_Field_ID=200274
+;
+
+-- Nov 16, 2012 3:22:07 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Field SET SeqNoGrid=190,IsDisplayedGrid='Y' WHERE AD_Field_ID=139
+;
+
+-- Nov 16, 2012 3:22:07 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Field SET SeqNoGrid=200,IsDisplayedGrid='Y' WHERE AD_Field_ID=138
+;
+
+-- Nov 16, 2012 3:22:07 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Field SET SeqNoGrid=210,IsDisplayedGrid='Y' WHERE AD_Field_ID=8343
+;
+
+-- Nov 16, 2012 3:22:07 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Field SET SeqNoGrid=220,IsDisplayedGrid='Y' WHERE AD_Field_ID=140
+;
+
+-- Nov 16, 2012 3:22:07 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Field SET SeqNoGrid=230,IsDisplayedGrid='Y' WHERE AD_Field_ID=141
+;
+
+-- Nov 16, 2012 3:22:07 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Field SET SeqNoGrid=240,IsDisplayedGrid='Y' WHERE AD_Field_ID=13425
+;
+
+-- Nov 16, 2012 3:22:07 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Field SET SeqNoGrid=250,IsDisplayedGrid='Y' WHERE AD_Field_ID=54401
+;
+
+-- Nov 16, 2012 3:22:07 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Field SET SeqNoGrid=260,IsDisplayedGrid='Y' WHERE AD_Field_ID=54402
+;
+
+-- Nov 16, 2012 3:22:07 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Field SET SeqNoGrid=270,IsDisplayedGrid='Y' WHERE AD_Field_ID=13424
+;
+
+-- Nov 16, 2012 3:22:07 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Field SET SeqNoGrid=280,IsDisplayedGrid='Y' WHERE AD_Field_ID=62468
+;
+
+-- Nov 16, 2012 3:22:07 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Field SET SeqNoGrid=290,IsDisplayedGrid='Y' WHERE AD_Field_ID=200771
+;
+
+-- Nov 16, 2012 3:22:07 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Field SET SeqNoGrid=300,IsDisplayedGrid='Y' WHERE AD_Field_ID=53280
+;
+
+-- Nov 16, 2012 3:22:07 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Field SET SeqNoGrid=310,IsDisplayedGrid='Y' WHERE AD_Field_ID=200275
+;
+
+-- Nov 16, 2012 3:22:07 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Field SET SeqNoGrid=320,IsDisplayedGrid='Y' WHERE AD_Field_ID=200350
+;
+
+-- Nov 16, 2012 3:22:07 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Field SET SeqNoGrid=330,IsDisplayedGrid='Y' WHERE AD_Field_ID=200348
+;
+
+-- Nov 16, 2012 3:22:07 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Field SET SeqNoGrid=340,IsDisplayedGrid='Y' WHERE AD_Field_ID=200349
+;
+
+-- Nov 16, 2012 3:25:30 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Tab SET SeqNo=70,Updated=TO_TIMESTAMP('2012-11-16 15:25:30','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=149
+;
+
+-- Nov 16, 2012 3:25:34 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Tab SET SeqNo=80,Updated=TO_TIMESTAMP('2012-11-16 15:25:34','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=312
+;
+
+-- Nov 16, 2012 3:25:38 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Tab SET SeqNo=20,Updated=TO_TIMESTAMP('2012-11-16 15:25:38','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=122
+;
+
+-- Nov 16, 2012 3:25:43 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Tab SET SeqNo=30,Updated=TO_TIMESTAMP('2012-11-16 15:25:43','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=577
+;
+
+-- Nov 16, 2012 3:25:46 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Tab SET SeqNo=40,Updated=TO_TIMESTAMP('2012-11-16 15:25:46','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=123
+;
+
+-- Nov 16, 2012 3:25:51 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Tab SET SeqNo=50,Updated=TO_TIMESTAMP('2012-11-16 15:25:51','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=124
+;
+
+-- Nov 16, 2012 3:25:55 PM COT
+-- IDEMPIERE-369 Master Detail layout improvements
+UPDATE AD_Tab SET SeqNo=60,Updated=TO_TIMESTAMP('2012-11-16 15:25:55','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=644
+;
+
+SELECT register_migration_script('201211231024_IDEMPIERE-369_ReorgTabs.sql') FROM dual
+;
+
