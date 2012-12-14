2ce8f6279b2430735acd6b1ccccf3a5d115923fa
Carlos Ruiz <carg67@gmail.com>
2012-12-14 12:31:31 -0500

    IDEMPIERE-246 Integrate Manufacturing Light / Enable Move Confirmation window Fix problem reported by Francisco Vannini https://groups.google.com/d/topic/idempiere/qwvi1WLzirI/discussion

---
+++ migration/i1.0a-release/oracle/201212141227_IDEMPIERE-246.sql
@@ -0,0 +1,178 @@
+-- Dec 14, 2012 6:20:27 PM CET
+-- IDEMPIERE-246 Integrate Manufacturing Light
+UPDATE AD_Menu SET IsActive='Y',Updated=TO_DATE('2012-12-14 18:20:27','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Menu_ID=484
+;
+
+-- Dec 14, 2012 6:20:36 PM CET
+-- IDEMPIERE-246 Integrate Manufacturing Light
+UPDATE AD_Preference SET Value='Y',Updated=TO_DATE('2012-12-14 18:20:36','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Preference_ID=1000007
+;
+
+-- Dec 14, 2012 6:21:02 PM CET
+-- IDEMPIERE-246 Integrate Manufacturing Light
+UPDATE AD_TreeNodeMM SET Parent_ID=53066, SeqNo=0, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=53067
+;
+
+-- Dec 14, 2012 6:21:02 PM CET
+-- IDEMPIERE-246 Integrate Manufacturing Light
+UPDATE AD_TreeNodeMM SET Parent_ID=53066, SeqNo=1, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=53088
+;
+
+-- Dec 14, 2012 6:21:02 PM CET
+-- IDEMPIERE-246 Integrate Manufacturing Light
+UPDATE AD_TreeNodeMM SET Parent_ID=53066, SeqNo=2, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=53068
+;
+
+-- Dec 14, 2012 6:21:02 PM CET
+-- IDEMPIERE-246 Integrate Manufacturing Light
+UPDATE AD_TreeNodeMM SET Parent_ID=53066, SeqNo=3, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=53185
+;
+
+-- Dec 14, 2012 6:21:02 PM CET
+-- IDEMPIERE-246 Integrate Manufacturing Light
+UPDATE AD_TreeNodeMM SET Parent_ID=53066, SeqNo=4, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=445
+;
+
+-- Dec 14, 2012 6:21:02 PM CET
+-- IDEMPIERE-246 Integrate Manufacturing Light
+UPDATE AD_TreeNodeMM SET Parent_ID=53066, SeqNo=5, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=53184
+;
+
+-- Dec 14, 2012 6:21:02 PM CET
+-- IDEMPIERE-246 Integrate Manufacturing Light
+UPDATE AD_TreeNodeMM SET Parent_ID=53066, SeqNo=6, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=472
+;
+
+-- Dec 14, 2012 6:21:02 PM CET
+-- IDEMPIERE-246 Integrate Manufacturing Light
+UPDATE AD_TreeNodeMM SET Parent_ID=53066, SeqNo=7, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=53069
+;
+
+-- Dec 14, 2012 6:21:02 PM CET
+-- IDEMPIERE-246 Integrate Manufacturing Light
+UPDATE AD_TreeNodeMM SET Parent_ID=53066, SeqNo=8, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=53070
+;
+
+-- Dec 14, 2012 6:21:02 PM CET
+-- IDEMPIERE-246 Integrate Manufacturing Light
+UPDATE AD_TreeNodeMM SET Parent_ID=53066, SeqNo=9, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=53188
+;
+
+-- Dec 14, 2012 6:21:02 PM CET
+-- IDEMPIERE-246 Integrate Manufacturing Light
+UPDATE AD_TreeNodeMM SET Parent_ID=183, SeqNo=0, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=167
+;
+
+-- Dec 14, 2012 6:21:02 PM CET
+-- IDEMPIERE-246 Integrate Manufacturing Light
+UPDATE AD_TreeNodeMM SET Parent_ID=183, SeqNo=1, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=357
+;
+
+-- Dec 14, 2012 6:21:02 PM CET
+-- IDEMPIERE-246 Integrate Manufacturing Light
+UPDATE AD_TreeNodeMM SET Parent_ID=183, SeqNo=2, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=229
+;
+
+-- Dec 14, 2012 6:21:02 PM CET
+-- IDEMPIERE-246 Integrate Manufacturing Light
+UPDATE AD_TreeNodeMM SET Parent_ID=183, SeqNo=3, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=412
+;
+
+-- Dec 14, 2012 6:21:02 PM CET
+-- IDEMPIERE-246 Integrate Manufacturing Light
+UPDATE AD_TreeNodeMM SET Parent_ID=183, SeqNo=4, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=256
+;
+
+-- Dec 14, 2012 6:21:03 PM CET
+-- IDEMPIERE-246 Integrate Manufacturing Light
+UPDATE AD_TreeNodeMM SET Parent_ID=183, SeqNo=5, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=197
+;
+
+-- Dec 14, 2012 6:21:03 PM CET
+-- IDEMPIERE-246 Integrate Manufacturing Light
+UPDATE AD_TreeNodeMM SET Parent_ID=183, SeqNo=6, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=477
+;
+
+-- Dec 14, 2012 6:21:03 PM CET
+-- IDEMPIERE-246 Integrate Manufacturing Light
+UPDATE AD_TreeNodeMM SET Parent_ID=183, SeqNo=7, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=181
+;
+
+-- Dec 14, 2012 6:21:03 PM CET
+-- IDEMPIERE-246 Integrate Manufacturing Light
+UPDATE AD_TreeNodeMM SET Parent_ID=183, SeqNo=8, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=484
+;
+
+-- Dec 14, 2012 6:21:03 PM CET
+-- IDEMPIERE-246 Integrate Manufacturing Light
+UPDATE AD_TreeNodeMM SET Parent_ID=183, SeqNo=9, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=179
+;
+
+-- Dec 14, 2012 6:21:03 PM CET
+-- IDEMPIERE-246 Integrate Manufacturing Light
+UPDATE AD_TreeNodeMM SET Parent_ID=183, SeqNo=10, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=503
+;
+
+-- Dec 14, 2012 6:21:03 PM CET
+-- IDEMPIERE-246 Integrate Manufacturing Light
+UPDATE AD_TreeNodeMM SET Parent_ID=183, SeqNo=11, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=196
+;
+
+-- Dec 14, 2012 6:21:03 PM CET
+-- IDEMPIERE-246 Integrate Manufacturing Light
+UPDATE AD_TreeNodeMM SET Parent_ID=183, SeqNo=12, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=479
+;
+
+-- Dec 14, 2012 6:21:03 PM CET
+-- IDEMPIERE-246 Integrate Manufacturing Light
+UPDATE AD_TreeNodeMM SET Parent_ID=183, SeqNo=13, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=482
+;
+
+-- Dec 14, 2012 6:21:03 PM CET
+-- IDEMPIERE-246 Integrate Manufacturing Light
+UPDATE AD_TreeNodeMM SET Parent_ID=183, SeqNo=14, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=481
+;
+
+-- Dec 14, 2012 6:21:03 PM CET
+-- IDEMPIERE-246 Integrate Manufacturing Light
+UPDATE AD_TreeNodeMM SET Parent_ID=183, SeqNo=15, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=411
+;
+
+-- Dec 14, 2012 6:21:03 PM CET
+-- IDEMPIERE-246 Integrate Manufacturing Light
+UPDATE AD_TreeNodeMM SET Parent_ID=183, SeqNo=16, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=53253
+;
+
+-- Dec 14, 2012 6:21:03 PM CET
+-- IDEMPIERE-246 Integrate Manufacturing Light
+UPDATE AD_TreeNodeMM SET Parent_ID=183, SeqNo=17, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=426
+;
+
+-- Dec 14, 2012 6:21:03 PM CET
+-- IDEMPIERE-246 Integrate Manufacturing Light
+UPDATE AD_TreeNodeMM SET Parent_ID=183, SeqNo=18, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=537
+;
+
+-- Dec 14, 2012 6:21:03 PM CET
+-- IDEMPIERE-246 Integrate Manufacturing Light
+UPDATE AD_TreeNodeMM SET Parent_ID=183, SeqNo=19, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=311
+;
+
+-- Dec 14, 2012 6:21:03 PM CET
+-- IDEMPIERE-246 Integrate Manufacturing Light
+UPDATE AD_TreeNodeMM SET Parent_ID=183, SeqNo=20, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=292
+;
+
+-- Dec 14, 2012 6:21:03 PM CET
+-- IDEMPIERE-246 Integrate Manufacturing Light
+UPDATE AD_TreeNodeMM SET Parent_ID=183, SeqNo=21, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=504
+;
+
+-- Dec 14, 2012 6:21:03 PM CET
+-- IDEMPIERE-246 Integrate Manufacturing Light
+UPDATE AD_TreeNodeMM SET Parent_ID=183, SeqNo=22, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=515
+;
+
+SELECT register_migration_script('201212141227_IDEMPIERE-246.sql') FROM dual
+;
+
