5495a2f2cb8614a473d442ff7515951292b933e9
Carlos Ruiz <carg67@gmail.com>
2012-12-17 10:03:30 -0500

    IDEMPIERE-460 Integrate and migrate 3e services / Peer review migration scripts

--- migration/i1.0a-release/oracle/201212131727_WS005_IdempiereWebservice.sql
+++ migration/i1.0a-release/oracle/201212131727_WS005_IdempiereWebservice.sql
@@ -251,16 +251,6 @@ INSERT INTO AD_Column_Trl (AD_Language,AD_Column_ID, Name, IsTranslated,AD_Clien
 ALTER TABLE WS_WebServiceTypeAccess ADD WS_WebServiceTypeAccess_UU NVARCHAR2(36) DEFAULT NULL 
 ;
 
--- Dec 17, 2012 12:03:20 PM IST
--- IDEMPIERE-460 Adding support for composite service
-INSERT INTO AD_Column (IsEncrypted,Version,AD_Table_ID,AD_Column_ID,EntityType,IsMandatory,IsTranslated,IsIdentifier,IsParent,FieldLength,IsSelectionColumn,AD_Reference_ID,IsKey,AD_Element_ID,AD_Column_UU,IsUpdateable,ColumnName,Name,CreatedBy,Updated,AD_Org_ID,IsActive,Created,UpdatedBy,AD_Client_ID,IsAlwaysUpdateable) VALUES ('N',0,53163,205535,'D','N','N','N','N',36,'N',10,'N',1000001,'4a981564-a0d8-494e-b5a2-ae60bbb54404','Y','WS_WebServiceMethod_UU','WS_WebServiceMethod_UU',100,TO_DATE('2012-12-17 12:03:18','YYYY-MM-DD HH24:MI:SS'),0,'Y',TO_DATE('2012-12-17 12:03:18','YYYY-MM-DD HH24:MI:SS'),100,0,'N')
-;
-
--- Dec 17, 2012 12:03:20 PM IST
--- IDEMPIERE-460 Adding support for composite service
-INSERT INTO AD_Column_Trl (AD_Language,AD_Column_ID, Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,AD_Column_Trl_UU ) SELECT l.AD_Language,t.AD_Column_ID, t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,Generate_UUID() FROM AD_Language l, AD_Column t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Column_ID=205535 AND NOT EXISTS (SELECT * FROM AD_Column_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Column_ID=t.AD_Column_ID)
-;
-
 
 
 
