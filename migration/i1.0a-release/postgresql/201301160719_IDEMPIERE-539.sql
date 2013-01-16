c6d8a41e5a3bb24339080037de374c1fbb5dae82
Carlos Ruiz <carg67@gmail.com>
2013-01-16 07:21:58 -0800

    IDEMPIERE-539 Import GL Window / reorg zk window

---
+++ migration/i1.0a-release/postgresql/201301160719_IDEMPIERE-539.sql
@@ -0,0 +1,73 @@
+-- Jan 16, 2013 7:18:51 AM PST
+-- IDEMPIERE-539 Import GL Window does not allow Key Values for Activity, Campaign, Sales Region
+UPDATE AD_Field SET SeqNo=510,Updated=TO_TIMESTAMP('2013-01-16 07:18:51','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=201652
+;
+
+-- Jan 16, 2013 7:18:51 AM PST
+-- IDEMPIERE-539 Import GL Window does not allow Key Values for Activity, Campaign, Sales Region
+UPDATE AD_Field SET SeqNo=520, IsDisplayed='Y', XPosition=4,Updated=TO_TIMESTAMP('2013-01-16 07:18:51','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=7114
+;
+
+-- Jan 16, 2013 7:18:51 AM PST
+-- IDEMPIERE-539 Import GL Window does not allow Key Values for Activity, Campaign, Sales Region
+UPDATE AD_Field SET SeqNo=530,Updated=TO_TIMESTAMP('2013-01-16 07:18:51','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=201651
+;
+
+-- Jan 16, 2013 7:18:51 AM PST
+-- IDEMPIERE-539 Import GL Window does not allow Key Values for Activity, Campaign, Sales Region
+UPDATE AD_Field SET SeqNo=540,Updated=TO_TIMESTAMP('2013-01-16 07:18:51','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=7109
+;
+
+-- Jan 16, 2013 7:18:51 AM PST
+-- IDEMPIERE-539 Import GL Window does not allow Key Values for Activity, Campaign, Sales Region
+UPDATE AD_Field SET SeqNo=550,Updated=TO_TIMESTAMP('2013-01-16 07:18:51','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=201653
+;
+
+-- Jan 16, 2013 7:18:51 AM PST
+-- IDEMPIERE-539 Import GL Window does not allow Key Values for Activity, Campaign, Sales Region
+UPDATE AD_Field SET SeqNo=560, IsDisplayed='Y', XPosition=4,Updated=TO_TIMESTAMP('2013-01-16 07:18:51','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=7157
+;
+
+-- Jan 16, 2013 7:18:52 AM PST
+-- IDEMPIERE-539 Import GL Window does not allow Key Values for Activity, Campaign, Sales Region
+UPDATE AD_Field SET SeqNo=570,Updated=TO_TIMESTAMP('2013-01-16 07:18:52','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=7107
+;
+
+-- Jan 16, 2013 7:18:52 AM PST
+-- IDEMPIERE-539 Import GL Window does not allow Key Values for Activity, Campaign, Sales Region
+UPDATE AD_Field SET SeqNo=580,Updated=TO_TIMESTAMP('2013-01-16 07:18:52','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=7142
+;
+
+-- Jan 16, 2013 7:18:52 AM PST
+-- IDEMPIERE-539 Import GL Window does not allow Key Values for Activity, Campaign, Sales Region
+UPDATE AD_Field SET SeqNo=590,Updated=TO_TIMESTAMP('2013-01-16 07:18:52','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=7110
+;
+
+-- Jan 16, 2013 7:18:52 AM PST
+-- IDEMPIERE-539 Import GL Window does not allow Key Values for Activity, Campaign, Sales Region
+UPDATE AD_Field SET SeqNo=600,Updated=TO_TIMESTAMP('2013-01-16 07:18:52','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=7133
+;
+
+-- Jan 16, 2013 7:18:52 AM PST
+-- IDEMPIERE-539 Import GL Window does not allow Key Values for Activity, Campaign, Sales Region
+UPDATE AD_Field SET SeqNo=610,Updated=TO_TIMESTAMP('2013-01-16 07:18:52','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=7117
+;
+
+-- Jan 16, 2013 7:18:52 AM PST
+-- IDEMPIERE-539 Import GL Window does not allow Key Values for Activity, Campaign, Sales Region
+UPDATE AD_Field SET SeqNo=620,Updated=TO_TIMESTAMP('2013-01-16 07:18:52','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=7131
+;
+
+-- Jan 16, 2013 7:18:52 AM PST
+-- IDEMPIERE-539 Import GL Window does not allow Key Values for Activity, Campaign, Sales Region
+UPDATE AD_Field SET SeqNo=630,Updated=TO_TIMESTAMP('2013-01-16 07:18:52','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=7138
+;
+
+-- Jan 16, 2013 7:18:52 AM PST
+-- IDEMPIERE-539 Import GL Window does not allow Key Values for Activity, Campaign, Sales Region
+UPDATE AD_Field SET SeqNo=640,Updated=TO_TIMESTAMP('2013-01-16 07:18:52','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=7136
+;
+
+SELECT register_migration_script('201301160719_IDEMPIERE-539.sql') FROM dual
+;
+
