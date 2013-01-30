395108fe01d0eba5e1a72da89cf47143befc56b7
Carlos Ruiz <carg67@gmail.com>
2013-01-29 17:21:27 -0800

    IDEMPIERE-294 Maintenance / Fix wrong datatype of column on DB (oracle)

--- migration/i1.0a-release/oracle/201301291717_IDEMPIERE-294.sql
+++ migration/i1.0a-release/oracle/201301291717_IDEMPIERE-294.sql
@@ -2,7 +2,7 @@
 ALTER TABLE ad_broadcastmessage DROP COLUMN notification_client_id;
 ;
 
-ALTER TABLE ad_broadcastmessage ADD COLUMN notification_client_id numeric(10,0) NULL;
+ALTER TABLE ad_broadcastmessage ADD (notification_client_id NUMBER(10,0) NULL);
 ;
 
 SELECT register_migration_script('201301291717_IDEMPIERE-294.sql') FROM dual
