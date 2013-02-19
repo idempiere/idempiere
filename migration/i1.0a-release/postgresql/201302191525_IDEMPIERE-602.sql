397ecfbde9d85ee08de4b9e1b94579e5a0399652
Elaine Tan <etan@trekglobal.com>
2013-02-19 15:27:33 +0800

    IDEMPIERE-602 - Remove Furniture/Fertilizer Internal Shipper from System

---
+++ migration/i1.0a-release/postgresql/201302191525_IDEMPIERE-602.sql
@@ -0,0 +1,19 @@
+-- Feb 19, 2013 2:59:58 PM SGT
+-- IDEMPIERE-602 Remove Furniture/Fertilizer Internal Shipper from System
+DELETE FROM X_Shipper WHERE X_Shipper_ID=200000
+;
+
+-- Feb 19, 2013 3:00:01 PM SGT
+-- IDEMPIERE-602 Remove Furniture/Fertilizer Internal Shipper from System
+DELETE FROM X_Shipper WHERE X_Shipper_ID=200001
+;
+
+-- Feb 19, 2013 3:00:04 PM SGT
+-- IDEMPIERE-602 Remove Furniture/Fertilizer Internal Shipper from System
+DELETE FROM X_Shipper WHERE X_Shipper_ID=200002
+;
+
+UPDATE M_Shipper SET X_Shipper_ID = NULL WHERE X_Shipper_ID NOT IN (SELECT X_Shipper_ID FROM X_Shipper);
+
+SELECT register_migration_script('201302191525_IDEMPIERE-602.sql') FROM dual
+;
