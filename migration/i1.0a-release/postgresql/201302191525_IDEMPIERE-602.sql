6f59d2772ef12318da7f6488bd7e32ac247b83d4
Elaine Tan <etan@trekglobal.com>
2013-02-22 17:13:18 +0800

    IDEMPIERE-602 - Remove Furniture/Fertilizer Internal Shipper from System - fix migration script

--- migration/i1.0a-release/postgresql/201302191525_IDEMPIERE-602.sql
+++ migration/i1.0a-release/postgresql/201302191525_IDEMPIERE-602.sql
@@ -1,3 +1,5 @@
+ALTER TABLE M_Shipper DROP CONSTRAINT m_shipper_unique_columns;
+
 -- Feb 19, 2013 2:59:58 PM SGT
 -- IDEMPIERE-602 Remove Furniture/Fertilizer Internal Shipper from System
 DELETE FROM X_Shipper WHERE X_Shipper_ID=200000
