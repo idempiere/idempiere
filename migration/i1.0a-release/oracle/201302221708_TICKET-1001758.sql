a4aeb85c5cb755abe8ae266e7b1eec079ec4c3e1
Elaine Tan <etan@trekglobal.com>
2013-02-22 17:35:34 +0800

    Ticket #1001758: FedEx & UPS - include register_migration_script statement

--- migration/i1.0a-release/oracle/201302221708_TICKET-1001758.sql
+++ migration/i1.0a-release/oracle/201302221708_TICKET-1001758.sql
@@ -643,3 +643,5 @@ UPDATE C_Order SET IsPriviledgedRate='N' WHERE IsPriviledgedRate IS NULL
 UPDATE AD_Process_Para SET DefaultValue='@IsPriviledgedRate@',Updated=TO_DATE('2013-02-22 16:10:58','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=200049
 ;
 
+SELECT register_migration_script('201302221708_TICKET-1001758.sql') FROM dual
+;
