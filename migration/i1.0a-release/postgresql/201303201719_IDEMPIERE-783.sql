1cfb0bc2e09e9cfd99d3de9a509b27333c7aba54
Carlos Ruiz <carg67@gmail.com>
2013-03-22 15:26:28 -0500

    IDEMPIERE-783 search fields in Window Process Audit / peer review

--- migration/i1.0a-release/postgresql/201303201719_IDEMPIERE-783.sql
+++ migration/i1.0a-release/postgresql/201303201719_IDEMPIERE-783.sql
@@ -1,11 +1,22 @@
 -- Mar 20, 2013 5:14:16 PM COT
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+-- IDEMPIERE-783 search fields in Window Process Audit 
 UPDATE AD_Column SET IsSelectionColumn='Y',Updated=TO_TIMESTAMP('2013-03-20 17:14:16','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=2781
 ;
 
 -- Mar 20, 2013 5:14:43 PM COT
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
 UPDATE AD_Column SET IsSelectionColumn='Y',Updated=TO_TIMESTAMP('2013-03-20 17:14:43','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=5951
+;
+
+-- Mar 22, 2013 3:19:07 PM COT
+UPDATE AD_Column SET SeqNo=3,Updated=TO_TIMESTAMP('2013-03-22 15:19:07','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=2809
+;
+
+-- Mar 22, 2013 3:19:13 PM COT
+UPDATE AD_Column SET SeqNo=2,Updated=TO_TIMESTAMP('2013-03-22 15:19:13','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=4023
+;
+
+-- Mar 22, 2013 3:19:19 PM COT
+UPDATE AD_Column SET SeqNo=1,Updated=TO_TIMESTAMP('2013-03-22 15:19:19','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=2809
 ;
 
 SELECT register_migration_script('201303201719_IDEMPIERE-783.sql') FROM dual;
