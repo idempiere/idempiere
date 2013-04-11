242d5ccba43566392a7236fde5190676eb828137
Heng Sin Low <hengsin@gmail.com>
2013-04-11 14:16:09 +0800

    IDEMPIERE-854 Wrong length for AD_SessionInfo_v.ServerName

---
+++ migration/i1.0a-release/postgresql/201304110613_IDEMPIERE-854.sql
@@ -0,0 +1,13 @@
+-- Apr 11, 2013 2:08:24 PM MYT
+-- IDEMPIERE-854 Wrong length for AD_SessionInfo_v.ServerName
+UPDATE AD_Column SET FieldLength=80,Updated=TO_TIMESTAMP('2013-04-11 14:08:24','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=200602
+;
+
+-- Apr 11, 2013 2:10:13 PM MYT
+-- IDEMPIERE-854 Wrong length for AD_SessionInfo_v.ServerName
+UPDATE AD_Field SET ColumnSpan=2,Updated=TO_TIMESTAMP('2013-04-11 14:10:13','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=200612
+;
+
+SELECT register_migration_script('201304110613_IDEMPIERE-854.sql') FROM dual
+;
+
