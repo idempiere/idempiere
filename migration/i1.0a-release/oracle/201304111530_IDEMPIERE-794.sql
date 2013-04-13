1360e20a4709acf4a81ae3b87547bfb11c475e80
David Peñuela <david.penuela@globalqss.com>
2013-04-12 20:17:30 -0500

    IDEMPIERE-794 Ticket #1001025 - Expand the views to provide more information / Open orders

---
+++ migration/i1.0a-release/oracle/201304111530_IDEMPIERE-794.sql
@@ -0,0 +1,8 @@
+-- Apr 11, 2013 3:33:35 PM COT
+UPDATE AD_ReportView SET WhereClause='(QtyToDeliver<>0 OR QtyToInvoice<>0) AND RV_OrderDetail.DocStatus NOT IN (''VO'',''CL'')',Updated=TO_DATE('2013-04-11 15:33:35','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_ReportView_ID=104
+;
+
+SELECT register_migration_script('201304111530_IDEMPIERE-794.sql') FROM dual
+;
+
+
