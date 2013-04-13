578578b3945233520b8a781460ae49e4ab765443
David Peñuela <david.penuela@globalqss.com>
2013-04-13 10:57:51 -0500

    IDEMPIERE-794 Ticket #1001025 - Expand the views to provide more information / Fix invoice not realized

---
+++ migration/i1.0a-release/oracle/201304121120_IDEMPIERE-794.sql
@@ -0,0 +1,7 @@
+-- Apr 12, 2013 10:41:36 AM COT
+UPDATE AD_Column SET AD_Reference_Value_ID=170,Updated=TO_DATE('2013-04-12 10:41:36','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=205479
+;
+
+SELECT register_migration_script('201304121120_IDEMPIERE-794.sql') FROM dual
+;
+
