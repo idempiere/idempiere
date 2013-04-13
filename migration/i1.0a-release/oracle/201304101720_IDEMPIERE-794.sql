ad3f38b92f8a67df361cf89ba53d55e6cd824a53
Carlos Ruiz <carg67@gmail.com>
2013-04-12 20:12:25 -0500

    IDEMPIERE-794 Ticket #1001025 - Expand the views to provide more information / Commission Run Detail / Complete

--- migration/i1.0a-release/oracle/201304101720_IDEMPIERE-794.sql
+++ migration/i1.0a-release/oracle/201304101720_IDEMPIERE-794.sql
@@ -6,6 +6,11 @@ UPDATE AD_Column SET AD_Reference_Value_ID=271,Updated=TO_DATE('2013-04-10 17:13
 UPDATE AD_Column SET AD_Reference_Value_ID=290,Updated=TO_DATE('2013-04-10 17:14:38','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=204935
 ;
 
+-- Apr 12, 2013 8:04:22 PM COT
+-- IDEMPIERE-794 Ticket #1001025 - Expand the views to provide more information
+UPDATE AD_Column SET AD_Reference_Value_ID=200037, AD_Reference_ID=18, IsToolbarButton='N',Updated=TO_DATE('2013-04-12 20:04:22','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=204935
+;
+
 SELECT register_migration_script('201304101720_IDEMPIERE-794.sql') FROM dual
 ;
 
