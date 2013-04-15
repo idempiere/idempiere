37ca5f423a69330f6216bd7fde1b0aa6e567262b
Deepak Pansheriya <deepakpansheriya@gmail.com>
2013-04-15 17:18:20 +0530

    IDEMPIERE-294 Setting System+client permission for expire broad cast message process

---
+++ migration/i1.0a-release/oracle/201304150457_IDEMPIERE-294.sql
@@ -0,0 +1,7 @@
+-- Apr 15, 2013 3:54:49 PM IST
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET AccessLevel='6',Updated=TO_DATE('2013-04-15 15:54:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=200018
+;
+
+SELECT register_migration_script('201304150457_IDEMPIERE-294.sql') FROM dual
+;
