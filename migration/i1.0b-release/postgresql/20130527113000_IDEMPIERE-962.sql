4ca2592057dbb03e436aa1b7921a4a4057981c7e
Carlos Ruiz <carg67@gmail.com>
2013-05-29 07:55:42 -0500

    IDEMPIERE-962 Misspelled words and/or sentences / Fix typos on DocAction reflist

--- migration/i1.0b-release/postgresql/20130527113000_IDEMPIERE-962.sql
+++ migration/i1.0b-release/postgresql/20130527113000_IDEMPIERE-962.sql
@@ -18,6 +18,17 @@ UPDATE AD_Process SET Description='Check data consistency of Business Partner',U
 UPDATE AD_Menu SET Name='Validate Business Partner', Description='Check data consistency of Business Partner', IsActive='Y',Updated=TO_TIMESTAMP('2013-05-27 10:59:41','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Menu_ID=506
 ;
 
-SELECT register_migration_script('20130527113000_IDEMPIERE-962.sql') FROM dual
+-- May 29, 2013 7:52:39 AM COT
+UPDATE AD_Ref_List SET Description='Check Document consistency and check Inventory',Updated=TO_TIMESTAMP('2013-05-29 07:52:39','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Ref_List_ID=345
 ;
 
+-- May 29, 2013 7:53:14 AM COT
+UPDATE AD_Ref_List SET Description='Reopen Document and Reverse automatically generated documents; You need to Complete the transaction after the change. ',Updated=TO_TIMESTAMP('2013-05-29 07:53:14','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Ref_List_ID=188
+;
+
+-- May 29, 2013 7:53:31 AM COT
+UPDATE AD_Ref_List SET Description='Wait Condition ok Complete Document',Updated=TO_TIMESTAMP('2013-05-29 07:53:31','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Ref_List_ID=691
+;
+
+SELECT register_migration_script('20130527113000_IDEMPIERE-962.sql') FROM dual
+;
