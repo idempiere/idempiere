ef037df1b1053b8d52b3c1a1e08d8086561b30fe
Carlos Ruiz <carg67@gmail.com>
2013-05-29 17:17:41 -0500

    IDEMPIERE-933 Window Customization Security Hole

--- migration/i1.0b-release/postgresql/201305171202_IDEMPIERE-933.sql
+++ migration/i1.0b-release/postgresql/201305171202_IDEMPIERE-933.sql
@@ -1,7 +1,6 @@
 -- May 17, 2013 11:59:06 AM COT
 -- IDEMPIERE-933 Window Customization Security Hole
-INSERT INTO AD_Message (MsgType,MsgText,AD_Message_ID,EntityType,AD_Message_UU,Value,IsActive,Updated,CreatedBy,UpdatedBy,AD_Org_ID,Created,AD_Client_ID) VALUES ('I','The reference of an encripted field cannot be changed
-',200175,'D','f09382d4-62bb-48a8-abb9-d71ec5fbc5fe','NotChangeReference','Y',TO_TIMESTAMP('2013-05-17 11:59:05','YYYY-MM-DD HH24:MI:SS'),100,100,0,TO_TIMESTAMP('2013-05-17 11:59:05','YYYY-MM-DD HH24:MI:SS'),0)
+INSERT INTO AD_Message (MsgType,MsgText,AD_Message_ID,EntityType,AD_Message_UU,Value,IsActive,Updated,CreatedBy,UpdatedBy,AD_Org_ID,Created,AD_Client_ID) VALUES ('I','The reference of an encrypted field cannot be changed',200175,'D','f09382d4-62bb-48a8-abb9-d71ec5fbc5fe','NotChangeReference','Y',TO_TIMESTAMP('2013-05-17 11:59:05','YYYY-MM-DD HH24:MI:SS'),100,100,0,TO_TIMESTAMP('2013-05-17 11:59:05','YYYY-MM-DD HH24:MI:SS'),0)
 ;
 
 -- May 17, 2013 11:59:06 AM COT
