96be1963fbd88a01719ee3e93cf2ad09f76b173b
Carlos Augusto Sanchez <carlos.sanchez@globalqss.com>
2013-06-05 16:08:05 -0500

    IDEMPIERE-962 Misspelled words and/or sentences

---
+++ migration/i1.0b-release/postgresql/20130605095600_IDEMPIERE-962.sql
@@ -0,0 +1,7 @@
+-- Jun 5, 2013 9:55:17 AM COT
+-- IDEMPIERE-962  Misspelled words and/or sentences 
+UPDATE AD_Tab SET Name='Activity Dimension', Description='Maintain Activity Accounting Dimension Tree',Updated=TO_TIMESTAMP('2013-06-05 09:55:17','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=517
+;
+
+SELECT register_migration_script('20130605095600_IDEMPIERE-962.sql') FROM dual
+;
