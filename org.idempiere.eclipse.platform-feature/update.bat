4f600b50602cb1d769ccf456e61d80d1af25a2f3
Heng Sin Low <hengsin@gmail.com>
2013-07-15 17:13:07 +0800

    IDEMPIERE-1175 iDempiere Server update script should preserve tomcat configuration file.

--- org.idempiere.eclipse.platform-feature/update.bat
+++ org.idempiere.eclipse.platform-feature/update.bat
@@ -4,9 +4,17 @@
 cd %~dp0
 
 cp idempiere.ini idempiere.ini.sav
+if exist server.xml.sav del /q server.xml.sav
+if exist plugins/org.adempiere.tomcat.config_1.0.0/META-INF/tomcat/server.xml (
+   copy plugins/org.adempiere.tomcat.config_1.0.0/META-INF/tomcat/server.xml server.xml.sav
+)
 
 java -Dosgi.noShutdown=false -Dosgi.compatibility.bootdelegation=true -Dosgi.install.area=director -jar plugins/org.eclipse.osgi_3.7.*.jar -application org.eclipse.equinox.p2.director -consoleLog -profileProperties org.eclipse.update.install.features=true -destination %~dp0 -repository %1 -u org.adempiere.server.product
 
 java -Dosgi.noShutdown=false -Dosgi.compatibility.bootdelegation=true -Dosgi.install.area=director -jar plugins/org.eclipse.osgi_3.7.*.jar -application org.eclipse.equinox.p2.director -consoleLog -profileProperties org.eclipse.update.install.features=true -destination %~dp0 -repository %1 -i org.adempiere.server.product
 
 cp idempiere.ini.sav idempiere.ini
+if exist server.xml.sav (
+   copy server.xml.sav plugins/org.adempiere.tomcat.config_1.0.0/META-INF/tomcat/server.xml 
+   del /q server.xml.sav
+)
