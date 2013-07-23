6eefdc62e8e8cfd0d9005f961d70ac714c265513
Heng Sin Low <hengsin@gmail.com>
2013-07-23 13:23:10 +0800

    IDEMPIERE-1168 Master-detail issue with context variables (problem paying multiple invoices). Product cost movement tab break by the context fixed.

---
+++ migration/i1.0c-release/oracle/201307230308_IDEMPIERE-1168.sql
@@ -0,0 +1,9 @@
+-- Jul 23, 2013 7:43:57 AM MYT
+-- IDEMPIERE-1168
+UPDATE AD_Tab SET AD_Column_ID=200042, WhereClause='M_CostMovement_v.AD_Client_ID=@AD_Client_ID@ AND M_CostMovement_v.M_Product_ID=@M_Product_ID@ AND M_CostMovement_v.M_CostType_ID=@M_CostType_ID@ AND M_CostMovement_v.C_AcctSchema_ID=@C_AcctSchema_ID@ AND M_CostMovement_v.M_CostElement_ID=@M_CostElement_ID@ AND M_CostMovement_v.M_AttributeSetInstance_ID=@M_AttributeSetInstance_ID@',Updated=TO_DATE('2013-07-23 07:43:57','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=200000
+;
+
+SELECT register_migration_script('201307230308_IDEMPIERE-1168.sql') FROM dual
+;
+
+
