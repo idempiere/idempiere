37e24fb680193e40485cca41ff5f03d750974066
Deepak Pansheriya <deepakpansheriya@gmail.com>
2013-07-31 16:00:54 +0530

    IDEMPIERE-1020 BOM error if product searchkey is longer than 22 characters

---
+++ migration/i1.0c-release/oracle/201307311545_IDEMPIERE-1020.sql
@@ -0,0 +1,5 @@
+-- Jul 31, 2013 3:43:04 PM IST
+-- Changes in the layout of Web Service Security window
+UPDATE AD_Column SET FieldLength=40,Updated=TO_DATE('2013-07-31 15:43:04','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=61999
+;
+
