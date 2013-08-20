af853db842355aaaa154abd921fd5950f8f40ae4
Heng Sin Low <hengsin@gmail.com>
2013-08-20 13:57:22 +0800

    IDEMPIERE-1285 Estimated Landed Cost for PO. Fixed field position for landed cost clearing account at accounting schema default tab.

---
+++ migration/i1.0c-release/postgresql/201308200554_IDEMPIERE-1285.sql
@@ -0,0 +1,78 @@
+-- Aug 20, 2013 10:53:02 AM MYT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET SeqNo=290, IsDisplayed='Y', XPosition=1,Updated=TO_TIMESTAMP('2013-08-20 10:53:02','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=202403
+;
+
+-- Aug 20, 2013 10:53:02 AM MYT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET SeqNo=300,Updated=TO_TIMESTAMP('2013-08-20 10:53:02','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=2662
+;
+
+-- Aug 20, 2013 10:53:02 AM MYT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET SeqNo=310,Updated=TO_TIMESTAMP('2013-08-20 10:53:02','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=3842
+;
+
+-- Aug 20, 2013 10:53:02 AM MYT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET SeqNo=320,Updated=TO_TIMESTAMP('2013-08-20 10:53:02','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=3841
+;
+
+-- Aug 20, 2013 10:53:02 AM MYT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET SeqNo=330,Updated=TO_TIMESTAMP('2013-08-20 10:53:02','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=5132
+;
+
+-- Aug 20, 2013 10:53:02 AM MYT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET SeqNo=340,Updated=TO_TIMESTAMP('2013-08-20 10:53:02','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=5133
+;
+
+-- Aug 20, 2013 10:53:02 AM MYT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET SeqNo=350,Updated=TO_TIMESTAMP('2013-08-20 10:53:02','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=3845
+;
+
+-- Aug 20, 2013 10:53:03 AM MYT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET SeqNo=360,Updated=TO_TIMESTAMP('2013-08-20 10:53:02','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=3844
+;
+
+-- Aug 20, 2013 10:53:03 AM MYT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET SeqNo=370,Updated=TO_TIMESTAMP('2013-08-20 10:53:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=3839
+;
+
+-- Aug 20, 2013 10:53:03 AM MYT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET SeqNo=380,Updated=TO_TIMESTAMP('2013-08-20 10:53:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=3840
+;
+
+-- Aug 20, 2013 10:53:03 AM MYT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET SeqNo=390,Updated=TO_TIMESTAMP('2013-08-20 10:53:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=3836
+;
+
+-- Aug 20, 2013 10:53:03 AM MYT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET SeqNo=400,Updated=TO_TIMESTAMP('2013-08-20 10:53:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=3851
+;
+
+-- Aug 20, 2013 10:53:03 AM MYT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET SeqNo=410,Updated=TO_TIMESTAMP('2013-08-20 10:53:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=2654
+;
+
+-- Aug 20, 2013 10:53:03 AM MYT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET SeqNo=420,Updated=TO_TIMESTAMP('2013-08-20 10:53:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=3835
+;
+
+-- Aug 20, 2013 10:53:03 AM MYT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET SeqNo=430,Updated=TO_TIMESTAMP('2013-08-20 10:53:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=3823
+;
+
+SELECT register_migration_script('201308200554_IDEMPIERE-1285.sql') FROM dual
+;
+
