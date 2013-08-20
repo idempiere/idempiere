25f2528b551364dd3ca4c24f1f8b9b0dc9afe810
Heng Sin Low <hengsin@gmail.com>
2013-08-20 16:23:50 +0800

    IDEMPIERE-1219 Error message when saving 2 default Tax Rates are not translated. Fixed typo in migration script.

--- migration/i1.0c-release/postgresql/201308141001_IDEMPIERE-1219.sql
+++ migration/i1.0c-release/postgresql/201308141001_IDEMPIERE-1219.sql
@@ -8,6 +8,5 @@ INSERT INTO AD_Message (MsgType,MsgText,AD_Message_ID,EntityType,AD_Message_UU,V
 INSERT INTO AD_Message_Trl (AD_Language,AD_Message_ID, MsgText,MsgTip, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,AD_Message_Trl_UU ) SELECT l.AD_Language,t.AD_Message_ID, t.MsgText,t.MsgTip, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,Generate_UUID() FROM AD_Language l, AD_Message t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Message_ID=200220 AND NOT EXISTS (SELECT * FROM AD_Message_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Message_ID=t.AD_Message_ID)
 ;
 
-SELECT register_migration_script('201308141001_IDEMPIERE-1269.sql') FROM dual
+SELECT register_migration_script('201308141001_IDEMPIERE-1219.sql') FROM dual
 ;
-
