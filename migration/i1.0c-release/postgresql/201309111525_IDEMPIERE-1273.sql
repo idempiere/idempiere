0af5dda930a89a547411e3478e56e6be39da3f32
Heng Sin Low <hengsin@gmail.com>
2013-09-12 10:36:41 +0800

    IDEMPIERE-1273 "Invoice Payment Schedule" window isn't suitable. Disable insert for invoice header.

---
+++ migration/i1.0c-release/postgresql/201309111525_IDEMPIERE-1273.sql
@@ -0,0 +1,12 @@
+-- Sep 11, 2013 3:25:16 PM COT
+-- IDEMPIERE-1273 Invoice Payment Schedule" window isn't suitable
+UPDATE AD_Tab SET IsInsertRecord='N',Updated=TO_TIMESTAMP('2013-09-11 15:25:16','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=501
+;
+
+-- Sep 11, 2013 3:25:23 PM COT
+-- IDEMPIERE-1273 Invoice Payment Schedule" window isn't suitable
+UPDATE AD_Tab SET IsInsertRecord='N',Updated=TO_TIMESTAMP('2013-09-11 15:25:23','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=666
+;
+
+SELECT register_migration_script('201309111525_IDEMPIERE-1273.sql') FROM dual
+;
