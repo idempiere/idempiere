93c9beea4b647825744ec912933b9fa1884faff3
Carlos Ruiz <carg67@gmail.com>
2013-09-23 08:03:50 -0500

    IDEMPIERE-1390 Revenue Recognition window is not active / IDEMPIERE-362

---
+++ migration/i1.0c-release/postgresql/201309230801_IDEMPIERE-1390.sql
@@ -0,0 +1,28 @@
+-- Sep 23, 2013 7:56:22 AM COT
+-- IDEMPIERE-1390 Revenue Recognition window is not active
+UPDATE AD_Field SET IsDisplayed='N', IsDisplayedGrid='N',Updated=TO_TIMESTAMP('2013-09-23 07:56:22','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=11767
+;
+
+-- Sep 23, 2013 7:56:24 AM COT
+UPDATE AD_Field SET IsDisplayed='N', IsDisplayedGrid='N',Updated=TO_TIMESTAMP('2013-09-23 07:56:24','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=5529
+;
+
+-- Sep 23, 2013 7:56:26 AM COT
+UPDATE AD_Field SET IsDisplayed='N', IsDisplayedGrid='N',Updated=TO_TIMESTAMP('2013-09-23 07:56:26','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=5431
+;
+
+UPDATE AD_Field SET IsDisplayed='N', IsDisplayedGrid='N',Updated=TO_TIMESTAMP('2013-09-23 07:56:26','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=3079
+;
+
+UPDATE AD_Field SET IsDisplayed='N', IsDisplayedGrid='N',Updated=TO_TIMESTAMP('2013-09-23 07:56:26','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=53577
+;
+
+UPDATE AD_Field SET IsDisplayed='N', IsDisplayedGrid='N',Updated=TO_TIMESTAMP('2013-09-23 07:56:26','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=4821
+;
+
+UPDATE AD_Field SET IsDisplayed='N', IsDisplayedGrid='N',Updated=TO_TIMESTAMP('2013-09-23 07:56:26','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=53875
+;
+
+SELECT register_migration_script('201309230801_IDEMPIERE-1390.sql') FROM dual
+;
+
