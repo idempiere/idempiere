3df36ec2ed8184705b855f5ec774dacbfc94c32f
Carlos Ruiz <carg67@gmail.com>
2013-10-16 17:48:40 -0500

    IDEMPIERE-1408 User window must not show System users

---
+++ migration/i1.0c-release/oracle/201309302139_IDEMPIERE-1408.sql
@@ -0,0 +1,11 @@
+SET SQLBLANKLINES ON
+SET DEFINE OFF
+
+-- Sep 30, 2013 9:38:50 PM COT
+-- IDEMPIERE-1408 User window must not show System users
+UPDATE AD_Tab SET WhereClause='(AD_User.AD_Client_ID=@#AD_Client_ID@)',Updated=TO_DATE('2013-09-30 21:38:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=118
+;
+
+SELECT register_migration_script('201309302139_IDEMPIERE-1408.sql') FROM dual
+;
+
