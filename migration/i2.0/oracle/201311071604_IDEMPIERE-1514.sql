e1f90dc2c955b617169a395400f96572e115b350
Carlos Ruiz <carg67@gmail.com>
2013-11-16 09:57:54 -0500

    IDEMPIERE-1514 field sales transaction in cash plan window is horizontal-deflecting / register_migration_script

--- migration/i2.0/oracle/201311071604_IDEMPIERE-1514.sql
+++ migration/i2.0/oracle/201311071604_IDEMPIERE-1514.sql
@@ -5,3 +5,6 @@ SET DEFINE OFF
 UPDATE AD_Field SET SeqNo=70, IsDisplayed='Y', XPosition=1,Updated=TO_DATE('2013-11-07 21:30:44','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=60934
 ;
 
+SELECT register_migration_script('201311071604_IDEMPIERE-1514.sql') FROM dual
+;
+
