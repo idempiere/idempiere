bf8171b20018c8ea0255a2d0226113fc0a923f24
Carlos Ruiz <carg67@gmail.com>
2013-11-16 09:58:16 -0500

    IDEMPIERE-1533 Constraint Name shouldn't be copied / Thanks to Nicolas Micoud (nmicoud)

---
+++ migration/i2.0/postgresql/201311151945_IDEMPIERE-1533.sql
@@ -0,0 +1,7 @@
+-- Nov 15, 2013 7:35:45 PM CET
+-- IDEMPIERE-1533 Constraint Name shouldn't be copied
+UPDATE AD_Column SET IsAllowCopy='N',Updated=TO_TIMESTAMP('2013-11-15 19:35:45','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=210871
+;
+
+SELECT register_migration_script('201311151945_IDEMPIERE-1533.sql') FROM dual
+;
