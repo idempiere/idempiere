84f93dd0ac016516a9408f6ebf7249af6fe73bcd
Heng Sin Low <hengsin@gmail.com>
2013-11-22 08:17:27 +0800

    Update core product version to 2.0.0. Fixed installation and script.

--- org.idempiere.eclipse.platform-feature/update.bat
+++ org.idempiere.eclipse.platform-feature/update.bat
@@ -7,6 +7,10 @@ cp idempiere.ini idempiere.ini.sav
 if exist server.xml.sav del /q server.xml.sav
 if exist plugins/org.adempiere.tomcat.config_1.0.0/META-INF/tomcat/server.xml (
    copy plugins/org.adempiere.tomcat.config_1.0.0/META-INF/tomcat/server.xml server.xml.sav
+   del /q plugins/org.adempiere.tomcat.config_1.0.0/META-INF/tomcat/server.xml
+)
+if exist plugins/org.adempiere.tomcat.config_2.0.0/META-INF/tomcat/server.xml (
+   copy plugins/org.adempiere.tomcat.config_2.0.0/META-INF/tomcat/server.xml server.xml.sav
 )
 
 java -Dosgi.noShutdown=false -Dosgi.compatibility.bootdelegation=true -Dosgi.install.area=director -jar plugins/org.eclipse.osgi_3.7.*.jar -application org.eclipse.equinox.p2.director -consoleLog -profileProperties org.eclipse.update.install.features=true -destination %~dp0 -repository %1 -u org.adempiere.server.product
@@ -15,6 +19,6 @@ java -Dosgi.noShutdown=false -Dosgi.compatibility.bootdelegation=true -Dosgi.ins
 
 cp idempiere.ini.sav idempiere.ini
 if exist server.xml.sav (
-   copy server.xml.sav plugins/org.adempiere.tomcat.config_1.0.0/META-INF/tomcat/server.xml 
+   copy server.xml.sav plugins/org.adempiere.tomcat.config_2.0.0/META-INF/tomcat/server.xml 
    del /q server.xml.sav
 )
