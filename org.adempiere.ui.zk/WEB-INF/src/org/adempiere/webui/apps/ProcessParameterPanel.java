486836db27a9b8859873e4e3276e3c544ec6907a
Elaine Tan <etan@trekglobal.com>
2013-12-10 15:43:43 +0800

    IDEMPIERE-1621 Mandatory field in process parameter panel not in red label

--- org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/apps/ProcessParameterPanel.java
+++ org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/apps/ProcessParameterPanel.java
@@ -281,7 +281,8 @@ public class ProcessParameterPanel extends Panel implements
 		Row row = new Row();
 
 		// The Editor
-		WEditor editor = WebEditorFactory.getEditor(mField, false);		
+		WEditor editor = WebEditorFactory.getEditor(mField, false);
+		editor.setProcessParameter(true);
 		editor.getComponent().addEventListener(Events.ON_FOCUS, this);
 		editor.getComponent().addEventListener(Events.ON_BLUR, this);		
 		editor.addValueChangeListener(this);
@@ -336,6 +337,7 @@ public class ProcessParameterPanel extends Panel implements
 			m_mFields2.add(mField2);
 			// The Editor
 			WEditor editor2 = WebEditorFactory.getEditor(mField2, false);
+			editor2.setProcessParameter(true);
 			//override attribute
 			editor2.getComponent().setWidgetAttribute("columnName", mField2.getColumnName()+"_To");
 			editor2.getComponent().addEventListener(Events.ON_FOCUS, this);
