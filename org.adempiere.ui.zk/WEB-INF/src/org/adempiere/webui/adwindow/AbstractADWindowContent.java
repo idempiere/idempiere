dc1ac231756722bc22767b0faaaccb46b1b65173
Heng Sin Low <hengsin@gmail.com>
2013-12-13 17:11:33 +0800

    1003574 The "Print" button is mute and not working. Always show process info if there's error.

--- org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/adwindow/AbstractADWindowContent.java
+++ org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/adwindow/AbstractADWindowContent.java
@@ -2900,7 +2900,7 @@ public abstract class AbstractADWindowContent extends AbstractUIPart implements 
 
 		if (Executions.getCurrent() != null)
 		{
-			if (notPrint)		//	refresh if not print
+			if (notPrint || pi.isError()) // show process info if it is not print or have error
 			{
 				updateUI(pi);
 			}
@@ -2911,7 +2911,7 @@ public abstract class AbstractADWindowContent extends AbstractUIPart implements 
 				//acquire desktop, 2 second timeout
 				Executions.activate(getComponent().getDesktop(), 2000);
 				try {
-					if (notPrint)		//	refresh if not print
+					if (notPrint || pi.isError()) // show process info if it is not print or have error
 					{
 						updateUI(pi);
 					}
