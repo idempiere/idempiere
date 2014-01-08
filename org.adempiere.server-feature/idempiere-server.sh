bf5560119f8c610264f32779ab62e6ac16cc64b3
Thomas Bayen <tbayen@bayen.de>
2014-01-08 08:41:43 -0500

    IDEMPIERE-1676 zk tries to connect to X11 when DISPLAY is set

--- org.adempiere.server-feature/idempiere-server.sh
+++ org.adempiere.server-feature/idempiere-server.sh
@@ -13,5 +13,6 @@ echo ===================================
 echo Starting iDempiere Server
 echo ===================================
 
+unset DISPLAY
 BASE=`dirname $( readlink -f idempiere-server.sh )`
 $JAVA -Dosgi.compatibility.bootdelegation=true -Dosgi.noShutdown=true -Dosgi.framework.activeThreadType=normal -XX:MaxPermSize=192m -jar $BASE/plugins/org.eclipse.equinox.launcher_1.*.jar -console 12612 -application org.adempiere.server.application
