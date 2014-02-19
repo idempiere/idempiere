8981c65aa0094da97e8dc7098ca4818394de3055
Carlos Ruiz <carg67@gmail.com>
2014-02-18 23:05:38 -0500

    IDEMPIERE-1265 Add Adaxa Sales Management / Summary menu not exported to trl

---
+++ migration/i2.0/oracle/201402182258_IDEMPIERE-1265.sql
@@ -0,0 +1,11 @@
+SET SQLBLANKLINES ON
+SET DEFINE OFF
+
+-- Feb 18, 2014 10:57:39 PM COT
+-- IDEMPIERE-1265 Add Adaxa Sales Management
+UPDATE AD_Menu SET IsCentrallyMaintained='N',Updated=TO_DATE('2014-02-18 22:57:39','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Menu_ID=53363
+;
+
+SELECT register_migration_script('201402182258_IDEMPIERE-1265.sql') FROM dual
+;
+
