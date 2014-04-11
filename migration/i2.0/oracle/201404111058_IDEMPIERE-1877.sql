e7730780ba34b7fb953ae956a6564de7c6785216
Carlos Ruiz <carg67@gmail.com>
2014-04-11 12:10:56 -0500

    IDEMPIERE-1877 Product Info Window Implementation broke the usage of barcode reader

---
+++ migration/i2.0/oracle/201404111058_IDEMPIERE-1877.sql
@@ -0,0 +1,15 @@
+SET SQLBLANKLINES ON
+SET DEFINE OFF
+
+-- Apr 11, 2014 10:56:44 AM COT
+-- IDEMPIERE-1877 Product Info Window Implementation broke the usage of barcode reader
+UPDATE AD_InfoColumn SET IsIdentifier='Y',Updated=TO_DATE('2014-04-11 10:56:44','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_InfoColumn_ID=200003
+;
+
+-- Apr 11, 2014 10:56:47 AM COT
+UPDATE AD_InfoColumn SET IsIdentifier='Y',Updated=TO_DATE('2014-04-11 10:56:47','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_InfoColumn_ID=200004
+;
+
+SELECT register_migration_script('201404111058_IDEMPIERE-1877.sql') FROM dual
+;
+
