5f90a9f0cf5515b8fc22f4253a3eeeba7b2e8f0b
Elaine Tan <etan@trekglobal.com>
2014-06-26 14:41:41 +0800

    IDEMPIERE-2023 Scheduler Recipient record has incorrect defaults

---
+++ migration/i2.0z/postgresql/201406261439_IDEMPIERE-2023.sql
@@ -0,0 +1,11 @@
+-- Jun 26, 2014 1:04:37 PM SGT
+-- IDEMPIERE-2023 Scheduler Recipient record has incorrect defaults
+UPDATE AD_Column SET DefaultValue='-1',Updated=TO_TIMESTAMP('2014-06-26 13:04:36','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=11504
+;
+
+-- Jun 26, 2014 1:04:54 PM SGT
+UPDATE AD_Column SET DefaultValue='-1',Updated=TO_TIMESTAMP('2014-06-26 13:04:54','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=11512
+;
+
+SELECT register_migration_script('201406261439_IDEMPIERE-2023.sql') FROM dual
+;
