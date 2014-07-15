98512e04c0ae265e3d33b1589c8d96792ad0a752
Carlos Ruiz <carg67@gmail.com>
2014-07-15 13:38:15 -0500

    IDEMPIERE-1998 for backward compatibility insert the creator as scheduler recipient

---
+++ migration/i2.0z/oracle/201407151334_IDEMPIERE-1998.sql
@@ -0,0 +1,27 @@
+-- IDEMPIERE-1998 for backward compatibility insert the creator as scheduler recipient
+insert 
+into ad_schedulerrecipient 
+    (
+        ad_schedulerrecipient_id,
+        ad_client_id,
+        ad_org_id,
+        isactive,
+        created,
+        createdby,
+        updated,
+        updatedby,
+        ad_scheduler_id,
+        ad_user_id,
+        ad_role_id,
+        ad_schedulerrecipient_uu
+    )
+select nextidfunc(921,'N'), s.ad_client_id, s.ad_org_id, s.isactive, sysdate, 100, sysdate, 100, s.ad_scheduler_id, s.createdby, null, generate_uuid()
+from ad_scheduler s 
+where not exists (  select 1 
+                    from ad_schedulerrecipient sr 
+                    where s.ad_scheduler_id=sr.ad_scheduler_id)
+;
+
+SELECT register_migration_script('201407151334_IDEMPIERE-1998.sql') FROM dual
+;
+
