c132c4d0fc4162a47f7e32023b74668a3bafae51
Carlos Ruiz <carg67@gmail.com>
2014-10-30 23:08:23 -0500

    Update sources to release 2.1

--- org.adempiere.base/src/org/compiere/Adempiere.java
+++ org.adempiere.base/src/org/compiere/Adempiere.java
@@ -69,11 +69,11 @@ public final class Adempiere
 	/** Main Version String         */
 	// Conventions for naming second number is even for stable, and odd for unstable
 	// the releases will have a suffix (a) for alpha - (b) for beta - (t) for trunk - (s) for stable - and (LTS) for long term support
-	static public String	MAIN_VERSION	= "Release 2.0";
+	static public String	MAIN_VERSION	= "Release 2.1";
 	/** Detail Version as date      Used for Client/Server		*/
-	static public String	DATE_VERSION	= "2013-10-31";
+	static public String	DATE_VERSION	= "2014-10-31";
 	/** Database Version as date    Compared with AD_System		*/
-	static public String	DB_VERSION		= "2013-10-31";
+	static public String	DB_VERSION		= "2014-10-31";
 
 	/** Product Name            */
 	static public final String	NAME 			= "iDempiere\u00AE";
