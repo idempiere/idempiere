e875d72fb0ab88a6313d39a6c7095cba09342af5
Carlos Ruiz <carg67@gmail.com>
2014-12-17 20:06:23 -0500

    IDEMPIERE-1996 InventoryLine inherits bad default / prefill the default locator in M_Inventory and M_InOut documents

---
+++ migration/i2.1/postgresql/201412171953_IDEMPIERE-1996.sql
@@ -0,0 +1,12 @@
+-- Dec 17, 2014 7:50:03 PM COT
+-- IDEMPIERE-1996 InventoryLine inherits bad default
+UPDATE AD_Column SET Callout='org.idempiere.model.CalloutFillLocator.fillLocator',Updated=TO_TIMESTAMP('2014-12-17 19:50:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=3537
+;
+
+-- Dec 17, 2014 7:51:10 PM COT
+UPDATE AD_Column SET Callout='org.idempiere.model.CalloutFillLocator.fillLocator;org.compiere.model.CalloutInventory.product',Updated=TO_TIMESTAMP('2014-12-17 19:51:10','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=3564
+;
+
+SELECT register_migration_script('201412171953_IDEMPIERE-1996.sql') FROM dual
+;
+
