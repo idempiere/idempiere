fef106746ad880bc02c7418e849a1dde7db43766
Carlos Ruiz <carg67@gmail.com>
2015-01-05 18:33:53 -0500

    IDEMPIERE-448 Add CopyFrom on Bank Statement / fix problem copying lines without charges - throwing error about Charge is mandatory

---
+++ migration/i2.1/oracle/201501051826_IDEMPIERE-448.sql
@@ -0,0 +1,15 @@
+SET SQLBLANKLINES ON
+SET DEFINE OFF
+
+-- Jan 5, 2015 6:14:51 PM COT
+-- IDEMPIERE-448 Add CopyFrom on Bank Statement
+UPDATE AD_Column SET IsMandatory='N',Updated=TO_DATE('2015-01-05 18:14:51','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=4939
+;
+
+-- Jan 5, 2015 6:14:59 PM COT
+UPDATE AD_Field SET IsMandatory='Y',Updated=TO_DATE('2015-01-05 18:14:59','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=4009
+;
+
+SELECT register_migration_script('201501051826_IDEMPIERE-448.sql') FROM dual
+;
+
