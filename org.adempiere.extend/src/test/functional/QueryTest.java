bf81fe07a44526641e6f5d6576667ea4c62e0a42
hieplq <hieplq@hasuvimex.vn>
2015-01-14 09:20:08 -0500

    IDEMPIERE-2152:Info Window - Join related tabs on other columns than primary key combine and repatch for 2.1

--- org.adempiere.extend/src/test/functional/QueryTest.java
+++ org.adempiere.extend/src/test/functional/QueryTest.java
@@ -14,6 +14,8 @@
 package test.functional;
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -26,6 +28,7 @@ import org.compiere.model.Query;
 import org.compiere.model.X_AD_Element;
 import org.compiere.util.DB;
 import org.compiere.util.Env;
+import org.compiere.util.KeyNamePair;
 
 import test.AdempiereTestCase;
 
@@ -287,11 +290,11 @@ public class QueryTest extends AdempiereTestCase
 		assertTrue(AD_PInstance_ID > 0);
 
 		// Create selection list
-		Map<Integer, List<String>> elements = new HashMap<Integer, List<String>> ();
-		elements.put(102, null); // AD_Element_ID=102 => AD_Client_ID
-		elements.put(104, null); // AD_Element_ID=104 => AD_Column_ID
+		List<KeyNamePair> elements = new ArrayList<KeyNamePair> ();
+		elements.add(new KeyNamePair(102, null)); // AD_Element_ID=102 => AD_Client_ID
+		elements.add(new KeyNamePair(104, null)); // AD_Element_ID=104 => AD_Column_ID
 		DB.executeUpdateEx("DELETE FROM T_Selection WHERE AD_PInstance_ID="+AD_PInstance_ID, getTrxName());
-		DB.createT_Selection(AD_PInstance_ID, elements, 0, getTrxName());
+		DB.createT_SelectionNew (AD_PInstance_ID, elements, getTrxName());
 		
 		String whereClause = "1=1"; // some dummy where clause
 		int[] ids = new Query(getCtx(), X_AD_Element.Table_Name, whereClause, getTrxName())
@@ -300,12 +303,9 @@ public class QueryTest extends AdempiereTestCase
 		.getIDs();
 		assertEquals("Resulting number of elements differ", elements.size(), ids.length);
 		
-		Integer[] keys = new Integer[elements.keySet().size()]; 
-		keys = elements.keySet().toArray(keys);
-				
-		for (int i = 0; i < keys .length; i++)
+		for (int i = 0; i < elements.size(); i++)
 		{
-			int expected = keys[i];
+			int expected = elements.get(i).getKey();
 			assertEquals("Element "+i+" not equals", expected, ids[i]);
 		}
 	}
