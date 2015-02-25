2e8a31d4d87a6ce16e46dd8382ca4dacf616c6de
Carlos Ruiz <carg67@gmail.com>
2015-02-25 15:38:30 -0500

    IDEMPIERE-2427 minor remove unused import

--- org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/panel/CustomizeGridViewPanel.java
+++ org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/panel/CustomizeGridViewPanel.java
@@ -18,7 +18,6 @@ package org.adempiere.webui.panel;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
