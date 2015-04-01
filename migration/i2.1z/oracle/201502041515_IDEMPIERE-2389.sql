d482ac83a4151f0dbe28820a15f766008c04c6be
Carlos Ruiz <carg67@gmail.com>
2015-04-01 10:14:31 -0500

    IDEMPIERE-2389 Print format and View Report selection on process dialog. / fix oracle migration script

--- migration/i2.1z/oracle/201502041515_IDEMPIERE-2389.sql
+++ migration/i2.1z/oracle/201502041515_IDEMPIERE-2389.sql
@@ -1,8 +1,12 @@
 SET SQLBLANKLINES ON
 SET DEFINE OFF
 
--- Feb 4, 2015 3:20:47 PM IST
+-- Feb 4, 2015 3:19:23 PM IST
 -- IDEMPIERE-2389 Add ReportType and AD_PrintFormat_ID in AD_PInstance Table
+INSERT INTO AD_Element (AD_Element_ID,AD_Client_ID,AD_Org_ID,IsActive,Created,CreatedBy,Updated,UpdatedBy,ColumnName,Name,PrintName,EntityType,AD_Element_UU) VALUES (202814,0,0,'Y',TO_DATE('2015-02-04 15:19:22','YYYY-MM-DD HH24:MI:SS'),100,TO_DATE('2015-02-04 15:19:22','YYYY-MM-DD HH24:MI:SS'),100,'ReportType','Report Type','Report Type','D','84422b03-ffa2-435b-a485-9ac5de986f0c')
+;
+
+-- Feb 4, 2015 3:20:47 PM IST
 INSERT INTO AD_Column (AD_Column_ID,Version,Name,AD_Table_ID,ColumnName,DefaultValue,FieldLength,IsKey,IsParent,IsMandatory,IsTranslated,IsIdentifier,SeqNo,IsEncrypted,AD_Reference_ID,AD_Reference_Value_ID,AD_Client_ID,AD_Org_ID,IsActive,Created,CreatedBy,Updated,UpdatedBy,AD_Element_ID,IsUpdateable,IsSelectionColumn,EntityType,IsSyncDatabase,IsAlwaysUpdateable,IsAutocomplete,IsAllowLogging,AD_Column_UU,IsAllowCopy,SeqNoSelection,IsToolbarButton,IsSecure) VALUES (211830,0,'Report Type',282,'ReportType',NULL,5,'N','N','N','N','N',0,'N',10,NULL,0,0,'Y',TO_DATE('2015-02-04 15:20:46','YYYY-MM-DD HH24:MI:SS'),100,TO_DATE('2015-02-04 15:20:46','YYYY-MM-DD HH24:MI:SS'),100,202814,'Y','N','D','N','N','N','Y','613c626e-e9a4-4efb-a39c-88529c27aea4','Y',0,'N','N')
 ;
 
