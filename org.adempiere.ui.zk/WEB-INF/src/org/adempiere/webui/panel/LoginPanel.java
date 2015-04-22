8f1996c2ea9a81227618373eb548ebfdbea9ba72
Carlos Ruiz <carg67@gmail.com>
2015-04-22 10:49:22 -0500

    IDEMPIERE-2585 Sessiontimeout button text is not translated users language / integrate patch from Flemming Birch (sjeffen)

--- org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/panel/LoginPanel.java
+++ org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/panel/LoginPanel.java
@@ -621,12 +621,14 @@ public class LoginPanel extends Window implements EventListener<Event>
     }
 
 	private String getUpdateTimeoutTextScript() {
-		String msg = Msg.getMsg(Env.getCtx(), "SessionTimeoutText");
+		String msg = Msg.getMsg(Env.getCtx(), "SessionTimeoutText").trim();  //IDEMPIERE-847
+		String continueNsg = Msg.getMsg(Env.getCtx(), "continue").trim();   //IDEMPIERE-847
 		if (msg == null || msg.equals("SessionTimeoutText")) {
 			return null;
 		}
 		msg = Strings.escape(msg, "\"");
-		String s = "adempiere.store.set(\"zkTimeoutText\", \"" + msg + "\")";
+		String s = "adempiere.store.set(\"zkTimeoutText\", \"" + msg + "\");";//IDEMPIERE-847
+		s = s + " adempiere.store.set(\"zkContinueText\", \"" + continueNsg + "\");"; //IDEMPIERE-847
 		return s;
 	}
 	
