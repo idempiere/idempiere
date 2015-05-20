119f51f56416b5e329854c83337f9baf2982ab81
Carlos Ruiz <carg67@gmail.com>
2015-05-20 13:01:08 -0500

    IDEMPIERE-2625 BOMPriceStd function incorre / thanks to red1 and Dietmar (didiber) for suggestion

--- db/ddlutils/oracle/functions/BOM_PriceLimit.sql
+++ db/ddlutils/oracle/functions/BOM_PriceLimit.sql
@@ -27,8 +27,6 @@ AS
 		WHERE b.M_ProductBOM_ID=p.M_Product_ID
 		  AND b.M_Product_ID=Product_ID
 		  AND b.M_ProductBOM_ID != Product_ID
-		  AND p.IsBOM='Y'
-		  AND p.IsVerified='Y'
 		  AND b.IsActive='Y';
 	--
 BEGIN
--- db/ddlutils/postgresql/functions/BOM_PriceStd.sql
+++ db/ddlutils/postgresql/functions/BOM_PriceStd.sql
@@ -20,8 +20,6 @@ BEGIN
 			WHERE b.M_ProductBOM_ID=p.M_Product_ID
 			AND b.M_Product_ID=Product_ID
 			AND b.M_ProductBOM_ID != Product_ID
-			AND p.IsBOM='Y'
-			AND p.IsVerified='Y'
 			AND b.IsActive='Y'
 		LOOP
 			v_ProductPrice := bomPriceStd (bom.M_ProductBOM_ID, PriceList_Version_ID);
