a353c9cad0588c74dd8c9b46e58ff65f37cb23f7
Carlos Ruiz <carg67@gmail.com>
2015-08-19 17:19:30 -0500

    IDEMPIERE-2230 Info Window - remember selection during pagination - fix official ID on migration script

--- migration/i2.1z/postgresql/201514081982-IDEMPIERE-2230.sql
+++ migration/i2.1z/postgresql/201514081982-IDEMPIERE-2230.sql
@@ -1,6 +1,6 @@
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+-- IDEMPIERE-2230 Info Window - remember selection during pagination
 -- Aug 14, 2015 6:52:36 AM WITA
-INSERT INTO AD_Message (MsgType,MsgText,AD_Client_ID,AD_Org_ID,IsActive,Created,CreatedBy,Updated,UpdatedBy,AD_Message_ID,Value,EntityType,AD_Message_UU) VALUES ('I','Selected {0} rows',0,0,'Y',TO_TIMESTAMP('2015-08-14 06:52:36','YYYY-MM-DD HH24:MI:SS'),100,TO_TIMESTAMP('2015-08-14 06:52:36','YYYY-MM-DD HH24:MI:SS'),100,1000000,'IWStatusSelected','D','316251b1-093c-4f33-bf37-4d5af0f000d5')
+INSERT INTO AD_Message (MsgType,MsgText,AD_Client_ID,AD_Org_ID,IsActive,Created,CreatedBy,Updated,UpdatedBy,AD_Message_ID,Value,EntityType,AD_Message_UU) VALUES ('I','Selected {0} rows',0,0,'Y',TO_TIMESTAMP('2015-08-14 06:52:36','YYYY-MM-DD HH24:MI:SS'),100,TO_TIMESTAMP('2015-08-14 06:52:36','YYYY-MM-DD HH24:MI:SS'),100,200355,'IWStatusSelected','D','316251b1-093c-4f33-bf37-4d5af0f000d5')
 ;
 SELECT register_migration_script('201514081982-IDEMPIERE-2230.sql') FROM dual
 ;
