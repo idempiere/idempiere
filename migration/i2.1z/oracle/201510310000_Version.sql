8cbcb58ff4f7b39b91d063ae8032d807a1776023
Carlos Ruiz <carg67@gmail.com>
2015-10-30 20:25:06 -0500

    Set version to 3.1

---
+++ migration/i2.1z/oracle/201510310000_Version.sql
@@ -0,0 +1,12 @@
+UPDATE AD_SYSTEM
+   SET releaseno = '3.1',
+       VERSION = '2015-10-31'
+ WHERE ad_system_id = 0 AND ad_client_id = 0
+;
+
+COMMIT
+;
+
+SELECT register_migration_script('201510310000_Version.sql') FROM dual
+;
+
