71f8c3c3a923073552d10c0a82574fdc7dfaf8e9
Carlos Ruiz <carg67@gmail.com>
2016-01-27 17:06:25 +0100

    IDEMPIERE-3015 Wrong AD_Sequence_UU shown in Field record panel

---
+++ migration/i3.1/oracle/201601271704_IDEMPIERE-3015.sql
@@ -0,0 +1,11 @@
+SET SQLBLANKLINES ON
+SET DEFINE OFF
+
+-- IDEMPIERE-3015
+-- Jan 27, 2016 5:03:50 PM CET
+UPDATE AD_Column SET IsParent='Y', IsUpdateable='N',Updated=TO_DATE('2016-01-27 17:03:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=749
+;
+
+SELECT register_migration_script('201601271704_IDEMPIERE-3015.sql') FROM dual
+;
+
