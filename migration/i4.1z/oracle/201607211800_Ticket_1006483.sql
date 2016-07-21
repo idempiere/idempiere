d4401c3ed5396a71f887295437c3fa9342f64816
Heng Sin Low <hengsin@gmail.com>
2016-07-21 18:12:33 +0800

    1006483 Ease edit of Field label, description and Help. Show approve and processed field at field suggestion window. / IDEMPIERE-3438

---
+++ migration/i4.1z/oracle/201607211800_Ticket_1006483.sql
@@ -0,0 +1,35 @@
+SET SQLBLANKLINES ON
+SET DEFINE OFF
+
+-- Jul 21, 2016 5:51:17 PM MYT
+-- 1006483 Ease edit of Field label, description and Help
+UPDATE AD_Field SET SeqNo=130,IsDisplayed='Y', Updated=Now(), UpdatedBy=100 WHERE AD_Field_ID=204236
+;
+
+-- Jul 21, 2016 5:51:17 PM MYT
+UPDATE AD_Field SET SeqNo=140,IsDisplayed='Y', Updated=Now(), UpdatedBy=100 WHERE AD_Field_ID=204237
+;
+
+-- Jul 21, 2016 5:51:17 PM MYT
+UPDATE AD_Field SET SeqNo=150,IsDisplayed='Y', Updated=Now(), UpdatedBy=100 WHERE AD_Field_ID=204240
+;
+
+-- Jul 21, 2016 5:51:17 PM MYT
+UPDATE AD_Field SET SeqNo=160,IsDisplayed='Y', Updated=Now(), UpdatedBy=100 WHERE AD_Field_ID=204241
+;
+
+-- Jul 21, 2016 5:51:17 PM MYT
+UPDATE AD_Field SET SeqNo=170,IsDisplayed='Y', Updated=Now(), UpdatedBy=100 WHERE AD_Field_ID=204242
+;
+
+-- Jul 21, 2016 6:03:18 PM MYT
+UPDATE AD_Field SET SeqNo=130, XPosition=2,Updated=TO_DATE('2016-07-21 18:03:18','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=204236
+;
+
+-- Jul 21, 2016 6:03:18 PM MYT
+UPDATE AD_Field SET SeqNo=140, XPosition=5,Updated=TO_DATE('2016-07-21 18:03:18','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=204237
+;
+
+SELECT register_migration_script('201607211800_Ticket_1006483.sql') FROM dual
+;
+
