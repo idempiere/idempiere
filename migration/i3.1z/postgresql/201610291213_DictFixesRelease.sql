3c9b9f4ef836b69bbcbe70bb6c6ab5c1ea86cc89
Carlos Ruiz <carg67@gmail.com>
2016-10-29 12:28:39 +0200

    add registration to script fix entity types

--- migration/i3.1z/postgresql/201610291213_DictFixesRelease.sql
+++ migration/i3.1z/postgresql/201610291213_DictFixesRelease.sql
@@ -3,3 +3,7 @@ update ad_column set entitytype='D' where ad_column_id=211840;
 update ad_field set entitytype='D' where ad_field_id=203499;
 
 update ad_val_rule set entitytype='D' where ad_val_rule_id=200082;
+
+SELECT register_migration_script('201610291213_DictFixesRelease.sql') FROM dual
+;
+
