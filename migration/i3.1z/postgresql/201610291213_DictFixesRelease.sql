e92b968191b2cde37f86700409884ffbdd8ba02f
Carlos Ruiz <carg67@gmail.com>
2016-10-29 12:16:02 +0200

    Fix entity types

---
+++ migration/i3.1z/postgresql/201610291213_DictFixesRelease.sql
@@ -0,0 +1,5 @@
+update ad_column set entitytype='D' where ad_column_id=211840;
+
+update ad_field set entitytype='D' where ad_field_id=203499;
+
+update ad_val_rule set entitytype='D' where ad_val_rule_id=200082;
