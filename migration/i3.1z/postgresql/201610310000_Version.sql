3dd33fafad60834b4078ee1d940f7437ec587598
Carlos Ruiz <carg67@gmail.com>
2016-10-30 20:18:09 +0100

    script version

---
+++ migration/i3.1z/postgresql/201610310000_Version.sql
@@ -0,0 +1,9 @@
+UPDATE AD_SYSTEM
+   SET releaseno = '4.1',
+       VERSION = '2016-10-31'
+ WHERE ad_system_id = 0 AND ad_client_id = 0
+;
+
+SELECT register_migration_script('201610310000_Version.sql') FROM dual
+;
+
