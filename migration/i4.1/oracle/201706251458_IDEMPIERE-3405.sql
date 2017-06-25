3a0e748f1e63a50d2b147117e14098f5cd4e5a78
Carlos Ruiz <carg67@gmail.com>
2017-06-25 14:59:26 +0200

    IDEMPIERE-3405 When Window Preference doesn't exists then Default Doctype Taken - also from another client / IDEMPIERE-2672

---
+++ migration/i4.1/oracle/201706251458_IDEMPIERE-3405.sql
@@ -0,0 +1,15 @@
+SET SQLBLANKLINES ON
+SET DEFINE OFF
+
+-- IDEMPIERE-3405
+-- Jun 25, 2017 2:58:01 PM CEST
+UPDATE AD_Val_Rule SET Code='(C_DocType.DocBaseType IN (''ARI'', ''API'',''ARC'',''APC'') AND C_DocType.IsSOTrx=''@IsSOTrx@''  AND C_DocType.AD_Client_ID=@#AD_Client_ID@) OR C_DocType.C_DocType_ID=0',Updated=TO_DATE('2017-06-25 14:58:01','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Val_Rule_ID=200097
+;
+
+-- Jun 25, 2017 2:58:15 PM CEST
+UPDATE AD_Val_Rule SET Code='(C_DocType.DocBaseType IN (''SOO'', ''POO'') AND C_DocType.IsSOTrx=''@IsSOTrx@'' AND COALESCE(C_DocType.DocSubTypeSO,'' '')<>''RM'' AND C_DocType.AD_Client_ID=@#AD_Client_ID@) OR C_DocType.C_DocType_ID=0',Updated=TO_DATE('2017-06-25 14:58:15','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Val_Rule_ID=200096
+;
+
+SELECT register_migration_script('201706251458_IDEMPIERE-3405.sql') FROM dual
+;
+
