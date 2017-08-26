19a04cf3cfe85b64f42c59cd58224859f661e291
Carlos Ruiz <carg67@gmail.com>
2017-08-26 19:30:18 +0200

    IDEMPIERE-3451 switch update*bat files

--- org.idempiere.eclipse.platform-feature/update-prd.bat
+++ org.idempiere.eclipse.platform-feature/update-prd.bat
@@ -6,38 +6,6 @@ set DESTINATION=%cd%
 @echo %DESTINATION%
 @echo %1%
 
-copy idempiere.ini idempiere.ini.sav
-
-if exist jetty.xml.sav del /q jetty.xml.sav
-if exist jettyhome\etc\jetty.xml (
-   copy jettyhome\etc\jetty.xml jetty.xml.sav
-)
-
-if exist jetty-ssl.xml.sav del /q jetty-ssl.xml.sav
-if exist jettyhome\etc\jetty-ssl.xml (
-   copy jettyhome\etc\jetty-ssl.xml jetty-ssl.xml.sav
-)
-
-if exist jetty-selector.xml.sav del /q jetty-selector.xml.sav
-if exist jettyhome\etc\jetty-selector.xml (
-   copy jettyhome\etc\jetty-selector.xml jetty-selector.xml.sav
-)
-
-@call %DESTINATION%\update-prd %1% org.adempiere.server.product
-
-copy idempiere.ini.sav idempiere.ini
-
-if exist jetty.xml.sav (
-   copy jetty.xml.sav jettyhome\etc\jetty.xml 
-   del /q jetty.xml.sav
-)
-
-if exist jetty-ssl.xml.sav (
-   copy jetty-ssl.xml.sav jettyhome\etc\jetty-ssl.xml 
-   del /q jetty-ssl.xml.sav
-)
-
-if exist jetty-selector.xml.sav (
-   copy jetty-selector.xml.sav jettyhome\etc\jetty-selector.xml 
-   del /q jetty-selector.xml.sav
-)
+FOR %%c in (plugins\org.eclipse.equinox.launcher_1.*.jar) DO set JARFILE=%%c
+java -Dorg.eclipse.ecf.provider.filetransfer.excludeContributors=org.eclipse.ecf.provider.filetransfer.httpclient4 -Djava.net.preferIPv4Stack=true -jar %JARFILE% -install director -configuration director/configuration -application org.eclipse.equinox.p2.director -consoleLog -profileProperties org.eclipse.update.install.features=true -destination %DESTINATION% -repository %1% -u %2%
+java -Dorg.eclipse.ecf.provider.filetransfer.excludeContributors=org.eclipse.ecf.provider.filetransfer.httpclient4 -Djava.net.preferIPv4Stack=true -jar %JARFILE% -install director -configuration director/configuration -application org.eclipse.equinox.p2.director -consoleLog -profileProperties org.eclipse.update.install.features=true -destination %DESTINATION% -repository %1% -i %2%
