840673e4e5ff178b34c9bce7b2ddfeb4bb9d4300
Carlos Ruiz <carg67@gmail.com>
2017-10-13 17:40:12 +0200

    IDEMPIERE-2877 Adapt jasper reporting to latest (actually using deprecated methods) / thanks to Tiago Ceridorio (tifece) for the patch

--- org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/window/ZkJRViewerProvider.java
+++ org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/window/ZkJRViewerProvider.java
@@ -3,6 +3,8 @@ package org.adempiere.webui.window;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperPrint;
 
+import java.util.List;
+
 import org.adempiere.report.jasper.JRViewerProvider;
 import org.adempiere.webui.apps.AEnv;
 import org.adempiere.webui.component.Window;
@@ -14,11 +16,28 @@ public class ZkJRViewerProvider implements JRViewerProvider {
 	public void openViewer(final JasperPrint jasperPrint, final String title)
 			throws JRException {
 		Runnable runnable = new Runnable() {
-			
+
 			@Override
 			public void run() {
 				Window viewer = new ZkJRViewer(jasperPrint, title);
-				
+
+				viewer.setAttribute(Window.MODE_KEY, Window.MODE_EMBEDDED);
+				viewer.setAttribute(Window.INSERT_POSITION_KEY, Window.INSERT_NEXT);
+				viewer.setAttribute(WindowContainer.DEFER_SET_SELECTED_TAB, Boolean.TRUE);
+				SessionManager.getAppDesktop().showWindow(viewer);
+			}
+		};
+		AEnv.executeAsyncDesktopTask(runnable);
+	}
+
+	public void openViewer(final List<JasperPrint> jasperPrintList, final String title)
+			throws JRException {
+		Runnable runnable = new Runnable() {
+
+			@Override
+			public void run() {
+				Window viewer = new ZkJRViewer(jasperPrintList, title);
+
 				viewer.setAttribute(Window.MODE_KEY, Window.MODE_EMBEDDED);
 				viewer.setAttribute(Window.INSERT_POSITION_KEY, Window.INSERT_NEXT);
 				viewer.setAttribute(WindowContainer.DEFER_SET_SELECTED_TAB, Boolean.TRUE);
