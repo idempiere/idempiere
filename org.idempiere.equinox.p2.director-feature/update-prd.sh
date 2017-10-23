a5dbcb3f38ab07cd3e79ef275022d15e8f692b46
hieplq <hieplq@hasuvimex.vn>
2017-10-23 22:52:29 +0700

    IDEMPIERE-3353:sync development profile (eclipse) and package profile
    keep update-prd.sh can install/uninstall normal feature

--- org.idempiere.equinox.p2.director-feature/update-prd.sh
+++ org.idempiere.equinox.p2.director-feature/update-prd.sh
@@ -5,5 +5,12 @@ cd $(dirname "${0}")
 DESTINATION=$(pwd)
 
 VMOPTS="-Declipse.log.level=ALL -Dorg.eclipse.ecf.provider.filetransfer.excludeContributors=org.eclipse.ecf.provider.filetransfer.httpclient4 -Djava.net.preferIPv4Stack=true"
-java $VMOPTS -jar plugins/org.eclipse.equinox.launcher_1.*.jar -install director -configuration director/configuration -application org.eclipse.equinox.p2.director -consoleLog -profileProperties org.eclipse.update.install.features=true -destination $DESTINATION -repository $1 -u "$2, org.idempiere.fitnesse.feature.group"
-java $VMOPTS -jar plugins/org.eclipse.equinox.launcher_1.*.jar -install director -configuration director/configuration -application org.eclipse.equinox.p2.director -consoleLog -profileProperties org.eclipse.update.install.features=true -destination $DESTINATION -repository $1 -i "$2, org.eclipse.equinox.p2.director, org.idempiere.fitnesse.feature.group, org.idempiere.equinox.p2.director.feature.group"
+
+INSTALL_UI="$2"
+UNINSTALL_UI="$2"
+if [ -n "$3" ]; then
+	# case update org.adempiere.server.product, install and un-install is difference 
+	UNINSTALL_UI="$3"
+fi
+java $VMOPTS -jar plugins/org.eclipse.equinox.launcher_1.*.jar -install director -configuration director/configuration -application org.eclipse.equinox.p2.director -consoleLog -profileProperties org.eclipse.update.install.features=true -destination $DESTINATION -repository $1 -u "$UNINSTALL_UI"
+java $VMOPTS -jar plugins/org.eclipse.equinox.launcher_1.*.jar -install director -configuration director/configuration -application org.eclipse.equinox.p2.director -consoleLog -profileProperties org.eclipse.update.install.features=true -destination $DESTINATION -repository $1 -i "$INSTALL_UI"
