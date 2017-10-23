790cd64ed2033b1faaf3ecb473a18ef97c6e4e06
Carlos Ruiz <carg67@gmail.com>
2017-10-23 19:15:58 +0200

    IDEMPIERE-3173 Columns that must not be copied

---
+++ migration/i4.1/oracle/201710231915_IDEMPIERE-3173.sql
@@ -0,0 +1,15 @@
+SET SQLBLANKLINES ON
+SET DEFINE OFF
+
+-- IDEMPIERE-3173 Columns that must not be copied
+-- Oct 23, 2017 7:14:50 PM CEST
+UPDATE AD_Column SET IsAllowCopy='N',Updated=TO_DATE('2017-10-23 19:14:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=200558
+;
+
+-- Oct 23, 2017 7:15:09 PM CEST
+UPDATE AD_Column SET IsAllowCopy='N',Updated=TO_DATE('2017-10-23 19:15:09','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=200704
+;
+
+SELECT register_migration_script('201710231915_IDEMPIERE-3173.sql') FROM dual
+;
+
