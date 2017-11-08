8a1635f42c0feda63efcd4651e0a91bd698a364f
Nicolas Micoud <nmicoud@tgi.fr>
2017-11-08 08:26:46 +0100

    IDEMPIERE-3546 Use specific messages for range process parameters placeholder

---
+++ migration/i5.1/oracle/201711061600_IDEMPIERE-3546.sql
@@ -0,0 +1,14 @@
+SET SQLBLANKLINES ON
+SET DEFINE OFF
+
+-- IDEMPIERE-3546 - Use specific messages for range process parameters placeholder
+-- Nov 6, 2017 4:07:29 PM CET
+INSERT INTO AD_Message (MsgType,MsgText,AD_Client_ID,AD_Org_ID,IsActive,Created,CreatedBy,Updated,UpdatedBy,AD_Message_ID,Value,EntityType,AD_Message_UU) VALUES ('I','From',0,0,'Y',TO_DATE('2017-11-06 16:07:28','YYYY-MM-DD HH24:MI:SS'),0,TO_DATE('2017-11-06 16:07:28','YYYY-MM-DD HH24:MI:SS'),0,200438,'ProcessParameterRangeFrom','D','44852250-153e-462a-b206-897d0c2cd1ab')
+;
+
+-- Nov 6, 2017 4:07:42 PM CET
+INSERT INTO AD_Message (MsgType,MsgText,AD_Client_ID,AD_Org_ID,IsActive,Created,CreatedBy,Updated,UpdatedBy,AD_Message_ID,Value,EntityType,AD_Message_UU) VALUES ('I','To',0,0,'Y',TO_DATE('2017-11-06 16:07:42','YYYY-MM-DD HH24:MI:SS'),0,TO_DATE('2017-11-06 16:07:42','YYYY-MM-DD HH24:MI:SS'),0,200439,'ProcessParameterRangeTo','D','07220887-ecb2-42e9-9665-260041080b52')
+;
+
+SELECT register_migration_script('201711061600_IDEMPIERE-3546.sql') FROM dual
+;
