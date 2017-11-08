5f127882c183fb2a19dd01c8cf39971ce6657727
Carlos Ruiz <carg67@gmail.com>
2017-11-08 08:32:06 +0100

    IDEMPIERE-3546 Use specific messages for range process parameters placeholder / peer review, insert default trl from previous messages

--- migration/i5.1/oracle/201711061621_IDEMPIERE-3547.sql
+++ migration/i5.1/oracle/201711061621_IDEMPIERE-3547.sql
@@ -1,31 +1,22 @@
-SET SQLBLANKLINES ON
-SET DEFINE OFF
-
--- IDEMPIERE-3547 Mismatches in oracle foreign keys delete rule
--- Nov 6, 2017 4:21:15 PM CET
-ALTER TABLE C_OrderLine DROP CONSTRAINT linkorderline_corderline
+-- IDEMPIERE-3546 - Use specific messages for range process parameters placeholder
+-- Nov 6, 2017 4:07:29 PM CET
+INSERT INTO AD_Message (MsgType,MsgText,AD_Client_ID,AD_Org_ID,IsActive,Created,CreatedBy,Updated,UpdatedBy,AD_Message_ID,Value,EntityType,AD_Message_UU) VALUES ('I','From',0,0,'Y',TO_DATE('2017-11-06 16:07:28','YYYY-MM-DD HH24:MI:SS'),0,TO_DATE('2017-11-06 16:07:28','YYYY-MM-DD HH24:MI:SS'),0,200438,'ProcessParameterRangeFrom','D','44852250-153e-462a-b206-897d0c2cd1ab')
 ;
 
--- Nov 6, 2017 4:21:15 PM CET
-ALTER TABLE C_OrderLine ADD CONSTRAINT linkorderline_corderline FOREIGN KEY (Link_OrderLine_ID) REFERENCES c_orderline(c_orderline_id) DEFERRABLE INITIALLY DEFERRED
+-- Nov 6, 2017 4:07:42 PM CET
+INSERT INTO AD_Message (MsgType,MsgText,AD_Client_ID,AD_Org_ID,IsActive,Created,CreatedBy,Updated,UpdatedBy,AD_Message_ID,Value,EntityType,AD_Message_UU) VALUES ('I','To',0,0,'Y',TO_DATE('2017-11-06 16:07:42','YYYY-MM-DD HH24:MI:SS'),0,TO_DATE('2017-11-06 16:07:42','YYYY-MM-DD HH24:MI:SS'),0,200439,'ProcessParameterRangeTo','D','07220887-ecb2-42e9-9665-260041080b52')
 ;
 
--- Nov 6, 2017 4:21:26 PM CET
-ALTER TABLE A_Asset_Acct DROP CONSTRAINT aasset_aassetacct
-;
+INSERT INTO ad_message_trl
+(ad_message_id, ad_language, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, msgtext, msgtip, istranslated, ad_message_trl_uu)
+SELECT 200438, ad_language, ad_client_id, ad_org_id, isactive, SysDate, 100, SysDate, 100, msgtext, msgtip, istranslated, generate_uuid()
+FROM ad_message_trl
+WHERE ad_message_id=280;
 
--- Nov 6, 2017 4:21:26 PM CET
-ALTER TABLE A_Asset_Acct ADD CONSTRAINT aasset_aassetacct FOREIGN KEY (A_Asset_ID) REFERENCES a_asset(a_asset_id) ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED
-;
+INSERT INTO ad_message_trl
+(ad_message_id, ad_language, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, msgtext, msgtip, istranslated, ad_message_trl_uu)
+SELECT 200439, ad_language, ad_client_id, ad_org_id, isactive, SysDate, 100, SysDate, 100, msgtext, msgtip, istranslated, generate_uuid()
+FROM ad_message_trl
+WHERE ad_message_id=565;
 
--- Nov 6, 2017 4:21:36 PM CET
-ALTER TABLE A_Asset_Group_Acct DROP CONSTRAINT aassetgroup_aassetgroupacct
-;
-
--- Nov 6, 2017 4:21:36 PM CET
-ALTER TABLE A_Asset_Group_Acct ADD CONSTRAINT aassetgroup_aassetgroupacct FOREIGN KEY (A_Asset_Group_ID) REFERENCES a_asset_group(a_asset_group_id) ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED
-;
-
-SELECT register_migration_script('201711061621_IDEMPIERE-3547.sql') FROM dual
-;
-
+SELECT register_migration_script('201711061600_IDEMPIERE-3546.sql') FROM dual
