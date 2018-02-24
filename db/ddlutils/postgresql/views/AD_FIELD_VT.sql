3de59aa95b35baac1b47d10f86c7b6a47e755bb3
Carlos Ruiz <carg67@gmail.com>
2018-02-24 14:29:46 +0100

    IDEMPIERE-3639 Placeholders for every fields / peer review

--- db/ddlutils/postgresql/views/AD_FIELD_VT.sql
+++ db/ddlutils/postgresql/views/AD_FIELD_VT.sql
@@ -6,13 +6,14 @@ CREATE OR REPLACE VIEW ad_field_vt AS
  c.isalwaysupdateable) AS isalwaysupdateable, COALESCE(f.readonlylogic, c.readonlylogic) AS readonlylogic, COALESCE(f.mandatorylogic, c.mandatorylogic) AS mandatorylogic, COALESCE(f.isupdateable, c.isupdateable) AS isupdateable,
  c.isencrypted AS isencryptedcolumn, c.isselectioncolumn, tbl.tablename, c.valuemin, c.valuemax, fgt.name AS fieldgroup, vr.code AS validationcode, f.included_tab_id, fg.fieldgrouptype,
  fg.iscollapsedbydefault, COALESCE(f.infofactoryclass, c.infofactoryclass) AS infofactoryclass, c.isautocomplete, COALESCE(f.isallowcopy, c.isallowcopy) AS isallowcopy, f.isdisplayedgrid,
- f.seqnogrid, c.seqnoselection, f.xposition, f.columnspan, f.numlines, COALESCE(f.istoolbarbutton, c.istoolbarbutton) AS istoolbarbutton, c.formatpattern, f.isadvancedfield, f.isdefaultfocus, c.ad_chart_id, f.ad_labelstyle_id, f.ad_fieldstyle_id, c.pa_dashboardcontent_id, trl.placeholder
+ f.seqnogrid, c.seqnoselection, f.xposition, f.columnspan, f.numlines, COALESCE(f.istoolbarbutton, c.istoolbarbutton) AS istoolbarbutton, c.formatpattern, f.isadvancedfield, f.isdefaultfocus, c.ad_chart_id, f.ad_labelstyle_id, f.ad_fieldstyle_id, c.pa_dashboardcontent_id, COALESCE(trl.placeholder, ct.placeholder) AS placeholder
    FROM ad_field f
    JOIN ad_field_trl trl ON f.ad_field_id = trl.ad_field_id
    JOIN ad_tab t ON f.ad_tab_id = t.ad_tab_id
    LEFT JOIN ad_fieldgroup fg ON f.ad_fieldgroup_id = fg.ad_fieldgroup_id
    LEFT JOIN ad_fieldgroup_trl fgt ON f.ad_fieldgroup_id = fgt.ad_fieldgroup_id AND trl.ad_language::text = fgt.ad_language::text
    LEFT JOIN ad_column c ON f.ad_column_id = c.ad_column_id
+   LEFT JOIN ad_column_trl ct ON f.ad_column_id = ct.ad_column_id
    JOIN ad_table tbl ON c.ad_table_id = tbl.ad_table_id
    LEFT JOIN ad_val_rule vr ON vr.ad_val_rule_id = COALESCE(f.ad_val_rule_id, c.ad_val_rule_id)
   WHERE f.isactive = 'Y' AND c.isactive = 'Y'
--- migration/i5.1z/postgresql/201802151000_IDEMPIERE-3639.sql
+++ migration/i5.1z/postgresql/201802151000_IDEMPIERE-3639.sql
@@ -1936,17 +1936,26 @@ CREATE OR REPLACE VIEW ad_field_vt AS
  c.isalwaysupdateable) AS isalwaysupdateable, COALESCE(f.readonlylogic, c.readonlylogic) AS readonlylogic, COALESCE(f.mandatorylogic, c.mandatorylogic) AS mandatorylogic, COALESCE(f.isupdateable, c.isupdateable) AS isupdateable,
  c.isencrypted AS isencryptedcolumn, c.isselectioncolumn, tbl.tablename, c.valuemin, c.valuemax, fgt.name AS fieldgroup, vr.code AS validationcode, f.included_tab_id, fg.fieldgrouptype,
  fg.iscollapsedbydefault, COALESCE(f.infofactoryclass, c.infofactoryclass) AS infofactoryclass, c.isautocomplete, COALESCE(f.isallowcopy, c.isallowcopy) AS isallowcopy, f.isdisplayedgrid,
- f.seqnogrid, c.seqnoselection, f.xposition, f.columnspan, f.numlines, COALESCE(f.istoolbarbutton, c.istoolbarbutton) AS istoolbarbutton, c.formatpattern, f.isadvancedfield, f.isdefaultfocus, c.ad_chart_id, f.ad_labelstyle_id, f.ad_fieldstyle_id, c.pa_dashboardcontent_id, trl.placeholder
+ f.seqnogrid, c.seqnoselection, f.xposition, f.columnspan, f.numlines, COALESCE(f.istoolbarbutton, c.istoolbarbutton) AS istoolbarbutton, c.formatpattern, f.isadvancedfield, f.isdefaultfocus, c.ad_chart_id, f.ad_labelstyle_id, f.ad_fieldstyle_id, c.pa_dashboardcontent_id, COALESCE(trl.placeholder, ct.placeholder) AS placeholder
    FROM ad_field f
    JOIN ad_field_trl trl ON f.ad_field_id = trl.ad_field_id
    JOIN ad_tab t ON f.ad_tab_id = t.ad_tab_id
    LEFT JOIN ad_fieldgroup fg ON f.ad_fieldgroup_id = fg.ad_fieldgroup_id
    LEFT JOIN ad_fieldgroup_trl fgt ON f.ad_fieldgroup_id = fgt.ad_fieldgroup_id AND trl.ad_language::text = fgt.ad_language::text
    LEFT JOIN ad_column c ON f.ad_column_id = c.ad_column_id
+   LEFT JOIN ad_column_trl ct ON f.ad_column_id = ct.ad_column_id
    JOIN ad_table tbl ON c.ad_table_id = tbl.ad_table_id
    LEFT JOIN ad_val_rule vr ON vr.ad_val_rule_id = COALESCE(f.ad_val_rule_id, c.ad_val_rule_id)
   WHERE f.isactive = 'Y' AND c.isactive = 'Y'
 ;
+
+update ad_process_para set placeholder = (select msgtext from ad_message where ad_message_id = 200438) where isrange='Y';
+
+update ad_process_para set placeholder2 = (select msgtext from ad_message where ad_message_id = 200439) where isrange='Y';
+
+update ad_process_para_trl set placeholder = (select msgtext from ad_message_trl where ad_message_id = 200438 and ad_process_para_trl.ad_language = ad_message_trl.ad_language) where ad_process_para_id in (select ad_process_para_id from ad_process_para where isrange='Y');
+
+update ad_process_para_trl set placeholder2 = (select msgtext from ad_message_trl where ad_message_id = 200439 and ad_process_para_trl.ad_language = ad_message_trl.ad_language) where ad_process_para_id in (select ad_process_para_id from ad_process_para where isrange='Y');
 
 SELECT register_migration_script('201802151000_IDEMPIERE-3639.sql') FROM dual
 ;
