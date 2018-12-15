36c3c55f9ce283ca601531a1b58a6d23451b4d15
Nicolas Micoud <nmicoud@tgi.fr>
2018-12-15 17:02:39 +0100

    IDEMPIERE-1052 Improve Performance using Search instead of Table/Table Direct

---
+++ migration/i6.1/postgresql/201807301600_IDEMPIERE-1052.sql
@@ -0,0 +1,19 @@
+-- IDEMPIERE-1052: Improve Performance using Search instead of Table/Table Direct
+-- Jul 30, 2018 4:01:01 PM CEST
+UPDATE AD_Column SET AD_Reference_ID=30, FKConstraintType=NULL,Updated=TO_TIMESTAMP('2018-07-30 16:01:01','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=200590
+;
+
+-- Jul 30, 2018 4:01:07 PM CEST
+UPDATE AD_Column SET AD_Reference_ID=30, IsUpdateable='N',Updated=TO_TIMESTAMP('2018-07-30 16:01:07','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=200588
+;
+
+-- Jul 30, 2018 4:01:21 PM CEST
+UPDATE AD_Column SET AD_Reference_ID=30, FKConstraintType=NULL,Updated=TO_TIMESTAMP('2018-07-30 16:01:21','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=200593
+;
+
+-- Jul 30, 2018 4:01:33 PM CEST
+UPDATE AD_Column SET AD_Reference_ID=30, FKConstraintType=NULL,Updated=TO_TIMESTAMP('2018-07-30 16:01:33','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=200595
+;
+
+SELECT register_migration_script('201807301600_IDEMPIERE-1052.sql') FROM dual
+;
