6ef649afa247e426760cffdb7efa955130be57a9
Carlos Ruiz <carg67@gmail.com>
2019-02-08 13:10:18 +0100

    Backed out changeset 76cb3821607f - wrong changes committed by mistake

--- org.adempiere.base/src/org/compiere/model/MQuery.java
+++ org.adempiere.base/src/org/compiere/model/MQuery.java
@@ -1023,10 +1023,8 @@ class Restriction  implements Serializable
 		//
 		this.Operator = operator;
 		//	Boolean
-		if (code instanceof Boolean) {
+		if (code instanceof Boolean)
 			Code = ((Boolean)code).booleanValue() ? "Y" : "N";
-			Code = Msg.getMsg(Env.getAD_Language(Env.getCtx()), Code.toString());
-		}
 		else if (code instanceof KeyNamePair)
 			Code = Integer.valueOf(((KeyNamePair)code).getKey());
 		else if (code instanceof ValueNamePair)
