8641a1540f9ef5574c0823fb37c9b06f54386947
Carlos Ruiz <carg67@gmail.com>
2019-03-25 16:27:43 -0300

    IDEMPIERE-3861 Switch LDAP type by System Configuration / based on suggestion from Ray Lee

--- org.adempiere.base/src/org/compiere/db/LDAP.java
+++ org.adempiere.base/src/org/compiere/db/LDAP.java
@@ -26,6 +26,7 @@ import javax.naming.directory.Attribute;
 import javax.naming.directory.Attributes;
 import javax.naming.ldap.InitialLdapContext;
 
+import org.compiere.model.MSysConfig;
 import org.compiere.util.CLogger;
 
 
@@ -52,10 +53,12 @@ public class LDAP
 		//	ldap://dc.compiere.org
 		env.put(Context.PROVIDER_URL, ldapURL);
 		env.put(Context.SECURITY_AUTHENTICATION, "simple");
-		//	jjanke@compiere.org
-		// For OpenLDAP uncomment the next line  
-		// StringBuffer principal = new StringBuffer("uid=").append(userName).append(",").append(domain);
-		StringBuffer principal = new StringBuffer(userName).append("@").append(domain);
+		StringBuffer principal;
+		if ("openldap".equals(MSysConfig.getValue(MSysConfig.LDAP_TYPE))) {
+			principal = new StringBuffer("uid=").append(userName).append(",").append(domain);
+		} else {
+			principal = new StringBuffer(userName).append("@").append(domain);
+		}
 		env.put(Context.SECURITY_PRINCIPAL, principal.toString());
 		env.put(Context.SECURITY_CREDENTIALS, password);
 		//
