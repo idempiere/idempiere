d42372df509c2c1fd71dfe02d13fb593e545056d
Carlos Ruiz <carg67@gmail.com>
2019-03-26 15:58:30 -0300

    IDEMPIERE-1085 Loading Info windows dynamically - fix wrong default found on AD_InfoWindow.SeqNo

---
+++ migration/i6.2/oracle/201903261557_IDEMPIERE-1085.sql
@@ -0,0 +1,11 @@
+SET SQLBLANKLINES ON
+SET DEFINE OFF
+
+-- IDEMPIERE-1085 Loading Info windows dynamically
+-- Mar 26, 2019, 3:56:49 PM BRT
+UPDATE AD_Column SET DefaultValue='@SQL=SELECT NVL(MAX(SeqNo),0)+10 AS DefaultValue FROM AD_InfoWindow',Updated=TO_DATE('2019-03-26 15:56:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=210510
+;
+
+SELECT register_migration_script('201903261557_IDEMPIERE-1085.sql') FROM dual
+;
+
