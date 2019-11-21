ccd51b994ee78170e6d6601211452859a9e90853
Carlos Ruiz <carg67@gmail.com>
2019-11-21 22:28:32 +0100

    release 7.1 process - script to set version

---
+++ migration/i6.2z/oracle/201911212227_Version.sql
@@ -0,0 +1,14 @@
+-- Set Version
+UPDATE AD_SYSTEM
+   SET releaseno = '7.1',
+       VERSION = '2019-11-22'
+ WHERE ad_system_id = 0 AND ad_client_id = 0
+;
+
+COMMIT
+;
+
+
+SELECT register_migration_script('201911212227_Version.sql') FROM dual
+;
+
