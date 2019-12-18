dbed99a875762fd784382516ee3c6455c33d76d4
Carlos Ruiz <carg67@gmail.com>
2019-12-18 16:35:54 +0100

    IDEMPIERE-2836 SQL Searches for advanced users / peer review - fix oracle script

--- migration/i7.1/oracle/201911211857_IDEMPIERE-2836.sql
+++ migration/i7.1/oracle/201911211857_IDEMPIERE-2836.sql
@@ -8,7 +8,7 @@ UPDATE AD_Field SET Help='The Validation Code displays the date, time and messag
 You can add advanced SQL queries to your searches by filling this field with @SQL=[WHERE CLAUSE].
 Do not include the WHERE statement and use Fully qualified SQL statements. F.e:
 
-@SQL=C_Order.isActive=''Y'' AND SalesRep_ID=@AD_User_ID@ 
+'||chr(64)||'SQL=C_Order.isActive=''Y'' AND SalesRep_ID=@AD_User_ID@ 
 
 will filter orders by active and where the sales representative is the current user.', IsCentrallyMaintained='N', AD_Reference_Value_ID=NULL, AD_Val_Rule_ID=NULL, IsToolbarButton=NULL,Updated=TO_DATE('2019-11-21 22:28:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=12249
 ;
@@ -19,7 +19,7 @@ UPDATE AD_Field SET Help='The Validation Code displays the date, time and messag
 You can add advanced SQL queries to your searches by filling this field with @SQL=[WHERE CLAUSE].
 Do not include the WHERE statement and use Fully qualified SQL statements. F.e:
 
-@SQL=C_Order.isActive=''Y'' AND SalesRep_ID=@AD_User_ID@ 
+'||chr(64)||'SQL=C_Order.isActive=''Y'' AND SalesRep_ID=@AD_User_ID@ 
 
 will filter orders by active and where the sales representative is the current user.', IsCentrallyMaintained='N', AD_Reference_Value_ID=NULL, AD_Val_Rule_ID=NULL, IsToolbarButton=NULL,Updated=TO_DATE('2019-11-21 18:54:31','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=206305
 ;
