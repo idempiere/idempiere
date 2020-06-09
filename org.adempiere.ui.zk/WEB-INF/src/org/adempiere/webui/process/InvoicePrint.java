b958a23cc672e2895561dca81a001bd735d90c16
Carlos Ruiz <carg67@gmail.com>
2020-06-09 12:39:00 +0200

    IDEMPIERE-4254 get oracle error ORA-01795 when has more than 1000 org

--- org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/process/InvoicePrint.java
+++ org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/process/InvoicePrint.java
@@ -428,7 +428,8 @@ public class InvoicePrint extends SvrProcess
 		}
 		String orgWhere = MRole.getDefault(getCtx(), false).getOrgWhere(MRole.SQL_RO);
 		if (!Util.isEmpty(orgWhere, true)) {
-			sql.append(" AND i.");
+			orgWhere = orgWhere.replaceAll("AD_Org_ID", "i.AD_Org_ID");
+			sql.append(" AND ");
 			sql.append(orgWhere);
 		}
 		sql.append(" ORDER BY i.C_Invoice_ID, pf.AD_Org_ID DESC");	//	more than 1 PF record
