de6b89913311ac442c3478520302aebaaa86033e
igorpojzl <59439434+igorpojzl@users.noreply.github.com>
2020-07-20 14:51:48 +0200

    IDEMPIERE-4368: Allow Define Default Saved Query for User (#169)
    * Update AD_UserQuery models
    
    * Implementation of initializing Default User Queries
    
    * Added Migration Scripts
    
    * Update 202007161400_IDEMPIERE-4368.sql
    
    Added missing register_migration_script.
    
    * IDEMPIERE-4368 Added missing script.
    
    * IDEMPIERE-4368 Allow Define Default Saved Query for User
    
    Fix Load defaults only on Initialization.
    
    * IDEMPIERE-4368
    
    Move Migration Script to 7.1 folder
    
    * IDEMPEIRE-4368
    
    Apply of patch.

--- org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/adwindow/AbstractADWindowContent.java
+++ org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/adwindow/AbstractADWindowContent.java
@@ -380,6 +380,9 @@ public abstract class AbstractADWindowContent extends AbstractUIPart implements 
 
         toolbar.updateToolbarAccess(adWindowId);
         updateToolbar();
+        if (toolbar.initDefaultQuery()) {
+        	doOnQueryChange();
+        }
         
         if (detailQuery != null && zoomToDetailTab(detailQuery))
         {
@@ -1396,7 +1399,7 @@ public abstract class AbstractADWindowContent extends AbstractUIPart implements 
 		if (callback != null)
 			callback.onCallback(true);
 	}
-
+	
 	private void updateToolbar()
 	{
 		toolbar.enableTabNavigation(breadCrumb.hasParentLink(), adTabbox.getSelectedDetailADTabpanel() != null);
@@ -1429,6 +1432,7 @@ public abstract class AbstractADWindowContent extends AbstractUIPart implements 
 				(!isNewRow && (m_onlyCurrentRows || m_onlyCurrentDays > 0)));
 		
 		toolbar.refreshUserQuery(adTabbox.getSelectedGridTab().getAD_Tab_ID(), findWindow != null ? findWindow.getAD_UserQuery_ID() : 0);
+
 	}
 
 	/**
