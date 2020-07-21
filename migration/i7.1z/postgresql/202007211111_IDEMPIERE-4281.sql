8be41cb4335a1aca147087f5f5cd10f3c3352674
Nicolas Micoud <nmicoud@tgi.fr>
2020-07-21 11:19:12 +0200

    IDEMPIERE-4281 : Credit memo linked to an invoice should generate automatically an allocation
    Inactivation of the InvoiceCreditCreditMemo IsCreateAllocation parameter

---
+++ migration/i7.1z/postgresql/202007211111_IDEMPIERE-4281.sql
@@ -0,0 +1,7 @@
+-- IDEMPIERE-4281
+-- Jul 21, 2020, 11:10:14 AM CEST
+UPDATE AD_Process_Para SET IsActive='N',Updated=TO_TIMESTAMP('2020-07-21 11:10:14','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Process_Para_ID=200279
+;
+
+SELECT register_migration_script('202007211111_IDEMPIERE-4281.sql') FROM dual
+;
