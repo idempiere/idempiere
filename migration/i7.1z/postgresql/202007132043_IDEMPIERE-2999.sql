92794b5d58113b8c15a5ac127a738970a2883493
Andreas <sumerauer@kanzlei-wmv.de>
2020-07-29 01:44:41 +0200

    string constants representing timestamps replaced with the proper conversion functions in sql migration files

--- migration/i7.1z/postgresql/202007132043_IDEMPIERE-2999.sql
+++ migration/i7.1z/postgresql/202007132043_IDEMPIERE-2999.sql
@@ -1,5 +1,5 @@
 INSERT INTO ad_val_rule(ad_val_rule_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, name, description, type, code, entitytype, ad_val_rule_uu) 
-VALUES(200089, 0, 0, 'Y', '2020-07-13 20:43:31', 0, '2020-07-13 20:43:31', 0, 'AD_Reference List or Table (Attribute)', 'List or Table validation choices', 'S',
+VALUES(200089, 0, 0, 'Y', TO_TIMESTAMP('2020-07-13 20:43:31','YYYY-MM-DD HH24:MI:SS'), 0, TO_TIMESTAMP('2020-07-13 20:43:31','YYYY-MM-DD HH24:MI:SS'), 0, 'AD_Reference List or Table (Attribute)', 'List or Table validation choices', 'S',
 '(
     (@AD_Reference_ID@ IN (17, 200161)  
     AND AD_Reference.ValidationType= ''L'')  
@@ -19,21 +19,21 @@ VALUES(200089, 0, 0, 'Y', '2020-07-13 20:43:31', 0, '2020-07-13 20:43:31', 0, 'A
 
 UPDATE ad_val_rule
 SET code = 'AD_Reference_ID NOT IN (19,21,23,28,35,53370) AND EntityType = ''D''',
-updated = '2020-07-13 20:43:31',
+updated = TO_TIMESTAMP('2020-07-13 20:43:31','YYYY-MM-DD HH24:MI:SS'),
 updatedby = 0
 WHERE ad_val_rule_id = 200090
 ;
 
 UPDATE ad_field
 SET displaylogic = '@AD_Reference_ID@=17 | @AD_Reference_ID@=18 | @AD_Reference_ID@=30 | @AD_Reference_ID@=200161 | @AD_Reference_ID@=200162 | @AD_Reference_ID@=200163 & @AttributeValueType@=''R''',
-updated = '2020-07-13 20:43:31',
+updated = TO_TIMESTAMP('2020-07-13 20:43:31','YYYY-MM-DD HH24:MI:SS'),
 updatedby = 0
 WHERE ad_field_id = 204144
 ;
 
 UPDATE ad_column
 SET ad_val_rule_id = 200089,
-updated = '2020-07-13 20:43:31',
+updated = TO_TIMESTAMP('2020-07-13 20:43:31','YYYY-MM-DD HH24:MI:SS'),
 updatedby = 0
 WHERE ad_column_id = 212644
 ;
