9a7dbf9daa380d8e7bf6b501bfb9d5991b43d23f
Nicolas Micoud <nmicoud@tgi.fr>
2020-08-20 19:14:39 +0200

    IDEMPIERE-4422 : GL Distribution - add SeqNo
    Add SeqNo column on GL Distribution to allow users to choose the lowest one when several records are retrieved.
    Fix typo

--- org.adempiere.base/src/org/compiere/acct/Fact.java
+++ org.adempiere.base/src/org/compiere/acct/Fact.java
@@ -726,8 +726,8 @@ public final class Fact
 			//end AZ
 			//	Just the first
 			if (distributions.length > 1)
-				log.warning("More then one Distributiion for " + dLine.getAccount());
-			MDistribution distribution = distributions[0]; 
+				log.warning("More then one Distribution for " + dLine.getAccount());
+			MDistribution distribution = distributions[0];
 
 			// FR 2685367 - GL Distribution delete line instead reverse
 			if (distribution.isCreateReversal()) {
