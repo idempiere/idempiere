140d26c0acdc4aaf39c831f02a503780dff514f3
Carlos Ruiz <carg67@gmail.com>
2020-09-05 17:53:35 +0200

    IDEMPIERE-4441 FreeBSD find utility requires a target directory in postgresql/SyncDB.sh
    Verify all scripts with shellcheck
    https://www.shellcheck.net/

--- org.adempiere.server-feature/utils.unix/oracle/DBExportdp.sh
+++ org.adempiere.server-feature/utils.unix/oracle/DBExportdp.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-echo	Adempiere Database Export 	$Revision: 1.5 $
+echo	iDempiere Database Export 	"$Revision": 1.5 $
 
 # $Id: DBExport.sh,v 1.5 2005/12/20 07:12:17 jjanke Exp $
 
-DATE=`date +%Y%m%d_%H%M%S`
-echo Saving database $1@$ADEMPIERE_DB_NAME to $IDEMPIERE_HOME/data/ExpDat_$DATE.dmp
+DATE=$(date +%Y%m%d_%H%M%S)
+echo Saving database "$1"@"$ADEMPIERE_DB_NAME" to "$IDEMPIERE_HOME"/data/ExpDat_"$DATE".dmp
 
-if [ $# -eq 0 ] 
+if [ $# -eq 0 ]
   then
     echo "Usage:		$0 <userAccount>"
     echo "Example:	$0 adempiere adempiere"
     exit 1
 fi
-if [ "$IDEMPIERE_HOME" = "" -o  "$ADEMPIERE_DB_NAME" = "" ]
+if [ "$IDEMPIERE_HOME" = "" ] || [ "$ADEMPIERE_DB_NAME" = "" ]
   then
     echo "Please make sure that the environment variables are set correctly:"
     echo "	IDEMPIERE_HOME	e.g. /idempiere"
@@ -22,17 +22,17 @@ if [ "$IDEMPIERE_HOME" = "" -o  "$ADEMPIERE_DB_NAME" = "" ]
 fi
 
 # Cleanup
-sqlplus $1/$2@$ADEMPIERE_DB_SERVER:$ADEMPIERE_DB_PORT/$ADEMPIERE_DB_NAME @$IDEMPIERE_HOME/utils/$ADEMPIERE_DB_PATH/Daily.sql
+sqlplus "$1"/"$2"@"$ADEMPIERE_DB_SERVER":"$ADEMPIERE_DB_PORT"/"$ADEMPIERE_DB_NAME" @"$IDEMPIERE_HOME"/utils/"$ADEMPIERE_DB_PATH"/Daily.sql
 
 echo -------------------------------------
 echo Re-Create DataPump directory
 echo -------------------------------------
-sqlplus $3@$ADEMPIERE_DB_SERVER:$ADEMPIERE_DB_PORT/$ADEMPIERE_DB_NAME @$IDEMPIERE_HOME/utils/$ADEMPIERE_DB_PATH/CreateDataPumpDir.sql $IDEMPIERE_HOME/data
-chgrp dba $IDEMPIERE_HOME/data
-chmod 770 $IDEMPIERE_HOME/data
+sqlplus "$3"@"$ADEMPIERE_DB_SERVER":"$ADEMPIERE_DB_PORT"/"$ADEMPIERE_DB_NAME" @"$IDEMPIERE_HOME"/utils/"$ADEMPIERE_DB_PATH"/CreateDataPumpDir.sql "$IDEMPIERE_HOME"/data
+chgrp dba "$IDEMPIERE_HOME"/data
+chmod 770 "$IDEMPIERE_HOME"/data
 
 # Export
-expdp $1/$2@$ADEMPIERE_DB_SERVER:$ADEMPIERE_DB_PORT/$ADEMPIERE_DB_NAME DIRECTORY=ADEMPIERE_DATA_PUMP_DIR DUMPFILE=ExpDat_$DATE.dmp LOGFILE=ExpDat_$DATE.log EXCLUDE=STATISTICS SCHEMAS=$1
+expdp "$1"/"$2"@"$ADEMPIERE_DB_SERVER":"$ADEMPIERE_DB_PORT"/"$ADEMPIERE_DB_NAME" DIRECTORY=ADEMPIERE_DATA_PUMP_DIR DUMPFILE=ExpDat_"$DATE".dmp LOGFILE=ExpDat_"$DATE".log EXCLUDE=STATISTICS SCHEMAS="$1"
 
-cd $IDEMPIERE_HOME/data
-jar cvfM ExpDat.jar ExpDat_$DATE.dmp ExpDat_$DATE.log
+cd "$IDEMPIERE_HOME"/data || exit
+jar cvfM ExpDat.jar ExpDat_"$DATE".dmp ExpDat_"$DATE".log
