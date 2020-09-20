c9fa57c1e243bba72b020b18db925c3632d76028
Nicolas Micoud <58596990+nmicoud@users.noreply.github.com>
2020-09-20 15:53:56 +0200

    IDEMPIERE-4460 : User suggestions for field/context help : check perm… (#257)
    * IDEMPIERE-4460 : User suggestions for field/context help : check permission
    
    new method to check exclusion of a table for current role, used to determine if popups should be displayed or not
    
    * IDEMPIERE-4460 : User suggestions for field/context help : check permission
    
    Following @globalqss's recommandations : rename the method + add more controls inside
    
    Co-Authored-By: Carlos Ruiz <carg67@gmail.com>
    
    Co-authored-by: Carlos Ruiz <carg67@gmail.com>

--- org.adempiere.base/src/org/compiere/model/MRole.java
+++ org.adempiere.base/src/org/compiere/model/MRole.java
@@ -63,7 +63,7 @@ public final class MRole extends X_AD_Role
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = -4649095180532036099L;
+	private static final long serialVersionUID = 8041397465891798378L;
 
 	/**
 	 * 	Get Default (Client) Role
@@ -3343,5 +3343,26 @@ public final class MRole extends X_AD_Role
 		}
 		return access;
 	}
+
+	/**
+	 * Does the table is excluded for current role (this method doesn't check the level of the table, use isTableAccess for this purpose)
+	 * @param tableID ID of the table
+	 * @return
+	 */
+	public boolean isTableAccessExcluded(int tableID)
+	{
+		loadTableAccess(false);
+
+		for (int i = 0; i < m_tableAccess.length; i++) {
+			if (   m_tableAccess[i].isExclude()
+					&& m_tableAccess[i].getAD_Table_ID() == tableID
+					&& ! m_tableAccess[i].isReadOnly()
+					&& MTableAccess.ACCESSTYPERULE_Accessing.equals(m_tableAccess[i].getAccessTypeRule())
+					)
+				return true;
+		}
+
+		return false;
+	}	//	isTableAccessExcluded
 
 }	//	MRole
