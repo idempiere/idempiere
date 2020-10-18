42fb939fcb9b1ed49c59fcc9f200dd5eaa767384
Carlos Ruiz <carg67@gmail.com>
2020-10-18 05:04:54 +0200

    IDEMPIERE-4495 github code scanning alerts (#308)
    * IDEMPIERE-4495 github code scanning alerts
    
    Failure to use secure cookies
    
    * Failure to use secure cookies - one more
    
    * Fix: Arbitrary file write during archive extraction ("Zip Slip")
    
    * Fix: Resolving XML external entity in user-controlled data

--- org.adempiere.base/src/org/compiere/util/WebUtil.java
+++ org.adempiere.base/src/org/compiere/util/WebUtil.java
@@ -941,6 +941,7 @@ public final class WebUtil
 		cookie.setComment("adempiere Web User");
 		cookie.setPath(request.getContextPath());
 		cookie.setMaxAge(1);      //  second
+		cookie.setSecure(true);
 		response.addCookie(cookie);
 	}	//	deleteCookieWebUser
 	
@@ -970,6 +971,7 @@ public final class WebUtil
 		cookie.setComment("adempiere Web User");
 		cookie.setPath(request.getContextPath());
 		cookie.setMaxAge(2592000);      //  30 days in seconds   60*60*24*30
+		cookie.setSecure(true);
 		response.addCookie(cookie);
 	  } catch (UnsupportedEncodingException e) {
 		e.printStackTrace();
