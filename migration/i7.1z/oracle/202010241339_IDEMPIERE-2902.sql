7e942182bfb5ea141c8c35c15b2f2291b938951a
Carlos Ruiz <carg67@gmail.com>
2020-10-24 15:38:52 +0200

    IDEMPIERE-2902 UX: Implement Radio buttons
    * For testing and UX, configure as Radio Group in detail tab:
    Info Window > Process > LayoutType

--- migration/i7.1z/oracle/202010241339_IDEMPIERE-2902.sql
+++ migration/i7.1z/oracle/202010241339_IDEMPIERE-2902.sql
@@ -74,6 +74,10 @@ UPDATE AD_Field SET DisplayLogic='@AD_Reference_ID@=17 | @AD_Reference_ID@=18 | 
 UPDATE AD_Field SET DisplayLogic='@AD_Reference_ID@=17 | @AD_Reference_ID@=18 | @AD_Reference_ID@=19 | @AD_Reference_ID@=28 | @AD_Reference_ID@=30 | @AD_Reference_ID@=200012 | @AD_Reference_ID@=31 | @AD_Reference_ID@=200152 | @AD_Reference_ID@=200161 | @AD_Reference_ID@=200162 | @AD_Reference_ID@=200163', AD_Reference_Value_ID=NULL, AD_Val_Rule_ID=NULL, IsToolbarButton=NULL,Updated=TO_DATE('2020-10-24 13:38:25','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=206130
 ;
 
+-- Oct 24, 2020, 1:42:01 PM CEST
+UPDATE AD_Column SET AD_Reference_ID=200152,Updated=TO_DATE('2020-10-24 13:42:01','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=211257
+;
+
 SELECT register_migration_script('202010241339_IDEMPIERE-2902.sql') FROM dual
 ;
 
