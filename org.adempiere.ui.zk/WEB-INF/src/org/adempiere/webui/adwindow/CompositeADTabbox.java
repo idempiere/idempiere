3d457d5dc5183e5cd8d953bb151f8c179bf851e9
hengsin <hengsin@gmail.com>
2020-11-01 00:18:07 +0800

    IDEMPIERE-4514 Grid View Layout Bug (#339)
    Fix NPE
    
    Co-authored-by: Carlos Ruiz <carg67@gmail.com>

--- org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/adwindow/CompositeADTabbox.java
+++ org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/adwindow/CompositeADTabbox.java
@@ -923,7 +923,9 @@ public class CompositeADTabbox extends AbstractADTabbox
 			if (tabPanel.getDesktop() != null) {
 				Executions.schedule(tabPanel.getDesktop(), e -> {
 					invalidateTabPanel(tabPanel);
-				}, new Event("onPostActivateDetail", tabPanel));	
+				}, new Event("onPostActivateDetail", tabPanel));
+			} else {
+				invalidateTabPanel(tabPanel);
 			}
 		} else {
 			invalidateTabPanel(tabPanel);
