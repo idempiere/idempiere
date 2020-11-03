d0611a170bda191978e73c9e2a49c82e04392804
Carlos Ruiz <carg67@gmail.com>
2020-11-03 15:51:54 +0100

    IDEMPIERE-4519 Zk: Implement optional defer rendering for Grid View (#352)
    Add migration script for the SysConfig key

---
+++ migration/i7.1z/oracle/202011031550_IDEMPIERE-4519.sql
@@ -0,0 +1,11 @@
+SET SQLBLANKLINES ON
+SET DEFINE OFF
+
+-- IDEMPIERE-4519 Zk: Implement optional defer rendering for Grid View
+-- Nov 3, 2020, 3:39:40 PM CET
+INSERT INTO AD_SysConfig (AD_SysConfig_ID,AD_Client_ID,AD_Org_ID,Created,Updated,CreatedBy,UpdatedBy,IsActive,Name,Value,Description,EntityType,ConfigurationLevel,AD_SysConfig_UU) VALUES (200165,0,0,TO_DATE('2020-11-03 15:39:39','YYYY-MM-DD HH24:MI:SS'),TO_DATE('2020-11-03 15:39:39','YYYY-MM-DD HH24:MI:SS'),100,100,'Y','ZK_GRID_VIEW_USE_DEFER_RENDERING','N','Defer row rendering for grid view','D','C','6a107269-96ba-45f9-9cda-6df7d04c3d3e')
+;
+
+SELECT register_migration_script('202011031550_IDEMPIERE-4519.sql') FROM dual
+;
+
