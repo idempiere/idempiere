5b5bebaf58c3fc9c91c3270b2490e3fba5a3801f
hengsin <hengsin@gmail.com>
2020-11-03 20:58:54 +0800

    IDEMPIERE-4514 Grid View Layout Bug (#348)

--- org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/adwindow/ADTabpanel.java
+++ org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/adwindow/ADTabpanel.java
@@ -1367,6 +1367,8 @@ DataStatusListener, IADTabpanel, IdSpace, IFieldEditorContainer
     	if (tabPanel != null) {
     		if (!tabPanel.isActivated()) {
     			tabPanel.activate(true);
+    		} else {
+    			tabPanel.getGridView().invalidateGridView();
     		}
 	    	if (!tabPanel.isGridView()) {
 	    		tabPanel.switchRowPresentation();	
@@ -1568,7 +1570,7 @@ DataStatusListener, IADTabpanel, IdSpace, IFieldEditorContainer
         	listPanel.dynamicDisplay(col);
         	if (GridTable.DATA_REFRESH_MESSAGE.equals(e.getAD_Message()) || 
         		"Sorted".equals(e.getAD_Message())) {
-        		listPanel.getListbox().invalidate();
+        		listPanel.invalidateGridView();
         	}
         }
     }
