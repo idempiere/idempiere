8c9243ded353a6318b7c6893f670451ad8eeeaf6
Carlos Ruiz <carg67@gmail.com>
2020-11-22 12:16:24 +0100

    Update 202011121122_IDEMPIERE-4569.sql

--- migration/i8.1z/oracle/202011121122_IDEMPIERE-4569.sql
+++ migration/i8.1z/oracle/202011121122_IDEMPIERE-4569.sql
@@ -8,3 +8,6 @@ UPDATE AD_Column SET ColumnSql  = '(case when UseLifeMonths - (case when A_Curre
 
 UPDATE AD_Column SET ColumnSql  = '(case when UseLifeMonths_F - (case when A_Current_Period = 0 then 1 else A_Current_Period end )+ 1 > 0 then (A_Asset_Cost - A_Accumulated_Depr_F)/(UseLifeMonths_F - (case when A_Current_Period = 0 then 1 else A_Current_Period end ) + 1) else 0 end)' WHERE  AD_Column_ID=59288
 ;
+
+SELECT register_migration_script('202011121122_IDEMPIERE-4569.sql') FROM dual
+;
