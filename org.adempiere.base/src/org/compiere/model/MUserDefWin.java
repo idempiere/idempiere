2c9df40e6e69f9df9fda0e844c24a6fb66ddafd6
Carlos Ruiz <carg67@gmail.com>
2020-12-08 13:46:29 +0100

    IDEMPIERE-4268 Web Services : Read miss cross-tenant check
    Implement thread safe mechanism
    https://mattermost.idempiere.org/idempiere/pl/hmpd8freutbhbgcjr9tu6ntitr

--- org.adempiere.base/src/org/compiere/model/MUserDefWin.java
+++ org.adempiere.base/src/org/compiere/model/MUserDefWin.java
@@ -104,19 +104,13 @@ public class MUserDefWin extends X_AD_UserDef_Win implements ImmutablePOSupport
 	private static MUserDefWin[] getAll (Properties ctx, int window_ID )
 	{
 		if (m_fullList == null) {
-			int cid = Env.getAD_Client_ID(Env.getCtx());
 			try {
-				if (cid > 0) {
-					// forced potential cross tenant read - requires System client in context
-					Env.setContext(Env.getCtx(), Env.AD_CLIENT_ID, 0);
-				}
+				PO.setCrossTenantSafe();
 				m_fullList = new Query(ctx, MUserDefWin.Table_Name, null, null)
 						.setOnlyActiveRecords(true)
 						.list();
 			} finally {
-				if (cid > 0) {
-					Env.setContext(Env.getCtx(), Env.AD_CLIENT_ID, cid);
-				}
+				PO.clearCrossTenantSafe();
 			}
 		}
 		
