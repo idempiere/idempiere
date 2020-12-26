b54302fe71b3051ea1e13a46c4d89c857137246e
hengsin <hengsin@gmail.com>
2020-12-27 01:19:37 +0800

    IDEMPIERE-4610 Remove obsolete LdapProcessor
    Inactivate menu entry (AD_Menu_ID=594) and window (AD_Window_ID=389)

---
+++ migration/i8.2/oracle/202012261710_IDEMPIERE-4610.sql
@@ -0,0 +1,14 @@
+SET SQLBLANKLINES ON
+SET DEFINE OFF
+
+-- Dec 27, 2020, 1:10:12 AM MYT
+UPDATE AD_Window SET IsActive='N',Updated=TO_DATE('2020-12-27 01:10:12','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Window_ID=389
+;
+
+-- Dec 27, 2020, 1:10:12 AM MYT
+UPDATE AD_Menu SET Name='LDAP Server', Description='LDAP Server to authenticate and authorize external systems based on iDempiere', IsActive='N',Updated=TO_DATE('2020-12-27 01:10:12','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Menu_ID=594
+;
+
+SELECT register_migration_script('202012261710_IDEMPIERE-4610.sql') FROM dual
+;
+
