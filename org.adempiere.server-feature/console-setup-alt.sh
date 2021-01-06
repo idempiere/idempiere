d94298e7cbd0719eb79b93b1ff72fb12b4a30a94
Carlos Ruiz <carg67@gmail.com>
2021-01-06 20:07:05 +0100

    IDEMPIERE-4620 Improvements for Setup programs (#514)
    Allow passing the log parameter from command line in setup programs

--- org.adempiere.server-feature/console-setup-alt.sh
+++ org.adempiere.server-feature/console-setup-alt.sh
@@ -14,7 +14,7 @@ fi
 
 
 # Setup idempiere.properties and idempiereEnv.properties
-$JAVA -jar plugins/org.eclipse.equinox.launcher_1.*.jar -install setup -configuration setup/configuration -application org.adempiere.install.console-application
+$JAVA -jar plugins/org.eclipse.equinox.launcher_1.*.jar -install setup -configuration setup/configuration -application org.adempiere.install.console-application $@
 
 # Setup Jetty
 $JAVA -jar plugins/org.eclipse.equinox.launcher_1.*.jar -install setup -configuration setup/configuration -application org.eclipse.ant.core.antRunner -buildfile build.xml
