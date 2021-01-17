ebc67db1ea2b73f4985519888d10b72ef67707ad
Carlos Ruiz <carg67@gmail.com>
2021-01-17 16:16:20 +0100

    * Set GardenWorld to use multi-lingual documents

--- migration/i8.2/postgresql/202101152101_IDEMPIERE-4650.sql
+++ migration/i8.2/postgresql/202101152101_IDEMPIERE-4650.sql
@@ -13,6 +13,10 @@ Please note that base language cannot be a system language.<br>
 After running this process you must close all sessions and login again.<br>',Updated=TO_TIMESTAMP('2021-01-15 20:51:26','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=200040
 ;
 
+-- Jan 17, 2021, 4:15:10 PM CET
+UPDATE AD_Client SET IsMultiLingualDocument='Y',Updated=TO_TIMESTAMP('2021-01-17 16:15:10','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Client_ID=11
+;
+
 SELECT register_migration_script('202101152101_IDEMPIERE-4650.sql') FROM dual
 ;
 
