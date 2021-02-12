89a095ac78c646642f4c99fcf30d9bf8f7a9e2b0
Carlos Ruiz <carg67@gmail.com>
2021-02-12 12:12:31 +0100

    IDEMPIERE-4700 PackOut is creating index and view data for Dictionary entries when export dictionary is disabled (#578)

--- org.adempiere.pipo.handlers/src/org/adempiere/pipo2/handler/IndexColumnElementHandler.java
+++ org.adempiere.pipo.handlers/src/org/adempiere/pipo2/handler/IndexColumnElementHandler.java
@@ -122,11 +122,8 @@ public class IndexColumnElementHandler extends AbstractElementHandler {
 		int AD_IndexColumn_ID = Env.getContextAsInt(ctx.ctx, MIndexColumn.COLUMNNAME_AD_IndexColumn_ID);
 		MIndexColumn m_IndexColumn = new MIndexColumn(ctx.ctx, AD_IndexColumn_ID, getTrxName(ctx));
 
-		if (ctx.packOut.getFromDate() != null) {
-			if (m_IndexColumn.getUpdated().compareTo(ctx.packOut.getFromDate()) < 0) {
-				return;
-			}
-		}
+		if (!isPackOutElement(ctx, m_IndexColumn))
+			return;
 
 		AttributesImpl atts = new AttributesImpl();
 		addTypeName(atts, "table");
