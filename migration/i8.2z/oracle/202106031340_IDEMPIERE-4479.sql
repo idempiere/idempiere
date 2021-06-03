7453dc3365abe346ff7e5f5f6d209fd2ef4568de
igorpojzl <59439434+igorpojzl@users.noreply.github.com>
2021-06-03 16:00:05 +0200

    IDEMPIERE-4479 Added View Update, ColumnSql fix FieldLength

--- migration/i8.2z/oracle/202106031340_IDEMPIERE-4479.sql
+++ migration/i8.2z/oracle/202106031340_IDEMPIERE-4479.sql
@@ -68,5 +68,187 @@ UPDATE AD_Field SET SeqNo=490, AD_Reference_Value_ID=NULL, AD_Val_Rule_ID=NULL, 
 UPDATE AD_Field SET SeqNo=500, AD_Reference_Value_ID=NULL, AD_Val_Rule_ID=NULL, IsToolbarButton=NULL,Updated=TO_DATE('2021-06-03 13:40:24','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=139
 ;
 
+-- Jun 3, 2021, 3:42:52 PM CEST
+UPDATE AD_Column SET FieldLength=4000,Updated=TO_DATE('2021-06-03 15:42:52','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=214499
+;
+
+-- Jun 3, 2021, 3:42:57 PM CEST
+ALTER TABLE AD_Field MODIFY ColumnSQL VARCHAR2(4000 CHAR) DEFAULT NULL 
+;
+
+CREATE OR REPLACE VIEW ad_field_v 
+AS
+SELECT t.ad_window_id,
+       f.ad_tab_id,
+       f.ad_field_id,
+       tbl.ad_table_id,
+       f.ad_column_id,
+       f.name,
+       f.description,
+       f.help,
+       f.isdisplayed,
+       f.displaylogic,
+       f.displaylength,
+       f.seqno,
+       f.sortno,
+       f.issameline,
+       f.isheading,
+       f.isfieldonly,
+       f.isreadonly,
+       f.isencrypted AS isencryptedfield,
+       f.obscuretype,
+       c.columnname,
+       COALESCE(f.columnsql,c.columnsql) AS columnsql,
+       c.fieldlength,
+       COALESCE(f.VFormat,c.VFormat) AS VFormat,
+       COALESCE(f.defaultvalue,c.defaultvalue) AS defaultvalue,
+       c.iskey,
+       c.isparent,
+       COALESCE(f.ismandatory,c.ismandatory) AS ismandatory,
+       c.isidentifier,
+       c.istranslated,
+       COALESCE(f.ad_reference_value_id,c.ad_reference_value_id) AS ad_reference_value_id,
+       c.callout,
+       COALESCE(f.ad_reference_id,c.ad_reference_id) AS ad_reference_id,
+       COALESCE(f.ad_val_rule_id,c.ad_val_rule_id) AS ad_val_rule_id,
+       c.ad_process_id,
+       COALESCE(f.isalwaysupdateable,c.isalwaysupdateable) AS isalwaysupdateable,
+       COALESCE(f.readonlylogic,c.readonlylogic) AS readonlylogic,
+       COALESCE(f.mandatorylogic,c.mandatorylogic) AS mandatorylogic,
+       COALESCE(f.isupdateable,c.isupdateable) AS isupdateable,
+       c.isencrypted AS isencryptedcolumn,
+       COALESCE(f.isselectioncolumn,c.isselectioncolumn) AS isselectioncolumn,
+       tbl.tablename,
+       c.valuemin,
+       c.valuemax,
+       fg.name AS fieldgroup,
+       vr.code AS validationcode,
+       f.included_tab_id,
+       fg.fieldgrouptype,
+       fg.iscollapsedbydefault,
+       COALESCE(f.infofactoryclass,c.infofactoryclass) AS infofactoryclass,
+       c.isautocomplete,
+       COALESCE(f.isallowcopy,c.isallowcopy) AS isallowcopy,
+       f.isdisplayedgrid,
+       f.seqnogrid,
+       c.seqnoselection,
+       f.xposition,
+       f.columnspan,
+       f.numlines,
+       COALESCE(f.istoolbarbutton,c.istoolbarbutton) AS istoolbarbutton,
+       c.formatpattern,
+       f.isadvancedfield,
+       f.isdefaultfocus,
+       c.ad_chart_id,
+       f.ad_labelstyle_id,
+       f.ad_fieldstyle_id,
+       c.pa_dashboardcontent_id,
+       COALESCE(f.placeholder,c.placeholder) AS placeholder,
+       c.ishtml,
+       f.isquickform,
+       COALESCE(f.ad_val_rule_Lookup_id,c.ad_val_rule_Lookup_id) AS ad_val_rule_Lookup_id,
+       vrf.code AS validationcodeLookup
+FROM ad_field f
+  JOIN ad_tab t ON f.ad_tab_id = t.ad_tab_id
+  LEFT JOIN ad_fieldgroup fg ON f.ad_fieldgroup_id = fg.ad_fieldgroup_id
+  LEFT JOIN ad_column c ON f.ad_column_id = c.ad_column_id
+  JOIN ad_table tbl ON c.ad_table_id = tbl.ad_table_id
+  LEFT JOIN ad_val_rule vr ON vr.ad_val_rule_id = COALESCE (f.ad_val_rule_id,c.ad_val_rule_id)
+  LEFT JOIN ad_val_rule vrf ON vrf.ad_val_rule_id = COALESCE (f.ad_val_rule_Lookup_id,c.ad_val_rule_Lookup_id)
+WHERE f.isactive = 'Y'
+AND   c.isactive = 'Y';
+
+
+CREATE OR REPLACE VIEW ad_field_vt 
+AS
+SELECT trl.ad_language,
+       t.ad_window_id,
+       f.ad_tab_id,
+       f.ad_field_id,
+       tbl.ad_table_id,
+       f.ad_column_id,
+       trl.name,
+       trl.description,
+       trl.help,
+       f.isdisplayed,
+       f.displaylogic,
+       f.displaylength,
+       f.seqno,
+       f.sortno,
+       f.issameline,
+       f.isheading,
+       f.isfieldonly,
+       f.isreadonly,
+       f.isencrypted AS isencryptedfield,
+       f.obscuretype,
+       c.columnname,
+       COALESCE(f.columnsql,c.columnsql) AS columnsql,
+       c.fieldlength,
+       COALESCE(f.VFormat,c.VFormat) AS VFormat,
+       COALESCE(f.defaultvalue,c.defaultvalue) AS defaultvalue,
+       c.iskey,
+       c.isparent,
+       COALESCE(f.ismandatory,c.ismandatory) AS ismandatory,
+       c.isidentifier,
+       c.istranslated,
+       COALESCE(f.ad_reference_value_id,c.ad_reference_value_id) AS ad_reference_value_id,
+       c.callout,
+       COALESCE(f.ad_reference_id,c.ad_reference_id) AS ad_reference_id,
+       COALESCE(f.ad_val_rule_id,c.ad_val_rule_id) AS ad_val_rule_id,
+       c.ad_process_id,
+       COALESCE(f.isalwaysupdateable,c.isalwaysupdateable) AS isalwaysupdateable,
+       COALESCE(f.readonlylogic,c.readonlylogic) AS readonlylogic,
+       COALESCE(f.mandatorylogic,c.mandatorylogic) AS mandatorylogic,
+       COALESCE(f.isupdateable,c.isupdateable) AS isupdateable,
+       c.isencrypted AS isencryptedcolumn,
+       COALESCE(f.isselectioncolumn,c.isselectioncolumn) AS isselectioncolumn,
+       tbl.tablename,
+       c.valuemin,
+       c.valuemax,
+       fgt.name AS fieldgroup,
+       vr.code AS validationcode,
+       f.included_tab_id,
+       fg.fieldgrouptype,
+       fg.iscollapsedbydefault,
+       COALESCE(f.infofactoryclass,c.infofactoryclass) AS infofactoryclass,
+       c.isautocomplete,
+       COALESCE(f.isallowcopy,c.isallowcopy) AS isallowcopy,
+       f.isdisplayedgrid,
+       f.seqnogrid,
+       c.seqnoselection,
+       f.xposition,
+       f.columnspan,
+       f.numlines,
+       COALESCE(f.istoolbarbutton,c.istoolbarbutton) AS istoolbarbutton,
+       c.formatpattern,
+       f.isadvancedfield,
+       f.isdefaultfocus,
+       c.ad_chart_id,
+       f.ad_labelstyle_id,
+       f.ad_fieldstyle_id,
+       c.pa_dashboardcontent_id,
+       COALESCE(trl.placeholder,ct.placeholder) AS placeholder,
+       c.ishtml,
+       f.isquickform,
+       COALESCE(f.ad_val_rule_Lookup_id,c.ad_val_rule_Lookup_id) AS ad_val_rule_Lookup_id,
+       vrf.code AS validationcodeLookup
+FROM ad_field f
+  JOIN ad_field_trl trl ON f.ad_field_id = trl.ad_field_id
+  JOIN ad_tab t ON f.ad_tab_id = t.ad_tab_id
+  LEFT JOIN ad_fieldgroup fg ON f.ad_fieldgroup_id = fg.ad_fieldgroup_id
+  LEFT JOIN ad_fieldgroup_trl fgt
+         ON f.ad_fieldgroup_id = fgt.ad_fieldgroup_id
+        AND trl.ad_language = fgt.ad_language
+  LEFT JOIN ad_column c ON f.ad_column_id = c.ad_column_id
+  LEFT JOIN ad_column_trl ct
+         ON f.ad_column_id = ct.ad_column_id
+        AND trl.ad_language = ct.ad_language
+  JOIN ad_table tbl ON c.ad_table_id = tbl.ad_table_id
+  LEFT JOIN ad_val_rule vr ON vr.ad_val_rule_id = COALESCE (f.ad_val_rule_id,c.ad_val_rule_id)
+  LEFT JOIN ad_val_rule vrf ON vrf.ad_val_rule_id = COALESCE (f.ad_val_rule_Lookup_id,c.ad_val_rule_Lookup_id)
+WHERE f.isactive = 'Y'
+AND   c.isactive = 'Y';
+
+
 SELECT Register_Migration_Script ('202106031340_IDEMPIERE-4479.sql') FROM DUAL
 ;
