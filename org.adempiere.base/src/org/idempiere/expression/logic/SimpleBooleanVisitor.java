cfb8118371d48cf8996506d0041f985a027ded4a
hengsin <hengsin@gmail.com>
2021-06-11 17:12:05 +0800

    IDEMPIERE-4824 Boolean Logic Expression Enhancements
    Regenerated source with correct antlr4 version to remove deprecation
    warnings.

--- org.adempiere.base/src/org/idempiere/expression/logic/SimpleBooleanVisitor.java
+++ org.adempiere.base/src/org/idempiere/expression/logic/SimpleBooleanVisitor.java
@@ -1,8 +1,7 @@
-// Generated from SimpleBoolean.g4 by ANTLR 4.4
+// Generated from SimpleBoolean.g4 by ANTLR 4.9.2
 
     package org.idempiere.expression.logic;
 
-import org.antlr.v4.runtime.misc.NotNull;
 import org.antlr.v4.runtime.tree.ParseTreeVisitor;
 
 /**
@@ -14,97 +13,97 @@ import org.antlr.v4.runtime.tree.ParseTreeVisitor;
  */
 public interface SimpleBooleanVisitor<T> extends ParseTreeVisitor<T> {
 	/**
-	 * Visit a parse tree produced by the {@code boolExpression}
-	 * labeled alternative in {@link SimpleBooleanParser#expression}.
-	 * @param ctx the parse tree
-	 * @return the visitor result
-	 */
-	T visitBoolExpression(@NotNull SimpleBooleanParser.BoolExpressionContext ctx);
-	/**
-	 * Visit a parse tree produced by {@link SimpleBooleanParser#bool}.
-	 * @param ctx the parse tree
-	 * @return the visitor result
-	 */
-	T visitBool(@NotNull SimpleBooleanParser.BoolContext ctx);
-	/**
-	 * Visit a parse tree produced by the {@code notExpression}
-	 * labeled alternative in {@link SimpleBooleanParser#expression}.
-	 * @param ctx the parse tree
-	 * @return the visitor result
-	 */
-	T visitNotExpression(@NotNull SimpleBooleanParser.NotExpressionContext ctx);
-	/**
 	 * Visit a parse tree produced by {@link SimpleBooleanParser#parse}.
 	 * @param ctx the parse tree
 	 * @return the visitor result
 	 */
-	T visitParse(@NotNull SimpleBooleanParser.ParseContext ctx);
-	/**
-	 * Visit a parse tree produced by the {@code parenExpression}
-	 * labeled alternative in {@link SimpleBooleanParser#expression}.
-	 * @param ctx the parse tree
-	 * @return the visitor result
-	 */
-	T visitParenExpression(@NotNull SimpleBooleanParser.ParenExpressionContext ctx);
-	/**
-	 * Visit a parse tree produced by the {@code doubleQuotedText}
-	 * labeled alternative in {@link SimpleBooleanParser#expression}.
-	 * @param ctx the parse tree
-	 * @return the visitor result
-	 */
-	T visitDoubleQuotedText(@NotNull SimpleBooleanParser.DoubleQuotedTextContext ctx);
+	T visitParse(SimpleBooleanParser.ParseContext ctx);
 	/**
 	 * Visit a parse tree produced by the {@code binaryExpression}
 	 * labeled alternative in {@link SimpleBooleanParser#expression}.
 	 * @param ctx the parse tree
 	 * @return the visitor result
 	 */
-	T visitBinaryExpression(@NotNull SimpleBooleanParser.BinaryExpressionContext ctx);
-	/**
-	 * Visit a parse tree produced by {@link SimpleBooleanParser#comparator}.
-	 * @param ctx the parse tree
-	 * @return the visitor result
-	 */
-	T visitComparator(@NotNull SimpleBooleanParser.ComparatorContext ctx);
+	T visitBinaryExpression(SimpleBooleanParser.BinaryExpressionContext ctx);
 	/**
 	 * Visit a parse tree produced by the {@code decimalExpression}
 	 * labeled alternative in {@link SimpleBooleanParser#expression}.
 	 * @param ctx the parse tree
 	 * @return the visitor result
 	 */
-	T visitDecimalExpression(@NotNull SimpleBooleanParser.DecimalExpressionContext ctx);
+	T visitDecimalExpression(SimpleBooleanParser.DecimalExpressionContext ctx);
 	/**
-	 * Visit a parse tree produced by {@link SimpleBooleanParser#binary}.
+	 * Visit a parse tree produced by the {@code boolExpression}
+	 * labeled alternative in {@link SimpleBooleanParser#expression}.
 	 * @param ctx the parse tree
 	 * @return the visitor result
 	 */
-	T visitBinary(@NotNull SimpleBooleanParser.BinaryContext ctx);
+	T visitBoolExpression(SimpleBooleanParser.BoolExpressionContext ctx);
 	/**
 	 * Visit a parse tree produced by the {@code contextVariables}
 	 * labeled alternative in {@link SimpleBooleanParser#expression}.
 	 * @param ctx the parse tree
 	 * @return the visitor result
 	 */
-	T visitContextVariables(@NotNull SimpleBooleanParser.ContextVariablesContext ctx);
+	T visitContextVariables(SimpleBooleanParser.ContextVariablesContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code notExpression}
+	 * labeled alternative in {@link SimpleBooleanParser#expression}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitNotExpression(SimpleBooleanParser.NotExpressionContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code parenExpression}
+	 * labeled alternative in {@link SimpleBooleanParser#expression}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitParenExpression(SimpleBooleanParser.ParenExpressionContext ctx);
 	/**
 	 * Visit a parse tree produced by the {@code text}
 	 * labeled alternative in {@link SimpleBooleanParser#expression}.
 	 * @param ctx the parse tree
 	 * @return the visitor result
 	 */
-	T visitText(@NotNull SimpleBooleanParser.TextContext ctx);
+	T visitText(SimpleBooleanParser.TextContext ctx);
 	/**
 	 * Visit a parse tree produced by the {@code quotedText}
 	 * labeled alternative in {@link SimpleBooleanParser#expression}.
 	 * @param ctx the parse tree
 	 * @return the visitor result
 	 */
-	T visitQuotedText(@NotNull SimpleBooleanParser.QuotedTextContext ctx);
+	T visitQuotedText(SimpleBooleanParser.QuotedTextContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code doubleQuotedText}
+	 * labeled alternative in {@link SimpleBooleanParser#expression}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitDoubleQuotedText(SimpleBooleanParser.DoubleQuotedTextContext ctx);
 	/**
 	 * Visit a parse tree produced by the {@code comparatorExpression}
 	 * labeled alternative in {@link SimpleBooleanParser#expression}.
 	 * @param ctx the parse tree
 	 * @return the visitor result
 	 */
-	T visitComparatorExpression(@NotNull SimpleBooleanParser.ComparatorExpressionContext ctx);
+	T visitComparatorExpression(SimpleBooleanParser.ComparatorExpressionContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link SimpleBooleanParser#comparator}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitComparator(SimpleBooleanParser.ComparatorContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link SimpleBooleanParser#binary}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitBinary(SimpleBooleanParser.BinaryContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link SimpleBooleanParser#bool}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitBool(SimpleBooleanParser.BoolContext ctx);
 }
