4363ef43ad4f6df399b3f532e9753de93e8ea8f9
hengsin <hengsin@gmail.com>
2021-06-23 15:58:47 +0800

    IDEMPIERE-4578 generic DocAction
    Refactor generic docaction implementation to a separate delegate class.
    This should make it easier for any model class to implement the
    DocAction interface using the DocActionDelegate class.

--- org.adempiere.base/src/org/adempiere/model/GenericPO.java
+++ org.adempiere.base/src/org/adempiere/model/GenericPO.java
@@ -24,23 +24,12 @@ import java.io.File;
 import java.math.BigDecimal;
 // import for GenericPO
 import java.sql.ResultSet;
-import java.sql.Timestamp;
 import java.util.Properties;
-import java.util.logging.Level;
 
-import org.adempiere.exceptions.PeriodClosedException;
-import org.compiere.model.MDocType;
-import org.compiere.model.MPeriod;
 import org.compiere.model.MTable;
-import org.compiere.model.ModelValidationEngine;
-import org.compiere.model.ModelValidator;
 import org.compiere.model.PO;
 import org.compiere.model.POInfo;
 import org.compiere.process.DocAction;
-import org.compiere.process.DocumentEngine;
-import org.compiere.util.DB;
-import org.compiere.util.TimeUtil;
-import org.compiere.util.Util;
 
 /**
  * Generic PO implementation, this can be use together with ModelValidator as alternative to the classic 
@@ -50,12 +39,14 @@ import org.compiere.util.Util;
  * @author Marco LOMBARDO
  * @contributor Low Heng Sin
  */
-public class GenericPO extends PO implements DocAction {
+public class GenericPO extends PO implements DocAction {	
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = -7571440071937294477L;
-
+	private static final long serialVersionUID = 3180937588404433030L;
+	
+	private DocActionDelegate docActionDelegate = new DocActionDelegate(this);
+	
 	/**
 	 * @param tableName
 	 * @param ctx
@@ -155,8 +146,7 @@ public class GenericPO extends PO implements DocAction {
 	 */
 	@Override
 	public void setDocStatus(String DocStatus) {
-		columnExists(DOC_COLUMNNAME_DocStatus, true);
-		set_Value (DOC_COLUMNNAME_DocStatus, DocStatus);
+		docActionDelegate.setDocStatus(DocStatus);
 	}
 
 	/**
@@ -165,8 +155,7 @@ public class GenericPO extends PO implements DocAction {
 	 */
 	@Override
 	public String getDocStatus() {
-		columnExists(DOC_COLUMNNAME_DocStatus, true);
-		return (String)get_Value(DOC_COLUMNNAME_DocStatus);
+		return docActionDelegate.getDocStatus();
 	}
 
 	/**************************************************************************
@@ -176,15 +165,8 @@ public class GenericPO extends PO implements DocAction {
 	 */
 	@Override
 	public boolean processIt (String processAction) {
-		m_processMsg = null;
-		DocumentEngine engine = new DocumentEngine (this, getDocStatus());
-		return engine.processIt (processAction, getDocAction());
+		return docActionDelegate.processIt(processAction);
 	}	//	process
-
-	/**	Process Message 			*/
-	private String		m_processMsg = null;
-	/**	Just Prepared Flag			*/
-	private boolean		m_justPrepared = false;
 
 	/**
 	 * 	Unlock Document.
@@ -193,28 +175,8 @@ public class GenericPO extends PO implements DocAction {
 	@Override
 	public boolean unlockIt()
 	{
-		if (log.isLoggable(Level.INFO)) log.info("unlockIt - " + toString());
-		setProcessing(false);
-		return true;
+		return docActionDelegate.unlockIt();
 	}	//	unlockIt
-
-	/** Set Process Now.
-		@param Processing Process Now
-	 */
-	public void setProcessing (boolean Processing)
-	{
-		columnExists(DOC_COLUMNNAME_Processing, true);
-		set_Value (DOC_COLUMNNAME_Processing, Boolean.valueOf(Processing));
-	}
-
-	/**
-	 *  Set Document Action.
-	 *  @param DocAction The targeted status of the document
-	 */
-	public void setDocAction (String DocAction) {
-		columnExists(DOC_COLUMNNAME_DocAction, true);
-		set_Value(DOC_COLUMNNAME_DocAction, DocAction);
-	}
 
 	/**
 	 * Get Document Action.
@@ -222,42 +184,7 @@ public class GenericPO extends PO implements DocAction {
 	 */
 	@Override
 	public String getDocAction() {
-		columnExists(DOC_COLUMNNAME_DocAction, true);
-		return (String)get_Value(DOC_COLUMNNAME_DocAction);
-	}
-
-	/**
-	 * Set Approved.
-	 * @param IsApproved Indicates if this document requires approval
-	 */
-	public void setIsApproved (boolean IsApproved) {
-		columnExists(DOC_COLUMNNAME_IsApproved, true);
-		set_ValueNoCheck (DOC_COLUMNNAME_IsApproved, Boolean.valueOf(IsApproved));
-	}
-
-	/**
-	 * Set Processed.
-	 * @param Processed	The document has been processed
-	 */
-	public void setProcessed (boolean Processed) {
-		columnExists(DOC_COLUMNNAME_Processed, true);
-		set_ValueNoCheck (DOC_COLUMNNAME_Processed, Boolean.valueOf(Processed));
-	}
-
-	/**
-	 * Get Approved.
-	 * 	@return Indicates if this document requires approval
-	 */
-	public boolean isApproved() {
-		if (columnExists(DOC_COLUMNNAME_IsApproved)) {
-			Object oo = get_Value(DOC_COLUMNNAME_IsApproved);
-			if (oo != null) {
-				if (oo instanceof Boolean) 
-					return ((Boolean)oo).booleanValue(); 
-				return "Y".equals(oo);
-			}
-		}
-		return true;
+		return docActionDelegate.getDocAction();
 	}
 
 	/**
@@ -267,9 +194,7 @@ public class GenericPO extends PO implements DocAction {
 	@Override
 	public boolean invalidateIt()
 	{
-		if (log.isLoggable(Level.INFO)) log.info("invalidateIt - " + toString());
-		setDocAction(DocAction.ACTION_Prepare);
-		return true;
+		return docActionDelegate.invalidateIt();
 	}	//	invalidateIt
 
 	/**
@@ -278,37 +203,7 @@ public class GenericPO extends PO implements DocAction {
 	 */
 	@Override
 	public String prepareIt() {
-		if (log.isLoggable(Level.INFO)) log.info(toString());
-		m_processMsg = ModelValidationEngine.get().fireDocValidate(this, ModelValidator.TIMING_BEFORE_PREPARE);
-		if (m_processMsg != null)
-			return DocAction.STATUS_Invalid;
-
-		int doctype = -1;
-		if (columnExists(DOC_COLUMNNAME_C_DocTypeTarget_ID)) {
-			doctype = get_ValueAsInt(DOC_COLUMNNAME_C_DocTypeTarget_ID);
-		} else if (columnExists(DOC_COLUMNNAME_C_DocType_ID)) {
-			doctype = get_ValueAsInt(DOC_COLUMNNAME_C_DocType_ID);
-		}
-		Timestamp date = null;
-		if (columnExists(DOC_COLUMNNAME_DateAcct)) {
-			date = (Timestamp) get_Value(DOC_COLUMNNAME_DateAcct);
-		} else if (columnExists(DOC_COLUMNNAME_DateTrx)) {
-			date = (Timestamp) get_Value(DOC_COLUMNNAME_DateTrx);
-		} else {
-			date = TimeUtil.getDay(0);
-		}
-		if (doctype >= 0) {
-			MPeriod.testPeriodOpen(getCtx(), date, doctype, getAD_Org_ID());
-		}
-
-		m_processMsg = ModelValidationEngine.get().fireDocValidate(this, ModelValidator.TIMING_AFTER_PREPARE);
-		if (m_processMsg != null)
-			return DocAction.STATUS_Invalid;
-
-		m_justPrepared = true;
-		if (!DocAction.ACTION_Complete.equals(getDocAction()))
-			setDocAction(DocAction.ACTION_Complete);
-		return DocAction.STATUS_InProgress;
+		return docActionDelegate.prepareIt();
 	}	//	prepareIt
 
 	/**
@@ -317,9 +212,7 @@ public class GenericPO extends PO implements DocAction {
 	 */
 	@Override
 	public boolean approveIt() {
-		if (log.isLoggable(Level.INFO)) log.info(toString());
-		setIsApproved(true);
-		return true;
+		return docActionDelegate.approveIt();
 	}	//	approveIt
 
 	/**
@@ -328,9 +221,7 @@ public class GenericPO extends PO implements DocAction {
 	 */
 	@Override
 	public boolean rejectIt() {
-		if (log.isLoggable(Level.INFO)) log.info(toString());
-		setIsApproved(false);
-		return true;
+		return docActionDelegate.rejectIt();
 	}	//	rejectIt
 
 	/**
@@ -339,78 +230,8 @@ public class GenericPO extends PO implements DocAction {
 	 */
 	@Override
 	public String completeIt() {
-		//	Re-Check
-		if (!m_justPrepared)
-		{
-			String status = prepareIt();
-			m_justPrepared = false;
-			if (!DocAction.STATUS_InProgress.equals(status))
-				return status;
-		}
-
-		// Set the definite document number after completed (if needed)
-		setDefiniteDocumentNo();
-
-		m_processMsg = ModelValidationEngine.get().fireDocValidate(this, ModelValidator.TIMING_BEFORE_COMPLETE);
-		if (m_processMsg != null)
-			return DocAction.STATUS_Invalid;
-
-		//	Implicit Approval
-		if (!isApproved())
-			approveIt();
-		if (log.isLoggable(Level.INFO)) log.info(toString());
-
-		//	User Validation
-		String valid = ModelValidationEngine.get().fireDocValidate(this, ModelValidator.TIMING_AFTER_COMPLETE);
-		if (valid != null)
-		{
-			m_processMsg = valid;
-			return DocAction.STATUS_Invalid;
-		}
-
-		setProcessed(true);
-		setDocAction(DocAction.ACTION_Close);
-		return DocAction.STATUS_Completed;
+		return docActionDelegate.completeIt();
 	}	//	completeIt
-
-	/**
-	 * 	Set the definite document number after completed
-	 */
-	private void setDefiniteDocumentNo() {
-		int doctype = -1;
-		if (columnExists(DOC_COLUMNNAME_C_DocType_ID)) {
-			doctype = get_ValueAsInt(DOC_COLUMNNAME_C_DocType_ID);
-		}
-		Timestamp dateacct = null;
-		if (columnExists(DOC_COLUMNNAME_DateAcct)) {
-			dateacct = (Timestamp) get_Value(DOC_COLUMNNAME_DateAcct);
-		}
-		Timestamp datetrx = null;
-		if (columnExists(DOC_COLUMNNAME_DateTrx)) {
-			datetrx = (Timestamp) get_Value(DOC_COLUMNNAME_DateTrx);
-		}
-		if (doctype >= 0) {
-			MPeriod.testPeriodOpen(getCtx(), (dateacct != null ? dateacct : datetrx), doctype, getAD_Org_ID());
-			MDocType dt = MDocType.get(doctype);
-			if (dt.isOverwriteDateOnComplete()) {
-				if (columnExists(DOC_COLUMNNAME_DateTrx)) {
-					set_Value(DOC_COLUMNNAME_DateTrx, TimeUtil.getDay(0));
-				}
-				if (dateacct.before(datetrx)) {
-					set_Value(DOC_COLUMNNAME_DateAcct, datetrx);
-					MPeriod.testPeriodOpen(getCtx(), datetrx, doctype, getAD_Org_ID());
-				}
-			}
-			if (dt.isOverwriteSeqOnComplete()) {
-				if (columnExists(DOC_COLUMNNAME_DocumentNo)) {
-					String value = DB.getDocumentNo(doctype, get_TrxName(), true, this);
-					if (value != null)
-						set_Value(DOC_COLUMNNAME_DocumentNo, value);
-				}
-			}
-		}
-
-	}
 
 	/**
 	 * 	Void Document.
@@ -419,62 +240,7 @@ public class GenericPO extends PO implements DocAction {
 	@Override
 	public boolean voidIt()
 	{
-		if (log.isLoggable(Level.INFO)) log.info(toString());
-
-		if (   DocAction.STATUS_Closed.equals(getDocStatus())
-			|| DocAction.STATUS_Reversed.equals(getDocStatus())
-			|| DocAction.STATUS_Voided.equals(getDocStatus())) {
-			m_processMsg = "Document Closed: " + getDocStatus();
-			setDocAction(DocAction.ACTION_None);
-			return false;
-		}
-
-		//	Not Processed
-		if (   DocAction.STATUS_Drafted.equals(getDocStatus())
-			|| DocAction.STATUS_Invalid.equals(getDocStatus())
-			|| DocAction.STATUS_InProgress.equals(getDocStatus())
-			|| DocAction.STATUS_Approved.equals(getDocStatus())
-			|| DocAction.STATUS_NotApproved.equals(getDocStatus())) {
-			// Before Void
-			m_processMsg = ModelValidationEngine.get().fireDocValidate(this,ModelValidator.TIMING_BEFORE_VOID);
-			if (m_processMsg != null)
-				return false;
-		} else {
-			boolean accrual = false;
-			int doctype = -1;
-			if (columnExists(DOC_COLUMNNAME_C_DocTypeTarget_ID)) {
-				doctype = get_ValueAsInt(DOC_COLUMNNAME_C_DocTypeTarget_ID);
-			} else if (columnExists(DOC_COLUMNNAME_C_DocType_ID)) {
-				doctype = get_ValueAsInt(DOC_COLUMNNAME_C_DocType_ID);
-			}
-			Timestamp date = null;
-			if (columnExists(DOC_COLUMNNAME_DateAcct)) {
-				date = (Timestamp) get_Value(DOC_COLUMNNAME_DateAcct);
-			} else if (columnExists(DOC_COLUMNNAME_DateTrx)) {
-				date = (Timestamp) get_Value(DOC_COLUMNNAME_DateTrx);
-			} else {
-				date = TimeUtil.getDay(0);
-			}
-			try {
-				MPeriod.testPeriodOpen(getCtx(), date, doctype, getAD_Org_ID());
-			} catch (PeriodClosedException e) {
-				accrual = true;
-			}
-
-			if (accrual)
-				return reverseAccrualIt();
-			else
-				return reverseCorrectIt();
-		}
-
-		// After Void
-		m_processMsg = ModelValidationEngine.get().fireDocValidate(this,ModelValidator.TIMING_AFTER_VOID);
-		if (m_processMsg != null)
-			return false;
-
-		setProcessed(true);
-		setDocAction(DocAction.ACTION_None);
-		return true;
+		return docActionDelegate.voidIt();
 	}	//	voidIt
 
 	/**
@@ -484,20 +250,7 @@ public class GenericPO extends PO implements DocAction {
 	@Override
 	public boolean closeIt()
 	{
-		if (log.isLoggable(Level.INFO)) log.info(toString());
-		// Before Close
-		m_processMsg = ModelValidationEngine.get().fireDocValidate(this,ModelValidator.TIMING_BEFORE_CLOSE);
-		if (m_processMsg != null)
-			return false;
-
-		setProcessed(true);
-		setDocAction(DocAction.ACTION_None);
-
-		// After Close
-		m_processMsg = ModelValidationEngine.get().fireDocValidate(this,ModelValidator.TIMING_AFTER_CLOSE);
-		if (m_processMsg != null)
-			return false;
-		return true;
+		return docActionDelegate.closeIt();
 	}	//	closeIt
 
 	/**
@@ -507,18 +260,7 @@ public class GenericPO extends PO implements DocAction {
 	@Override
 	public boolean reverseCorrectIt()
 	{
-		if (log.isLoggable(Level.INFO)) log.info(toString());
-		// Before reverseCorrect
-		m_processMsg = ModelValidationEngine.get().fireDocValidate(this,ModelValidator.TIMING_BEFORE_REVERSECORRECT);
-		if (m_processMsg != null)
-			return false;
-
-		// After reverseCorrect
-		m_processMsg = ModelValidationEngine.get().fireDocValidate(this,ModelValidator.TIMING_AFTER_REVERSECORRECT);
-		if (m_processMsg != null)
-			return false;
-
-		return false; // reverse needs to be implemented to create a negative document
+		return docActionDelegate.reverseCorrectIt();
 	}	//	reverseCorrectIt
 
 	/**
@@ -528,18 +270,7 @@ public class GenericPO extends PO implements DocAction {
 	@Override
 	public boolean reverseAccrualIt()
 	{
-		if (log.isLoggable(Level.INFO)) log.info(toString());
-		// Before reverseAccrual
-		m_processMsg = ModelValidationEngine.get().fireDocValidate(this,ModelValidator.TIMING_BEFORE_REVERSEACCRUAL);
-		if (m_processMsg != null)
-			return false;
-
-		// After reverseAccrual
-		m_processMsg = ModelValidationEngine.get().fireDocValidate(this,ModelValidator.TIMING_AFTER_REVERSEACCRUAL);
-		if (m_processMsg != null)
-			return false;
-
-		return false; // reverse needs to be implemented to create a negative document
+		return docActionDelegate.reverseAccrualIt();
 	}	//	reverseAccrualIt
 
 	/**
@@ -549,20 +280,7 @@ public class GenericPO extends PO implements DocAction {
 	@Override
 	public boolean reActivateIt()
 	{
-		if (log.isLoggable(Level.INFO)) log.info(toString());
-		// Before reActivate
-		m_processMsg = ModelValidationEngine.get().fireDocValidate(this,ModelValidator.TIMING_BEFORE_REACTIVATE);
-		if (m_processMsg != null)
-			return false;	
-
-		// After reActivate
-		m_processMsg = ModelValidationEngine.get().fireDocValidate(this,ModelValidator.TIMING_AFTER_REACTIVATE);
-		if (m_processMsg != null)
-			return false;
-
-		setDocAction(DocAction.ACTION_Complete);
-		setProcessed(false);
-		return false; // reactivate needs to be implemented to reverse the effect of complete and post
+		return docActionDelegate.reActivateIt();
 	}	//	reActivateIt
 
 	/*************************************************************************
@@ -571,22 +289,12 @@ public class GenericPO extends PO implements DocAction {
 	 */
 	@Override
 	public String getSummary() {
-		StringBuilder sb = new StringBuilder();
-		sb.append(getDocumentNo());
-		String description = null;
-		if (columnExists(DOC_COLUMNNAME_Description))
-			description = get_ValueAsString(DOC_COLUMNNAME_Description);
-		if (!Util.isEmpty(description))
-			sb.append(" - ").append(description);
-		return sb.toString();
+		return docActionDelegate.getSummary();
 	}	//	getSummary
 
 	@Override
 	public String getDocumentNo() {
-		if (columnExists(DOC_COLUMNNAME_DocumentNo)) {
-			return get_ValueAsString(DOC_COLUMNNAME_DocumentNo);
-		}
-		return null;
+		return docActionDelegate.getDocumentNo();
 	}
 
 	/**
@@ -595,23 +303,12 @@ public class GenericPO extends PO implements DocAction {
 	 */
 	@Override
 	public String getDocumentInfo() {
-		int doctype = -1;
-		if (columnExists(DOC_COLUMNNAME_C_DocTypeTarget_ID)) {
-			doctype = get_ValueAsInt(DOC_COLUMNNAME_C_DocTypeTarget_ID);
-		} else if (columnExists(DOC_COLUMNNAME_C_DocType_ID)) {
-			doctype = get_ValueAsInt(DOC_COLUMNNAME_C_DocType_ID);
-		}
-		MDocType dt = null;
-		if (doctype > 0) {
-			dt = MDocType.get(getCtx(), doctype);
-		}
-		StringBuilder msgreturn = new StringBuilder().append((dt != null ? dt.getNameTrl() : "")).append(" ").append(getDocumentNo());
-		return msgreturn.toString();
+		return docActionDelegate.getDocumentInfo();
 	}	//	getDocumentInfo
 
 	@Override
 	public File createPDF() {
-		return null;
+		return docActionDelegate.createPDF();
 	}
 
 	/**
@@ -620,20 +317,12 @@ public class GenericPO extends PO implements DocAction {
 	 */
 	@Override
 	public String getProcessMsg() {
-		return m_processMsg;
+		return docActionDelegate.getProcessMsg();
 	}	//	getProcessMsg
 
 	@Override
 	public int getDoc_User_ID() {
-		int userid = 0;
-		if (columnExists(DOC_COLUMNNAME_SalesRep_ID)) {
-			userid = get_ValueAsInt(DOC_COLUMNNAME_SalesRep_ID);
-		} else if (columnExists(DOC_COLUMNNAME_AD_User_ID)) {
-			userid = get_ValueAsInt(DOC_COLUMNNAME_AD_User_ID);
-		} else {
-			userid = getCreatedBy();
-		}
-		return userid;
+		return docActionDelegate.getDoc_User_ID();
 	}
 
 	/**
@@ -642,17 +331,13 @@ public class GenericPO extends PO implements DocAction {
 	 */
 	@Override
 	public int getC_Currency_ID() {
-		int currency = 0;
-		if (columnExists(DOC_COLUMNNAME_C_Currency_ID)) {
-			currency = get_ValueAsInt(DOC_COLUMNNAME_C_Currency_ID);
-		}
-		return currency;
+		return docActionDelegate.getC_Currency_ID();
 	}
 
 
 	@Override
 	public BigDecimal getApprovalAmt() {
-		return null;
+		return docActionDelegate.getApprovalAmt();
 	}
 
 } // GenericPO
