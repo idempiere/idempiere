c9c83948d886a4940f62649c4b700bb737b64da7
Lukas <lukas@schoenbeck.de>
2021-08-31 09:10:25 +0200

    Regenerated scripts, added register line and increased the size up to 4k to safely accomodate multiple callouts

--- migration/i8.2z/postgresql/202108301100_IDEMPIERE-1575.sql
+++ migration/i8.2z/postgresql/202108301100_IDEMPIERE-1575.sql
@@ -1,8 +1,11 @@
 -- IDEMPIERE-1575 with regards to IDEMPIERE-4724
--- 30.08.2021, 10:54:12 MESZ
-UPDATE AD_Column SET FieldLength=255,Updated=TO_TIMESTAMP('2021-08-30 10:54:12','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=4704
+-- 31.08.2021, 08:57:56 MESZ
+UPDATE AD_Column SET FieldLength=4000,Updated=TO_TIMESTAMP('2021-08-31 08:57:56','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=4704
 ;
 
--- 30.08.2021, 10:56:36 MESZ
-INSERT INTO t_alter_column values('ad_impformat_row','Callout','VARCHAR(255)',null,'NULL')
+-- 31.08.2021, 08:58:02 MESZ
+INSERT INTO t_alter_column values('ad_impformat_row','Callout','VARCHAR(4000)',null,'NULL')
+;
+
+SELECT register_migration_script('202108301100_IDEMPIERE-1575.sql') FROM dual
 ;
