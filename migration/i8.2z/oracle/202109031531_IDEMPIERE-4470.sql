5c455e8fb9c9c279e1e8dca530a7c8c25f807f31
Carlos Ruiz <carg67@gmail.com>
2021-09-03 15:33:05 +0200

    IDEMPIERE-4470 Window Advanced Search - New operator types: AND Not, Or Not
    Rename migration scripts as suggested by Heng Sin

--- migration/i8.2z/oracle/202109031531_IDEMPIERE-4470.sql
+++ migration/i8.2z/oracle/202109031531_IDEMPIERE-4470.sql
@@ -46,7 +46,6 @@ UPDATE AD_Field SET SeqNo=170,IsDisplayed='Y', Updated=getDate(), UpdatedBy=100 
 UPDATE AD_Field SET DisplayLogic='@DataType@=S', AD_Reference_Value_ID=NULL, AD_Val_Rule_ID=NULL, IsToolbarButton=NULL,Updated=TO_DATE('2021-03-07 21:09:10','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=206602
 ;
 
-SELECT register_migration_script('202103081000_IDEMPIERE-4724.sql') FROM dual
+SELECT register_migration_script('202109031531_IDEMPIERE-4470.sql') FROM dual
 ;
-
 
