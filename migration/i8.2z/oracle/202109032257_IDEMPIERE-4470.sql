482e335f1b68ab3a09ef3268fb65de3e31198693
Carlos Ruiz <carg67@gmail.com>
2021-09-03 22:59:28 +0200

    IDEMPIERE-4470 Window Advanced Search - New operator types: AND Not, Or Not
    Fix naming of migration scripts as suggested by Heng Sin

--- migration/i8.2z/oracle/202109032257_IDEMPIERE-4470.sql
+++ migration/i8.2z/oracle/202109032257_IDEMPIERE-4470.sql
@@ -10,5 +10,6 @@ INSERT INTO AD_Message (MsgType,MsgText,AD_Client_ID,AD_Org_ID,IsActive,Created,
 INSERT INTO AD_Message (MsgType,MsgText,AD_Client_ID,AD_Org_ID,IsActive,Created,CreatedBy,Updated,UpdatedBy,AD_Message_ID,Value,EntityType,AD_Message_UU) VALUES ('I','OR NOT',0,0,'Y',TO_DATE('2020-07-17 09:35:20','YYYY-MM-DD HH24:MI:SS'),100,TO_DATE('2020-07-17 09:35:20','YYYY-MM-DD HH24:MI:SS'),100,200622,'ORNOT','D','dc113e24-35bc-4736-8de0-22a036e9f01b')
 ;
 
-SELECT register_migration_script('202007171100_IDEMPIERE-4364.sql') FROM dual
+SELECT register_migration_script('202109032257_IDEMPIERE-4470.sql') FROM dual
 ;
+
