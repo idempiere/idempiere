7090d064892cc6b5a0d7deca280b427608c2aa8d
Nicolas Micoud <58596990+nmicoud@users.noreply.github.com>
2021-09-15 14:14:45 +0200

    IDEMPIERE-4946 : Add IsShowInDashboard, SeqNo and ImageURL on AD_UserDef_Info - fix migration scripts name

--- migration/i8.2z/oracle/202109061500_IDEMPIERE-4946.sql
+++ migration/i8.2z/oracle/202109061500_IDEMPIERE-4946.sql
@@ -58,5 +58,5 @@ UPDATE AD_Column SET AD_Reference_ID=17, AD_Reference_Value_ID=319,Updated=TO_DA
 UPDATE AD_Field SET AD_Reference_Value_ID=NULL, AD_Val_Rule_ID=NULL, XPosition=1, IsToolbarButton=NULL,Updated=TO_DATE('2021-09-06 15:25:38','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Field_ID=206744
 ;
 
-SELECT register_migration_script('202108271700_IDEMPIERE-49346.sql') FROM dual
+SELECT register_migration_script('202109061500_IDEMPIERE-4946.sql') FROM dual
 ;
--- migration/i8.2z/postgresql/202109061500_IDEMPIERE-4946.sql
+++ migration/i8.2z/postgresql/202109061500_IDEMPIERE-4946.sql
@@ -55,5 +55,5 @@ UPDATE AD_Column SET AD_Reference_ID=17, AD_Reference_Value_ID=319,Updated=TO_TI
 UPDATE AD_Field SET AD_Reference_Value_ID=NULL, AD_Val_Rule_ID=NULL, XPosition=1, IsToolbarButton=NULL,Updated=TO_TIMESTAMP('2021-09-06 15:25:38','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Field_ID=206744
 ;
 
-SELECT register_migration_script('202108271700_IDEMPIERE-49346.sql') FROM dual
+SELECT register_migration_script('202109061500_IDEMPIERE-4946.sql') FROM dual
 ;
