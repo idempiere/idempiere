b5484b5c4dd758e1819843d89d4de5c951e855f6
Carlos Ruiz <carg67@gmail.com>
2021-09-21 17:04:43 +0200

    IDEMPIERE-4968 Enable 2Pack to run java class or script (FHCA-3112 FHCA-3113) / Java Shell script -> beanshell/groovy

--- org.adempiere.pipo.handlers/src/org/adempiere/pipo2/handler/JavaShellScriptElementHandler.java
+++ org.adempiere.pipo.handlers/src/org/adempiere/pipo2/handler/JavaShellScriptElementHandler.java
@@ -26,18 +26,15 @@
 
 package org.adempiere.pipo2.handler;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.attribute.PosixFilePermission;
-import java.nio.file.attribute.PosixFilePermissions;
-import java.util.Set;
 import java.util.logging.Level;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptException;
 import javax.xml.transform.sax.TransformerHandler;
 
+import org.adempiere.base.Core;
 import org.adempiere.exceptions.AdempiereException;
 import org.adempiere.pipo2.AbstractElementHandler;
 import org.adempiere.pipo2.Element;
@@ -46,68 +43,55 @@ import org.adempiere.pipo2.PIPOContext;
 import org.adempiere.pipo2.PackOut;
 import org.adempiere.pipo2.PackoutItem;
 import org.compiere.model.MPackageExpDetail;
+import org.compiere.model.MRule;
 import org.compiere.model.X_AD_Package_Imp_Detail;
 import org.compiere.util.Env;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.AttributesImpl;
 
-public class ShellScriptElementHandler extends AbstractElementHandler implements ElementHandler {
+public class JavaShellScriptElementHandler extends AbstractElementHandler implements ElementHandler {
 
-	private static final String SHELL_SCRIPT = "ShellScript";
-	private Path m_tmpfile = null;
+	private static final String JAVA_SHELL_SCRIPT = "JavaShellScript";
 
 	@Override
 	public void startElement(PIPOContext ctx, Element element) throws SAXException {
-		String script = getStringValue(element, SHELL_SCRIPT);
-		// create a temporary file with the contents of the script
-		Set<PosixFilePermission> fp = PosixFilePermissions.fromString("rwx------");
-		try {
-			m_tmpfile = Files.createTempFile("2PackShellScript_", ".sh", PosixFilePermissions.asFileAttribute(fp));
-			Files.write(m_tmpfile, script.getBytes());
-		} catch (IOException e) {
-			throw new AdempiereException(e);
-		}
-
 		X_AD_Package_Imp_Detail impDetail = createImportDetail(ctx, element.qName, "", 0);
 
-		// Execute temporary bash file
-		int exitCode = -1;
-		StringBuilder out = new StringBuilder();
-		StringBuilder err = new StringBuilder();
-		try {
-			String line;
-			ProcessBuilder processBuilder = new ProcessBuilder();
-			processBuilder.command(m_tmpfile.toString());
-			Process process = processBuilder.start();
-			BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
-			BufferedReader stdError = new BufferedReader(new InputStreamReader(process.getErrorStream()));
-			// read the output from the command
-			while ((line = stdInput.readLine()) != null)
-				out.append(line);
-			// read any errors from the attempted command
-			while ((line = stdError.readLine()) != null)
-				err.append(line);
-			exitCode = process.waitFor();
+		String script = getStringValue(element, JAVA_SHELL_SCRIPT);
+
+		String engineName = "beanshell";  // the default
+		if (script.contains("@script:")) {
+			Pattern pattern = Pattern.compile(".*@script:\\b(.*)\\b.*");
+			Matcher matcher = pattern.matcher(script);
+			matcher.find();
+			if (matcher.groupCount() > 0)
+				engineName = matcher.group(1);
 		}
-		catch (IOException | InterruptedException e) {
-			ctx.packIn.getNotifier().addFailureLine("Shell Script failed, error (" + e.getLocalizedMessage() + "):");
-			logImportDetail(ctx, impDetail, 0, SHELL_SCRIPT, exitCode, "Execute");
-			if (out.length() > 0) ctx.packIn.getNotifier().addFailureLine("out -> " + out.toString());
-			if (err.length() > 0) ctx.packIn.getNotifier().addFailureLine("err -> " + err.toString());
-			log.log(Level.SEVERE, SHELL_SCRIPT, e);
+		if (engineName == null || 
+				(!   (engineName.equals("groovy")
+						|| engineName.equals("beanshell")))) {
+			throw new AdempiereException("Script engine not supported, just groovy and beanshell");
+		}
+		ScriptEngine engine = Core.getScriptEngine(engineName);
+		engine.put(MRule.ARGUMENTS_PREFIX + "Ctx", ctx);
+		engine.put(MRule.ARGUMENTS_PREFIX + "Trx", ctx.trx);
+		engine.put(MRule.ARGUMENTS_PREFIX + "TrxName", ctx.trx.getTrxName());
+		engine.put(MRule.ARGUMENTS_PREFIX + "AD_Client_ID", Env.getAD_Client_ID(ctx.ctx));
+		engine.put(MRule.ARGUMENTS_PREFIX + "AD_User_ID", Env.getAD_User_ID(ctx.ctx));
+
+		String msg = null;
+		try {
+			msg = engine.eval(script).toString();
+		} catch (ScriptException e) {
+			ctx.packIn.getNotifier().addFailureLine("Java Shell Script failed, error (" + e.getLocalizedMessage() + "):");
+			logImportDetail(ctx, impDetail, 0, JAVA_SHELL_SCRIPT, -1, "Execute");
+			ctx.packIn.getNotifier().addFailureLine("-> " + msg);
+			log.log(Level.SEVERE, JAVA_SHELL_SCRIPT, e);
 			throw new AdempiereException(e);
 		}
-		if (out.length() > 0 && log.isLoggable(Level.INFO)) log.info("Executed Shell Script with stdout: " + out.toString());
-		if (err.length() > 0 &&  log.isLoggable(Level.WARNING)) log.warning("Executed Shell Script with stderr: " + err.toString());
-		if (exitCode != 0) {
-			logImportDetail(ctx, impDetail, 0, SHELL_SCRIPT, exitCode, "Execute");
-			if (out.length() > 0) ctx.packIn.getNotifier().addFailureLine("out -> " + out.toString());
-			if (err.length() > 0) ctx.packIn.getNotifier().addFailureLine("err -> " + err.toString());
-			throw new AdempiereException("Failed execution of shell script");
-		}
-		logImportDetail(ctx, impDetail, 1, SHELL_SCRIPT, exitCode, "Execute");
-		if (out.length() > 0) ctx.packIn.getNotifier().addSuccessLine("out -> " + out.toString());
-		if (err.length() > 0) ctx.packIn.getNotifier().addSuccessLine("err -> " + err.toString());
+
+		logImportDetail(ctx, impDetail, 1, JAVA_SHELL_SCRIPT, 0, "Execute");
+		ctx.packIn.getNotifier().addSuccessLine("out -> " + msg);
 	}
 
 	@Override
@@ -118,18 +102,18 @@ public class ShellScriptElementHandler extends AbstractElementHandler implements
 		String execCode = Env.getContext(ctx.ctx, MPackageExpDetail.COLUMNNAME_ExecCode);
 		AttributesImpl atts = new AttributesImpl();
 		addTypeName(atts, "custom");
-		document.startElement("","",SHELL_SCRIPT,atts);
+		document.startElement("","",JAVA_SHELL_SCRIPT,atts);
 		createShellScriptBinding(document, execCode);
-		document.endElement("","",SHELL_SCRIPT);
+		document.endElement("","",JAVA_SHELL_SCRIPT);
 	}
 
 	private void createShellScriptBinding( TransformerHandler document, String execCode) throws SAXException {
-		document.startElement("","",SHELL_SCRIPT, new AttributesImpl());
+		document.startElement("","",JAVA_SHELL_SCRIPT, new AttributesImpl());
 		char [] contents = execCode.toCharArray();
 		document.startCDATA();
 		document.characters(contents,0,contents.length);
 		document.endCDATA();
-		document.endElement("","",SHELL_SCRIPT);
+		document.endElement("","",JAVA_SHELL_SCRIPT);
 	}
 
 	@Override
