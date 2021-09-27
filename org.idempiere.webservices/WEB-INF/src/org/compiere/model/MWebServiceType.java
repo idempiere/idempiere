338fecb344696a7909e0c71ea8827863f84fb2c6
hengsin <hengsin@gmail.com>
2021-09-27 18:50:59 +0800

    IDEMPIERE-4980 Update model factory of org.idempiere.webservices (#899)

--- org.idempiere.webservices/WEB-INF/src/org/compiere/model/MWebServiceType.java
+++ org.idempiere.webservices/WEB-INF/src/org/compiere/model/MWebServiceType.java
@@ -48,6 +48,7 @@ import org.idempiere.cache.ImmutablePOSupport;
  *  @author Carlos Ruiz
  *  @author Deepak Pansheriya - Updated to support CreateUpdate service
  */
+@org.adempiere.base.Model(table = X_WS_WebServiceType.Table_Name)
 public class MWebServiceType extends X_WS_WebServiceType implements ImmutablePOSupport
 {
 	/**
