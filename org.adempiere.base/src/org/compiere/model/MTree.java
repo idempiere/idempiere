85984e346dafa978c51f293b97130fd8c66b3222
HideakiHagiwara <58879487+HideakiHagiwara@users.noreply.github.com>
2021-10-21 16:48:09 +0900

    IDEMPIERE-5008 Performance improvement of "NF3.0 Tree On Any Table". (#938)
    Co-authored-by: jpiere <h.hagiwara@compiere-distribution-lab.net>

--- org.adempiere.base/src/org/compiere/model/MTree.java
+++ org.adempiere.base/src/org/compiere/model/MTree.java
@@ -281,7 +281,7 @@ public class MTree extends MTree_Base
 		try
 		{
 			// load Node details - addToTree -> getNodeDetail
-			getNodeDetails(); 
+			getNodeDetails(linkColName, linkID); 
 			//
 			pstmt = DB.prepareStatement(sql.toString(), get_TrxName());
 			int idx = 1;
@@ -445,7 +445,7 @@ public class MTree extends MTree_Base
 	 *  - Node_ID
 	 *  The SQL contains security/access control
 	 */
-	private void getNodeDetails ()
+	private void getNodeDetails (String linkColName, int linkID)
 	{
 		//  SQL for Node Info
 		StringBuilder sqlNode = new StringBuilder();
@@ -506,7 +506,19 @@ public class MTree extends MTree_Base
 			sqlNode.append("t.Description,t.IsSummary,").append(color)
 			.append(" FROM ").append(tableName).append(" t ");
 			if (!m_editable)
-			sqlNode.append(" WHERE t.IsActive='Y'");
+			{
+				if (Util.isEmpty(linkColName) || linkID==0 )
+					sqlNode.append(" WHERE t.IsActive='Y'");
+				else
+					sqlNode.append(" WHERE t.IsActive='Y' AND t.").append(linkColName).append("=").append(linkID);
+
+			}else {
+
+				if (!Util.isEmpty(linkColName) && linkID > 0)
+					sqlNode.append(" WHERE t.").append(linkColName).append("=").append(linkID);
+
+			}
+			
 		}  else if (isValueDisplayed()) {
 			sqlNode.append("SELECT t.").append(columnNameX)
 			.append("_ID, t.Value || ' - ' || t.Name, t.Description, t.IsSummary,").append(color)
