1687a4956a233867d263b6dfcb36945aeaa451e3
hengsin <hengsin@gmail.com>
2021-11-01 05:29:59 +0800

    IDEMPIERE-4472 Window Advanced Search - Allow select column from window tabs - 2pack compatibility
    minor

--- org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/window/FindWindow.java
+++ org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/window/FindWindow.java
@@ -727,8 +727,7 @@ public class FindWindow extends Window implements EventListener<Event>, ValueCha
         winAdvanced = new Window();
         winLookupRecord = new Window();
         Tabpanel tabPanel = new Tabpanel();
-//        tabPanel.setStyle("height: 100%; width: 100%;");
-        tabPanel.setStyle("height: 100%; width: 100%; padding-right: 2px; margin: auto;");
+        tabPanel.setStyle("height: 100%; width: 100%;");
         tabPanel.appendChild(winLookupRecord);
         tabPanel.setId("simpleSearch");
         winMain.addTab(tabPanel, Msg.getMsg(Env.getCtx(), "Find").replaceAll("&", ""),false, true);
