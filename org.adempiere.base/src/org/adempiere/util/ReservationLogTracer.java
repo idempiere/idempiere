3183eb62f1cfc152eed6b771faaa25702f987706
hengsin <hengsin@gmail.com>
2021-11-02 20:16:10 +0800

    IDEMPIERE-5021 Implement Storage Reservation Log
    update javadoc and license header

--- org.adempiere.base/src/org/adempiere/util/ReservationLogTracer.java
+++ org.adempiere.base/src/org/adempiere/util/ReservationLogTracer.java
@@ -1,6 +1,27 @@
-/**
- * 
- */
+/***********************************************************************
+ * This file is part of iDempiere ERP Open Source                      *
+ * http://www.idempiere.org                                            *
+ *                                                                     *
+ * Copyright (C) Contributors                                          *
+ *                                                                     *
+ * This program is free software; you can redistribute it and/or       *
+ * modify it under the terms of the GNU General Public License         *
+ * as published by the Free Software Foundation; either version 2      *
+ * of the License, or (at your option) any later version.              *
+ *                                                                     *
+ * This program is distributed in the hope that it will be useful,     *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of      *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the        *
+ * GNU General Public License for more details.                        *
+ *                                                                     *
+ * You should have received a copy of the GNU General Public License   *
+ * along with this program; if not, write to the Free Software         *
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,          *
+ * MA 02110-1301, USA.                                                 *
+ *                                                                     *
+ * Contributors:                                                       *
+ * - hengsin                         								   *
+ **********************************************************************/
 package org.adempiere.util;
 
 import java.math.BigDecimal;
@@ -27,6 +48,16 @@ public class ReservationLogTracer implements IReservationTracer {
 
 	/**
 	 * 
+	 * @param C_DocType_ID
+	 * @param documentNo
+	 * @param lineNo
+	 * @param AD_Table_ID
+	 * @param Record_ID
+	 * @param M_Warehouse_ID
+	 * @param M_Product_ID
+	 * @param M_AttributeSetInstance_ID
+	 * @param IsSOTrx
+	 * @param trxName
 	 */
 	public ReservationLogTracer(int C_DocType_ID, String documentNo, int lineNo, 
 			int AD_Table_ID, int Record_ID, int M_Warehouse_ID, int M_Product_ID, 
