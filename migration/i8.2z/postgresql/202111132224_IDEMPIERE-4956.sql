c00d68d8a9d36aa3ba4e0444dc5abc375f3180bc
Tony Snook <tonyasnook@outlook.com>
2021-11-14 21:57:27 +1100

    IDEMPIERE-4956 fix unmatched quotes in displaylogic

---
+++ migration/i8.2z/postgresql/202111132224_IDEMPIERE-4956.sql
@@ -0,0 +1,13 @@
+﻿-- Fix quotes in displaylogic
+
+UPDATE AD_Field SET DisplayLogic='@IsDropShip@=''Y''', Updated=statement_timestamp(), UpdatedBy=100 WHERE DisplayLogic='@IsDropShip@=Y''' AND AD_Field_ID = 55427
+;
+
+UPDATE AD_Field SET DisplayLogic='@IsDropShip@=''Y''', Updated=statement_timestamp(), UpdatedBy=100 WHERE DisplayLogic='@IsDropShip@=Y''' AND AD_Field_ID = 57734
+;
+
+UPDATE AD_Field SET DisplayLogic='@A_SourceType@=''MAN''', Updated=statement_timestamp(), UpdatedBy=100 WHERE DisplayLogic='@A_SourceType@=''MAN''''' AND AD_Field_ID = 59348
+;
+
+SELECT Register_Migration_Script ('202111132224_IDEMPIERE-4956.sql') FROM DUAL
+;
