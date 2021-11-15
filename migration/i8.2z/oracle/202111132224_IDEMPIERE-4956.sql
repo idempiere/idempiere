c8ae1e9f1dea4fa9b54d560a5bc41d776a14834c
Tony Snook <tonyasnook@outlook.com>
2021-11-16 10:55:38 +1100

    IDEMPIERE-4956 fix additional logic fields

--- migration/i8.2z/oracle/202111132224_IDEMPIERE-4956.sql
+++ migration/i8.2z/oracle/202111132224_IDEMPIERE-4956.sql
@@ -24,5 +24,17 @@ UPDATE AD_Field SET DisplayLogic='@Type@=''D''|@Type@=''SQL''', Updated=getDate(
 UPDATE AD_Column SET ReadOnlyLogic=NULL, Updated=getDate(), UpdatedBy=100 WHERE ReadOnlyLogic='N' AND AD_Column_ID = 200452
 ;
 
+UPDATE AD_Field SET DisplayLogic='@$Element_PJ@=Y & (@C_Invoice_ID@=0 | @C_Invoice_ID@='''')', Updated=getDate(), UpdatedBy=100 WHERE DisplayLogic='@$Element_PJ@=Y & @C_Invoice_ID@=0 | @C_Invoice_ID@=''''' AND AD_Field_ID IN (200705,6551)
+;
+
+UPDATE AD_Field SET DisplayLogic='@OrderType@=''OB'' | @OrderType@=''SO'' | @Processed@=''Y''', Updated=getDate(), UpdatedBy=100 WHERE DisplayLogic='@DirectShip@=''N'' & @OrderType@=''OB'' | @OrderType@=''SO'' | @Processed@=''Y''' AND AD_Field_ID IN (1129,62343)
+;
+
+UPDATE AD_Field SET DisplayLogic='@IsSummary@=''N'' & (@ProductType@=I | @ProductType@=S)', Updated=getDate(), UpdatedBy=100 WHERE DisplayLogic='@IsSummary@=''N'' & @ProductType@=I | @ProductType@=S'	AND AD_Field_ID IN (3743,11782,59804,53592,53890)
+;
+
+UPDATE AD_Field SET DisplayLogic='1=0', Updated=getDate(), UpdatedBy=100 WHERE DisplayLogic='@DirectShip@=''N''' AND AD_Field_ID = 3388
+;
+
 SELECT Register_Migration_Script ('202111132224_IDEMPIERE-4956.sql') FROM DUAL
 ;
--- migration/i8.2z/postgresql/202111132224_IDEMPIERE-4956.sql
+++ migration/i8.2z/postgresql/202111132224_IDEMPIERE-4956.sql
@@ -21,5 +21,17 @@ UPDATE AD_Field SET DisplayLogic='@Type@=''D''|@Type@=''SQL''', Updated=statemen
 UPDATE AD_Column SET ReadOnlyLogic=NULL, Updated=statement_timestamp(), UpdatedBy=100 WHERE ReadOnlyLogic='N' AND AD_Column_ID = 200452
 ;
 
+UPDATE AD_Field SET DisplayLogic='@$Element_PJ@=Y & (@C_Invoice_ID@=0 | @C_Invoice_ID@='''')', Updated=statement_timestamp(), UpdatedBy=100 WHERE DisplayLogic='@$Element_PJ@=Y & @C_Invoice_ID@=0 | @C_Invoice_ID@=''''' AND AD_Field_ID IN (200705,6551)
+;
+
+UPDATE AD_Field SET DisplayLogic='@OrderType@=''OB'' | @OrderType@=''SO'' | @Processed@=''Y''', Updated=statement_timestamp(), UpdatedBy=100 WHERE DisplayLogic='@DirectShip@=''N'' & @OrderType@=''OB'' | @OrderType@=''SO'' | @Processed@=''Y''' AND AD_Field_ID IN (1129,62343)
+;
+
+UPDATE AD_Field SET DisplayLogic='@IsSummary@=''N'' & (@ProductType@=I | @ProductType@=S)', Updated=statement_timestamp(), UpdatedBy=100 WHERE DisplayLogic='@IsSummary@=''N'' & @ProductType@=I | @ProductType@=S'	AND AD_Field_ID IN (3743,11782,59804,53592,53890)
+;
+
+UPDATE AD_Field SET DisplayLogic='1=0', Updated=statement_timestamp(), UpdatedBy=100 WHERE DisplayLogic='@DirectShip@=''N''' AND AD_Field_ID = 3388
+;
+
 SELECT Register_Migration_Script ('202111132224_IDEMPIERE-4956.sql') FROM DUAL
 ;
