712cbfc3467a91780e11e57d5c01d541c1dec2bc
Carlos Ruiz <carg67@gmail.com>
2021-11-16 15:24:15 +0100

    IDEMPIERE-2620 Customer Drop Ships not implemented / fix help

--- migration/i8.2/postgresql/202111161449_IDEMPIERE-2620.sql
+++ migration/i8.2/postgresql/202111161449_IDEMPIERE-2620.sql
@@ -32,7 +32,29 @@ GROUP BY o.AD_Client_ID, o.AD_Org_ID, o.C_BPartner_ID, o.C_Order_ID,
 	o.DocumentNo, o.DateOrdered, o.C_DocType_ID,
     o.POReference, o.Description, o.SalesRep_ID, l.M_Warehouse_ID;
 
+-- Nov 16, 2021, 3:13:12 PM CET
+UPDATE AD_Element SET Description='Drop Shipments are sent directly to the Drop Shipment Location', Help='Drop Shipments are sent directly to the Drop Shipment Location using the Drop Ship Business Partner name and contact.',Updated=TO_TIMESTAMP('2021-11-16 15:13:12','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=2466
+;
 
+-- Nov 16, 2021, 3:13:12 PM CET
+UPDATE AD_Column SET ColumnName='IsDropShip', Name='Drop Shipment', Description='Drop Shipments are sent directly to the Drop Shipment Location', Help='Drop Shipments are sent directly to the Drop Shipment Location using the Drop Ship Business Partner name and contact.', Placeholder=NULL WHERE AD_Element_ID=2466
+;
+
+-- Nov 16, 2021, 3:13:12 PM CET
+UPDATE AD_Process_Para SET ColumnName='IsDropShip', Name='Drop Shipment', Description='Drop Shipments are sent directly to the Drop Shipment Location', Help='Drop Shipments are sent directly to the Drop Shipment Location using the Drop Ship Business Partner name and contact.', AD_Element_ID=2466 WHERE UPPER(ColumnName)='ISDROPSHIP' AND IsCentrallyMaintained='Y' AND AD_Element_ID IS NULL
+;
+
+-- Nov 16, 2021, 3:13:12 PM CET
+UPDATE AD_Process_Para SET ColumnName='IsDropShip', Name='Drop Shipment', Description='Drop Shipments are sent directly to the Drop Shipment Location', Help='Drop Shipments are sent directly to the Drop Shipment Location using the Drop Ship Business Partner name and contact.', Placeholder=NULL WHERE AD_Element_ID=2466 AND IsCentrallyMaintained='Y'
+;
+
+-- Nov 16, 2021, 3:13:12 PM CET
+UPDATE AD_InfoColumn SET ColumnName='IsDropShip', Name='Drop Shipment', Description='Drop Shipments are sent directly to the Drop Shipment Location', Help='Drop Shipments are sent directly to the Drop Shipment Location using the Drop Ship Business Partner name and contact.', Placeholder=NULL WHERE AD_Element_ID=2466 AND IsCentrallyMaintained='Y'
+;
+
+-- Nov 16, 2021, 3:13:12 PM CET
+UPDATE AD_Field SET Name='Drop Shipment', Description='Drop Shipments are sent directly to the Drop Shipment Location', Help='Drop Shipments are sent directly to the Drop Shipment Location using the Drop Ship Business Partner name and contact.', Placeholder=NULL WHERE AD_Column_ID IN (SELECT AD_Column_ID FROM AD_Column WHERE AD_Element_ID=2466) AND IsCentrallyMaintained='Y'
+;
 
 SELECT register_migration_script('202111161449_IDEMPIERE-2620.sql') FROM dual
 ;
