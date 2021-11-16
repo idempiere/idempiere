b06c39a3b48228121a5fc6cc3afbf3be05d4a057
Carlos Ruiz <carg67@gmail.com>
2021-11-16 15:55:43 +0100

    IDEMPIERE-2620 Customer Drop Ships not implemented / clear display logic in sales order
    In sales order the drop ship doesn't change anything important in the system behavior,
    is more like a printing agreement with the customer,
    so it doesn't make sense to restrict it just to standard orders,
    it can be used in any type of order

--- migration/i8.2/postgresql/202111161449_IDEMPIERE-2620.sql
+++ migration/i8.2/postgresql/202111161449_IDEMPIERE-2620.sql
@@ -56,6 +56,10 @@ UPDATE AD_InfoColumn SET ColumnName='IsDropShip', Name='Drop Shipment', Descript
 UPDATE AD_Field SET Name='Drop Shipment', Description='Drop Shipments are sent directly to the Drop Shipment Location', Help='Drop Shipments are sent directly to the Drop Shipment Location using the Drop Ship Business Partner name and contact.', Placeholder=NULL WHERE AD_Column_ID IN (SELECT AD_Column_ID FROM AD_Column WHERE AD_Element_ID=2466) AND IsCentrallyMaintained='Y'
 ;
 
+-- Nov 16, 2021, 3:51:54 PM CET
+UPDATE AD_Field SET DisplayLogic=NULL, AD_Reference_Value_ID=NULL, AD_Val_Rule_ID=NULL, IsToolbarButton=NULL,Updated=TO_TIMESTAMP('2021-11-16 15:51:54','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=10124
+;
+
 SELECT register_migration_script('202111161449_IDEMPIERE-2620.sql') FROM dual
 ;
 
