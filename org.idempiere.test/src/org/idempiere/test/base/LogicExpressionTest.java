7b1c9322f9a669737402a214735b8e644a7fd8a9
hengsin <hengsin@gmail.com>
2021-12-02 09:23:36 +0800

    IDEMPIERE-5066 Order Line Create Production not visible in the process toolbar dropdown (#1021)

--- org.idempiere.test/src/org/idempiere/test/base/LogicExpressionTest.java
+++ org.idempiere.test/src/org/idempiere/test/base/LogicExpressionTest.java
@@ -42,6 +42,7 @@ import org.compiere.model.MTable;
 import org.compiere.model.PO;
 import org.compiere.model.Query;
 import org.compiere.util.DB;
+import org.compiere.util.DefaultEvaluatee;
 import org.compiere.util.Env;
 import org.compiere.util.Evaluatee;
 import org.compiere.util.LegacyLogicEvaluator;
@@ -574,6 +575,25 @@ public class LogicExpressionTest  extends AbstractTestCase {
 		assertTrue(LegacyLogicEvaluator.evaluateLogic(evaluatee, expr));
 	}
 
+	@Test
+	public void testNestedProperty() {
+		String expr = "@Processed@=Y & @M_Product_ID.IsBOM@=Y";
+		Env.setContext(Env.getCtx(), 1, "Processed", (String)null);
+		assertFalse(LegacyLogicEvaluator.evaluateLogic(new DefaultEvaluatee(null, 1, 0), expr));
+		
+		int pchair = 133;
+		Env.setContext(Env.getCtx(), 1, "Processed", "Y");
+		Env.setContext(Env.getCtx(), 1, "M_Product_ID", pchair);
+		assertTrue(LegacyLogicEvaluator.evaluateLogic(new DefaultEvaluatee(null, 1, 0), expr));
+		
+		Env.setContext(Env.getCtx(), 1, "Processed", (String)null);
+		assertFalse(LogicEvaluator.evaluateLogic(new DefaultEvaluatee(null, 1, 0), expr));
+		
+		Env.setContext(Env.getCtx(), 1, "Processed", "Y");
+		Env.setContext(Env.getCtx(), 1, "M_Product_ID", pchair);
+		assertTrue(LogicEvaluator.evaluateLogic(new DefaultEvaluatee(null, 1, 0), expr));
+	}
+	
 	private static class ContextEvaluatee implements Evaluatee {
 
 		@Override
