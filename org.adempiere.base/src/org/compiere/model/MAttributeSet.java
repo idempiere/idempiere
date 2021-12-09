d4dcc587c4ac7de4a3e9daf75e32e2af9fb184a7
hengsin <hengsin@gmail.com>
2021-12-09 19:32:44 +0800

    IDEMPIERE-5095 MAttributeSet: add getMAttributeUse method

--- org.adempiere.base/src/org/compiere/model/MAttributeSet.java
+++ org.adempiere.base/src/org/compiere/model/MAttributeSet.java
@@ -525,4 +525,15 @@ public class MAttributeSet extends X_M_AttributeSet implements ImmutablePOSuppor
 		return this;
 	}
 
+	/**
+	 * 
+	 * @return Arrays of {@link MAttributeUse}
+	 */
+	public MAttributeUse[] getMAttributeUse()
+	{
+		Query query = new Query(getCtx(), I_M_AttributeUse.Table_Name, "M_AttributeSet_ID=?", get_TrxName());
+		List<MAttributeUse> list = query.setOnlyActiveRecords(true).setParameters(get_ID()).list();
+		return list.toArray(new MAttributeUse[0]);
+	}
+	
 }	//	MAttributeSet
