dc698ff6558a18d74e3cdda0597164695fa60365
hengsin <hengsin@gmail.com>
2021-12-14 22:35:04 +0800

    IDEMPIERE-5073 M_InOutLineMA.DateMaterialPolicy should comes from MStorageOnHand or Shipment instead of through user entry (#1027)

---
+++ migration/i8.2z/postgresql/202112050910_IDEMPIERE-5073.sql
@@ -0,0 +1,8 @@
+-- IDEMPIERE-5073 M_InOutLineMA.DateMaterialPolicy should comes from MStorageOnHand or Shipment instead of through user entry
+-- Dec 5, 2021, 5:06:51 PM MYT
+UPDATE AD_Field SET IsReadOnly='Y', AD_Reference_Value_ID=NULL, AD_Val_Rule_ID=NULL, IsToolbarButton=NULL,Updated=TO_TIMESTAMP('2021-12-05 17:06:51','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=201618
+;
+
+SELECT register_migration_script('202112050910_IDEMPIERE-5073.sql') FROM dual
+;
+
