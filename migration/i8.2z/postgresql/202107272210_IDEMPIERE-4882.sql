9181126f2a5c06feb4373809df03a8cfa24bab4f
Carlos Ruiz <carg67@gmail.com>
2021-12-23 10:27:41 +0100

    IDEMPIERE-4882 On Inventory Move, Adding Warehouse and Warehouse To field on header / peer review

--- migration/i8.2z/postgresql/202107272210_IDEMPIERE-4882.sql
+++ migration/i8.2z/postgresql/202107272210_IDEMPIERE-4882.sql
@@ -71,5 +71,21 @@ UPDATE AD_Field SET AD_Reference_Value_ID=NULL, AD_Val_Rule_ID=NULL, SeqNoGrid=9
 UPDATE AD_Column SET Callout='org.compiere.model.CalloutMovement.warehouse',Updated=TO_TIMESTAMP('2021-07-27 12:13:55','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=214565
 ;
 
+-- Nov 15, 2021, 1:05:33 PM CET
+UPDATE AD_Column SET DefaultValue='NULL', Callout=NULL, IsUpdateable='Y',Updated=TO_TIMESTAMP('2021-11-15 13:05:33','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=214565
+;
+
+-- Nov 15, 2021, 1:06:49 PM CET
+UPDATE AD_Column SET AD_Val_Rule_ID=189,Updated=TO_TIMESTAMP('2021-11-15 13:06:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=214565
+;
+
+-- Nov 15, 2021, 1:11:53 PM CET
+UPDATE AD_Column SET ReadOnlyLogic='@SQL=SELECT 1 FROM M_MovementLine WHERE M_Movement_ID=@M_Movement_ID:0@',Updated=TO_TIMESTAMP('2021-11-15 13:11:53','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=214565
+;
+
+-- Nov 15, 2021, 1:13:03 PM CET
+UPDATE AD_Column SET ReadOnlyLogic='@SQL=SELECT 1 FROM M_MovementLine WHERE M_Movement_ID=@M_Movement_ID:0@',Updated=TO_TIMESTAMP('2021-11-15 13:13:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=214566
+;
+
 SELECT register_migration_script('202107272210_IDEMPIERE-4882.sql') FROM dual
 ;
