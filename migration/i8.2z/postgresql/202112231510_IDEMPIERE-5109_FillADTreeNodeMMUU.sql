7514cb7d438b03eee44f49de27ecb1964cdca8c3
Carlos Ruiz <carg67@gmail.com>
2021-12-23 15:12:38 +0100

    IDEMPIERE-5109 Release 9 Horizon
    - Fill UUID in missing AD_TreeNodeMM_UU

---
+++ migration/i8.2z/postgresql/202112231510_IDEMPIERE-5109_FillADTreeNodeMMUU.sql
@@ -0,0 +1,42 @@
+UPDATE ad_treenodemm SET ad_treenodemm_uu='cd4d5683-75dc-4a64-9a08-408af11cb030' WHERE ad_tree_id=10 AND node_id=200178
+;
+
+UPDATE ad_treenodemm SET ad_treenodemm_uu='01018cf7-d98a-463b-811c-10792bc59a1b' WHERE ad_tree_id=10 AND node_id=200179
+;
+
+UPDATE ad_treenodemm SET ad_treenodemm_uu='ffcf07ce-2753-429e-8fcb-aee2d7c1d22d' WHERE ad_tree_id=10 AND node_id=200180
+;
+
+UPDATE ad_treenodemm SET ad_treenodemm_uu='a90806bb-5956-450d-bcb6-a15b84e25c07' WHERE ad_tree_id=10 AND node_id=200182
+;
+
+UPDATE ad_treenodemm SET ad_treenodemm_uu='e8e3d5f5-ef4b-497b-b42a-3b45acdf1ba4' WHERE ad_tree_id=10 AND node_id=200184
+;
+
+UPDATE ad_treenodemm SET ad_treenodemm_uu='1808ab8d-4c91-4ee7-b2d8-f708c0f5ceda' WHERE ad_tree_id=10 AND node_id=200185
+;
+
+UPDATE ad_treenodemm SET ad_treenodemm_uu='53b32c2d-9195-4301-aa6f-f5dc570936a2' WHERE ad_tree_id=10 AND node_id=200186
+;
+
+UPDATE ad_treenodemm SET ad_treenodemm_uu='a10dcc3e-f5d4-4a2e-946a-646756774dea' WHERE ad_tree_id=10 AND node_id=200187
+;
+
+UPDATE ad_treenodemm SET ad_treenodemm_uu='29c090ab-6c5f-4d86-9a00-b6355ba29ed2' WHERE ad_tree_id=10 AND node_id=200188
+;
+
+UPDATE ad_treenodemm SET ad_treenodemm_uu='149259b2-30f6-4e37-b147-c4d60d2024dc' WHERE ad_tree_id=10 AND node_id=200189
+;
+
+UPDATE ad_treenodemm SET ad_treenodemm_uu='f0b44a4b-92be-4025-aad4-be507bd0d9f3' WHERE ad_tree_id=10 AND node_id=200190
+;
+
+UPDATE ad_treenodemm SET ad_treenodemm_uu='3455abf1-cf4b-425d-b028-e483a26532c2' WHERE ad_tree_id=10 AND node_id=200191
+;
+
+UPDATE ad_treenodemm SET ad_treenodemm_uu='041f2711-e5d4-48d6-a053-ca291cdb776a' WHERE ad_tree_id=10 AND node_id=200192
+;
+
+SELECT register_migration_script('202112231510_IDEMPIERE-5109_FillADTreeNodeMMUU.sql') FROM dual
+;
+
