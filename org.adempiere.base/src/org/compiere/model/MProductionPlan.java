c9e16e2fc92a9fd8b4a0209cb331fa7ef2e0442c
hengsin <hengsin@gmail.com>
2022-01-06 13:54:27 +0800

    IDEMPIERE-4849 Nonsensical code in MProduction.createLines()

--- org.adempiere.base/src/org/compiere/model/MProductionPlan.java
+++ org.adempiere.base/src/org/compiere/model/MProductionPlan.java
@@ -122,8 +122,6 @@ public class MProductionPlan extends X_M_ProductionPlan {
 		
 		int M_Warehouse_ID = finishedLocator.getM_Warehouse_ID();
 		
-		int asi = 0;
-
 		// products used in production
 		String sql = " SELECT bl.M_Product_ID, bl.QtyBOM" + " FROM PP_Product_BOMLine bl"
 				+ " JOIN PP_Product_BOM b ON b.PP_Product_BOM_ID = bl.PP_Product_BOM_ID "
@@ -209,7 +207,6 @@ public class MProductionPlan extends X_M_ProductionPlan {
 
 						MProductionLine BOMLine = null;
 						int prevLoc = -1;
-						int previousAttribSet = -1;
 						// Create lines from storage until qty is reached
 						for (int sl = 0; sl < storages.length; sl++) {
 
@@ -218,15 +215,9 @@ public class MProductionPlan extends X_M_ProductionPlan {
 								if (lineQty.compareTo(BOMMovementQty) > 0)
 									lineQty = BOMMovementQty;
 
-
 								int loc = storages[sl].getM_Locator_ID();
-								int slASI = storages[sl].getM_AttributeSetInstance_ID();
-								int locAttribSet = new MAttributeSetInstance(getCtx(), asi,
-										get_TrxName()).getM_AttributeSet_ID();
-
-								// roll up costing attributes if in the same locator
-								if (locAttribSet == 0 && previousAttribSet == 0
-										&& prevLoc == loc) {
+								// same locator
+								if (prevLoc == loc) {
 									BOMLine.setQtyUsed(BOMLine.getQtyUsed()
 											.add(lineQty));
 									BOMLine.setPlannedQty(BOMLine.getQtyUsed());
@@ -241,15 +232,12 @@ public class MProductionPlan extends X_M_ProductionPlan {
 									BOMLine.setM_Locator_ID( loc );
 									BOMLine.setQtyUsed( lineQty);
 									BOMLine.setPlannedQty( lineQty);
-									if ( slASI != 0 && locAttribSet != 0 )  // ie non costing attribute
-										BOMLine.setM_AttributeSetInstance_ID(slASI);
 									BOMLine.saveEx(get_TrxName());
 
 									lineno = lineno + 10;
 									count++;
 								}
 								prevLoc = loc;
-								previousAttribSet = locAttribSet;
 								// enough ?
 								BOMMovementQty = BOMMovementQty.subtract(lineQty);
 								if (BOMMovementQty.signum() == 0)
@@ -261,10 +249,8 @@ public class MProductionPlan extends X_M_ProductionPlan {
 						if (BOMMovementQty.signum() != 0 ) {
 							if (!mustBeStocked)
 							{
-
-								// roll up costing attributes if in the same locator
-								if ( previousAttribSet == 0
-										&& prevLoc == defaultLocator) {
+								//same locator
+								if (prevLoc == defaultLocator) {
 									BOMLine.setQtyUsed(BOMLine.getQtyUsed()
 											.add(BOMMovementQty));
 									BOMLine.setPlannedQty(BOMLine.getQtyUsed());
@@ -273,7 +259,6 @@ public class MProductionPlan extends X_M_ProductionPlan {
 								}
 								// otherwise create new line
 								else {
-
 									BOMLine = new MProductionLine( this );
 									BOMLine.setLine( lineno );
 									BOMLine.setM_Product_ID( BOMProduct_ID );
@@ -285,7 +270,6 @@ public class MProductionPlan extends X_M_ProductionPlan {
 									lineno = lineno + 10;
 									count++;
 								}
-
 							}
 							else
 							{
