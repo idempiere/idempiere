cb84e3c3b78e3220137687f6677bb25ae9125b66
Peter Takács <takacspeti300@gmail.com>
2022-02-22 12:42:48 +0100

    IDEMPIERE-3831 - append style with function ZkCssHelper.appendStyle(component, style)

--- org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/component/WInfoWindowListItemRenderer.java
+++ org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/component/WInfoWindowListItemRenderer.java
@@ -37,7 +37,6 @@ import org.compiere.model.MStyle;
 import org.compiere.util.Env;
 import org.compiere.util.Evaluatee;
 import org.compiere.util.KeyNamePair;
-import org.compiere.util.Util;
 import org.zkoss.zul.Listcell;
 
 public class WInfoWindowListItemRenderer extends WListItemRenderer
@@ -157,15 +156,7 @@ public class WInfoWindowListItemRenderer extends WListItemRenderer
 					String zclass = styleStr.substring(MStyle.ZCLASS_PREFIX.length());
 					listcell.setZclass(zclass);
 				} else {
-					
-					String prevStyle = listcell.getStyle();
-										
-					if(!Util.isEmpty(prevStyle)) {
-						listcell.setStyle(prevStyle + ";" + styleStr);
-					}
-					else {
-						listcell.setStyle(styleStr);
-					}
+					ZkCssHelper.appendStyle(listcell, styleStr);
 				}
 			}
 		}
