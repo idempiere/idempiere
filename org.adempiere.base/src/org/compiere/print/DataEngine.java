ec86fb56503bb206787e645929e4838ebbd9d0f5
Carlos Ruiz <carg67@gmail.com>
2022-04-05 04:35:52 +0200

    Implement @LINE@ script in print format item (#1280)

--- org.adempiere.base/src/org/compiere/print/DataEngine.java
+++ org.adempiere.base/src/org/compiere/print/DataEngine.java
@@ -1305,7 +1305,7 @@ public class DataEngine
 		if (expression == null || expression.length() == 0)
 			return "";
 
-		log.info("Analyzing Expression " + expression);
+		if (log.isLoggable(Level.CONFIG)) log.config("Analyzing Expression " + expression);
 		String token;
 		String inStr = new String(expression);
 		StringBuffer outStr = new StringBuffer();
@@ -1348,6 +1348,17 @@ public class DataEngine
 				if (tokenPDE == null)
 					return "\"Item not found: " + token + "\"";
 				Object value = ((PrintDataElement)tokenPDE).getValue();
+				outStr.append(value);
+			}
+			else if (token.equals("LINE"))
+			{
+				BigDecimal value = Env.ONE;
+				if (m_summarized.containsKey(pdc))
+				{
+					value = ((BigDecimal)m_summarized.get(pdc)).add(value);
+					m_summarized.remove(pdc);
+				}
+				m_summarized.put(pdc, value);
 				outStr.append(value);
 			}
 
