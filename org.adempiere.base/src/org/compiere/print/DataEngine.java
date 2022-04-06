942dc4c1f478509a2ee7bc4b17ff30746398de7e
Peter Takács <takacspeti300@gmail.com>
2022-04-06 10:55:40 +0200

    IDEMPIERE-5248 - inconsistancy fixed

--- org.adempiere.base/src/org/compiere/print/DataEngine.java
+++ org.adempiere.base/src/org/compiere/print/DataEngine.java
@@ -535,8 +535,7 @@ public class DataEngine
 					}
 					// 	TableName.ColumnName,
 					sqlSELECT.append(lookupSQL).append(" AS ").append(ColumnName).append(",");
-					if (!IsGroupFunction)
-						groupByColumns.add(ColumnName); 
+					groupByColumns.add(lookupSQL); 
 					pdc = new PrintDataColumn(AD_PrintFormatItem_ID, AD_Column_ID, ColumnName, AD_Reference_ID, FieldLength, orderName, isPageBreak);
 					synonymNext();
 				}
