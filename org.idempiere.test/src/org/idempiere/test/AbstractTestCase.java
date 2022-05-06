b6191ecee3525e7b2da1246259d43b79f7b15eeb
Carlos Ruiz <carg67@gmail.com>
2022-05-06 11:56:10 +0200

    IDEMPIERE-5063 Improve Unit Tests (#1315)
    * IDEMPIERE-5063 Improve Unit Tests
    
    - revert -> Close DB connections after each unit test class
    - implement a different approach to close the transactions on tearDown even when not active
    
    * Remove Adempiere.stop as suggested by Heng Sin

--- org.idempiere.test/src/org/idempiere/test/AbstractTestCase.java
+++ org.idempiere.test/src/org/idempiere/test/AbstractTestCase.java
@@ -33,7 +33,6 @@ import org.compiere.Adempiere;
 import org.compiere.model.MAcctSchema;
 import org.compiere.model.MClientInfo;
 import org.compiere.model.MRole;
-import org.compiere.util.DB;
 import org.compiere.util.Env;
 import org.compiere.util.Language;
 import org.compiere.util.Trx;
@@ -226,7 +225,5 @@ public abstract class AbstractTestCase {
 	 * shutdown for class
 	 */
 	static void shutdown() {
-		Adempiere.stop();
-		DB.getDatabase().close();
 	}
 }
