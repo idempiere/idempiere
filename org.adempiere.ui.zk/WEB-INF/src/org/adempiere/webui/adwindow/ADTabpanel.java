5a2afa216266fdbfeba7c741630d099b202a3d83
Anozi Mada <anozimada@gmail.com>
2022-05-31 17:40:24 +0700

    IDEMPIERE-4661 Fix Password fields show double context "Field Suggestions" (#1351)

--- org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/adwindow/ADTabpanel.java
+++ org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/adwindow/ADTabpanel.java
@@ -835,7 +835,6 @@ DataStatusListener, IADTabpanel, IdSpace, IFieldEditorContainer
 	        			if (popupMenu == null) 
 	        			{
 	        				popupMenu = new WEditorPopupMenu(false, false, false, false, false, false, null);
-	        				popupMenu.addSuggestion(field);
 	        			}
 	        			if (popupMenu != null)
 	        			{
