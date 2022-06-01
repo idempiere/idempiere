53b79c128b59ae7b7d086a7211410a2287340ed2
Peter Takács <takacspeti300@gmail.com>
2022-06-01 13:28:01 +0200

    IDEMPIERE-4523 - fixed workflow use case

--- org.adempiere.ui/src/org/compiere/apps/AbstractProcessCtl.java
+++ org.adempiere.ui/src/org/compiere/apps/AbstractProcessCtl.java
@@ -167,14 +167,15 @@ public abstract class AbstractProcessCtl implements Runnable
 		 */
 		if (AD_Workflow_ID > 0)	
 		{
-			startWorkflow (AD_Workflow_ID);
 			MPInstance pinstance = new MPInstance(Env.getCtx(), m_pi.getAD_PInstance_ID(), null);
-			String errmsg = m_pi.getSummary();
-			pinstance.setResult(!m_pi.isError());
-			pinstance.setErrorMsg(errmsg);
 			pinstance.setIsProcessing(true);
 			pinstance.saveEx();
 			try {
+				startWorkflow (AD_Workflow_ID);
+				String errmsg = m_pi.getSummary();
+				pinstance.setResult(!m_pi.isError());
+				pinstance.setErrorMsg(errmsg);	
+				pinstance.saveEx();
 				unlock();
 			}
 			finally {
