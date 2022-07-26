9840c72846d3fb71a44288adf74de51b46faf3df
Carlos Ruiz <carg67@gmail.com>
2022-07-26 08:08:32 +0200

    IDEMPIERE-5355 Improve default trx name (FHCA-3765) (#1407)
    * IDEMPIERE-5355 Improve default trx name (FHCA-3765)
    
    * IDEMPIERE-5355 Improve default trx name (FHCA-3765)
    
    Implement suggestions from Heng Sin
    Added SysConfig TRX_AUTOSET_DISPLAY_NAME (default false)
    
    * Delete test/debugging code committed by mistake
    
    * IDEMPIERE-5355 fix method wrongly named

--- org.adempiere.base/src/org/compiere/model/MSysConfig.java
+++ org.adempiere.base/src/org/compiere/model/MSysConfig.java
@@ -44,7 +44,7 @@ public class MSysConfig extends X_AD_SysConfig
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = -3851200335563922376L;
+	private static final long serialVersionUID = -8268386308280263368L;
 
 	public static final String ADDRESS_VALIDATION = "ADDRESS_VALIDATION";
     public static final String ALERT_SEND_ATTACHMENT_AS_XLS = "ALERT_SEND_ATTACHMENT_AS_XLS";
@@ -162,6 +162,7 @@ public class MSysConfig extends X_AD_SysConfig
     public static final String SYSTEM_NATIVE_SEQUENCE = "SYSTEM_NATIVE_SEQUENCE";
     public static final String TOP_MARGIN_PIXELS_FOR_HEADER = "TOP_MARGIN_PIXELS_FOR_HEADER";
     public static final String TRACE_ALL_TRX_CONNECTION_GET = "TRACE_ALL_TRX_CONNECTION_GET";
+	public static final String TRX_AUTOSET_DISPLAY_NAME = "TRX_AUTOSET_DISPLAY_NAME";
     public static final String TWOPACK_COMMIT_DDL = "2PACK_COMMIT_DDL";
     public static final String TWOPACK_HANDLE_TRANSLATIONS = "2PACK_HANDLE_TRANSLATIONS";
     public static final String USE_EMAIL_FOR_LOGIN = "USE_EMAIL_FOR_LOGIN";
