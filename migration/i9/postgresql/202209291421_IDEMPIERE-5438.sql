cdd3922adf39d6f4c9d15680efea821031ccd6a5
Nicolas Micoud <58596990+nmicoud@users.noreply.github.com>
2022-09-29 14:49:03 +0200

    IDEMPIERE-5438: Web Service Security > Access tab should allow to select WS roles only
    Role with RoleType = null can also be used for WS, so must be included in available roles
    
    Co-Authored-By: Carlos Ruiz <carg67@gmail.com>

--- migration/i9/postgresql/202209291421_IDEMPIERE-5438.sql
+++ migration/i9/postgresql/202209291421_IDEMPIERE-5438.sql
@@ -13,3 +13,11 @@ UPDATE AD_Column SET AD_Reference_ID=18, AD_Reference_Value_ID=200215, IsUpdatea
 INSERT INTO AD_Ref_Table (AD_Reference_ID,AD_Table_ID,AD_Key,AD_Display,WhereClause,AD_Client_ID,AD_Org_ID,IsActive,Created,CreatedBy,Updated,UpdatedBy,IsValueDisplayed,EntityType,AD_Ref_Table_UU) VALUES (200215,156,531,531,'AD_Role.RoleType = ''WS''',0,0,'Y',TO_TIMESTAMP('2022-09-29 14:22:42','YYYY-MM-DD HH24:MI:SS'),100,TO_TIMESTAMP('2022-09-29 14:22:42','YYYY-MM-DD HH24:MI:SS'),100,'N','D','27d57bf9-6dd4-426b-a8c9-c0e1a95b44d7')
 ;
 
+-- Sep 29, 2022, 2:44:59 PM CEST
+UPDATE AD_Reference SET Description='Roles with RoleType NULL or WebService',Updated=TO_TIMESTAMP('2022-09-29 14:44:59','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Reference_ID=200215
+;
+
+-- Sep 29, 2022, 2:45:11 PM CEST
+UPDATE AD_Ref_Table SET WhereClause='(AD_Role.RoleType IS NULL OR AD_Role.RoleType = ''WS'')',Updated=TO_TIMESTAMP('2022-09-29 14:45:11','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Reference_ID=200215
+;
+
