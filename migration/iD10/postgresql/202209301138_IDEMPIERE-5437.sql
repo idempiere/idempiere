be6566b9cc1cb50291c559d318915294652c0d6b
Layda Salas <laydasalasc@gmail.com>
2022-09-30 12:23:17 +0200

    IDEMPIERE-5437 Improve description for SysConfig SYSTEM_NATIVE_SEQUENCE #resolve

--- migration/iD10/postgresql/202209301138_IDEMPIERE-5437.sql
+++ migration/iD10/postgresql/202209301138_IDEMPIERE-5437.sql
@@ -2,6 +2,6 @@
 SELECT register_migration_script('202209301138_IDEMPIERE-5437.sql') FROM dual;
 
 -- Sep 30, 2022, 11:38:44 AM CEST
-UPDATE AD_SysConfig SET Description='Please use Enable Native Sequence process to enable. Y - Yes Use Native Sequence , N - No Use iDempiere Sequence (AD_Sequence)',Updated=TO_TIMESTAMP('2022-09-30 11:38:44','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_SysConfig_ID=200013
+UPDATE AD_SysConfig SET Description='Please use Enable Native Sequence process to enable. Y - Yes Use Native Sequence , N - No Use iDempiere Sequence (AD_Sequence)',Updated=TO_TIMESTAMP('2022-09-30 11:38:44','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_SysConfig_ID=50016
 ;
 
