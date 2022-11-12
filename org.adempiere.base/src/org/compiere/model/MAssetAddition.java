56b4bd2bb2d50ea8f40f563f62034a9c7c111141
Carlos Ruiz <carg67@gmail.com>
2022-11-12 12:52:17 +0100

    - set asset value from project committed amount

--- org.adempiere.base/src/org/compiere/model/MAssetAddition.java
+++ org.adempiere.base/src/org/compiere/model/MAssetAddition.java
@@ -236,7 +236,7 @@ public class MAssetAddition extends X_A_Asset_Addition
 		if (project.get_ValueAsInt("C_ConversionType_ID")>0) {
 			setC_ConversionType_ID(project.get_ValueAsInt("C_ConversionType_ID"));
 		}
-		setSourceAmt(project.getProjectBalanceAmt());
+		setSourceAmt(project.getCommittedAmt());
 		setDateDoc(new Timestamp (System.currentTimeMillis()));
 		setA_CreateAsset(true); //added by @win as create from project will certainly for createnew
 	
