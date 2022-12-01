64de4840351580c4325048a4800384f7e36f9e6a
Peter Takács <takacspeti300@gmail.com>
2022-12-01 16:13:10 +0100

    IDEMPIERE-5238 - initial draft commit

---
+++ migration/iD10/oracle/202203221602_IDEMPIERE-5238.sql
@@ -0,0 +1,46 @@
+-- IDEMPIERE-5238
+SELECT register_migration_script('202203221602_IDEMPIERE-5238.sql') FROM dual;
+
+SET SQLBLANKLINES ON
+SET DEFINE OFF
+
+-- Mar 22, 2022, 4:02:23 PM CET
+INSERT INTO AD_Reference (AD_Reference_ID,Name,ValidationType,AD_Client_ID,AD_Org_ID,IsActive,Created,CreatedBy,Updated,UpdatedBy,EntityType,IsOrderByValue,AD_Reference_UU) VALUES (200202,'Related To','D',0,0,'Y',TO_TIMESTAMP('2022-03-22 16:02:23','YYYY-MM-DD HH24:MI:SS'),100,TO_TIMESTAMP('2022-03-22 16:02:23','YYYY-MM-DD HH24:MI:SS'),100,'D','N','55330dab-f2b6-499d-af8b-187848c47a40')
+;
+
+-- Mar 22, 2022, 4:03:11 PM CET
+INSERT INTO AD_Column (AD_Column_ID,Version,Name,Description,Help,AD_Table_ID,ColumnName,FieldLength,IsKey,IsParent,IsMandatory,IsTranslated,IsIdentifier,SeqNo,IsEncrypted,AD_Reference_ID,AD_Client_ID,AD_Org_ID,IsActive,Created,CreatedBy,Updated,UpdatedBy,AD_Element_ID,IsUpdateable,IsSelectionColumn,EntityType,IsSyncDatabase,IsAlwaysUpdateable,IsAutocomplete,IsAllowLogging,AD_Column_UU,IsAllowCopy,SeqNoSelection,IsToolbarButton,IsSecure,FKConstraintType,IsHtml) VALUES (214719,0,'Table','Database Table information','The Database Table provides the information of the table definition',53354,'AD_Table_ID',10,'N','N','N','N','N',0,'N',19,0,0,'Y',TO_TIMESTAMP('2022-03-22 16:03:11','YYYY-MM-DD HH24:MI:SS'),100,TO_TIMESTAMP('2022-03-22 16:03:11','YYYY-MM-DD HH24:MI:SS'),100,126,'Y','N','D','N','N','N','Y','b1ca962e-b18c-4b5e-8a04-a4b861f9289a','Y',0,'N','N','N','N')
+;
+
+-- Mar 22, 2022, 4:03:16 PM CET
+UPDATE AD_Column SET FKConstraintName='ADTable_CContactActivity', FKConstraintType='N',Updated=TO_TIMESTAMP('2022-03-22 16:03:16','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=214719
+;
+
+-- Mar 22, 2022, 4:03:16 PM CET
+ALTER TABLE C_ContactActivity ADD AD_Table_ID NUMBER(10) DEFAULT NULL 
+;
+
+-- Mar 22, 2022, 4:03:16 PM CET
+ALTER TABLE C_ContactActivity ADD CONSTRAINT ADTable_CContactActivity FOREIGN KEY (AD_Table_ID) REFERENCES ad_table(ad_table_id) DEFERRABLE INITIALLY DEFERRED
+;
+
+-- Mar 22, 2022, 4:04:08 PM CET
+INSERT INTO AD_Column (AD_Column_ID,Version,Name,Description,Help,AD_Table_ID,ColumnName,FieldLength,IsKey,IsParent,IsMandatory,IsTranslated,IsIdentifier,SeqNo,IsEncrypted,AD_Reference_ID,AD_Client_ID,AD_Org_ID,IsActive,Created,CreatedBy,Updated,UpdatedBy,AD_Element_ID,IsUpdateable,IsSelectionColumn,EntityType,IsSyncDatabase,IsAlwaysUpdateable,IsAutocomplete,IsAllowLogging,AD_Column_UU,IsAllowCopy,SeqNoSelection,IsToolbarButton,IsSecure,IsHtml) VALUES (214720,0,'Record ID','Direct internal record ID','The Record ID is the internal unique identifier of a record. Please note that zooming to the record may not be successful for Orders, Invoices and Shipment/Receipts as sometimes the Sales Order type is not known.',53354,'Record_ID',22,'N','N','N','N','N',0,'N',200202,0,0,'Y',TO_TIMESTAMP('2022-03-22 16:04:08','YYYY-MM-DD HH24:MI:SS'),100,TO_TIMESTAMP('2022-03-22 16:04:08','YYYY-MM-DD HH24:MI:SS'),100,538,'N','N','D','N','N','N','Y','b6f4fa3f-27dd-4c51-878b-a747d9995cea','Y',0,'N','N','N')
+;
+
+-- Mar 22, 2022, 4:04:12 PM CET
+ALTER TABLE C_ContactActivity ADD Record_ID NUMBER(10) DEFAULT NULL 
+;
+
+-- Mar 22, 2022, 4:11:30 PM CET
+INSERT INTO AD_Field (AD_Field_ID,Name,Description,Help,AD_Tab_ID,AD_Column_ID,IsDisplayed,DisplayLength,SeqNo,SortNo,IsSameLine,IsHeading,IsFieldOnly,IsEncrypted,AD_Client_ID,AD_Org_ID,IsActive,Created,CreatedBy,Updated,UpdatedBy,IsReadOnly,IsCentrallyMaintained,EntityType,AD_Field_UU,IsDisplayedGrid,SeqNoGrid,XPosition,ColumnSpan,NumLines,IsQuickEntry,IsDefaultFocus,IsAdvancedField,IsQuickForm) VALUES (206918,'Record ID','Direct internal record ID','The Record ID is the internal unique identifier of a record. Please note that zooming to the record may not be successful for Orders, Invoices and Shipment/Receipts as sometimes the Sales Order type is not known.',53454,214720,'Y',0,120,0,'N','N','N','N',0,0,'Y',TO_TIMESTAMP('2022-03-22 16:11:30','YYYY-MM-DD HH24:MI:SS'),100,TO_TIMESTAMP('2022-03-22 16:11:30','YYYY-MM-DD HH24:MI:SS'),100,'N','Y','D','b5c7c74b-555f-4dab-a0fe-772bf726bc76','Y',120,1,1,1,'N','N','N','N')
+;
+
+-- Mar 22, 2022, 4:20:12 PM CET
+UPDATE AD_Column SET IsUpdateable='Y',Updated=TO_TIMESTAMP('2022-03-22 16:20:12','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=214720
+;
+
+-- Mar 23, 2022, 9:37:19 AM CET
+INSERT INTO AD_Field (AD_Field_ID,Name,Description,Help,AD_Tab_ID,AD_Column_ID,IsDisplayed,DisplayLength,SeqNo,SortNo,IsSameLine,IsHeading,IsFieldOnly,IsEncrypted,AD_Client_ID,AD_Org_ID,IsActive,Created,CreatedBy,Updated,UpdatedBy,IsReadOnly,IsCentrallyMaintained,EntityType,AD_Field_UU,IsDisplayedGrid,SeqNoGrid,XPosition,ColumnSpan,NumLines,IsQuickEntry,IsDefaultFocus,IsAdvancedField,IsQuickForm) VALUES (206919,'Table','Database Table information','The Database Table provides the information of the table definition',53454,214719,'N',0,130,0,'N','N','N','N',0,0,'Y',TO_TIMESTAMP('2022-03-23 09:37:19','YYYY-MM-DD HH24:MI:SS'),100,TO_TIMESTAMP('2022-03-23 09:37:19','YYYY-MM-DD HH24:MI:SS'),100,'N','Y','D','243d1496-05b5-487f-a000-e5b8e0ab7f00','N',130,1,1,1,'N','N','N','N')
+;
+
---
+++ org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/editor/WRelatedToEditor.java
@@ -0,0 +1,444 @@
+package org.adempiere.webui.editor;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+
+import org.adempiere.webui.ValuePreference;
+import org.adempiere.webui.apps.AEnv;
+import org.adempiere.webui.component.Combobox;
+import org.adempiere.webui.component.ToolBarButton;
+import org.adempiere.webui.event.ContextMenuEvent;
+import org.adempiere.webui.event.ContextMenuListener;
+import org.adempiere.webui.event.ValueChangeEvent;
+import org.adempiere.webui.theme.ThemeManager;
+import org.adempiere.webui.util.GridTabDataBinder;
+import org.adempiere.webui.window.WFieldRecordInfo;
+import org.compiere.model.GridField;
+import org.compiere.model.MColumn;
+import org.compiere.model.MQuery;
+import org.compiere.model.MSysConfig;
+import org.compiere.model.MTable;
+import org.compiere.model.PO;
+import org.compiere.model.Query;
+import org.compiere.util.CLogger;
+import org.compiere.util.DB;
+import org.compiere.util.Env;
+import org.compiere.util.Util;
+import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zk.ui.event.Events;
+import org.zkoss.zul.Comboitem;
+import org.zkoss.zul.Div;
+import org.zkoss.zul.Vlayout;
+
+public class WRelatedToEditor extends WEditor implements ContextMenuListener, IZoomableEditor {
+
+	private static final CLogger log = CLogger.getCLogger(WRelatedToEditor.class);
+	
+	private boolean m_ReadWrite;
+	private Object oldValue;
+	private Object value;
+	private Object oldTableID;
+	private GridTabDataBinder dataBinder;
+	
+	private GridField tableIDGridField;
+	private WEditor tableIDEditor;
+	
+	private Div wrapperDiv;
+	private Combobox recordsCombobox;
+	private ToolBarButton okButton;
+	private ToolBarButton editButton;
+	
+	protected String selectedTableName = "";
+	protected String selectedRecordIdentifyer = "";
+	
+	private static final String IMAGES_CONTEXT_OK_PNG = "images/Ok16.png";
+	private static final String IMAGES_CONTEXT_EDIT_RECORD_PNG = "images/EditRecord16.png";
+	
+	private static final String EDITOR_MODE_EDIT = "e";
+	private static final String EDITOR_MODE_SAVED = "s";
+
+	public WRelatedToEditor(GridField gridField, boolean tableEditor, IEditorConfiguration editorConfiguration) {
+		super(new Vlayout(), gridField, tableEditor, editorConfiguration);
+		init();
+	}
+	
+	private void init() {
+		log.warning("----------");
+		tableIDGridField = gridTab.getField("AD_Table_ID");
+		tableIDEditor = WebEditorFactory.getEditor(tableIDGridField, true);
+		
+		tableIDGridField.addPropertyChangeListener(tableIDEditor);
+		this.dataBinder = new GridTabDataBinder(gridTab);
+		tableIDEditor.addValueChangeListener(dataBinder);
+		tableIDGridField.addPropertyChangeListener(new PropertyChangeListener() {
+
+			@Override
+			public void propertyChange(PropertyChangeEvent evt) {
+				if(evt.getNewValue() != oldTableID) {
+					oldTableID = evt.getNewValue();
+					initRecords();
+					recordsCombobox.setValue(value);
+				}
+			}
+		});
+		
+		wrapperDiv = new Div();
+		wrapperDiv.setStyle("display: flex;");
+		wrapperDiv.setParent(this.getComponent());
+
+		tableIDEditor.getComponent().addEventListener(Events.ON_SELECT, this);
+		wrapperDiv.appendChild(tableIDEditor.getComponent());
+		
+		recordsCombobox = new Combobox();
+		recordsCombobox.setAutocomplete(true);
+		recordsCombobox.setAutodrop(true);
+		recordsCombobox.setParent(wrapperDiv);
+		recordsCombobox.addEventListener(Events.ON_SELECT, this);
+		oldTableID = tableIDGridField.getValue();
+		initRecords();
+		
+		okButton = new ToolBarButton();
+		if (ThemeManager.isUseFontIconForImage())
+			okButton.setIconSclass("z-icon-Ok");
+		else
+			okButton.setImage(ThemeManager.getThemeResource(IMAGES_CONTEXT_OK_PNG));
+		okButton.setParent(wrapperDiv);
+		okButton.addEventListener(Events.ON_CLICK, this);
+		
+		editButton = new ToolBarButton();
+		if (ThemeManager.isUseFontIconForImage())
+			editButton.setIconSclass("z-icon-Edit");
+		else
+			editButton.setImage(ThemeManager.getThemeResource(IMAGES_CONTEXT_EDIT_RECORD_PNG));
+		editButton.setParent(wrapperDiv);
+		editButton.addEventListener(Events.ON_CLICK, this);
+		
+		if (gridField != null) 
+        {
+        	popupMenu = new WEditorPopupMenu(true, true, isShowPreference());
+        	popupMenu.addMenuListener(this);
+        	addChangeLogMenu(popupMenu);
+        }
+		
+		boolean noTableID = ((Integer) tableIDGridField.getValue() == null) || ((Integer) tableIDGridField.getValue() <= 0);
+		boolean noRecordID = ((Integer) gridField.getValue() == null) || ((Integer) gridField.getValue() <= 0);
+		if(noTableID || noRecordID) {
+			updateUI(EDITOR_MODE_EDIT);
+		}
+		else {
+//			tableIDEditor.setValue((Integer) tableIDGridField.getValue());
+//			for(Comboitem item : recordsCombobox.getItems()) {
+//				if(item.getValue() != null && item.getValue().equals((Integer) gridField.getValue())) {
+//					recordsCombobox.setSelectedItem(item);
+//					break;
+//				}
+//			}
+			updateUI(EDITOR_MODE_SAVED);
+//			recordsCombobox.setText(getIdentifierColumns(savedTableID, savedRecordID));
+		}
+		setMandatory();
+	}
+	
+	private void updateUI(String displayMode) {
+		log.warning("----------");
+		if(displayMode.equalsIgnoreCase(EDITOR_MODE_SAVED)) {
+			tableIDEditor.setVisible(false);
+			recordsCombobox.setWidth("100%");
+			recordsCombobox.setReadonly(true);
+			recordsCombobox.setButtonVisible(false);
+			okButton.setVisible(false);
+			editButton.setVisible(true);
+		}
+		else if(displayMode.equalsIgnoreCase(EDITOR_MODE_EDIT)) {
+			tableIDEditor.setVisible(true);
+			recordsCombobox.setWidth("60%");
+			recordsCombobox.setReadonly(false);
+			recordsCombobox.setButtonVisible(true);
+			okButton.setVisible(true);
+			editButton.setVisible(false);
+		}
+	}
+	
+	public void actionRefresh(Object value)
+    {    	
+		log.warning("----------");
+		setValue(value);
+    }
+	
+	@Override
+	public void actionZoom()
+	{
+		log.warning("----------");
+    	MTable savedTable = new MTable(Env.getCtx(), (Integer) tableIDGridField.getValue(), null);
+    	
+    	String[] keyColumns = savedTable.getKeyColumns();
+    	String keyColumn = null;
+    	if(keyColumns.length > 0) {
+    		keyColumn = keyColumns[0];	//	guess
+    	}
+    	
+    	String where = keyColumn + "=" + gridTab.getValue("Record_ID");
+    	PO po = new Query(Env.getCtx(), savedTable, where, null).first();
+    	int AD_Window_ID = savedTable.getAD_Window_ID();
+    	
+		if (AD_Window_ID > 0) {	
+	    	if(!Util.isEmpty(keyColumn)) {
+				MQuery zoomQuery = new MQuery();
+				zoomQuery.addRestriction(keyColumn, MQuery.EQUAL, (Integer)po.get_Value(keyColumn));
+				zoomQuery.setRecordCount(1);	//	guess
+				
+		    	AEnv.zoom(AD_Window_ID, zoomQuery);
+	    	}
+		}
+	}
+	
+	@Override
+	public void onMenu(ContextMenuEvent evt) 
+	{
+		log.warning("----------");
+		if (WEditorPopupMenu.REQUERY_EVENT.equals(evt.getContextEvent()))
+		{
+			actionRefresh(oldValue);
+		}
+		else if (WEditorPopupMenu.ZOOM_EVENT.equals(evt.getContextEvent()))
+		{
+			actionZoom();
+		}
+		else if (WEditorPopupMenu.CHANGE_LOG_EVENT.equals(evt.getContextEvent()))
+		{
+			WFieldRecordInfo.start(gridField);
+		} 
+		else if (WEditorPopupMenu.PREFERENCE_EVENT.equals(evt.getContextEvent()))
+		{
+			if (isShowPreference())
+				ValuePreference.start (getComponent(), gridField, getValue());
+		}
+	}
+
+	@Override
+	public void setReadWrite(boolean readWrite) {
+		log.warning("----------");
+		m_ReadWrite = readWrite;
+		if (gridField != null && gridTab != null && recordsCombobox != null) {
+			for (Comboitem item : recordsCombobox.getItems()) {
+				item.setDisabled(!readWrite);
+			}
+		}
+	}
+
+	@Override
+	public boolean isReadWrite() {
+		log.warning("----------");
+		return m_ReadWrite;
+	}
+
+	public  void propertyChange(PropertyChangeEvent evt)
+	{
+		if ("FieldValue".equals(evt.getPropertyName()))
+		{
+			setValue(evt.getNewValue());
+		}
+	}
+	
+	@Override
+	public void setValue(Object value) {
+		log.warning("----------");
+		this.value = value;
+		if (value == null || value.toString().trim().length() == 0)
+    	{
+    		oldValue = null;
+    		value = null;
+    		recordsCombobox.setValue(null);
+    	}
+		else {
+			if(oldTableID != tableIDGridField.getValue()) {
+				initRecords();
+				oldTableID = tableIDGridField.getValue();
+			}
+			recordsCombobox.setValue(value);
+			if(!recordsCombobox.isSelected(value)) {
+				recordsCombobox.setValue(null);
+			}
+		}
+
+			ValueChangeEvent changeEvent = new ValueChangeEvent(this, this.getColumnName(), oldValue, value);
+	        super.fireValueChange(changeEvent);
+			oldValue = value;
+//		}
+		
+	}
+
+	@Override
+	public Object getValue() {
+		log.warning("----------");
+		Object retVal = null;
+        Comboitem selItem = recordsCombobox.getSelectedItem();
+        if (selItem != null)
+        {
+            retVal = selItem.getValue();
+            if ((retVal instanceof Integer) && (Integer)retVal == -1)
+            	retVal = null;
+            else if ((retVal instanceof String) && "".equals(retVal))
+            	retVal = null;
+        }
+        return retVal;
+	}
+
+	@Override
+	public String getDisplay() {
+		log.warning("----------");
+		if(((Integer) tableIDGridField.getValue() != null) && ((Integer) gridField.getValue() != null))
+			return getIdentifierColumns((Integer) tableIDGridField.getValue(), (Integer) gridField.getValue());
+		else
+			return "";
+	}
+
+	@Override
+	public void onEvent(Event event) throws Exception {
+		log.warning("----------");
+		if(event.getName().equalsIgnoreCase(Events.ON_SELECT)) {
+			if (event.getTarget().equals(tableIDEditor.getComponent())) {
+				initRecords();
+				this.setValue(null);
+				updateUI(EDITOR_MODE_EDIT);
+			}
+			else if (event.getTarget().equals(recordsCombobox)) {
+				
+				Comboitem selectedItem = recordsCombobox.getSelectedItem();
+				if(selectedItem != null)
+					setValue(selectedItem.getValue());
+//					gridField.setValue(selectedItem.getValue(), gridTab.isInsertRecord());
+				updateUI(EDITOR_MODE_EDIT);
+			}
+		}
+		if(event.getName().equalsIgnoreCase(Events.ON_CLICK)) {
+			if(event.getTarget().equals(okButton)) {
+				
+				if(recordsCombobox.getSelectedItem() == null) {
+					recordsCombobox.setStyle("color:red;");
+					recordsCombobox.setPlaceholder("Select Record");
+				}
+				else {
+					updateUI(EDITOR_MODE_SAVED);
+				}
+			}
+			else if(event.getTarget().equals(editButton)) {
+				updateUI(EDITOR_MODE_EDIT);
+			}
+		}
+	}
+	
+	private void setMandatory() {
+		log.warning("----------");
+		gridTab.setValue("AD_Table_ID", (Integer) tableIDGridField.getValue());
+		gridTab.setValue("Record_ID", (Integer) gridField.getValue());
+		
+		boolean noTableID = ((Integer) tableIDGridField.getValue() == null) || ((Integer) tableIDGridField.getValue() <= 0);
+		boolean noRecordID = ((Integer) gridField.getValue() == null) || ((Integer) gridField.getValue() <= 0);
+		
+		if(noTableID && !noRecordID) {
+			gridField.setMandatory(false);
+			tableIDGridField.setMandatory(true);
+		}
+		else if(!noTableID && noRecordID) {
+			gridField.setMandatory(true);
+			tableIDGridField.setMandatory(false);
+		}
+		else {
+			gridField.setMandatory(false);
+			tableIDGridField.setMandatory(false);
+		}
+		this.setMandatory(gridField.isMandatory(false));
+		tableIDEditor.setMandatory(tableIDGridField.isMandatory(false));
+	}
+    
+    protected void initRecords() {
+    	log.warning("----------");
+    	recordsCombobox.removeAllItems();
+    	if(tableIDGridField.getValue() == null)
+    		return;
+    	MTable mTable = MTable.get(Env.getCtx(), (int)tableIDGridField.getValue(), null);
+    	String[] keyColumns = mTable.getKeyColumns();
+    	
+    	if(keyColumns.length > 0) {
+	    	String keyColumn = keyColumns[0];
+	    	
+	    	String sql = "SELECT " + keyColumn
+	    			+ " FROM " + mTable.getTableName();
+	    	
+	    	PreparedStatement pstmt = null;
+			ResultSet rs = null;
+			try
+			{
+				pstmt = DB.prepareStatement(sql, null);
+				rs = pstmt.executeQuery();
+				
+				while (rs.next()){
+					String identifier = getIdentifierColumns((int)tableIDGridField.getValue(), rs.getInt(1));
+					recordsCombobox.appendItem(identifier, rs.getInt(1));
+				}
+			}
+			catch (SQLException ex)
+			{
+				log.log(Level.SEVERE, sql, ex);
+			}
+			finally
+			{
+				DB.close(rs, pstmt);
+				rs = null;
+				pstmt = null;
+			}
+    	}
+//    	for(Comboitem item : recordsCombobox.getItems()) {
+//    		if(item.getValue() != null && item.getValue().equals((Integer) gridField.getValue())) {
+//    			recordsCombobox.setSelectedItem(item);
+//    			break;
+//    		}
+//    	}
+    	
+    }
+    
+    protected String getIdentifierColumns(int tableID, int recordID) {
+    	log.warning("----------");
+    	String value = String.valueOf(tableID);
+    	if ((tableID > 0) && (recordID > 0)) {
+			MTable mTable = MTable.get(Env.getCtx(), tableID);
+			String tableName = mTable.getTableName();
+			
+			ArrayList<MColumn> list = new ArrayList<MColumn>();
+			for (String idColumnName : mTable.getIdentifierColumns()) {
+				MColumn column = mTable.getColumn(idColumnName);
+				list.add (column);
+			}
+			if(list.size() > 0) {
+				StringBuilder displayColumn = new StringBuilder();
+				String separator = MSysConfig.getValue(MSysConfig.IDENTIFIER_SEPARATOR, "_", Env.getAD_Client_ID(Env.getCtx()));
+				
+				for(int i = 0; i < list.size(); i++) {
+					MColumn identifierColumn = list.get(i);
+					if(i > 0)
+						displayColumn.append("||'").append(separator).append("'||");
+					
+					displayColumn.append("NVL(")
+								.append(DB.TO_CHAR(identifierColumn.getColumnName(), 
+													identifierColumn.getAD_Reference_ID(), 
+													Env.getAD_Language(Env.getCtx())))
+								.append(",'')");
+				}
+				StringBuilder sql = new StringBuilder("SELECT ");
+				sql.append(displayColumn.toString());
+				sql.append(" FROM ").append(tableName);
+				sql.append(" WHERE ")
+					.append(tableName).append(".").append(tableName).append("_ID=?");
+				
+				value = DB.getSQLValueStringEx(null, sql.toString(), recordID);
+			}
+		}
+    	return value;
+    }
+}
