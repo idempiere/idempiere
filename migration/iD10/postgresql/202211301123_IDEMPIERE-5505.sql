e8f814885b91249ba66794aa0c10d410ec058e4e
Nicolas Micoud <58596990+nmicoud@users.noreply.github.com>
2022-12-02 12:18:41 +0100

    IDEMPIERE-5505 : Add Value and PrintName on GL_Category - set Value to 60 chars

--- migration/iD10/postgresql/202211301123_IDEMPIERE-5505.sql
+++ migration/iD10/postgresql/202211301123_IDEMPIERE-5505.sql
@@ -3,7 +3,7 @@ SELECT register_migration_script('202211301123_IDEMPIERE-5505.sql') FROM dual;
 
 -- Nov 30, 2022, 11:23:47 AM CET
 INSERT INTO AD_Column (AD_Column_ID,Version,Name,Description,Help,AD_Table_ID,ColumnName,FieldLength,IsKey,IsParent,IsMandatory,IsTranslated,IsIdentifier,SeqNo,IsEncrypted,AD_Reference_ID,AD_Client_ID,AD_Org_ID,IsActive,Created,CreatedBy,Updated,UpdatedBy,AD_Element_ID,IsUpdateable,IsSelectionColumn,EntityType,IsSyncDatabase,IsAlwaysUpdateable,IsAutocomplete,IsAllowLogging,AD_Column_UU,IsAllowCopy,SeqNoSelection,IsToolbarButton,IsSecure,IsHtml) VALUES (215691,0,'Search Key','Search key for the record in the format required - must be unique','A search key allows you a fast method of finding a particular record.
-If you leave the search key empty, the system automatically creates a numeric number.  The document sequence used for this fallback number is defined in the "Maintain Sequence" window with the name "DocumentNo_<TableName>", where TableName is the actual name of the table (e.g. C_Order).',218,'Value',40,'N','N','N','N','N',0,'N',10,0,0,'Y',TO_TIMESTAMP('2022-11-30 11:23:46','YYYY-MM-DD HH24:MI:SS'),100,TO_TIMESTAMP('2022-11-30 11:23:46','YYYY-MM-DD HH24:MI:SS'),100,620,'Y','N','D','N','N','N','Y','2b2fcbf5-873f-410e-b648-723c7096715a','Y',0,'N','N','N')
+If you leave the search key empty, the system automatically creates a numeric number.  The document sequence used for this fallback number is defined in the "Maintain Sequence" window with the name "DocumentNo_<TableName>", where TableName is the actual name of the table (e.g. C_Order).',218,'Value',60,'N','N','N','N','N',0,'N',10,0,0,'Y',TO_TIMESTAMP('2022-11-30 11:23:46','YYYY-MM-DD HH24:MI:SS'),100,TO_TIMESTAMP('2022-11-30 11:23:46','YYYY-MM-DD HH24:MI:SS'),100,620,'Y','N','D','N','N','N','Y','2b2fcbf5-873f-410e-b648-723c7096715a','Y',0,'N','N','N')
 ;
 
 -- Nov 30, 2022, 11:24:00 AM CET
@@ -11,7 +11,7 @@ INSERT INTO AD_Column (AD_Column_ID,Version,Name,Description,Help,AD_Table_ID,Co
 ;
 
 -- Nov 30, 2022, 11:24:05 AM CET
-ALTER TABLE GL_Category ADD COLUMN Value VARCHAR(40) DEFAULT NULL 
+ALTER TABLE GL_Category ADD COLUMN Value VARCHAR(60) DEFAULT NULL 
 ;
 
 -- Nov 30, 2022, 11:24:05 AM CET
@@ -32,7 +32,7 @@ ALTER TABLE GL_Category_Trl ADD COLUMN PrintName VARCHAR(60) DEFAULT NULL
 
 -- Nov 30, 2022, 11:24:44 AM CET
 INSERT INTO AD_Field (AD_Field_ID,Name,Description,Help,AD_Tab_ID,AD_Column_ID,IsDisplayed,DisplayLength,SeqNo,IsSameLine,IsHeading,IsFieldOnly,IsEncrypted,AD_Client_ID,AD_Org_ID,IsActive,Created,CreatedBy,Updated,UpdatedBy,IsReadOnly,IsCentrallyMaintained,EntityType,AD_Field_UU,IsDisplayedGrid,SeqNoGrid,ColumnSpan) VALUES (207452,'Search Key','Search key for the record in the format required - must be unique','A search key allows you a fast method of finding a particular record.
-If you leave the search key empty, the system automatically creates a numeric number.  The document sequence used for this fallback number is defined in the "Maintain Sequence" window with the name "DocumentNo_<TableName>", where TableName is the actual name of the table (e.g. C_Order).',158,215691,'Y',40,100,'N','N','N','N',0,0,'Y',TO_TIMESTAMP('2022-11-30 11:24:44','YYYY-MM-DD HH24:MI:SS'),100,TO_TIMESTAMP('2022-11-30 11:24:44','YYYY-MM-DD HH24:MI:SS'),100,'N','Y','D','8f22ff1f-d45f-4758-94c9-7bff1a0a8f99','Y',100,2)
+If you leave the search key empty, the system automatically creates a numeric number.  The document sequence used for this fallback number is defined in the "Maintain Sequence" window with the name "DocumentNo_<TableName>", where TableName is the actual name of the table (e.g. C_Order).',158,215691,'Y',60,100,'N','N','N','N',0,0,'Y',TO_TIMESTAMP('2022-11-30 11:24:44','YYYY-MM-DD HH24:MI:SS'),100,TO_TIMESTAMP('2022-11-30 11:24:44','YYYY-MM-DD HH24:MI:SS'),100,'N','Y','D','8f22ff1f-d45f-4758-94c9-7bff1a0a8f99','Y',100,2)
 ;
 
 -- Nov 30, 2022, 11:24:44 AM CET
@@ -83,14 +83,14 @@ UPDATE AD_Field SET SeqNo=0,Updated=TO_TIMESTAMP('2022-11-30 11:25:11','YYYY-MM-
 UPDATE AD_Field SET SeqNo=0,Updated=TO_TIMESTAMP('2022-11-30 11:25:11','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=204839
 ;
 
-update gl_category set value = substr(name, 0, 40);
+update gl_category set value = name;
 
 -- Dec 1, 2022, 11:38:22 AM CET
 UPDATE AD_Column SET IsMandatory='Y',Updated=TO_TIMESTAMP('2022-12-01 11:38:22','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=10 WHERE AD_Column_ID=215691
 ;
 
 -- Dec 1, 2022, 11:38:25 AM CET
-INSERT INTO t_alter_column values('gl_category','Value','VARCHAR(40)',null,null)
+INSERT INTO t_alter_column values('gl_category','Value','VARCHAR(60)',null,null)
 ;
 
 -- Dec 1, 2022, 11:38:25 AM CET
