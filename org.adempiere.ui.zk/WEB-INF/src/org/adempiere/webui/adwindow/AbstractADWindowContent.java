920bf2d6e9f1ab83e9707a9b435c11ed2674b1ed
Diego Ruiz <12065321+d-ruiz@users.noreply.github.com>
2023-01-09 21:50:07 +0100

    IDEMPIERE-5535 - Fixed warning dialog shown wrongly (#1631)
    * IDEMPIERE-5535 - Fixed warning dialog shown wrongly
    
    * IDEMPIERE-5535 - Fixed warning dialog shown wrongly in other classes

--- org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/adwindow/AbstractADWindowContent.java
+++ org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/adwindow/AbstractADWindowContent.java
@@ -1791,7 +1791,7 @@ public abstract class AbstractADWindowContent extends AbstractUIPart implements 
         		isImporting = gridTable.isImporting();
         	}
         	if (!isImporting) {
-        		Dialog.warn(curWindowNo, e.getAD_Message(), e.getInfo());
+        		Dialog.warn(curWindowNo, e.getAD_Message(), e.getInfo(), null);
         		e.setConfirmed(true);   //  show just once - if MTable.setCurrentRow is involved the status event is re-issued
         	}
         }
