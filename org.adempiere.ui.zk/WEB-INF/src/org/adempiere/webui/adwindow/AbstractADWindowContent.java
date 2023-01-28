16cda152614c7111465c476039925dce9a5a73ad
Carlos Ruiz <carg67@gmail.com>
2023-01-28 13:01:10 +0100

    IDEMPIERE-5528 Context parsing on process Parameter not considering tab no
    - Solve NPE

--- org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/adwindow/AbstractADWindowContent.java
+++ org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/adwindow/AbstractADWindowContent.java
@@ -3751,7 +3751,11 @@ public abstract class AbstractADWindowContent extends AbstractUIPart implements 
 				}
 			}
 
-			ProcessModalDialog dialog = new ProcessModalDialog(this, curWindowNo, adtabPanel.getGridTab().getTabNo(), pi, startWOasking);
+			ProcessModalDialog dialog;
+			if (adtabPanel != null && adtabPanel.getGridTab() != null)
+				dialog = new ProcessModalDialog(this, curWindowNo, adtabPanel.getGridTab().getTabNo(), pi, startWOasking);
+			else
+				dialog = new ProcessModalDialog(this, curWindowNo, pi, startWOasking);
 
 			if (dialog.isValid())
 			{
