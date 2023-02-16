c74ae96646cb6ab8ce89a39ee915bfa6596170dc
Peter Takács <takacspeti300@gmail.com>
2023-02-16 10:10:51 +0100

    IDEMPIERE-5579 - Missing Period Control When Reactivating Orders

--- org.adempiere.base/src/org/compiere/model/MOrder.java
+++ org.adempiere.base/src/org/compiere/model/MOrder.java
@@ -2762,6 +2762,9 @@ public class MOrder extends X_C_Order implements DocAction
 		if (m_processMsg != null)
 			return false;	
 		
+		// Test period
+		MPeriod.testPeriodOpen(getCtx(), getDateAcct(), getC_DocType_ID(), getAD_Org_ID());
+		
 		MDocType dt = MDocType.get(getCtx(), getC_DocType_ID());
 		String DocSubTypeSO = dt.getDocSubTypeSO();
 		
