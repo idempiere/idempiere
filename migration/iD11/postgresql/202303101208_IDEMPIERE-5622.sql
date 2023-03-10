989d05442e04fdcea30e6bbe55b3ca3715d8db87
Diego Ruiz <ruizg.diego@gmail.com>
2023-03-10 12:12:57 +0100

    IDEMPIERE-5622 - Language in Window and Info Window customization should be filtered by log in languages

---
+++ migration/iD11/postgresql/202303101208_IDEMPIERE-5622.sql
@@ -0,0 +1,11 @@
+-- IDEMPIERE-5622 - Language in Window and Info Window customization should be filtered by log in languages
+SELECT register_migration_script('202303101208_IDEMPIERE-5622.sql') FROM dual;
+
+-- Mar 10, 2023, 12:08:54 PM CET
+UPDATE AD_Column SET AD_Reference_Value_ID=327,Updated=TO_TIMESTAMP('2023-03-10 12:08:54','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_UU='0e32e607-dbc8-47bd-844f-9bc1fc772b39'
+;
+
+-- Mar 10, 2023, 12:09:05 PM CET
+UPDATE AD_Column SET AD_Reference_Value_ID=327,Updated=TO_TIMESTAMP('2023-03-10 12:09:05','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_UU='22154794-072c-4240-891e-ad3623df3b93'
+;
+
