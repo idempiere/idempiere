eab0574dae254ec3854ef41fde2415c5c70944d6
Carlos Ruiz <carg67@gmail.com>
2023-03-17 07:27:00 +0100

    IDEMPIERE-5565 Cannot create report with virtual column where reference is table with display column = ID (#1735)
    * IDEMPIERE-5565 Cannot create report with virtual column where reference is table with display column = ID
    
    * - make it consistent with the actual approach

--- org.adempiere.base/src/org/compiere/print/DataEngine.java
+++ org.adempiere.base/src/org/compiere/print/DataEngine.java
@@ -484,16 +484,10 @@ public class DataEngine
 						|| (AD_Reference_ID == DisplayType.Search && AD_Reference_Value_ID != 0)
 					)
 				{
-					String eSql;
+					String eSql = MLookupFactory.getLookup_TableEmbed(m_language, ColumnName, tableName, AD_Reference_Value_ID);
+
 					if (ColumnSQL.length() > 0)
-					{
 						lookupSQL = ColumnSQL;
-						eSql = MLookupFactory.getLookup_TableEmbed(m_language, ColumnSQL, tableName, AD_Reference_Value_ID);
-					}
-					else
-					{
-						eSql = MLookupFactory.getLookup_TableEmbed(m_language, ColumnName, tableName, AD_Reference_Value_ID);
-					}
 
 					//  DisplayColumn
 					String display = ColumnName;
