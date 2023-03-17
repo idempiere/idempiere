e479eb5cfb959ad6bd870b765736b6b4dead15a9
hengsin <hengsin@gmail.com>
2023-03-17 18:45:56 +0800

    IDEMPIERE-5609 Dashboard Performance Indicators incrementally redrawed on DC drag and drop (#1736)

--- org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/apps/graph/WPAPanel.java
+++ org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/apps/graph/WPAPanel.java
@@ -1,4 +1,35 @@
+/***********************************************************************
+ * This file is part of iDempiere ERP Open Source                      *
+ * http://www.idempiere.org                                            *
+ *                                                                     *
+ * Copyright (C) Contributors                                          *
+ *                                                                     *
+ * This program is free software; you can redistribute it and/or       *
+ * modify it under the terms of the GNU General Public License         *
+ * as published by the Free Software Foundation; either version 2      *
+ * of the License, or (at your option) any later version.              *
+ *                                                                     *
+ * This program is distributed in the hope that it will be useful,     *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of      *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the        *
+ * GNU General Public License for more details.                        *
+ *                                                                     *
+ * You should have received a copy of the GNU General Public License   *
+ * along with this program; if not, write to the Free Software         *
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,          *
+ * MA 02110-1301, USA.                                                 *
+ *                                                                     *
+ * Contributors:                                                       *
+ * - hengsin                         								   *
+ * - carlos                                                            *
+ * - elaine                                                            *
+ * - hieplq                                                            *
+ **********************************************************************/
 package org.adempiere.webui.apps.graph;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
 
 import org.adempiere.webui.apps.graph.WPerformanceIndicator.Options;
 import org.adempiere.webui.component.Grid;
@@ -16,15 +47,14 @@ import org.zkoss.zul.Label;
 
 public class WPAPanel extends Panel implements EventListener<Event>
 {	
-
 	/**
-	 * 
+	 * generated serial id 
 	 */
 	private static final long serialVersionUID = -6367672112341229048L;
 
 	/**
-	 * 	Get Panel if User has Performance Goals
-	 *	@return panel pr null
+	 * 	Load Performance Goals for current login user
+	 *	@return MGoal[]
 	 */
 	public static MGoal[] loadGoal()
 	{
@@ -33,16 +63,23 @@ public class WPAPanel extends Panel implements EventListener<Event>
 		return goals;
 	}
 	
-	/**************************************************************************
-	 * 	Constructor
+	/**
+	 * Default Constructor
 	 */
 	public WPAPanel ()
 	{
 		super ();
 	}
 	
+	/**
+	 * Set performance goals and render charts
+	 * @param goals
+	 * @param options color map options
+	 */
 	public void setGoals (MGoal[] goals, Options options){
-		m_goals = goals;		
+		m_goals = goals;
+		if (getChildren().size() > 0)
+			getChildren().clear();
 		init(options);
 	}
 	
@@ -53,7 +90,7 @@ public class WPAPanel extends Panel implements EventListener<Event>
 	private static final CLogger log = CLogger.getCLogger (WPAPanel.class);
 	
 	/**
-	 * 	Static/Dynamic Init
+	 * Layout panel and render charts
 	 * @param options 
 	 */
 	private void init(Options options)
@@ -67,6 +104,7 @@ public class WPAPanel extends Panel implements EventListener<Event>
 		grid.appendChild(rows);
 
 		Row row = null;
+		List<WPerformanceIndicator> list = new ArrayList<>();
 		for (int i = 0; i < m_goals.length; i++)
 		{
 			if (row == null || i % 2 == 0)
@@ -79,6 +117,14 @@ public class WPAPanel extends Panel implements EventListener<Event>
 			row.appendChild(div);
 			div.setSclass("performance-indicator-box");
 			WPerformanceIndicator pi = new WPerformanceIndicator(m_goals[i], options);
+			list.add(pi);
+			pi.addEventListener(WPerformanceIndicator.ON_AFTER_RENDER_CHART_EVENT, e -> {
+				boolean removed = list.remove(e.getTarget());
+				if (removed && list.isEmpty()) {
+					//notify all chart have been rendered
+					Events.sendEvent(WPAPanel.this, new Event(WPerformanceIndicator.ON_AFTER_RENDER_CHART_EVENT));
+				}
+			});
 			div.appendChild(pi);
 			pi.addEventListener(Events.ON_CLICK, this);
 			Div titleDiv = new Div();
@@ -90,15 +136,17 @@ public class WPAPanel extends Panel implements EventListener<Event>
 	}	//	init
 
 	/**
-	 * 	Action Listener for Drill Down
+	 * 	Event Listener for Drill Down
 	 *	@param e event
 	 */
+	@Override
 	public void onEvent(Event e) throws Exception 
 	{
 		if (e.getTarget() instanceof WPerformanceIndicator)
 		{
 			WPerformanceIndicator pi = (WPerformanceIndicator) e.getTarget();
-			log.info(pi.toString());
+			if (log.isLoggable(Level.INFO))
+				log.info(pi.toString());
 			MGoal goal = pi.getGoal();
 			if (goal.getMeasure() != null)
 				new WPerformanceDetail(goal);
