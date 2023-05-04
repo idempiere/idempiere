81c9ea9f96438e865326938900200a0fd9eea5da
Peter Takács <takacspeti300@gmail.com>
2023-05-04 17:31:34 +0200

    IDEMPIERE-5683 - fix Dynamic Validation

--- migration/iD11/postgresql/202304280940_IDEMPIERE-5683.sql
+++ migration/iD11/postgresql/202304280940_IDEMPIERE-5683.sql
@@ -16,3 +16,11 @@ SET FKConstraintType =
 WHERE AD_Reference_ID IN(200202,200240)	/* Record ID, Record UU */
 ;
 
+-- May 4, 2023, 5:28:36 PM CEST
+UPDATE AD_Val_Rule SET Code='(
+(AD_Ref_List.Value!=''M'' OR @AD_Reference_ID@ IN (18,19,30,200233,200234,200235,200202,200240)) 
+AND 
+((AD_Ref_List.Value=''C'' AND @AD_Reference_ID@ NOT IN (200202,200240)) OR (AD_Ref_List.Value!=''C''))
+)',Updated=TO_TIMESTAMP('2023-05-04 17:28:36','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Val_Rule_ID=200064
+;
+
