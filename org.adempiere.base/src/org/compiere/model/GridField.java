d5d7aa45a2d727b6725906523bc1ca80e4bb499d
Nicolas Micoud <58596990+nmicoud@users.noreply.github.com>
2023-06-07 21:59:11 +0200

    IDEMPIERE-5749: CreatedBy fields should not be forced to be Search if used from processes/info windows (#1870)
    https://idempiere.atlassian.net/browse/IDEMPIERE-5749

--- org.adempiere.base/src/org/compiere/model/GridField.java
+++ org.adempiere.base/src/org/compiere/model/GridField.java
@@ -83,7 +83,7 @@ public class GridField
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 405469916055906825L;
+	private static final long serialVersionUID = -4496344553246662012L;
 	
 	private static final Character SPECIAL_CASE_DEFAULT = '1';
 	private static final Character SQL_DEFAULT = '2';
@@ -221,7 +221,7 @@ public class GridField
 			return;
 		}
 		//	Prevent loading of CreatedBy/UpdatedBy
-		if (m_vo.displayType == DisplayType.Table
+		if (m_vo.displayType == DisplayType.Table && m_vo.AD_Tab_ID > 0
 			&& (m_vo.ColumnName.equals("CreatedBy") || m_vo.ColumnName.equals("UpdatedBy")) )
 		{
 			m_vo.lookupInfo.IsCreadedUpdatedBy = true;
