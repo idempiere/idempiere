7e225c4c8c3f1c48e793499524ed46d62ce61a48
Carlos Ruiz <carg67@gmail.com>
2023-06-23 03:52:25 +0200

    IDEMPIERE-5056 Order and Invoice: Tax lookup interface (#1907)
    - make the DeliveryViaRule field visible for all orders

---
+++ migration/iD10/oracle/202306221516_IDEMPIERE-5056.sql
@@ -0,0 +1,10 @@
+-- IDEMPIERE-5056 Order and Invoice: Tax lookup interface
+SELECT register_migration_script('202306221516_IDEMPIERE-5056.sql') FROM dual;
+
+SET SQLBLANKLINES ON
+SET DEFINE OFF
+
+-- Jun 22, 2023, 3:16:13 PM CEST
+UPDATE AD_Field SET DisplayLogic=NULL,Updated=TO_TIMESTAMP('2023-06-22 15:16:13','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=1108
+;
+
