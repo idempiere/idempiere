31f01dcd2812099893f2bb22a48f0b914fd88ba8
hengsin <hengsin@gmail.com>
2023-07-16 22:02:44 +0800

    IDEMPIERE-5800 Info Window Context Variables are not Cleared after Closing (#1937)

--- org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/panel/InfoPanel.java
+++ org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/panel/InfoPanel.java
@@ -227,6 +227,8 @@ public abstract class InfoPanel extends Window implements EventListener<Event>, 
 
 	private Button btnSelectAll;
 	private Button btnDeSelectAll;
+
+	private boolean registerWindowNo = false;
 	
 	/**************************************************
      *  Detail Constructor
@@ -290,6 +292,7 @@ public abstract class InfoPanel extends Window implements EventListener<Event>, 
 	{				
 		if (WindowNo <= 0) {
 			p_WindowNo = SessionManager.getAppDesktop().registerWindow(this);
+			registerWindowNo  = true;
 		} else {
 			p_WindowNo = WindowNo;
 		}
@@ -3050,6 +3053,8 @@ public abstract class InfoPanel extends Window implements EventListener<Event>, 
 				SessionManager.getSessionApplication().getKeylistener().removeEventListener(Events.ON_CTRL_KEY, this);
 			if (getFirstChild() != null)
 				saveWlistBoxColumnWidth(getFirstChild());
+			if (registerWindowNo && SessionManager.getAppDesktop() != null)
+				SessionManager.getAppDesktop().unregisterWindow(p_WindowNo);
 		} catch (Exception e){
 			log.log(Level.WARNING, e.getMessage(), e);
 		}
