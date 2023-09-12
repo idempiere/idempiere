91ce22efaed5a29e48424bb050e80af8a9c277d3
vitor.santos <vitor.santos@devcoffee.com.br>
2023-09-12 09:08:21 -0300

    IDEMPIERE-5852 Change broadcastmessage column size

---
+++ migration/iD11/oracle/202309111358_IDEMPIERE-5852.sql
@@ -0,0 +1,22 @@
+-- IDEMPIERE-3989
+SELECT register_migration_script('202309111358_IDEMPIERE-5852.sql') FROM dual;
+
+SET SQLBLANKLINES ON
+SET DEFINE OFF
+
+-- Sep 11, 2023, 1:58:52 PM BRT
+UPDATE AD_Column SET FieldLength=0, AD_Reference_ID=36,Updated=TO_TIMESTAMP('2023-09-11 13:58:52','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=200985
+;
+
+-- Sep 11, 2023, 1:58:58 PM BRT
+ALTER TABLE AD_BroadcastMessage MODIFY BroadcastMessage CLOB
+;
+
+-- Sep 11, 2023, 1:59:52 PM BRT
+UPDATE AD_Column SET FieldLength=0, AD_Reference_ID=36, FKConstraintType=NULL,Updated=TO_TIMESTAMP('2023-09-11 13:59:52','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=211151
+;
+
+-- Sep 11, 2023, 1:59:58 PM BRT
+ALTER TABLE AD_BroadcastMessage_Trl MODIFY BroadcastMessage CLOB
+;
+
---
+++ migration/iD11/postgresql/202309111358_IDEMPIERE-5852.sql
@@ -0,0 +1,19 @@
+-- IDEMPIERE-3989
+SELECT register_migration_script('202309111358_IDEMPIERE-5852.sql') FROM dual;
+
+-- Sep 11, 2023, 1:58:52 PM BRT
+UPDATE AD_Column SET FieldLength=0, AD_Reference_ID=36,Updated=TO_TIMESTAMP('2023-09-11 13:58:52','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=200985
+;
+
+-- Sep 11, 2023, 1:58:58 PM BRT
+INSERT INTO t_alter_column values('ad_broadcastmessage','BroadcastMessage','TEXT',null,null)
+;
+
+-- Sep 11, 2023, 1:59:52 PM BRT
+UPDATE AD_Column SET FieldLength=0, AD_Reference_ID=36, FKConstraintType=NULL,Updated=TO_TIMESTAMP('2023-09-11 13:59:52','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=211151
+;
+
+-- Sep 11, 2023, 1:59:58 PM BRT
+INSERT INTO t_alter_column values('ad_broadcastmessage_trl','BroadcastMessage','TEXT',null,null)
+;
+
