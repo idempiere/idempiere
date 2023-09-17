5b2d3481fba2918177a1a39c39b853e4499b7bbb
zuhriutama <zuhriutama@gmail.com>
2023-09-17 11:44:38 +0700

    IDEMPIERE-3590 : Can't open Preference popup if no field is set as QuickEntry

--- org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/window/WPreference.java
+++ org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/window/WPreference.java
@@ -101,10 +101,8 @@ public class WPreference extends WQuickEntry implements EventListener<Event>, Va
 		preferences = MUserPreference.getUserPreference(Env.getAD_User_ID(Env.getCtx()), Env.getAD_Client_ID(Env.getCtx()));
 		recordId = preferences.get_ID();
 
-		if(getQuickFields() == 0)
-			throw new AdempiereException(Msg.translate(Env.getCtx(), "NoQuickEntryPreference"));
-			
-		loadRecord(recordId);
+		if(getQuickFields()>0)
+			loadRecord(recordId);
 	} //loadPreferences
 
 	/**
