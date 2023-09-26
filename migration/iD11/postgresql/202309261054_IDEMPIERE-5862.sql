508198aef69d7539a2c4e7e81a3b83fe15874a99
Carlos Ruiz <carg67@gmail.com>
2023-09-26 13:43:06 +0200

    Update postgresql/202309261054_IDEMPIERE-5862.sql

--- migration/iD11/postgresql/202309261054_IDEMPIERE-5862.sql
+++ migration/iD11/postgresql/202309261054_IDEMPIERE-5862.sql
@@ -38,5 +38,10 @@ UPDATE AD_Column SET IsAlwaysUpdateable='Y',Updated=TO_TIMESTAMP('2023-09-26 10:
 UPDATE AD_Column SET IsAlwaysUpdateable='Y',Updated=TO_TIMESTAMP('2023-09-26 10:54:33','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=10 WHERE AD_Column_ID=5637
 ;
 
+UPDATE AD_Column SET IsAlwaysUpdateable='Y'
+WHERE IsAlwaysUpdateable='N' AND ColumnName='PaymentRule'
+AND EXISTS (SELECT 1 FROM AD_Table t WHERE t.AD_Table_ID=AD_Column.AD_Table_ID AND t.IsView='N')
+;
+
 -- IDEMPIERE-5862
 SELECT register_migration_script('202309261054_IDEMPIERE-5862.sql') FROM dual;
