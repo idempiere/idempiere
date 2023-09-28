810f862c38f315513b21267b6cee69507b182176
dpansheriya <deepakpansheriya@gmail.com>
2023-09-28 21:39:43 +0530

    IDEMPIERE-4877:In Financial Report PDF format, Footer is overlay on content.

--- org.adempiere.base/src/org/compiere/print/layout/TableElement.java
+++ org.adempiere.base/src/org/compiere/print/layout/TableElement.java
@@ -1692,11 +1692,14 @@ public class TableElement extends PrintElement
 			}
 
 			// Maintain financial report detail and column section Y position
-			if ((int) (rowYstart + rowHeight) > curY)
+			int rowYheight = (int) (rowYstart + rowHeight);
+			if (m_finReportSumRows != null && !m_finReportSumRows.isEmpty())
+				rowYheight = (int) (rowYstart + rowHeight - m_tFormat.getLineStroke().floatValue());
+			if (rowYheight > curY)
 			{
-				curY = (int) (rowYstart + rowHeight);
+				curY = rowYheight;
 			}
-
+			
             //  X end line
             if (row == m_data.getRowCount()-1)         //  last Line
             {
