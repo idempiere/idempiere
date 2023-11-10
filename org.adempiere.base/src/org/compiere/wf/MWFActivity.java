b0f06c9f6c96ee1c8535a796d662ddc47e49be7b
Carlos Ruiz <carg67@gmail.com>
2023-11-10 20:17:01 +0100

    IDEMPIERE-5261 Workflow EMail notification enhancement (#2102)
    - add @_noDocInfo_@ variable to allow sending the email without adding extra information

--- org.adempiere.base/src/org/compiere/wf/MWFActivity.java
+++ org.adempiere.base/src/org/compiere/wf/MWFActivity.java
@@ -1806,15 +1806,14 @@ public class MWFActivity extends X_AD_WF_Activity implements Runnable
 		//
 		String subject = null;
 		String raw = text.getMailHeader(false);
-		int first = raw != null ? raw.indexOf("@") : -1;
-		if (raw != null &&  first >= 0 && raw.indexOf("@", first) > first)
-			subject = text.getMailHeader();
+		if (raw != null && raw.contains("@_noDocInfo_@"))
+			subject = text.getMailHeader().replaceAll("@_noDocInfo_@", "");
 		else
 			subject = doc.getDocumentInfo() + ": " + text.getMailHeader();
 		String message = null;
 		raw = text.getMailText(true, false);
 		if (raw != null && (raw.contains("@=DocumentInfo") || raw.contains("@=documentInfo")
-				|| raw.contains("@=Summary") || raw.contains("@=summary")))
+				|| raw.contains("@=Summary") || raw.contains("@=summary") || raw.contains("@_noDocInfo_@")))
 			message = text.getMailText(true);
 		else
 			message = text.getMailText(true)
