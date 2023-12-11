5b22767e03f3a300edb37fc9e8fe10d8ed75e626
Carlos Ruiz <carg67@gmail.com>
2023-12-11 16:57:37 +0100

    IDEMPIERE-5954 Payment Info Window failing on Payments into Batch > Lines

---
+++ migration/iD10/postgresql/202312111654_IDEMPIERE-5954.sql
@@ -0,0 +1,7 @@
+-- IDEMPIERE-5954 Payment Info Window failing on Payments into Batch > Lines
+SELECT register_migration_script('202312111654_IDEMPIERE-5954.sql') FROM dual;
+
+-- Dec 11, 2023, 4:54:28 PM CET
+UPDATE AD_Val_Rule SET Code='C_Payment.IsReconciled=''N'' AND C_Payment.TrxType <> ''X'' AND (SELECT COALESCE(pi.C_DepositBatch_ID,0) FROM C_Payment pi WHERE pi.C_Payment_ID=C_Payment.C_Payment_ID)=0 AND C_Payment.C_DocType_ID=@C_DocType_ID@',Updated=TO_TIMESTAMP('2023-12-11 16:54:28','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Val_Rule_ID=200039
+;
+
