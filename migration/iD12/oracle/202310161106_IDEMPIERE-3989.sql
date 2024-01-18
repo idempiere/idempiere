a6d865b70918aeeae77d365a54d5bd60bd3d8f1a
vitor.santos <vitor.santos@devcoffee.com.br>
2024-01-18 13:58:25 -0300

    IDEMPIERE-3989 - Applied Patch

--- migration/iD12/oracle/202310161106_IDEMPIERE-3989.sql
+++ migration/iD12/oracle/202310161106_IDEMPIERE-3989.sql
@@ -9,15 +9,11 @@ INSERT INTO AD_Element (AD_Element_ID,AD_Client_ID,AD_Org_ID,IsActive,Created,Cr
 ;
 
 -- Oct 16, 2023, 11:06:36 AM BRT
-INSERT INTO AD_Column (AD_Column_ID,Version,Name,AD_Table_ID,ColumnName,DefaultValue,FieldLength,IsKey,IsParent,IsMandatory,IsTranslated,IsIdentifier,SeqNo,IsEncrypted,AD_Reference_ID,AD_Client_ID,AD_Org_ID,IsActive,Created,CreatedBy,Updated,UpdatedBy,AD_Element_ID,IsUpdateable,IsSelectionColumn,EntityType,IsSyncDatabase,IsAlwaysUpdateable,IsAutocomplete,IsAllowLogging,AD_Column_UU,IsAllowCopy,SeqNoSelection,IsToolbarButton,IsSecure,IsHtml) VALUES (nextidfunc(3,'N'),0,'Show Technical Information on Help',200174,'IsShowTechnicalInfOnHelp','N',1,'N','N','Y','N','N',0,'N',20,0,0,'Y',TO_TIMESTAMP('2023-10-16 11:06:36','YYYY-MM-DD HH24:MI:SS'),100,TO_TIMESTAMP('2023-10-16 11:06:36','YYYY-MM-DD HH24:MI:SS'),100,toRecordId('AD_Element','5a3fdeb9-c731-4a41-b635-98f0e5e81d55'),'Y','N','U','N','N','N','Y','74d7e55d-13c6-446d-9fa5-6efac88444b4','Y',0,'N','N','N')
+INSERT INTO AD_Column (AD_Column_ID,Version,Name,AD_Table_ID,ColumnName,DefaultValue,FieldLength,IsKey,IsParent,IsMandatory,IsTranslated,IsIdentifier,SeqNo,IsEncrypted,AD_Reference_ID,AD_Client_ID,AD_Org_ID,IsActive,Created,CreatedBy,Updated,UpdatedBy,AD_Element_ID,IsUpdateable,IsSelectionColumn,EntityType,IsSyncDatabase,IsAlwaysUpdateable,IsAutocomplete,IsAllowLogging,AD_Column_UU,IsAllowCopy,SeqNoSelection,IsToolbarButton,IsSecure,IsHtml) VALUES (nextidfunc(3,'N'),0,'Show Technical Information on Help',200174,'IsShowTechnicalInfOnHelp','N',1,'N','N','Y','N','N',0,'N',20,0,0,'Y',TO_TIMESTAMP('2023-10-16 11:06:36','YYYY-MM-DD HH24:MI:SS'),100,TO_TIMESTAMP('2023-10-16 11:06:36','YYYY-MM-DD HH24:MI:SS'),100,toRecordId('AD_Element','5a3fdeb9-c731-4a41-b635-98f0e5e81d55'),'Y','N','D','N','N','N','Y','74d7e55d-13c6-446d-9fa5-6efac88444b4','Y',0,'N','N','N')
 ;
 
 -- Oct 16, 2023, 11:06:49 AM BRT
 ALTER TABLE AD_UserPreference ADD IsShowTechnicalInfOnHelp CHAR(1) DEFAULT 'N' CHECK (IsShowTechnicalInfOnHelp IN ('Y','N')) NOT NULL
-;
-
--- Oct 16, 2023, 11:07:38 AM BRT
-UPDATE AD_Column SET EntityType='D',Updated=TO_TIMESTAMP('2023-10-16 11:07:38','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_UU='74d7e55d-13c6-446d-9fa5-6efac88444b4'
 ;
 
 -- Oct 16, 2023, 11:08:02 AM BRT
