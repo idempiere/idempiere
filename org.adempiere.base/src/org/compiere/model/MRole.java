c81dae1a1d8243fde4878a8d9875f2bba0927186
Carlos Ruiz <carg67@gmail.com>
2024-01-21 15:26:18 +0100

    - revert changes on MRole to try a different approach

--- org.adempiere.base/src/org/compiere/model/MRole.java
+++ org.adempiere.base/src/org/compiere/model/MRole.java
@@ -64,10 +64,10 @@ import org.idempiere.cache.POCopyCache;
  */
 public final class MRole extends X_AD_Role implements ImmutablePOSupport
 {
-    /**
-	 * 
+	/**
+	 * generated serial id
 	 */
-	private static final long serialVersionUID = 7266911648463503849L;
+	private static final long serialVersionUID = -8937680640915708588L;
 
 	/**
 	 * 	Get role for current session/context
@@ -115,7 +115,8 @@ public final class MRole extends X_AD_Role implements ImmutablePOSupport
 	 * @param defaultRole
 	 */
 	private static void setDefaultRole(MRole defaultRole) {
-		s_defaultrole.put(ROLE_KEY, defaultRole);
+		Env.getCtx().remove(ROLE_KEY);
+		Env.getCtx().put(ROLE_KEY, defaultRole);
 	}
 
 	/**
@@ -123,7 +124,7 @@ public final class MRole extends X_AD_Role implements ImmutablePOSupport
 	 * @return MRole
 	 */
 	private static MRole getDefaultRole() {
-		return s_defaultrole.get(ROLE_KEY);
+		return (MRole) Env.getCtx().get(ROLE_KEY);
 	}
 
 	/**
@@ -248,7 +249,6 @@ public final class MRole extends X_AD_Role implements ImmutablePOSupport
 		
 	/** Role/User Cache			*/
 	private static POCopyCache<String,MRole> s_roles = new POCopyCache<String,MRole>(Table_Name, 5);
-	private static POCopyCache<String,MRole> s_defaultrole = new POCopyCache<String,MRole>(Table_Name, Table_Name, 1, 0, false, 1);
 	/** Log						*/ 
 	private static CLogger			s_log = CLogger.getCLogger(MRole.class);
 	
@@ -467,7 +467,11 @@ public final class MRole extends X_AD_Role implements ImmutablePOSupport
 		//
 		else if (is_ValueChanged("UserLevel"))
 			updateAccessRecords();
-
+		
+		//	Default Role changed
+		if (getDefaultRole() != null 
+			&& getDefaultRole().get_ID() == get_ID())
+			setDefaultRole(this);
 		return success;
 	}	//	afterSave
 	
