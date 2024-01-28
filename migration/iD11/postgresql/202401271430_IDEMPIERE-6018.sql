cb6f5d39aaa37a9fef92b632f5221c4713a56137
Carlos Ruiz <carg67@gmail.com>
2024-01-28 11:01:25 +0100

    IDEMPIERE-6018 Dangerous 2Packs generated with UpdateDictionary=true (#2215)
    * IDEMPIERE-6018 Dangerous 2Packs generated with UpdateDictionary=true
    
    * - change message to make it clear that it doesn't export official dictionary entries

---
+++ migration/iD11/postgresql/202401271430_IDEMPIERE-6018.sql
@@ -0,0 +1,7 @@
+-- IDEMPIERE-6018 Dangerous 2Packs generated with UpdateDictionary=true
+SELECT register_migration_script('202401271430_IDEMPIERE-6018.sql') FROM dual;
+
+-- Jan 27, 2024, 2:30:04 PM CET
+UPDATE AD_Message SET MsgText='zip - 2Pack zip file (not Official Dictionary)',Updated=TO_TIMESTAMP('2024-01-27 14:30:04','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=53134
+;
+
