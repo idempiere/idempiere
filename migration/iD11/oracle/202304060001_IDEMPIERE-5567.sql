0749709108ada7813c15775fa5c2dc234a3ba2da
Carlos Ruiz <carg67@gmail.com>
2024-02-06 13:25:45 +0100

    IDEMPIERE-5567 Support of UUID as Key (FHCA-4195) - fix oracle migration script

--- migration/iD11/oracle/202304060001_IDEMPIERE-5567.sql
+++ migration/iD11/oracle/202304060001_IDEMPIERE-5567.sql
@@ -8,7 +8,7 @@ SET DEFINE OFF
 INSERT INTO AD_Message (MsgType,MsgText,AD_Client_ID,AD_Org_ID,IsActive,Created,CreatedBy,Updated,UpdatedBy,AD_Message_ID,Value,EntityType,AD_Message_UU) VALUES ('I','UUID based table is not compatible with Record_ID',0,0,'Y',TO_TIMESTAMP('2023-04-06 00:01:13','YYYY-MM-DD HH24:MI:SS'),100,TO_TIMESTAMP('2023-04-06 00:01:13','YYYY-MM-DD HH24:MI:SS'),100,200830,'UUTableNotCompatibleWithRecordID','D','5b220b22-2a21-493c-81f0-877dbc083874')
 ;
 
-ALTER TABLE t_selection ADD t_selection_uu VARCHAR2(36 CHAR) DEFAULT '' NOT NULL;
+ALTER TABLE t_selection ADD t_selection_uu VARCHAR2(36 CHAR) DEFAULT ' ' NOT NULL;
 
 ALTER TABLE T_SELECTION MODIFY T_SELECTION_ID DEFAULT 0;
 
