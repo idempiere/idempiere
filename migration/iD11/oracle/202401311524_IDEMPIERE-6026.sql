1932dd8e2169bd6eb036ff1d8228b51f7120712d
Vitor Henrique Dos Santos <82419632+VitorHenri@users.noreply.github.com>
2024-02-21 11:47:25 -0300

    IDEMPIERE-6026 - Set Model Cascade (#2220)

---
+++ migration/iD11/oracle/202401311524_IDEMPIERE-6026.sql
@@ -0,0 +1,14 @@
+-- IDEMPIERE-6026
+SELECT register_migration_script('202401311524_IDEMPIERE-6026.sql') FROM dual;
+
+SET SQLBLANKLINES ON
+SET DEFINE OFF
+
+-- Jan 31, 2024, 3:24:28 PM BRT
+UPDATE AD_Column SET IsUpdateable='N', FKConstraintType='M',Updated=TO_TIMESTAMP('2024-01-31 15:24:28','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=214314
+;
+
+-- Jan 31, 2024, 3:25:43 PM BRT
+UPDATE AD_Column SET IsUpdateable='N', FKConstraintType='M',Updated=TO_TIMESTAMP('2024-01-31 15:25:43','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=214254
+;
+
