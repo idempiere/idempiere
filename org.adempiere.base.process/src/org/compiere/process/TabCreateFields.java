e7a0d165a3e20f0740e5152f42b5228e5e067280
Diego Ruiz <12065321+d-ruiz@users.noreply.github.com>
2024-03-07 16:10:43 +0100

    IDEMPIERE-2981 - Implement JSON field type (#2255)
    * IDEMPIERE-2981 - Implement JSON Field Type
    * IDEMPIERE-2981 - Added field to the Test Window and Unit Tests
    * IDEMPIERE-2981 - Fixed migration scripts
    * IDEMPIERE-2981 - Changed oracle json syntax
    * IDEMPIERE-2981 - Increased the number of lines of jsonData test field
    * IDEMPIERE-2981 - Validate and Prettify JSON string on the field editor
    * IDEMPIERE-2981 - Support for oracle
    * IDEMPIERE-2981 - Applied patch from Carlos Ruiz

--- org.adempiere.base.process/src/org/compiere/process/TabCreateFields.java
+++ org.adempiere.base.process/src/org/compiere/process/TabCreateFields.java
@@ -167,7 +167,7 @@ public class TabCreateFields extends SvrProcess
 				}
 				if (column.getAD_Reference_ID() == DisplayType.Text) {
 					field.setNumLines(3);
-				} else if (column.getAD_Reference_ID() == DisplayType.TextLong) {
+				} else if (column.getAD_Reference_ID() == DisplayType.TextLong || column.getAD_Reference_ID() == DisplayType.JSON) {
 					field.setNumLines(5);
 				} else if (column.getAD_Reference_ID() == DisplayType.Memo) {
 					field.setNumLines(8);
