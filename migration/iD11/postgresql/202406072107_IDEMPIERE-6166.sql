dc5075cf9d6afff96d56df1a325c2f63edec7e77
Carlos Ruiz <carg67@gmail.com>
2024-06-07 21:11:27 +0200

    IDEMPIERE-6166 PostgreSQL DUAL table with more than one record
    - create unique index on dual.dummy for postgresql

---
+++ migration/iD11/postgresql/202406072107_IDEMPIERE-6166.sql
@@ -0,0 +1,6 @@
+-- IDEMPIERE-6166 PostgreSQL DUAL table with more than one record
+SELECT register_migration_script('202406072107_IDEMPIERE-6166.sql') FROM dual;
+
+CREATE UNIQUE INDEX idx_dual ON dual ( dummy )
+;
+
