9f14c96134f5d70590f625dbd718313ea43993d9
hengsin <hengsin@gmail.com>
2024-08-27 10:07:42 +0800

    IDEMPIERE-3040 Invoice Price Variations for Average PO costing method (#2428)
    * IDEMPIERE-3040 Invoice Price Variations for Average PO costing method
    
    Co-authored-by: dpansheriya <deepakpansheriya@gmail.com>

--- org.adempiere.base/src/org/compiere/model/MAccount.java
+++ org.adempiere.base/src/org/compiere/model/MAccount.java
@@ -277,13 +277,24 @@ public class MAccount extends X_C_ValidCombination implements ImmutablePOSupport
 	 */
 	public static MAccount get (X_Fact_Acct fa)
 	{
+		return get(fa, (String)null);
+	}
+	
+	/**
+	 * 	Get from existing Accounting fact
+	 *	@param fa accounting fact
+	 *  @param trxName
+	 *	@return account
+	 */
+	public static MAccount get (X_Fact_Acct fa, String trxName)
+	{
 		MAccount acct = get (fa.getCtx(),
 			fa.getAD_Client_ID(), fa.getAD_Org_ID(), fa.getC_AcctSchema_ID(), 
 			fa.getAccount_ID(), fa.getC_SubAcct_ID(),
 			fa.getM_Product_ID(), fa.getC_BPartner_ID(), fa.getAD_OrgTrx_ID(), 
 			fa.getC_LocFrom_ID(), fa.getC_LocTo_ID(), fa.getC_SalesRegion_ID(), 
 			fa.getC_Project_ID(), fa.getC_Campaign_ID(), fa.getC_Activity_ID(),
-			fa.getUser1_ID(), fa.getUser2_ID(), fa.getUserElement1_ID(), fa.getUserElement2_ID());
+			fa.getUser1_ID(), fa.getUser2_ID(), fa.getUserElement1_ID(), fa.getUserElement2_ID(), trxName);
 		return acct;
 	}	//	get
 	
