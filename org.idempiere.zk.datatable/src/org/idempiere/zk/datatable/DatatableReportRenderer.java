46a28501beb033fc1e22cd736a3848dcc1d896c0
hieplq <hieplq@hasuvimex.vn>
2024-11-10 19:37:18 +0700

    IDEMPIERE-6255:Incorrect column count on datatables

--- org.idempiere.zk.datatable/src/org/idempiere/zk/datatable/DatatableReportRenderer.java
+++ org.idempiere.zk.datatable/src/org/idempiere/zk/datatable/DatatableReportRenderer.java
@@ -436,15 +436,17 @@ public class DatatableReportRenderer implements IReportRenderer<DatatableReportR
 						if (item.isNextLine() && item.getBelowColumn() >= 1)
 							continue;
 						var printName = item.getPrintName(language);
+						
+						th th = new th();
+						th.addAttribute("data-dt-order", "disable");
+						tr.addElement(th);
+						input searchInput = new input();
+						
 						if (!Util.isEmpty(printName))
-						{
-							th th = new th();
-							th.addAttribute("data-dt-order", "disable");
-							tr.addElement(th);
-							input searchInput = new input();
-							searchInput.addAttribute("placeholder", "Search "+printName);								
-							th.addElement(searchInput);
-						}
+							searchInput.addAttribute("placeholder", "Search "+printName);
+
+						th.addElement(searchInput);
+						
 					}
 				}
 				thead.addElement(tr);
