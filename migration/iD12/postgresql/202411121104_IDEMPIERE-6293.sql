d64cf444fdc282851c72aff9fca666b3ca77a5a6
Carlos Ruiz <carg67@gmail.com>
2024-11-24 17:12:57 +0100

    Update 202411121104_IDEMPIERE-6293.sql

--- migration/iD12/postgresql/202411121104_IDEMPIERE-6293.sql
+++ migration/iD12/postgresql/202411121104_IDEMPIERE-6293.sql
@@ -581,9 +581,6 @@ UPDATE AD_Field SET EntityType='D',Updated=TO_TIMESTAMP('2024-11-12 12:27:25','Y
 UPDATE AD_Field SET SeqNo=90, ColumnSpan=2,Updated=TO_TIMESTAMP('2024-11-12 12:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=10 WHERE AD_Field_ID=208544
 ;
 
--- IDEMPIERE-6293
-SELECT register_migration_script('202411221344_IDEMPIERE-6293.sql') FROM dual;
-
 -- Nov 22, 2024, 1:44:20 PM CET
 UPDATE AD_Window SET Name='Theme Customization',Updated=TO_TIMESTAMP('2024-11-22 13:44:20','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=10 WHERE AD_Window_ID=200151
 ;
