38f452e4b0a37d3a928107f746af530c15fb2b1c
Carlos Ruiz <carg67@gmail.com>
2024-11-26 12:55:40 +0100

    IDEMPIERE-6314 Issues with attachment on System records (#2556)
    * IDEMPIERE-6314 Issues with attachment on System records
    
    * - change approach to make it read-only
    
    * - support for UUID key tables
    - IDEMPIERE-5567 fix for the PO.is_new method

--- org.adempiere.base/src/org/compiere/model/MAttachment.java
+++ org.adempiere.base/src/org/compiere/model/MAttachment.java
@@ -38,6 +38,7 @@ import org.compiere.tools.FileUtil;
 import org.compiere.util.CLogger;
 import org.compiere.util.DB;
 import org.compiere.util.Env;
+import org.compiere.util.Msg;
 import org.compiere.util.Util;
 
 /**
@@ -55,9 +56,9 @@ import org.compiere.util.Util;
 public class MAttachment extends X_AD_Attachment
 {
 	/**
-	 * generated serial id
+	 * 
 	 */
-	private static final long serialVersionUID = 5615231734722570658L;
+	private static final long serialVersionUID = 5422581050563711060L;
 
 	/**
 	 * @param ctx
@@ -209,7 +210,31 @@ public class MAttachment extends X_AD_Attachment
 	/** string replaces the attachment root in stored xml file
 	 * to allow the changing of the attachment root. */
 	public final String ATTACHMENT_FOLDER_PLACEHOLDER = "%ATTACHMENT_FOLDER%";
-	
+
+	/* Attachment files can be read, but not written/deleted */
+	private Boolean isReadOnly = null;
+
+	/**
+	 * If the related record is on System and the user is operating on Tenant, the attachment is read-only
+	 * @return
+	 */
+	public boolean isReadOnly() {
+		if (isReadOnly == null) {
+			isReadOnly = true;
+			MTable table = MTable.get(getAD_Table_ID());
+			if (table != null) {
+				PO po = null;
+				if (table.isUUIDKeyTable())
+					po = table.getPOByUU(getRecord_UU(), get_TrxName());
+				else
+					po = table.getPO(getRecord_ID(), get_TrxName());
+				if (po != null && ! po.is_new() && po.getAD_Client_ID() == Env.getAD_Client_ID(getCtx()))
+					isReadOnly = false;
+			}
+		}
+		return isReadOnly;
+	}
+
 	/**
 	 * Initialize storage provider
 	 * @param ctx
@@ -427,6 +452,8 @@ public class MAttachment extends X_AD_Attachment
 	 * @return true if deleted
 	 */
 	public boolean deleteEntry(int index) {
+		if (isReadOnly())
+			throw new AdempiereException(Msg.getMsg(getCtx(), "R/O"));
 		if (m_items == null)
 			loadLOBData();
 		if (index >= 0 && index < m_items.size()) {
@@ -570,6 +597,8 @@ public class MAttachment extends X_AD_Attachment
 	@Override
 	protected boolean beforeSave (boolean newRecord)
 	{
+		if (isReadOnly())
+			throw new AdempiereException(Msg.getMsg(getCtx(), "R/O"));
 		if (Util.isEmpty(getTitle()))
 			setTitle(NONE);
 		if (getRecord_ID() > 0 && getAD_Table_ID() > 0 && Util.isEmpty(getRecord_UU())) {
@@ -580,6 +609,13 @@ public class MAttachment extends X_AD_Attachment
 		}
 		return saveLOBData();		//	save in BinaryData
 	}	//	beforeSave
+
+	@Override
+	protected boolean beforeDelete() {
+		if (isReadOnly())
+			throw new AdempiereException(Msg.getMsg(getCtx(), "R/O"));
+		return true;
+	}
 
 	/**
 	 * 	Ask storage provider to remove attachment content
