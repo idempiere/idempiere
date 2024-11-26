be264655dcc65fe21568ac3b3658d87a1f2b5640
Nicolas Micoud <58596990+nmicoud@users.noreply.github.com>
2024-11-26 13:07:41 +0100

    IDEMPIERE-6316: Wrong Name on RecordInfo panel when the value of the … (#2560)
    * IDEMPIERE-6316: Wrong Name on RecordInfo panel when the value of the column is Context
    
    * IDEMPIERE-6316: Wrong Name on RecordInfo panel when the value of the column is Context
    
    Change all translate to getElement
    
    * IDEMPIERE-6316: Wrong Name on RecordInfo panel when the value of the column is Context - patch from Carlos
    
    Co-Authored-By: Carlos Ruiz <carg67@gmail.com>
    
    ---------
    
    Co-authored-by: Carlos Ruiz <carg67@gmail.com>

--- org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/window/WRecordInfo.java
+++ org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/window/WRecordInfo.java
@@ -171,6 +171,7 @@ public class WRecordInfo extends Window implements EventListener<Event>
 	/** Number Format		*/
 	private DecimalFormat		m_intFormat = DisplayType.getNumberFormat
 		(DisplayType.Integer, Env.getLanguage(Env.getCtx()));
+	private int windowNo;
 
 	/**
 	 * 	Layout dialog
@@ -281,7 +282,7 @@ public class WRecordInfo extends Window implements EventListener<Event>
 		//  Info
 		MUser user = MUser.get(Env.getCtx(), dse.CreatedBy.intValue());
 		m_info.append(" ")
-			.append(Msg.translate(Env.getCtx(), "CreatedBy"))
+			.append(Msg.getElement(Env.getCtx(), "CreatedBy"))
 			.append(": ").append(user.getName())
 			.append(" - ").append(m_dateTimeFormat.format(dse.Created)).append("\n");
 		
@@ -291,7 +292,7 @@ public class WRecordInfo extends Window implements EventListener<Event>
 			if (!dse.CreatedBy.equals(dse.UpdatedBy))
 				user = MUser.get(Env.getCtx(), dse.UpdatedBy.intValue());
 			m_info.append(" ")
-				.append(Msg.translate(Env.getCtx(), "UpdatedBy"))
+				.append(Msg.getElement(Env.getCtx(), "UpdatedBy"))
 				.append(": ").append(user.getName())
 				.append(" - ").append(m_dateTimeFormat.format(dse.Updated)).append("\n");
 		}
@@ -304,19 +305,25 @@ public class WRecordInfo extends Window implements EventListener<Event>
 		if (gridTab != null)
 		{
 			gridTable = gridTab.getTableModel();
+			windowNo = gridTab.getWindowNo();
 		}
 		else if (dse.getSource() instanceof GridTab) 
 		{
 			gridTab = (GridTab) dse.getSource();
 			gridTable = gridTab.getTableModel();			
 			tabName = gridTab.getName();
+			windowNo = gridTab.getWindowNo();
 		}
 		else if (dse.getSource() instanceof GridTable)
 		{
 			gridTable = (GridTable) dse.getSource();
 			GridField firstField = gridTable.getField(0);
-			if (firstField != null && firstField.getGridTab() != null)
-				tabName = firstField.getGridTab().getName();
+			if (firstField != null) {
+				windowNo = firstField.getWindowNo();
+				if (firstField.getGridTab() != null) {
+					tabName = firstField.getGridTab().getName();
+				}
+			}
 		}
 
 		int Record_ID = -1;
@@ -420,12 +427,12 @@ public class WRecordInfo extends Window implements EventListener<Event>
 		
 		//
 		ArrayList<String> columnNames = new ArrayList<String>();
-		columnNames.add(Msg.translate(Env.getCtx(), "Name"));
-		columnNames.add(Msg.translate(Env.getCtx(), "NewValue"));
-		columnNames.add(Msg.translate(Env.getCtx(), "OldValue"));
-		columnNames.add(Msg.translate(Env.getCtx(), "UpdatedBy"));
-		columnNames.add(Msg.translate(Env.getCtx(), "Updated"));
-		columnNames.add(Msg.translate(Env.getCtx(), "AD_Column_ID"));
+		columnNames.add(Msg.getElement(Env.getCtx(), "Name"));
+		columnNames.add(Msg.getElement(Env.getCtx(), "NewValue"));
+		columnNames.add(Msg.getElement(Env.getCtx(), "OldValue"));
+		columnNames.add(Msg.getElement(Env.getCtx(), "UpdatedBy"));
+		columnNames.add(Msg.getElement(Env.getCtx(), "Updated"));
+		columnNames.add(Msg.getElement(Env.getCtx(), "AD_Column_ID"));
 		
 		Listhead listhead = new Listhead();
 		listhead.setSizable(true);
@@ -458,7 +465,7 @@ public class WRecordInfo extends Window implements EventListener<Event>
 		Vector<String> line = new Vector<String>();
 		//	Column
 		MColumn column = MColumn.get (Env.getCtx(), AD_Column_ID);
-		line.add(Msg.translate(Env.getCtx(), column.getColumnName()));
+		line.add(Msg.getElement(Env.getCtx(), column.getColumnName(), Env.isSOTrx(Env.getCtx(), windowNo)));
 		//
 		if (OldValue != null && OldValue.equals(MChangeLog.NULL))
 			OldValue = null;
