ac067daca1ee249e01887fd238d39478d97b2def
jose.leite <jose.leite@devcoffee.com.br>
2024-12-06 09:22:30 -0300

    IDEMPIERE-6336 - Ajust getList method in reflist

--- org.adempiere.base/src/org/compiere/model/MRefList.java
+++ org.adempiere.base/src/org/compiere/model/MRefList.java
@@ -276,6 +276,7 @@ public class MRefList extends X_AD_Ref_List implements ImmutablePOSupport
 
 		String language = Env.getAD_Language(ctx);
 		boolean orderByValue = MReference.get(AD_Reference_ID).isOrderByValue();
+		boolean isShowInactive = MReference.get(AD_Reference_ID).isShowInactiveRecords();
 		if (Util.isEmpty(orderBy) || "N".equals(orderBy))
 			orderByValue = false;
 		else if ("V".equals(orderBy))
@@ -303,8 +304,10 @@ public class MRefList extends X_AD_Ref_List implements ImmutablePOSupport
 			.append("FROM AD_Ref_List INNER JOIN AD_Ref_List_Trl trl ")
 			.append(" ON (AD_Ref_List.AD_Ref_List_ID=trl.AD_Ref_List_ID AND trl.AD_Language='")
 			.append(language).append("')");
-		sql.append(" WHERE AD_Ref_List.AD_Reference_ID=").append(AD_Reference_ID)
-			.append(" AND AD_Ref_List.IsActive='Y'");
+		sql.append(" WHERE AD_Ref_List.AD_Reference_ID=").append(AD_Reference_ID);
+		
+		if(!isShowInactive)
+			sql.append(" AND AD_Ref_List.IsActive='Y'");
 
 		if (!Util.isEmpty(additionalWhereClause, true))
 			sql.append(" AND (").append(additionalWhereClause).append(")");
