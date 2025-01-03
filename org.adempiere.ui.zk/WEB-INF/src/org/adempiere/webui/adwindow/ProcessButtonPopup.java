627e179b9c3c25379beee28ec3868fcb140431e4
vitor.santos <vitor.santos@devcoffee.com.br>
2025-01-03 09:56:51 -0300

    IDEMPIERE-6361 - Document being closed when user does not have read and write access

--- org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/adwindow/ProcessButtonPopup.java
+++ org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/adwindow/ProcessButtonPopup.java
@@ -105,12 +105,15 @@ public class ProcessButtonPopup extends Menupopup implements EventListener<Event
 		//document actions
 		if (docActionBtn != null) {
 			IProcessButton processButton = (IProcessButton) docActionBtn.getAttribute(WButtonEditor.EDITOR_ATTRIBUTE);
+			GridTab gridTab = processButton.getADTabpanel().getGridTab();
+			Boolean isReadOnly = gridTab.getVO().IsReadOnly;
 			WDocActionPanel actionPanel = new WDocActionPanel(processButton.getADTabpanel().getGridTab(), true);
 			List<Listitem> actions = actionPanel.getDocActionItems();
 			if (actions.size() > 0) {
 				if (this.getChildren().size() > 0)
 					appendChild(new Menuseparator());
 				Menu menu = new Menu(Msg.getElement(Env.getCtx(), "DocAction"));
+				menu.setDisabled(isReadOnly);
 				appendChild(menu);
 				Menupopup popup = new Menupopup();
 				popup.setAttribute(BUTTON_ATTRIBUTE, docActionBtn);
