81bd1572cd5d1ce03b91b2c69913e428f894ed9b
Vitor Henrique Dos Santos <82419632+VitorHenri@users.noreply.github.com>
2025-01-16 13:55:06 -0300

    IDEMPIERE-6383 (#2639)

---
+++ migration/iD12/postgresql/202501131651_IDEMPIERE-6383.sql
@@ -0,0 +1,9 @@
+-- IDEMPIERE-6383
+SELECT register_migration_script('202501131651_IDEMPIERE-6383.sql') FROM dual;
+
+
+
+-- Jan 13, 2025, 4:52:04 PM BRT
+INSERT INTO AD_Message (MsgType,MsgText,AD_Client_ID,AD_Org_ID,IsActive,Created,CreatedBy,Updated,UpdatedBy,AD_Message_ID,Value,EntityType,AD_Message_UU) VALUES ('E','Total of Base values is 0 - {0}',0,0,'Y',TO_TIMESTAMP('2025-01-13 16:52:04','YYYY-MM-DD HH24:MI:SS'),100,TO_TIMESTAMP('2025-01-13 16:52:04','YYYY-MM-DD HH24:MI:SS'),100,200926,'BaseValuesTotalZero','D','a275f281-e73a-4b6c-96c0-17749f2f3b15')
+;
+
