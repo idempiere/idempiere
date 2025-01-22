c1793fcca1b5ac64e7b41dbd7c9891d6aa627238
Diego Ruiz <ruizg.diego@gmail.com>
2025-01-22 13:28:12 +0100

    IDEMPIERE-4925 - Adding column for UOM and QtyEntered on Inventory Move - fix javadoc client/tenant

--- org.adempiere.base/src/org/compiere/model/X_M_MovementLine.java
+++ org.adempiere.base/src/org/compiere/model/X_M_MovementLine.java
@@ -33,7 +33,7 @@ public class X_M_MovementLine extends PO implements I_M_MovementLine, I_Persiste
 	/**
 	 *
 	 */
-	private static final long serialVersionUID = 20250110L;
+	private static final long serialVersionUID = 20250122L;
 
     /** Standard Constructor */
     public X_M_MovementLine (Properties ctx, int M_MovementLine_ID, String trxName)
