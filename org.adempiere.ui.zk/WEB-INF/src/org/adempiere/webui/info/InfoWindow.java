a0dd8f3391975bd7a1a80ff2c052d054cc62412c
vitor.santos <vitor.santos@devcoffee.com.br>
2025-03-05 14:01:53 -0300

    IDEMPIERE-6408 - Problem when sorting columns in an Info Window with Distinct(Patch Applied)

--- org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/info/InfoWindow.java
+++ org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/info/InfoWindow.java
@@ -2288,8 +2288,10 @@ public class InfoWindow extends InfoPanel implements ValueChangeListener, EventL
         // for SELECT DISTINCT, ORDER BY expressions must appear in select list - applies for lookup columns and multiselection columns
         if(dataSql.startsWith("SELECT DISTINCT") && indexOrderColumn > 0) {
         	ColumnInfo orderColumnInfo = p_layout[indexOrderColumn];
-        	if (DisplayType.isLookup(orderColumnInfo.getAD_Reference_ID()) || DisplayType.isChosenMultipleSelection(orderColumnInfo.getAD_Reference_ID())) {
-        		dataSql = appendOrderByToSelectList(dataSql, orderClause,orderColumnInfo);
+        	if (   !Util.isEmpty(orderColumnInfo.getDisplayColumn())
+        		&& (   DisplayType.isLookup(orderColumnInfo.getAD_Reference_ID())
+        			|| DisplayType.isChosenMultipleSelection(orderColumnInfo.getAD_Reference_ID()))) {
+        		dataSql = appendOrderByToSelectList(dataSql, orderClause);
         	}
         }
 
