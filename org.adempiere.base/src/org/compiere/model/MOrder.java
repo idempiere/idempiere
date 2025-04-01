8c2878ce932237fa071f8d50750a4265ad4dfd07
vitor.santos <vitor.santos@devcoffee.com.br>
2025-04-01 10:55:24 -0300

    IDEMPIERE-6476 - Payment schedule is not getting updated with header record organization

--- org.adempiere.base/src/org/compiere/model/MOrder.java
+++ org.adempiere.base/src/org/compiere/model/MOrder.java
@@ -1465,6 +1465,12 @@ public class MOrder extends X_C_Order implements DocAction
 					line.setC_Currency_ID(getC_Currency_ID());
 				line.saveEx();
 			}
+			MOrderPaySchedule[] orderPaySchedules = MOrderPaySchedule.getOrderPaySchedule(getCtx(),this.getC_Order_ID(),0,get_TrxName());
+			for(MOrderPaySchedule orderPaySchedule : orderPaySchedules) {
+				if(is_ValueChanged(MOrder.COLUMNNAME_AD_Org_ID))
+					orderPaySchedule.setAD_Org_ID(getAD_Org_ID());
+				orderPaySchedule.saveEx();
+			}
 		}
 		//
 		return true;
