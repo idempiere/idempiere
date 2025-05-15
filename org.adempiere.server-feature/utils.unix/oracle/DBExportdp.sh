35aac91b0a14e5f8c0f01c6edbdd8b98b19a0012
hengsin <hengsin@gmail.com>
2025-05-15 23:26:18 +0800

    IDEMPIERE-6540 Import/Export script doesn't support Oracle running as… (#2790)
    * IDEMPIERE-6540 Import/Export script doesn't support Oracle running as Docker Container
    
    * IDEMPIERE-6540 Import/Export script doesn't support Oracle running as Docker Container
    
    - Remove Oracle use of "alter user x rename to y" as it is an
    undocumented Oracle feature that have been dropped.
    
    * IDEMPIERE-6540 Import/Export script doesn't support Oracle running as Docker Container
    
    - Replace use of local sqlplus client with docker exec -i sqlplus
    
    * IDEMPIERE-6540 Import/Export script doesn't support Oracle running as Docker Container
    
    Avoid potential "ORA-02304: invalid object identifier literal error" for
    the import of custom type  TABLE_OF_VARCHAR2
    
    * IDEMPIERE-6540 Import/Export script doesn't support Oracle running as Docker Container
    
    Merge patch from Carlos

--- org.adempiere.server-feature/utils.unix/oracle/DBExportdp.sh
+++ org.adempiere.server-feature/utils.unix/oracle/DBExportdp.sh
@@ -21,18 +21,46 @@ if [ "$IDEMPIERE_HOME" = "" ] || [ "$ADEMPIERE_DB_NAME" = "" ]
     exit 1
 fi
 
+DAILY_SCRIPT="$IDEMPIERE_HOME"/utils/"$ADEMPIERE_DB_PATH"/Daily.sql
+CREATE_DATAPUMP_DIR_SCRIPT="$IDEMPIERE_HOME"/utils/"$ADEMPIERE_DB_PATH"/CreateDataPumpDir.sql
+DOCKER_EXEC=
+if [ -n "$ORACLE_DOCKER_CONTAINER" ]; then
+  DOCKER_EXEC="docker exec -i $ORACLE_DOCKER_CONTAINER"
+  ORACLE_DOCKER_HOME=${ORACLE_DOCKER_HOME:-/opt/oracle}
+  $DOCKER_EXEC mkdir -p "$ORACLE_DOCKER_HOME"/idempiere/data
+  $DOCKER_EXEC mkdir -p "$ORACLE_DOCKER_HOME"/idempiere/script
+  docker cp "$DAILY_SCRIPT" "$ORACLE_DOCKER_CONTAINER:$ORACLE_DOCKER_HOME"/idempiere/script
+  docker cp "$CREATE_DATAPUMP_DIR_SCRIPT" "$ORACLE_DOCKER_CONTAINER:$ORACLE_DOCKER_HOME"/idempiere/script
+  docker exec -u 0 "$ORACLE_DOCKER_CONTAINER" chown -R oracle:dba "$ORACLE_DOCKER_HOME"/idempiere
+  DAILY_SCRIPT="$ORACLE_DOCKER_HOME"/idempiere/script/Daily.sql
+  CREATE_DATAPUMP_DIR_SCRIPT="$ORACLE_DOCKER_HOME"/idempiere/script/CreateDataPumpDir.sql
+fi
+
 # Cleanup
-sqlplus -S "$1"/"$2"@"$ADEMPIERE_DB_SERVER":"$ADEMPIERE_DB_PORT"/"$ADEMPIERE_DB_NAME" @"$IDEMPIERE_HOME"/utils/"$ADEMPIERE_DB_PATH"/Daily.sql
+$DOCKER_EXEC sqlplus -S "$1"/"$2"@"$ADEMPIERE_DB_SERVER":"$ADEMPIERE_DB_PORT"/"$ADEMPIERE_DB_NAME" @"$DAILY_SCRIPT"
+
+DATAPUMP_HOME="$IDEMPIERE_HOME"
+if [ -n "$ORACLE_DOCKER_CONTAINER" ]; then
+  DATAPUMP_HOME="$ORACLE_DOCKER_HOME"/idempiere
+fi
 
 echo -------------------------------------
 echo Re-Create DataPump directory
 echo -------------------------------------
-sqlplus -S "$3"@"$ADEMPIERE_DB_SERVER":"$ADEMPIERE_DB_PORT"/"$ADEMPIERE_DB_NAME" @"$IDEMPIERE_HOME"/utils/"$ADEMPIERE_DB_PATH"/CreateDataPumpDir.sql "$IDEMPIERE_HOME"/data
-chgrp dba "$IDEMPIERE_HOME"/data
-chmod 770 "$IDEMPIERE_HOME"/data
+$DOCKER_EXEC sqlplus -S "$3"@"$ADEMPIERE_DB_SERVER":"$ADEMPIERE_DB_PORT"/"$ADEMPIERE_DB_NAME" @"$CREATE_DATAPUMP_DIR_SCRIPT" "$DATAPUMP_HOME"/data
+
+if [ -z "$ORACLE_DOCKER_CONTAINER" ]; then
+  chgrp dba "$IDEMPIERE_HOME"/data
+  chmod 770 "$IDEMPIERE_HOME"/data
+fi
 
 # Export
-expdp "$1"/"$2"@"$ADEMPIERE_DB_SERVER":"$ADEMPIERE_DB_PORT"/"$ADEMPIERE_DB_NAME" DIRECTORY=ADEMPIERE_DATA_PUMP_DIR DUMPFILE=ExpDat_"$DATE".dmp LOGFILE=ExpDat_"$DATE".log EXCLUDE=STATISTICS SCHEMAS="$1"
+$DOCKER_EXEC expdp "$1"/"$2"@"$ADEMPIERE_DB_SERVER":"$ADEMPIERE_DB_PORT"/"$ADEMPIERE_DB_NAME" DIRECTORY=ADEMPIERE_DATA_PUMP_DIR DUMPFILE=ExpDat_"$DATE".dmp LOGFILE=ExpDat_"$DATE".log EXCLUDE=STATISTICS SCHEMAS="$1"
+
+if [ -n "$ORACLE_DOCKER_CONTAINER" ]; then
+  docker cp "$ORACLE_DOCKER_CONTAINER:$DATAPUMP_HOME"/data/ExpDat_"$DATE".dmp "$IDEMPIERE_HOME"/data
+  docker cp "$ORACLE_DOCKER_CONTAINER:$DATAPUMP_HOME"/data/ExpDat_"$DATE".log "$IDEMPIERE_HOME"/data
+fi
 
 cd "$IDEMPIERE_HOME"/data || exit
 jar cvfM ExpDat.jar ExpDat_"$DATE".dmp ExpDat_"$DATE".log
