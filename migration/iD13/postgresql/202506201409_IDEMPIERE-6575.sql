2afdb5caee3b97f1e6b2167406912f07c0a920ee
Peter Takács <takacspeti300@gmail.com>
2025-06-23 09:59:12 +0200

    IDEMPIERE-6575: Fixed migration scripts

--- migration/iD13/postgresql/202506201409_IDEMPIERE-6575.sql
+++ migration/iD13/postgresql/202506201409_IDEMPIERE-6575.sql
@@ -13,7 +13,11 @@ INSERT INTO AD_IndexColumn (AD_Client_ID,AD_Org_ID,AD_IndexColumn_ID,AD_IndexCol
 INSERT INTO AD_IndexColumn (AD_Client_ID,AD_Org_ID,AD_IndexColumn_ID,AD_IndexColumn_UU,Created,CreatedBy,EntityType,IsActive,Updated,UpdatedBy,AD_Column_ID,AD_TableIndex_ID,SeqNo) VALUES (0,0,201763,'bf557da7-d608-456d-a31a-ea6639c9f8af',TO_TIMESTAMP('2025-06-20 14:10:21','YYYY-MM-DD HH24:MI:SS'),100,'D','Y',TO_TIMESTAMP('2025-06-20 14:10:21','YYYY-MM-DD HH24:MI:SS'),100,262,201289,20)
 ;
 
--- Jun 20, 2025, 2:10:35 PM CEST
-ALTER TABLE AD_Sequence_No ADD CONSTRAINT ad_sequence_no_ad_org_sequencekey UNIQUE (AD_Org_ID,SequenceKey)
+-- Jun 23, 2025, 8:37:21 AM CEST
+INSERT INTO AD_IndexColumn (AD_Client_ID,AD_Org_ID,AD_IndexColumn_ID,AD_IndexColumn_UU,Created,CreatedBy,EntityType,IsActive,Updated,UpdatedBy,AD_Column_ID,AD_TableIndex_ID,SeqNo) VALUES (0,0,201764,'4d8306ee-88ba-4d06-a037-4c517597eeb2',TO_TIMESTAMP('2025-06-23 08:37:20','YYYY-MM-DD HH24:MI:SS'),100,'D','Y',TO_TIMESTAMP('2025-06-23 08:37:20','YYYY-MM-DD HH24:MI:SS'),100,1197,201289,5)
+;
+
+-- Jun 23, 2025, 8:37:24 AM CEST
+ALTER TABLE AD_Sequence_No ADD CONSTRAINT ad_sequence_no_ad_org_sequencekey UNIQUE (AD_Sequence_ID,AD_Org_ID,SequenceKey)
 ;
 
