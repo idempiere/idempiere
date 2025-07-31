e455a84fc8df2e2fefa9b202bcbfce3905e0f7eb
Peter Takács <takacspeti300@gmail.com>
2025-07-31 09:55:58 +0200

    Revert "IDEMPIERE-6575: Fixed Display Logic on Sequence No tab with regex"
    This reverts commit 0516cfd99f48df440dd1f4a49bf088d4b7a91796.

--- migration/iD13/oracle/202507281259_IDEMPIERE-6575.sql
+++ migration/iD13/oracle/202507281259_IDEMPIERE-6575.sql
@@ -5,6 +5,6 @@ SET SQLBLANKLINES ON
 SET DEFINE OFF
 
 -- Jul 28, 2025, 12:59:31 PM CEST
-UPDATE AD_Tab SET DisplayLogic='@StartNewYear@=''Y'' | @IsOrgLevelSequence@=Y | @Prefix@ ~ ''.*/K.*'' | @Suffix@ ~ ''.*/K.*''',Updated=TO_TIMESTAMP('2025-07-28 12:59:31','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=200001
+UPDATE AD_Tab SET DisplayLogic=NULL,Updated=TO_TIMESTAMP('2025-07-28 12:59:31','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=200001
 ;
 
