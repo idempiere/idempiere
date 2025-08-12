590ec1cfea89a6b5e153ea823f7e4252b15a46ff
Carlos Ruiz <carg67@gmail.com>
2025-08-12 05:06:55 +0200

    IDEMPIERE-6648 Ensure all scripts can run with an alternate db admin user (DAD-62) (#2882)
    * IDEMPIERE-6648 Ensure all scripts can run with an alternate db admin user (DAD-62)
    
    - cherry-pick changes from pull request 2855
    
    Co-authored-by: rpobulic <rade.pobulic@gmail.com>
    Co-authored-by: hengsin <hengsin@gmail.com>
    
    * - change all scripts to work with ADEMPIERE_DB_SYSTEM_USER variable
    
    ---------
    
    Co-authored-by: rpobulic <rade.pobulic@gmail.com>
    Co-authored-by: hengsin <hengsin@gmail.com>

--- org.adempiere.server-feature/utils.unix/RUN_ImportIdempiere.sh
+++ org.adempiere.server-feature/utils.unix/RUN_ImportIdempiere.sh
@@ -9,12 +9,17 @@ export ID_ENV=Server
 . ./myEnvironment.sh
 echo Import idempiere - "$IDEMPIERE_HOME" \("$ADEMPIERE_DB_NAME"\)
 
+if [ -z "$ADEMPIERE_DB_SYSTEM_USER" ]; then
+    if [ "$ADEMPIERE_DB_PATH" = "postgresql" ]; then
+        ADEMPIERE_DB_SYSTEM_USER=postgres
+    else
+        ADEMPIERE_DB_SYSTEM_USER=SYSTEM
+    fi
+fi
+
 SUFFIX=""
-SYSUSER=system
-if [ "$ADEMPIERE_DB_PATH" = "postgresql" ]
-then
+if [ "$ADEMPIERE_DB_PATH" = "postgresql" ]; then
     SUFFIX="_pg"
-    SYSUSER=postgres
 fi
 
 echo Re-Create idempiere User and import "$IDEMPIERE_HOME"/data/seed/Adempiere${SUFFIX}.dmp - \("$ADEMPIERE_DB_NAME"\)
@@ -26,9 +31,8 @@ ls -lsa "$IDEMPIERE_HOME"/data/seed/Adempiere${SUFFIX}.dmp
 echo Press enter to continue ...
 read -r _
 
-# Parameter: <systemAccount> <AdempiereID> <AdempierePwd>
-# globalqss - cruiz - 2007-10-09 - added fourth parameter for postgres(ignored in oracle)
-"$ADEMPIERE_DB_PATH"/ImportIdempiere.sh "$SYSUSER/$ADEMPIERE_DB_SYSTEM" "$ADEMPIERE_DB_USER" "$ADEMPIERE_DB_PASSWORD" "$ADEMPIERE_DB_SYSTEM" "$SUFFIX"
+# Parameter: <adempiereID> <adempierePwd> <systemUser> <systemPwd> <dmpSuffix>
+"$ADEMPIERE_DB_PATH"/ImportIdempiere.sh "$ADEMPIERE_DB_USER" "$ADEMPIERE_DB_PASSWORD" "$ADEMPIERE_DB_SYSTEM_USER" "$ADEMPIERE_DB_SYSTEM" "$SUFFIX"
 
 ./RUN_SyncDB.sh
 
--- org.adempiere.server-feature/utils.windows/RUN_ImportIdempiere.bat
+++ org.adempiere.server-feature/utils.windows/RUN_ImportIdempiere.bat
@@ -4,9 +4,14 @@
 @Title Import idempiere - %IDEMPIERE_HOME% (%ADEMPIERE_DB_NAME%)
 
 SET SUFFIX=
-SET SYSUSER=system
 if (%ADEMPIERE_DB_PATH%) == (postgresql) SET SUFFIX=_pg
-if (%ADEMPIERE_DB_PATH%) == (postgresql) SET SYSUSER=postgres
+if "%ADEMPIERE_DB_SYSTEM_USER%" == "" (
+    if "%ADEMPIERE_DB_PATH%" == "postgresql" (
+        SET ADEMPIERE_DB_SYSTEM_USER=postgres
+    ) else (
+        SET ADEMPIERE_DB_SYSTEM_USER=SYSTEM
+    )
+)
 
 @echo Re-Create idempiere User and import %IDEMPIERE_HOME%\data\Adempiere.dmp - (%ADEMPIERE_DB_NAME%)
 cd %IDEMPIERE_HOME%\data\seed
@@ -16,9 +21,8 @@ cd %IDEMPIERE_HOME%\utils
 @echo == The import will show warnings. This is OK ==
 @pause
 
-@Rem Parameter: <systemAccount> <AdempiereID> <AdempierePwd>
-@Rem globalqss - cruiz - 2007-10-09 - added fourth parameter for postgres (ignored in oracle)
-@call %ADEMPIERE_DB_PATH%\ImportIdempiere %SYSUSER%/%ADEMPIERE_DB_SYSTEM% %ADEMPIERE_DB_USER% %ADEMPIERE_DB_PASSWORD% %ADEMPIERE_DB_SYSTEM% %SUFFIX%
+@Rem Parameter: <AdempiereID> <AdempierePwd> <systemUser> <systemPwd>
+@call %ADEMPIERE_DB_PATH%\ImportIdempiere %ADEMPIERE_DB_USER% %ADEMPIERE_DB_PASSWORD% %ADEMPIERE_DB_SYSTEM_USER% %ADEMPIERE_DB_SYSTEM% %SUFFIX%
 
 @call RUN_SyncDB
 
