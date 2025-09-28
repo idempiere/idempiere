75a86d1c7bce9e883d5e018dafe950f5738125e4
Elaine Tan <51374241+etantg@users.noreply.github.com>
2025-09-28 22:15:59 +0800

    IDEMPIERE-6659 Estimated Landed Cost Scenarios for Purchase Orders and Incorrect Product Costs (#2905)
    * IDEMPIERE-6659 Estimated Landed Cost Scenarios for Purchase Orders and Incorrect Product Costs

--- org.adempiere.base/src/org/compiere/model/MCostDetail.java
+++ org.adempiere.base/src/org/compiere/model/MCostDetail.java
@@ -1434,9 +1434,28 @@ public class MCostDetail extends X_M_CostDetail
 		}
 
 		MCost cost = MCost.get(product, M_ASI_ID, as, Org_ID, ce.getM_CostElement_ID(), get_TrxName());
-		
+		MCostDetail cd = null;
+		boolean isOrderLandedCost = getC_OrderLine_ID() > 0 && getM_CostElement_ID() > 0;
+		boolean isReversedOrderLandedCost = isOrderLandedCost 
+				&& isDelta() && getDeltaQty().signum() == -1 && getDeltaAmt().signum() == -1
+				&& (ce.isAveragePO() || ce.isAverageInvoice());
+		if (isOrderLandedCost && !isReversedOrderLandedCost) {	
+			// order landed cost, get the cost info from previous order or order landed cost
+			StringBuilder whereClause = new StringBuilder();
+			whereClause.append("C_OrderLine_ID = ? ");
+			whereClause.append(" AND TRUNC(DateAcct) = "+DB.TO_DATE(getDateAcct(), true));
+			whereClause.append(" AND M_AttributeSetInstance_ID = ?");
+			whereClause.append(" AND C_AcctSchema_ID = ?");
+			whereClause.append(" AND M_CostDetail_ID < ?");
+			cd = new Query(as.getCtx(), I_M_CostDetail.Table_Name, whereClause.toString(), get_TrxName())
+					.setParameters(getC_OrderLine_ID(), M_ASI_ID, as.get_ID(), this.get_ID())
+					.setOrderBy("M_CostDetail_ID DESC")
+					.first();
+			
+		}
 		ICostInfo costInfo = MCost.getCostInfo(product.getCtx(), product.getAD_Client_ID(), Org_ID, product.getM_Product_ID(), 
-				as.getM_CostType_ID(), as.getC_AcctSchema_ID(), ce.getM_CostElement_ID(), M_ASI_ID, getDateAcct(), this, get_TrxName());
+					as.getM_CostType_ID(), as.getC_AcctSchema_ID(), ce.getM_CostElement_ID(), M_ASI_ID, getDateAcct(), 
+					cd != null ? cd : this, get_TrxName());
 		if (costInfo != null)
 		{
 			cost.setCurrentQty(costInfo.getCurrentQty());
@@ -1456,8 +1475,18 @@ public class MCostDetail extends X_M_CostDetail
 		BigDecimal amt = Env.ZERO;
 		if (isDelta())
 		{
-			qty = getDeltaQty();
-			amt = getDeltaAmt();
+			if (!isOrderLandedCost) {
+				qty = getDeltaQty();
+				amt = getDeltaAmt();
+			} else {
+				if (isReversedOrderLandedCost) {
+					qty = getDeltaQty();
+					amt = getDeltaAmt();
+				} else {
+					qty = getQty();
+					amt = getAmt();
+				}
+			}
 		}
 		else
 		{
@@ -1468,7 +1497,7 @@ public class MCostDetail extends X_M_CostDetail
 
 		//determine whether this is cost only adjustment entry
 		boolean costAdjustment = false;
-		if (this.getM_CostElement_ID() > 0 && this.getM_CostElement_ID() != ce.getM_CostElement_ID())
+		if (this.getM_CostElement_ID() > 0 && this.getM_CostElement_ID() != ce.getM_CostElement_ID() && !isReversedOrderLandedCost)
 		{
 			MCostElement thisCostElement = MCostElement.get(getCtx(), getM_CostElement_ID());
 			if (thisCostElement.getCostingMethod() == null && ce.getCostingMethod() != null)
