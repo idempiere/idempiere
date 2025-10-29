c5ee34c6be148dfaca5d5d0a06c3eeb38c0a7df0
Deepak Pansheriya <deepakpansheriya@gmail.com>
2025-10-29 13:04:06 +0530

    IDEMPIERE-6295: Fix logout issue when SSO provider is selected from multiple SSO providers

--- org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/AdempiereWebUI.java
+++ org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/AdempiereWebUI.java
@@ -516,13 +516,11 @@ public class AdempiereWebUI extends Window implements EventListener<Event>, IWeb
 	    boolean isSSOLogin = "Y".equals(Env.getContext(Env.getCtx(), Env.IS_SSO_LOGIN));
 		String provider = (String) desktop.getSession().getAttribute(ISSOPrincipalService.SSO_SELECTED_PROVIDER);
 	    String ssoLogoutURL = null;
-	    if (!isAdminLogin && (isSSOLogin && Util.isEmpty(provider)))
+	    if (!isAdminLogin && isSSOLogin)
 		{
 			ISSOPrincipalService service = SSOUtils.getSSOPrincipalService(provider);
 			if (service != null)
 				ssoLogoutURL = service.getLogoutURL();
-			else
-				throw new AdempiereException(Msg.getMsg(Env.getCtx(), "SSOServiceNotFound"));
 		}
 	    
 	    final Session session = logout0();
