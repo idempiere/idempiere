81e7d994037ee54f1aec28857e2517bb49aa2b4b
Carlos Ruiz <carg67@gmail.com>
2025-11-27 18:47:42 +0100

    - fix comment on assert

--- org.idempiere.test/src/org/idempiere/test/base/EnvTest.java
+++ org.idempiere.test/src/org/idempiere/test/base/EnvTest.java
@@ -337,7 +337,7 @@ public class EnvTest extends AbstractTestCase {
 		Env.setContext(Env.getCtx(), windowNo, "TestString", "MyString");
 		expr = "SELECT * FROM C_Charge WHERE Name='@TestString@'";
 		parsedText = Env.parseContext(Env.getCtx(), windowNo, expr, true, false, false, false);
-		assertEquals("SELECT * FROM C_Charge WHERE Name='MyString'", parsedText, "Unexpected parsed text forSQL=true for "+expr);
+		assertEquals("SELECT * FROM C_Charge WHERE Name='MyString'", parsedText, "Unexpected parsed text forSQL=false for "+expr);
 		parsedText = Env.parseContext(Env.getCtx(), windowNo, expr, true, false, false, true);
 		assertEquals("SELECT * FROM C_Charge WHERE Name='MyString'", parsedText, "Unexpected parsed text forSQL=true for "+expr);
 	}
