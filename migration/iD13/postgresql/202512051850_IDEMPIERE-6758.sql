ea90ccfefdee8759457439fb9040087d046b9b0c
Diego Ruiz <diego.ruiz@globalqss.com>
2025-12-05 18:51:38 +0100

    IDEMPIERE-6758 - In Material Receipt screen Create Lines From button for multiple Vendors Purchase Orders and Invoices Visible

---
+++ migration/iD13/postgresql/202512051850_IDEMPIERE-6758.sql
@@ -0,0 +1,7 @@
+-- IDEMPIERE-6758 - Hide Business Partner from material receipt info window
+SELECT register_migration_script('202512051850_IDEMPIERE-6758.sql') FROM dual;
+
+-- Dec 5, 2025, 6:50:50 PM CET
+UPDATE AD_InfoColumn SET IsQueryCriteria='N', IsMandatory='N',Updated=TO_TIMESTAMP('2025-12-05 18:50:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_InfoColumn_ID=200253
+;
+
