e737af71e19a00ce0714f9e83be57f8e1be2d6a0
hengsin <hengsin@gmail.com>
2026-01-05 05:01:38 +0800

    IDEMPIERE-6805 MUserRole should disallow assignment of role template … (#3047)
    * IDEMPIERE-6805 MUserRole should disallow assignment of role template to user
    
    * IDEMPIERE-6805 MUserRole should disallow assignment of role template to user

--- org.idempiere.test/src/org/idempiere/test/model/MRoleMockedTest.java
+++ org.idempiere.test/src/org/idempiere/test/model/MRoleMockedTest.java
@@ -23,19 +23,29 @@ package org.idempiere.test.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.spy;
 
 import java.util.Properties;
 
 import org.compiere.model.MRole;
+import org.compiere.model.MSysConfig;
+import org.compiere.model.MUserRoles;
 import org.compiere.model.PO;
+import org.compiere.model.Query;
+import org.compiere.util.CLogger;
 import org.compiere.util.Env;
+import org.compiere.util.Msg;
+import org.compiere.util.ValueNamePair;
 import org.idempiere.test.AbstractTestCase;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
@@ -79,5 +89,38 @@ public class MRoleMockedTest extends AbstractTestCase {
 		role.setUserLevel(MRole.USERLEVEL_System);
 		boolean result = role.save();
 		assertFalse(result, "BeforeSave should not allow saving with UserLevel System for non-zero Client ID");
+	}
+	
+	@Test
+	public void testBeforeSave_MasterRoleValidation() {
+		role.setIsMasterRole(true);
+		role.saveEx();
+		MUserRoles userRole = new MUserRoles(ctx, 0, getTrxName());
+		userRole.setAD_User_ID(SUPERUSER_ID);
+		userRole.setAD_Role_ID(role.getAD_Role_ID());
+		boolean result = userRole.save();
+		assertFalse(result, "Should not allow assigning a Role Template to a user");
+		ValueNamePair error = CLogger.retrieveError();
+		assertNotNull(error, "Expected error message not found");
+		assertEquals(Msg.getMsg(Env.getCtx(), "CannotAssignRoleTemplateToUser"), error.getName(),
+				"Expected error message for assigning Role Template to user");
+	}
+	
+	@Test
+	public void testAutoAssign_NonMasterRole() {
+		try (MockedStatic<MSysConfig> config = mockStatic(MSysConfig.class, Mockito.CALLS_REAL_METHODS)) {
+			config.when(() -> MSysConfig.getBooleanValue(MSysConfig.AUTO_ASSIGN_ROLE_TO_CREATOR_USER, false, getAD_Client_ID()))
+				.thenReturn(true);
+			role.setIsMasterRole(false);
+			role.saveEx();
+			
+			Query query = new Query(ctx, MUserRoles.Table_Name, "AD_User_ID=? AND AD_Role_ID=?", getTrxName());
+			MUserRoles userRoles = query.setParameters(SUPERUSER_ID, role.getAD_Role_ID()).first();
+			assertNotNull(userRoles, "UserRole should be auto-assigned for non-Template Role");
+			if (role.getCreatedBy() != SUPERUSER_ID) {
+				userRoles = query.setParameters(role.getCreatedBy(), role.getAD_Role_ID()).first();
+				assertNotNull(userRoles, "UserRole should be auto-assigned for non-Template Role");
+			}
+		}
 	}
 }
