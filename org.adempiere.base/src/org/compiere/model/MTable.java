c03e7b11b61aefa6c889b50b306566ff327aee1f
Carlos Ruiz <carg67@gmail.com>
2026-01-29 04:40:03 +0100

    IDEMPIERE-6122 Query class to accept a list of columns to select (#3073)
    * IDEMPIERE-6122 Query class to accept a list of columns to select
    
    - Make easy to identify if a PO was partially loaded and if a column was loaded
    
    * - implement suggestion from coderabbitai
    
    * - implement suggestion from Heng Sin
    - implement suggestion from coderabbitai
    
    * - implement suggestion from coderabbitai
    
    * - implement suggestion from Heng Sin
    
    * - change recommendation message and level
    
    * - add log level verification

--- org.adempiere.base/src/org/compiere/model/MTable.java
+++ org.adempiere.base/src/org/compiere/model/MTable.java
@@ -732,6 +732,8 @@ public class MTable extends X_AD_Table implements ImmutablePOSupport
 			partialPOResultSetColumns.set(map);
 			PO po = getPO(rs, trxName);
 			po.makeImmutable();
+			po.set_Partial(true);
+			po.set_ColumnLoaded(selectColumns);
 			return po;
 		} finally {
 			partialPOResultSetColumns.remove();
