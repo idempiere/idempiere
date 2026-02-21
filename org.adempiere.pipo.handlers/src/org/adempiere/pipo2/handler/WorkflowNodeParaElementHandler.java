da2f22f0f0fa1010ae760178ddded9da21ea37e0
Carlos Ruiz <carg67@gmail.com>
2026-02-22 00:19:39 +0100

    IDEMPIERE-6752 Workflow Packout missing Process Parameters
    - Implement export for AD_WF_Node_Para
    - enable the flag "Export Dictionary" also in tenants, otherwise a System workflow cannot be exported.  NOTE that in this case the entries are exported, but still the 2Pack is marked to not import in the target system, so just the tenant entries not marked as Dictionary would be imported safely

---
+++ org.adempiere.pipo.handlers/src/org/adempiere/pipo2/handler/WorkflowNodeParaElementHandler.java
@@ -0,0 +1,139 @@
+/***********************************************************************
+ * This file is part of iDempiere ERP Open Source                      *
+ * http://www.idempiere.org                                            *
+ *                                                                     *
+ * Copyright (C) Contributors                                          *
+ *                                                                     *
+ * This program is free software; you can redistribute it and/or       *
+ * modify it under the terms of the GNU General Public License         *
+ * as published by the Free Software Foundation; either version 2      *
+ * of the License, or (at your option) any later version.              *
+ *                                                                     *
+ * This program is distributed in the hope that it will be useful,     *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of      *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the        *
+ * GNU General Public License for more details.                        *
+ *                                                                     *
+ * You should have received a copy of the GNU General Public License   *
+ * along with this program; if not, write to the Free Software         *
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,          *
+ * MA 02110-1301, USA.                                                 *
+ *                                                                     *
+ * Contributors:                                                       *
+ * - Carlos Ruiz - globalqss - bxservice                               *
+ **********************************************************************/
+
+package org.adempiere.pipo2.handler;
+
+import java.util.List;
+
+import javax.xml.transform.sax.TransformerHandler;
+
+import org.adempiere.pipo2.AbstractElementHandler;
+import org.adempiere.pipo2.Element;
+import org.adempiere.pipo2.PIPOContext;
+import org.adempiere.pipo2.PackOut;
+import org.adempiere.pipo2.PoExporter;
+import org.adempiere.pipo2.PoFiller;
+import org.adempiere.pipo2.exception.POSaveFailedException;
+import org.compiere.model.I_AD_WF_Node_Para;
+import org.compiere.model.X_AD_Package_Imp_Detail;
+import org.compiere.model.X_AD_WF_Node_Para;
+import org.compiere.util.Env;
+import org.compiere.wf.MWFNodePara;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.AttributesImpl;
+
+public class WorkflowNodeParaElementHandler extends AbstractElementHandler {
+
+	public void startElement(PIPOContext ctx, Element element) throws SAXException {
+		List<String> excludes = defaultExcludeList(MWFNodePara.Table_Name);
+
+		String entitytype = getStringValue(element, "EntityType");
+		if (isProcessElement(ctx.ctx, entitytype)) {
+			/*if (isParentSkip(element, null)) {
+				element.skip = true;
+				return;
+			}*/
+
+			MWFNodePara MWFNodePara = findPO(ctx, element);
+			if (MWFNodePara == null) {
+				MWFNodePara = new MWFNodePara(ctx.ctx, 0, getTrxName(ctx));
+			}
+
+			PoFiller filler = new PoFiller(ctx, MWFNodePara, element, this);
+			List<String> notfounds = filler.autoFill(excludes);
+			if (notfounds.size() > 0) {
+				element.defer = true;
+				element.unresolved = notfounds.toString();
+				return;
+			}
+
+			if (MWFNodePara.is_new() || MWFNodePara.is_Changed()) {
+				X_AD_Package_Imp_Detail impDetail = createImportDetail(ctx, element.qName, X_AD_WF_Node_Para.Table_Name,
+						X_AD_WF_Node_Para.Table_ID);
+				String action = null;
+				if (!MWFNodePara.is_new()){
+					backupRecord(ctx, impDetail.getAD_Package_Imp_Detail_ID(), X_AD_WF_Node_Para.Table_Name,MWFNodePara);
+					action = "Update";
+				} else{
+					action = "New";
+				}
+				if (MWFNodePara.save(getTrxName(ctx)) == true){
+					log.info("m_WFNodePara save success");
+					logImportDetail (ctx, impDetail, 1, String.valueOf(MWFNodePara.get_ID()),MWFNodePara.get_ID(), action);
+				} else{
+					log.info("m_WFNodePara save failure");
+					logImportDetail (ctx, impDetail, 0, String.valueOf(MWFNodePara.get_ID()), MWFNodePara.get_ID(), action);
+					throw new POSaveFailedException("Failed to save WorkflowNodePara");
+				}
+			}
+		} else {
+			element.skip = true;
+		}
+	}
+
+	public void endElement(PIPOContext ctx, Element element) throws SAXException {
+	}
+
+	public void create(PIPOContext ctx, TransformerHandler document)
+			throws SAXException {
+		int AD_WF_Node_Para_id = Env.getContextAsInt(ctx.ctx, "AD_WF_Node_Para_ID");
+		if (ctx.packOut.isExported("AD_WF_Node_Para_ID"+"|"+AD_WF_Node_Para_id))
+			return;
+
+		MWFNodePara m_WF_NodePara = new MWFNodePara(
+				ctx.ctx, AD_WF_Node_Para_id, null);
+		if (!isPackOutElement(ctx, m_WF_NodePara))
+			return;
+		verifyPackOutRequirement(m_WF_NodePara);
+		AttributesImpl atts = new AttributesImpl();
+		addTypeName(atts, "table");
+		document.startElement("", "", I_AD_WF_Node_Para.Table_Name, atts);
+		createWorkflowNodeParaBinding(ctx, document, m_WF_NodePara);
+		document.endElement("", "", I_AD_WF_Node_Para.Table_Name);
+
+	}
+
+	private void createWorkflowNodeParaBinding(PIPOContext ctx, TransformerHandler document,
+			MWFNodePara m_WF_NodePara)
+	{
+
+		PoExporter filler = new PoExporter(ctx, document, m_WF_NodePara);
+		List<String> excludes = defaultExcludeList(X_AD_WF_Node_Para.Table_Name);
+
+		if (m_WF_NodePara.getAD_WF_Node_Para_ID() <= PackOut.MAX_OFFICIAL_ID)
+			filler.add("AD_WF_Node_Para_ID", new AttributesImpl());
+
+		filler.export(excludes);
+	}
+
+	@Override
+	public void packOut(PackOut packout, TransformerHandler packoutHandler,
+			TransformerHandler docHandler,
+			int recordId) throws Exception {
+		Env.setContext(packout.getCtx().ctx, I_AD_WF_Node_Para.COLUMNNAME_AD_WF_Node_Para_ID, recordId);
+		create(packout.getCtx(), packoutHandler);
+		packout.getCtx().ctx.remove(I_AD_WF_Node_Para.COLUMNNAME_AD_WF_Node_Para_ID);
+	}
+}
