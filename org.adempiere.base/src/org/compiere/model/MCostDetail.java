2dc63b61cfa5fa8ad9c03e0d99ef9a44f8b922fc
Deepak Pansheriya <deepakpansheriya@gmail.com>
2026-02-27 11:30:18 +0530

    IDEMPIERE-6198 : Fixing Bug of Incorrect Accumulated Amt and Accumula… (#2908)
    * IDEMPIERE-6198 : Fixing Bug of Incorrect Accumulated Amt and Accumulated Qty after reverse-correct a Physical Inventory
    
    * IDEMPIERE-6198: Review correction
    
    * Should use getSQLValueEx instead of getSQLValue
    
    ---------
    
    Co-authored-by: Sachin  Bhimani <sbhimani@logilite.com>
    Co-authored-by: hengsin <hengsin@gmail.com>

--- org.adempiere.base/src/org/compiere/model/MCostDetail.java
+++ org.adempiere.base/src/org/compiere/model/MCostDetail.java
@@ -1706,6 +1706,18 @@ public class MCostDetail extends X_M_CostDetail
 			boolean addition = qty.signum() > 0;
 			boolean adjustment = getM_InventoryLine_ID() > 0 && qty.signum() == 0 && amt.signum() != 0;
 			boolean isVendorRMA = isVendorRMA();
+			
+			//If not import and it is due to inventory then don't mark as addition
+			if(addition && getM_InventoryLine_ID() != 0) {
+				MInventoryLine invLine = new MInventoryLine(getCtx(), getM_InventoryLine_ID(), get_TrxName());
+				// Only apply import-line check for Physical Inventory, not Internal Use
+				if (!invLine.isInternalUseInventory()) {
+					int I_Inventory_ID = MInventoryLine.getImportLine_ID(getM_InventoryLine_ID(), get_TrxName());
+					if (I_Inventory_ID <= 0)
+						addition = false;
+				}
+			}
+			
 			//
 			if (ce.isAverageInvoice())
 			{
