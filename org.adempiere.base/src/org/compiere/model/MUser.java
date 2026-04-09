f3e0a1e352c3b06bed0897c0ac75c57c116ce460
Nicolas Micoud <58596990+nmicoud@users.noreply.github.com>
2026-04-09 09:09:43 +0200

    IDEMPIERE-6961: Make MUser.cleanValue public - change it to static
    Co-Authored-By: hengsin <152246+hengsin@users.noreply.github.com>

--- org.adempiere.base/src/org/compiere/model/MUser.java
+++ org.adempiere.base/src/org/compiere/model/MUser.java
@@ -500,7 +500,7 @@ public class MUser extends X_AD_User implements ImmutablePOSupport
 	 *	@param value
 	 *	@return lower case cleaned value
 	 */
-	public String cleanValue (String value)
+	public static String cleanValue (String value)
 	{
 		if (value == null)
 			return "";
