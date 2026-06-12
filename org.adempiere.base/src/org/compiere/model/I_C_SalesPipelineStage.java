571f4606ef7b3060f20cbaaab4c51b55fe5fcf73
Nicolas Micoud <58596990+nmicoud@users.noreply.github.com>
2026-06-12 12:59:00 +0200

    IDEMPIERE-7009: Sales Pipeline to contextualize Sales Stages in Opportunities (#3276)
    * IDEMPIERE-7009: Sales Pipeline to contextualize Sales Stages in Opportunities
    
    * IDEMPIERE-7009: Sales Pipeline to contextualize Sales Stages in Opportunities - make SalesPipeline mandatory
    
    suggested by CarlosRuiz-globalqss
    
    * IDEMPIERE-7009: Sales Pipeline to contextualize Sales Stages in Opportunities - address coderabbitai review
    
    * IDEMPIERE-7009: Sales Pipeline to contextualize Sales Stages in Opportunities - address coderabbitai review
    
    * IDEMPIERE-7009: Sales Pipeline to contextualize Sales Stages in Opportunities - error message if no corresponding record for sales pipeline/stage
    
    * IDEMPIERE-7009: Sales Pipeline to contextualize Sales Stages in Opportunities - address coderabbitai review
    
    * IDEMPIERE-7009: Sales Pipeline to contextualize Sales Stages in Opportunities - fix table Pipeline/Stage
    
    * IDEMPIERE-7009: Sales Pipeline to contextualize Sales Stages in Opportunities - fix C_SalesPipelineStage.C_SalesPipeline_ID IsParent
    
    see https://idempiere.atlassian.net/browse/IDEMPIERE-7026
    
    * IDEMPIERE-7009: Sales Pipeline to contextualize Sales Stages in Opportunities: fix UUID Constructor

---
+++ org.adempiere.base/src/org/compiere/model/I_C_SalesPipelineStage.java
@@ -0,0 +1,182 @@
+/******************************************************************************
+ * Product: iDempiere ERP & CRM Smart Business Solution                       *
+ * Copyright (C) 1999-2012 ComPiere, Inc. All Rights Reserved.                *
+ * This program is free software, you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY, without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program, if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ * For the text or an alternative of this public license, you may reach us    *
+ * ComPiere, Inc., 2620 Augustine Dr. #245, Santa Clara, CA 95054, USA        *
+ * or via info@compiere.org or http://www.compiere.org/license.html           *
+ *****************************************************************************/
+package org.compiere.model;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import org.compiere.util.KeyNamePair;
+
+/** Generated Interface for C_SalesPipelineStage
+ *  @author iDempiere (generated) 
+ *  @version Release 14
+ */
+public interface I_C_SalesPipelineStage 
+{
+
+    /** TableName=C_SalesPipelineStage */
+    public static final String Table_Name = "C_SalesPipelineStage";
+
+    /** AD_Table_ID=200448 */
+    public static final int Table_ID = 200448;
+
+    KeyNamePair Model = new KeyNamePair(Table_ID, Table_Name);
+
+    /** AccessLevel = 3 - Client - Org 
+     */
+    BigDecimal accessLevel = BigDecimal.valueOf(3);
+
+    /** Load Meta Data */
+
+    /** Column name AD_Client_ID */
+    public static final String COLUMNNAME_AD_Client_ID = "AD_Client_ID";
+
+	/** Get Tenant.
+	  * Tenant for this installation.
+	  */
+	public int getAD_Client_ID();
+
+    /** Column name AD_Org_ID */
+    public static final String COLUMNNAME_AD_Org_ID = "AD_Org_ID";
+
+	/** Set Organization.
+	  * Organizational entity within tenant
+	  */
+	public void setAD_Org_ID (int AD_Org_ID);
+
+	/** Get Organization.
+	  * Organizational entity within tenant
+	  */
+	public int getAD_Org_ID();
+
+    /** Column name C_SalesPipelineStage_UU */
+    public static final String COLUMNNAME_C_SalesPipelineStage_UU = "C_SalesPipelineStage_UU";
+
+	/** Set C_SalesPipelineStage_UU	  */
+	public void setC_SalesPipelineStage_UU (String C_SalesPipelineStage_UU);
+
+	/** Get C_SalesPipelineStage_UU	  */
+	public String getC_SalesPipelineStage_UU();
+
+    /** Column name C_SalesPipeline_ID */
+    public static final String COLUMNNAME_C_SalesPipeline_ID = "C_SalesPipeline_ID";
+
+	/** Set Sales Pipeline.
+	  * Defines a sales pipeline (e.g. &quot;
+Standard Sales&quot;
+, &quot;
+Complex Project&quot;
+, etc.)
+	  */
+	public void setC_SalesPipeline_ID (int C_SalesPipeline_ID);
+
+	/** Get Sales Pipeline.
+	  * Defines a sales pipeline (e.g. &quot;
+Standard Sales&quot;
+, &quot;
+Complex Project&quot;
+, etc.)
+	  */
+	public int getC_SalesPipeline_ID();
+
+	@Deprecated(since="13") // use better methods with cache
+	public org.compiere.model.I_C_SalesPipeline getC_SalesPipeline() throws RuntimeException;
+
+    /** Column name C_SalesStage_ID */
+    public static final String COLUMNNAME_C_SalesStage_ID = "C_SalesStage_ID";
+
+	/** Set Sales Stage.
+	  * Stages of the sales process
+	  */
+	public void setC_SalesStage_ID (int C_SalesStage_ID);
+
+	/** Get Sales Stage.
+	  * Stages of the sales process
+	  */
+	public int getC_SalesStage_ID();
+
+	@Deprecated(since="13") // use better methods with cache
+	public org.compiere.model.I_C_SalesStage getC_SalesStage() throws RuntimeException;
+
+    /** Column name Created */
+    public static final String COLUMNNAME_Created = "Created";
+
+	/** Get Created.
+	  * Date this record was created
+	  */
+	public Timestamp getCreated();
+
+    /** Column name CreatedBy */
+    public static final String COLUMNNAME_CreatedBy = "CreatedBy";
+
+	/** Get Created By.
+	  * User who created this records
+	  */
+	public int getCreatedBy();
+
+    /** Column name IsActive */
+    public static final String COLUMNNAME_IsActive = "IsActive";
+
+	/** Set Active.
+	  * The record is active in the system
+	  */
+	public void setIsActive (boolean IsActive);
+
+	/** Get Active.
+	  * The record is active in the system
+	  */
+	public boolean isActive();
+
+    /** Column name Probability */
+    public static final String COLUMNNAME_Probability = "Probability";
+
+	/** Set Probability	  */
+	public void setProbability (BigDecimal Probability);
+
+	/** Get Probability	  */
+	public BigDecimal getProbability();
+
+    /** Column name SeqNo */
+    public static final String COLUMNNAME_SeqNo = "SeqNo";
+
+	/** Set Sequence.
+	  * Method of ordering records;
+ lowest number comes first
+	  */
+	public void setSeqNo (int SeqNo);
+
+	/** Get Sequence.
+	  * Method of ordering records;
+ lowest number comes first
+	  */
+	public int getSeqNo();
+
+    /** Column name Updated */
+    public static final String COLUMNNAME_Updated = "Updated";
+
+	/** Get Updated.
+	  * Date this record was updated
+	  */
+	public Timestamp getUpdated();
+
+    /** Column name UpdatedBy */
+    public static final String COLUMNNAME_UpdatedBy = "UpdatedBy";
+
+	/** Get Updated By.
+	  * User who updated this records
+	  */
+	public int getUpdatedBy();
+}
