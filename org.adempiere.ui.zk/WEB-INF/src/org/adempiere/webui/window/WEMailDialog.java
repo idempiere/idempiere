cf31fd1de86a355359278c290337479bc7d93c5b
Diego Ruiz <diego.ruiz@globalqss.com>
2026-06-24 13:09:37 +0200

    IDEMPIERE-7048 - Improve Load Template button in Send Mail dialog

--- org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/window/WEMailDialog.java
+++ org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/window/WEMailDialog.java
@@ -408,7 +408,9 @@ public class WEMailDialog extends Window implements EventListener<Event>, ValueC
 			btn.setImage(ThemeManager.getThemeResource("images/Attachment24.png"));
 		btn.setUpload(AdempiereWebUI.getUploadSetting());
 		btn.addEventListener(Events.ON_UPLOAD, this);
+		btn.setLabel(Msg.getMsg(Env.getCtx(), "Attachment")); // or just "Attach"
 		btn.setTooltiptext(Msg.getMsg(Env.getCtx(), "Attachment"));
+		btn.setStyle("min-width: fit-content; white-space: nowrap;");
 		confirmPanel.addComponentsLeft(btn);
 
 		bAddDefaultMailText = new Button();
@@ -417,6 +419,8 @@ public class WEMailDialog extends Window implements EventListener<Event>, ValueC
 		else
 			bAddDefaultMailText.setImage(ThemeManager.getThemeResource("images/DefaultMailText.png"));
 		bAddDefaultMailText.addEventListener(Events.ON_CLICK, this);
+		bAddDefaultMailText.setLabel(Msg.getMsg(Env.getCtx(), "AddDefaultMailText"));
+		bAddDefaultMailText.setStyle("min-width: fit-content; white-space: nowrap;");
 		bAddDefaultMailText.setTooltiptext(Msg.getMsg(Env.getCtx(), "AddDefaultMailTextContent"));
 		if (new MUser(Env.getCtx(), Env.getAD_User_ID(Env.getCtx()), null).getR_DefaultMailText_ID() > 0)
 			confirmPanel.addComponentsLeft(bAddDefaultMailText);
