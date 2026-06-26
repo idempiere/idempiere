9b50f6d86228f8b1c6c1ed0b54660eea3e8ee372
pultzlucas <pultzlucas@gmail.com>
2026-06-26 15:25:43 -0300

    IDEMPIERE-7055 Improve Copy Order Process Error Messaging

--- org.adempiere.base.process/src/org/compiere/process/CopyOrder.java
+++ org.adempiere.base.process/src/org/compiere/process/CopyOrder.java
@@ -89,9 +89,7 @@ public class CopyOrder extends SvrProcess
 			dt.getC_DocType_ID(), dt.isSOTrx(), false, true, get_TrxName());		//	copy ASI
 		newOrder.setC_DocTypeTarget_ID(p_C_DocType_ID);
 		newOrder.setQuotationOrder_ID(from.getC_Order_ID()); // IDEMPIERE-475
-		boolean OK = newOrder.save();
-		if (!OK)
-			throw new IllegalStateException("Could not create new Order");
+		newOrder.saveEx();
 		//
 		if (p_IsCloseDocument)
 		{
