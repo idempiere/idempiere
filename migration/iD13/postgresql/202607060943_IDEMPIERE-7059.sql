c1e052f58987a84d6dc9847d5a2b80582a0aab92
Nicolas Micoud <58596990+nmicoud@users.noreply.github.com>
2026-07-06 09:45:40 +0200

    IDEMPIERE-7059: Scheduler window should be restricted to Advanced Users only

---
+++ migration/iD13/postgresql/202607060943_IDEMPIERE-7059.sql
@@ -0,0 +1,7 @@
+-- IDEMPIERE-7059
+SELECT register_migration_script('202607060943_IDEMPIERE-7059.sql') FROM dual;
+
+-- Jul 6, 2026, 9:44:13 AM CEST
+UPDATE AD_Tab SET IsAdvancedTab='Y',Updated=TO_TIMESTAMP('2026-07-06 09:44:13','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=10 WHERE AD_Tab_ID=589
+;
+
